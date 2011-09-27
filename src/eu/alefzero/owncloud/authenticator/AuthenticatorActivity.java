aab77eaa70f0d28a2bb04f86ca370fc4d6aa6a79
Sven Aßmann <sven.assmann@lubico.biz>
2011-09-27 11:26:48 +0200

    - adding contact (carddav) server url based on the current 2.0 server url layout - fixing compiler warning - fixing intends to android guide sdk conventions

--- src/eu/alefzero/owncloud/authenticator/AuthenticatorActivity.java
+++ src/eu/alefzero/owncloud/authenticator/AuthenticatorActivity.java
@@ -41,136 +41,144 @@ import eu.alefzero.owncloud.R;
 import eu.alefzero.owncloud.db.ProviderMeta.ProviderTableMeta;
 
 public class AuthenticatorActivity extends AccountAuthenticatorActivity {
-  private Thread mAuthThread;
-  private final Handler mHandler = new Handler();
-  
-  public static final String PARAM_USERNAME = "param_Username";
-  public static final String PARAM_HOSTNAME = "param_Hostname";
-  
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    getWindow().requestFeature(Window.FEATURE_NO_TITLE);
-    setContentView(R.layout.account_setup);
-    if (getIntent().hasExtra(PARAM_USERNAME)) {
-      String username =  getIntent().getStringExtra(PARAM_HOSTNAME);
-      TextView host_text, user_text;
-      host_text = (TextView) findViewById(R.id.host_URL);
-      user_text = (TextView) findViewById(R.id.account_username);
-      host_text.setText(host_text.getText() + username.substring(username.lastIndexOf('@')));
-      user_text.setText(user_text.getText() + username.substring(0, username.lastIndexOf('@')-1));
-    }
-  }
+    private Thread mAuthThread;
+    private final Handler mHandler = new Handler();
 
-  @Override
-  protected Dialog onCreateDialog(int id) {
-    final ProgressDialog dialog = new ProgressDialog(this);
-    dialog.setMessage("Trying to login");
-    dialog.setIndeterminate(true);
-    dialog.setCancelable(true);
-    dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
-      public void onCancel(DialogInterface dialog) {
-        Log.i(getClass().getName(), "Login canceled");
-        if (mAuthThread != null) {
-          mAuthThread.interrupt();
-          finish();
+    public static final String PARAM_USERNAME = "param_Username";
+    public static final String PARAM_HOSTNAME = "param_Hostname";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.account_setup);
+        if (getIntent().hasExtra(PARAM_USERNAME)) {
+            String username = getIntent().getStringExtra(PARAM_HOSTNAME);
+            TextView host_text, user_text;
+            host_text = (TextView) findViewById(R.id.host_URL);
+            user_text = (TextView) findViewById(R.id.account_username);
+            host_text.setText(host_text.getText() + username.substring(username.lastIndexOf('@')));
+            user_text.setText(user_text.getText() + username.substring(0, username.lastIndexOf('@') - 1));
         }
-      }
-    });
-    return dialog;
-  }
-  
-  public void onAuthenticationResult(boolean result, String message) {
-    if (result) {
-      TextView username_text = (TextView) findViewById(R.id.account_username),
-               password_text = (TextView) findViewById(R.id.account_password);
+    }
 
-      URL url = null;
-      try {
-        url = new URL(message);
-      } catch (MalformedURLException e) {
-        // should never happend
-        Log.e(getClass().getName(), "Malformed URL: " + message);
-        return;
-      }
-      
-      Account account = new Account(username_text.getText().toString() + "@" + url.getHost(), AccountAuthenticator.ACCOUNT_TYPE);
-      AccountManager accManager = AccountManager.get(this);
-      accManager.addAccountExplicitly(account, password_text.getText().toString(),null);
-      
-      final Intent intent = new Intent();
-      intent.putExtra(AccountManager.KEY_ACCOUNT_TYPE, AccountAuthenticator.ACCOUNT_TYPE);
-      intent.putExtra(AccountManager.KEY_ACCOUNT_NAME, account.name);
-      intent.putExtra(AccountManager.KEY_AUTHTOKEN, AccountAuthenticator.ACCOUNT_TYPE);
-      accManager.setUserData(account, AccountAuthenticator.KEY_OC_URL, url.toString());
-      setAccountAuthenticatorResult(intent.getExtras());
-      setResult(RESULT_OK, intent);
-      Bundle bundle = new Bundle();
-      bundle.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
-      getContentResolver().startSync(ProviderTableMeta.CONTENT_URI, bundle);
-      
-      dismissDialog(0);
-      finish();
-    } else {
-      Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-      dismissDialog(0);
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        final ProgressDialog dialog = new ProgressDialog(this);
+        dialog.setMessage("Trying to login");
+        dialog.setIndeterminate(true);
+        dialog.setCancelable(true);
+        dialog.setOnCancelListener(new DialogInterface.OnCancelListener() {
+            public void onCancel(DialogInterface dialog) {
+                Log.i(getClass().getName(), "Login canceled");
+                if (mAuthThread != null) {
+                    mAuthThread.interrupt();
+                    finish();
+                }
+            }
+        });
+        return dialog;
     }
-  }
-  
-  public void onCancelClick(View view) {
-    Log.i(getClass().getName(), "Account creating canceled");
-    this.finish();
-  }
-  
-  public void onOkClick(View view) {
-    TextView url_text = (TextView) findViewById(R.id.host_URL);
-    TextView username_text = (TextView) findViewById(R.id.account_username);
-    TextView password_text = (TextView) findViewById(R.id.account_password);
-    Log.i(getClass().getName(), "OK clicked");
-    boolean hasErrors = false;
-    
-    URL uri = null;
-    if (url_text.getText().toString().trim().length() == 0) {
-      url_text.setTextColor(Color.RED);
-      hasErrors = true; 
-    } else {
-      url_text.setTextColor(Color.BLACK);
+
+    public void onAuthenticationResult(boolean result, String message) {
+        if (result) {
+            TextView username_text = (TextView) findViewById(R.id.account_username),
+                    password_text = (TextView) findViewById(R.id.account_password);
+
+            URL url;
+            try {
+                url = new URL(message);
+            } catch (MalformedURLException e) {
+                // should never happend
+                Log.e(getClass().getName(), "Malformed URL: " + message);
+                return;
+            }
+
+            String username = username_text.getText().toString().trim();
+            Account account = new Account(username + "@" + url.getHost(), AccountAuthenticator.ACCOUNT_TYPE);
+            AccountManager accManager = AccountManager.get(this);
+            accManager.addAccountExplicitly(account, password_text.getText().toString(), null);
+
+            final Intent intent = new Intent();
+            intent.putExtra(AccountManager.KEY_ACCOUNT_TYPE, AccountAuthenticator.ACCOUNT_TYPE);
+            intent.putExtra(AccountManager.KEY_ACCOUNT_NAME, account.name);
+            intent.putExtra(AccountManager.KEY_AUTHTOKEN, AccountAuthenticator.ACCOUNT_TYPE);
+            accManager.setUserData(account, AccountAuthenticator.KEY_OC_URL, url.toString());
+
+            // TODO prepare this URL during a central service
+            intent.putExtra(AccountManager.KEY_USERDATA, username);
+            accManager.setUserData(account, AccountAuthenticator.KEY_CONTACT_URL,
+                    url.toString().replace(AuthUtils.WEBDAV_PATH_2_0, AuthUtils.CARDDAV_PATH_2_0)
+            );
+
+            setAccountAuthenticatorResult(intent.getExtras());
+            setResult(RESULT_OK, intent);
+            Bundle bundle = new Bundle();
+            bundle.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
+            getContentResolver().startSync(ProviderTableMeta.CONTENT_URI, bundle);
+
+            dismissDialog(0);
+            finish();
+        } else {
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            dismissDialog(0);
+        }
     }
-    try {
-      String url_str = url_text.getText().toString();
-      if (!url_str.startsWith("http://") &&
-          !url_str.startsWith("https://")) {
-        url_str = "http://" + url_str;
-      }
-      uri = new URL(url_str);
-    } catch (MalformedURLException e) {
-      url_text.setTextColor(Color.RED);
-      e.printStackTrace();
-      hasErrors = true;
+
+    public void onCancelClick(View view) {
+        Log.i(getClass().getName(), "Account creating canceled");
+        this.finish();
     }
-    
-    if (username_text.getText().toString().contains(" ") ||
-        username_text.getText().toString().trim().length() == 0) {
-      username_text.setTextColor(Color.RED);
-      hasErrors = true;
-    } else {
-      username_text.setTextColor(Color.BLACK);
+
+    public void onOkClick(View view) {
+        TextView url_text = (TextView) findViewById(R.id.host_URL);
+        TextView username_text = (TextView) findViewById(R.id.account_username);
+        TextView password_text = (TextView) findViewById(R.id.account_password);
+        Log.i(getClass().getName(), "OK clicked");
+        boolean hasErrors = false;
+
+        URL uri = null;
+        if (url_text.getText().toString().trim().length() == 0) {
+            url_text.setTextColor(Color.RED);
+            hasErrors = true;
+        } else {
+            url_text.setTextColor(Color.BLACK);
+        }
+        try {
+            String url_str = url_text.getText().toString();
+            if (!url_str.startsWith("http://") &&
+                    !url_str.startsWith("https://")) {
+                url_str = "http://" + url_str;
+            }
+            uri = new URL(url_str);
+        } catch (MalformedURLException e) {
+            url_text.setTextColor(Color.RED);
+            e.printStackTrace();
+            hasErrors = true;
+        }
+
+        if (username_text.getText().toString().contains(" ") ||
+                username_text.getText().toString().trim().length() == 0) {
+            username_text.setTextColor(Color.RED);
+            hasErrors = true;
+        } else {
+            username_text.setTextColor(Color.BLACK);
+        }
+
+        if (password_text.getText().toString().trim().length() == 0) {
+            password_text.setTextColor(Color.RED);
+            hasErrors = true;
+        } else {
+            password_text.setTextColor(Color.BLACK);
+        }
+        if (hasErrors) {
+            return;
+        }
+        showDialog(0);
+        mAuthThread = AuthUtils.attemptAuth(uri,
+                username_text.getText().toString(),
+                password_text.getText().toString(),
+                mHandler,
+                AuthenticatorActivity.this);
     }
-    
-    if (password_text.getText().toString().trim().length() == 0) {
-      password_text.setTextColor(Color.RED);
-      hasErrors = true;
-    } else {
-      password_text.setTextColor(Color.BLACK);
-    }
-    if (hasErrors) {
-      return;
-    }
-    showDialog(0);
-    mAuthThread = AuthUtils.attemptAuth(uri,
-                                        username_text.getText().toString(),
-                                        password_text.getText().toString(),
-                                        mHandler,
-                                        AuthenticatorActivity.this);
-  }
 }
