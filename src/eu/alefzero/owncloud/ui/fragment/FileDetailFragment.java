54909e5372874cc1e90fec0137927b975cd1fe21
Bartek Przybylski <bart.p.pl@gmail.com>
2012-05-16 19:16:18 +0200

    fix file/download display

--- src/eu/alefzero/owncloud/ui/fragment/FileDetailFragment.java
+++ src/eu/alefzero/owncloud/ui/fragment/FileDetailFragment.java
@@ -127,7 +127,7 @@ public class FileDetailFragment extends SherlockFragment implements
             VideoView videoView = (VideoView) mView
                     .findViewById(R.id.videoView1);
             videoView.setVisibility(View.INVISIBLE);
-            if (mFile.getPath() == null) {
+            if (mFile.getStoragePath() == null) {
                 ImageView imageView = (ImageView) getView().findViewById(
                         R.id.imageView2);
                 imageView.setImageResource(R.drawable.download);
@@ -136,11 +136,11 @@ public class FileDetailFragment extends SherlockFragment implements
                 if (mFile.getMimetype().startsWith("image/")) {
                     ImageView imageView = (ImageView) mView
                             .findViewById(R.id.imageView2);
-                    Bitmap bmp = BitmapFactory.decodeFile(mFile.getPath());
+                    Bitmap bmp = BitmapFactory.decodeFile(mFile.getStoragePath());
                     imageView.setImageBitmap(bmp);
                 } else if (mFile.getMimetype().startsWith("video/")) {
                     videoView.setVisibility(View.VISIBLE);
-                    videoView.setVideoPath(mFile.getPath());
+                    videoView.setVideoPath(mFile.getStoragePath());
                     videoView.start();
                 }
             }
