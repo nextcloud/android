baf003a555afed17457401d9061002fa3dfd75c0
Bartek Przybylski <bart.p.pl@gmail.com>
2012-06-26 18:08:01 +0200

    fix crash on list adapter when file has no mimetype

--- src/eu/alefzero/owncloud/ui/adapter/FileListListAdapter.java
+++ src/eu/alefzero/owncloud/ui/adapter/FileListListAdapter.java
@@ -104,7 +104,7 @@ public class FileListListAdapter implements ListAdapter {
 
             fileName.setText(name);
             ImageView fileIcon = (ImageView) view.findViewById(R.id.imageView1);
-            if (!file.getMimetype().equals("DIR")) {
+            if (file.getMimetype() == null || !file.getMimetype().equals("DIR")) {
                 fileIcon.setImageResource(R.drawable.file);
             } else {
                 fileIcon.setImageResource(R.drawable.ic_menu_archive);
