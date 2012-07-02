54539939f3e2d8afd111e85f677e4624b2e77801
Bartek Przybylski <bart.p.pl@gmail.com>
2012-07-02 18:28:31 +0200

    disable download button while downloading

--- src/eu/alefzero/owncloud/ui/fragment/FileDetailFragment.java
+++ src/eu/alefzero/owncloud/ui/fragment/FileDetailFragment.java
@@ -152,6 +152,7 @@ public class FileDetailFragment extends SherlockFragment implements
         i.putExtra(FileDownloader.EXTRA_REMOTE_PATH, mFile.getRemotePath());
         i.putExtra(FileDownloader.EXTRA_FILE_PATH, mFile.getURLDecodedRemotePath());
         i.putExtra(FileDownloader.EXTRA_FILE_SIZE, mFile.getFileLength());
+        v.setEnabled(false);
         getActivity().startService(i);
     }
 
@@ -373,6 +374,7 @@ public class FileDetailFragment extends SherlockFragment implements
     private class DownloadFinishReceiver extends BroadcastReceiver {
         @Override
         public void onReceive(Context context, Intent intent) {
+            getView().findViewById(R.id.fdDownloadBtn).setEnabled(true);
             if (intent.getAction().equals(FileDownloader.BAD_DOWNLOAD_MESSAGE)) {
                 Toast.makeText(context, R.string.downloader_download_failed , Toast.LENGTH_SHORT).show();
                 
