502ef603b7913e37cd2673380fb3e104469e0583
Bartek Przybylski <bart.p.pl@gmail.com>
2012-10-06 11:57:31 +0200

    force update support library for outdated actionbarsherlock library

--- setup_env.sh
+++ setup_env.sh
@@ -4,4 +4,6 @@ git submodule init
 git submodule update
 android update project -p actionbarsherlock/library
 android update project -p .
-
+cp third_party/android-support-library/android-support-v4.jar actionbarsherlock/library/libs/android-support-v4.jar 
+cd tests
+android update test-project -m .. -p .
