92080afefcab6de830d6a5c099b04e3c3a7db47f
Bartek Przybylski <bart.p.pl@gmail.com>
2012-10-28 00:12:15 +0200

    add licence header to files

--- src/com/owncloud/android/ui/activity/ConflictsResolveActivity.java
+++ src/com/owncloud/android/ui/activity/ConflictsResolveActivity.java
@@ -1,3 +1,21 @@
+/* ownCloud Android client application
+ *   Copyright (C) 2012 Bartek Przybylski
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package com.owncloud.android.ui.activity;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
@@ -11,6 +29,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 
+/**
+ * Wrapper activity which will be launched if keep-in-sync file will be modified by external
+ * application. 
+ * 
+ * @author Bartek Przybylski
+ *
+ */
 public class ConflictsResolveActivity extends SherlockFragmentActivity implements OnConflictDecisionMadeListener {
 
     private String TAG = ConflictsResolveActivity.class.getSimpleName();
--- src/com/owncloud/android/ui/dialog/ConflictsResolveDialog.java
+++ src/com/owncloud/android/ui/dialog/ConflictsResolveDialog.java
@@ -1,3 +1,21 @@
+/* ownCloud Android client application
+ *   Copyright (C) 2012 Bartek Przybylski
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package com.owncloud.android.ui.dialog;
 
 import android.app.AlertDialog;
@@ -11,6 +29,12 @@ import com.actionbarsherlock.app.SherlockDialogFragment;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.owncloud.android.R;
 
+/**
+ * Dialog which will be displayed to user upon keep-in-sync file conflict.
+ * 
+ * @author Bartek Przybylski
+ *
+ */
 public class ConflictsResolveDialog extends SherlockDialogFragment {
 
     public static enum Decision { 
@@ -76,8 +100,8 @@ public class ConflictsResolveDialog extends SherlockDialogFragment {
         this.show(ft, "dialog");
     }
 
-    public static void dismissDialog(SherlockFragmentActivity activity, String tag) {
-        Fragment prev = activity.getSupportFragmentManager().findFragmentByTag(tag);
+    public void dismissDialog(SherlockFragmentActivity activity) {
+        Fragment prev = activity.getSupportFragmentManager().findFragmentByTag(getTag());
         if (prev != null) {
             FragmentTransaction ft = activity.getSupportFragmentManager().beginTransaction();
             ft.remove(prev);
