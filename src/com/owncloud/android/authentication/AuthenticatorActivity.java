69b527c39d82546f053d7ef2b9a1f094f1b4a94e
David A. Velasco <dvelasco@solidgear.es>
2013-05-17 14:33:31 +0200

    Visibility icon in password field is toggled according to the visibility state; better touch detection of visibility and refresh buttons

--- src/com/owncloud/android/authentication/AuthenticatorActivity.java
+++ src/com/owncloud/android/authentication/AuthenticatorActivity.java
@@ -59,7 +59,6 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.TextView.OnEditorActionListener;
@@ -93,6 +92,7 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity
     private static final String KEY_SERVER_STATUS_TEXT = "SERVER_STATUS_TEXT";
     private static final String KEY_SERVER_STATUS_ICON = "SERVER_STATUS_ICON";
     private static final String KEY_IS_SSL_CONN = "IS_SSL_CONN";
+    private static final String KEY_PASSWORD_VISIBLE = "PASSWORD_VISIBLE";
     private static final String KEY_AUTH_STATUS_TEXT = "AUTH_STATUS_TEXT";
     private static final String KEY_AUTH_STATUS_ICON = "AUTH_STATUS_ICON";
     
@@ -129,7 +129,6 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity
     private byte mAction;
     private Account mAccount;
     
-    private ImageView mViewPasswordButton;
     private EditText mHostUrlInput;
     private EditText mUsernameInput;
     private EditText mPasswordInput;
@@ -154,7 +153,6 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity
         
         /// set view and get references to view elements
         setContentView(R.layout.account_setup);
-        mViewPasswordButton = (ImageView) findViewById(R.id.viewPasswordButton);
         mHostUrlInput = (EditText) findViewById(R.id.hostUrlInput);
         mUsernameInput = (EditText) findViewById(R.id.account_username);
         mPasswordInput = (EditText) findViewById(R.id.account_password);
@@ -214,6 +212,9 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity
             mIsSslConn = savedInstanceState.getBoolean(KEY_IS_SSL_CONN);
             mAuthStatusText = savedInstanceState.getInt(KEY_AUTH_STATUS_TEXT);
             mAuthStatusIcon = savedInstanceState.getInt(KEY_AUTH_STATUS_ICON);
+            if (savedInstanceState.getBoolean(KEY_PASSWORD_VISIBLE, false)) {
+                showPassword();
+            }
             
             /// server data
             String ocVersion = savedInstanceState.getString(KEY_OC_VERSION);
@@ -259,13 +260,24 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity
         mHostUrlInput.setOnTouchListener(new RightDrawableOnTouchListener() {
             @Override
             public boolean onDrawableTouch(final MotionEvent event) {
-                AuthenticatorActivity.this.onRefreshClick(mHostUrlInput);
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    AuthenticatorActivity.this.onRefreshClick();
+                }
                 return true;
             }
         });
         mPasswordInput.setOnFocusChangeListener(this);
         mPasswordInput.setImeOptions(EditorInfo.IME_ACTION_DONE);
         mPasswordInput.setOnEditorActionListener(this);
+        mPasswordInput.setOnTouchListener(new RightDrawableOnTouchListener() {
+            @Override
+            public boolean onDrawableTouch(final MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    AuthenticatorActivity.this.onViewPasswordClick();
+                }
+                return true;
+            }
+        });
     }
     
     /**
@@ -287,6 +299,7 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity
         outState.putBoolean(KEY_SERVER_CHECKED, mServerIsChecked);
         outState.putBoolean(KEY_SERVER_CHECK_IN_PROGRESS, (!mServerIsValid && mOcServerChkOperation != null));
         outState.putBoolean(KEY_IS_SSL_CONN, mIsSslConn);
+        outState.putBoolean(KEY_PASSWORD_VISIBLE, isPasswordVisible());
         outState.putInt(KEY_AUTH_STATUS_ICON, mAuthStatusIcon);
         outState.putInt(KEY_AUTH_STATUS_TEXT, mAuthStatusText);
         
@@ -434,15 +447,40 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity
      */
     private void onPasswordFocusChanged(TextView passwordInput, boolean hasFocus) {
         if (hasFocus) {
-            mViewPasswordButton.setVisibility(View.VISIBLE);
+            showViewPasswordButton();
         } else {
-            int input_type = InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD;
-            passwordInput.setInputType(input_type);
-            mViewPasswordButton.setVisibility(View.INVISIBLE);
+            hidePassword();
+            hidePasswordButton();
         }
     }
 
 
+    private void showViewPasswordButton() {
+        int drawable = android.R.drawable.ic_menu_view;
+        if (isPasswordVisible()) {
+            drawable = android.R.drawable.ic_secure;
+        }
+        mPasswordInput.setCompoundDrawablesWithIntrinsicBounds(0, 0, drawable, 0);
+    }
+
+    private boolean isPasswordVisible() {
+        return ((mPasswordInput.getInputType() & InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD) == InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+    }
+    
+    private void hidePasswordButton() {
+        mPasswordInput.setCompoundDrawablesWithIntrinsicBounds(0, 0, 0, 0);
+    }
+
+    private void showPassword() {
+        mPasswordInput.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD);
+        showViewPasswordButton();
+    }
+    
+    private void hidePassword() {
+        mPasswordInput.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        showViewPasswordButton();
+    }
+    
     
     /**
      * Cancels the authenticator activity
@@ -1088,30 +1126,25 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity
      * 
      * @param view      Refresh 'button'
      */
-    public void onRefreshClick(View view) {
+    public void onRefreshClick() {
         checkOcServer();
     }
+    
     
     
     /**
      * Called when the eye icon in the password field is clicked.
      * 
      * Toggles the visibility of the password in the field. 
-     * 
-     * @param view      'View password' 'button'
      */
-    public void onViewPasswordClick(View view) {
+    public void onViewPasswordClick() {
         int selectionStart = mPasswordInput.getSelectionStart();
         int selectionEnd = mPasswordInput.getSelectionEnd();
-        int input_type = mPasswordInput.getInputType();
-        if ((input_type & InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD) == InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD) {
-            input_type = InputType.TYPE_CLASS_TEXT
-                    | InputType.TYPE_TEXT_VARIATION_PASSWORD;
+        if (isPasswordVisible()) {
+            hidePassword();
         } else {
-            input_type = InputType.TYPE_CLASS_TEXT
-                    | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD;
+            showPassword();
         }
-        mPasswordInput.setInputType(input_type);
         mPasswordInput.setSelection(selectionStart, selectionEnd);
     }    
     
@@ -1142,13 +1175,11 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity
             mOAuthTokenEndpointText.setVisibility(View.VISIBLE);
             mUsernameInput.setVisibility(View.GONE);
             mPasswordInput.setVisibility(View.GONE);
-            mViewPasswordButton.setVisibility(View.GONE);
         } else {
             mOAuthAuthEndpointText.setVisibility(View.GONE);
             mOAuthTokenEndpointText.setVisibility(View.GONE);
             mUsernameInput.setVisibility(View.VISIBLE);
             mPasswordInput.setVisibility(View.VISIBLE);
-            mViewPasswordButton.setVisibility(View.INVISIBLE);
         }     
 
     }    
@@ -1196,23 +1227,21 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity
          */
         @Override
         public boolean onTouch(View view, MotionEvent event) {
-            if (event.getAction() == MotionEvent.ACTION_DOWN) {
-                Drawable rightDrawable = null;
-                if (view instanceof TextView) {
-                    Drawable[] drawables = ((TextView)view).getCompoundDrawables();
-                    if (drawables.length > 2) {
-                        rightDrawable = drawables[2];
-                    }
+            Drawable rightDrawable = null;
+            if (view instanceof TextView) {
+                Drawable[] drawables = ((TextView)view).getCompoundDrawables();
+                if (drawables.length > 2) {
+                    rightDrawable = drawables[2];
                 }
-                if (rightDrawable != null) {
-                    final int x = (int) event.getX();
-                    final int y = (int) event.getY();
-                    final Rect bounds = rightDrawable.getBounds();
-                    if (x >= (view.getRight() - bounds.width() - fuzz) && x <= (view.getRight() - view.getPaddingRight() + fuzz)
-                        && y >= (view.getPaddingTop() - fuzz) && y <= (view.getHeight() - view.getPaddingBottom()) + fuzz) {
-                        
-                        return onDrawableTouch(event);
-                    }
+            }
+            if (rightDrawable != null) {
+                final int x = (int) event.getX();
+                final int y = (int) event.getY();
+                final Rect bounds = rightDrawable.getBounds();
+                if (x >= (view.getRight() - bounds.width() - fuzz) && x <= (view.getRight() - view.getPaddingRight() + fuzz)
+                    && y >= (view.getPaddingTop() - fuzz) && y <= (view.getHeight() - view.getPaddingBottom()) + fuzz) {
+                    
+                    return onDrawableTouch(event);
                 }
             }
             return false;
