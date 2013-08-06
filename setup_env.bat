fb9a226be613cd7dbe6d6d5762d99af8a169c099
Luke Owncloud <owncloud@ohrt.org>
2013-08-06 13:46:53 +0200

    mc

--- setup_env.bat
+++ setup_env.bat
@@ -4,5 +4,5 @@ call android.bat update project -p actionbarsherlock\library --target 1
 call android.bat update project -p . --target 1
 copy /Y third_party\android-support-library\android-support-v4.jar actionbarsherlock\library\libs\android-support-v4.jar
 cd tests
-android.bat update test-project -m .. -p .
+call android.bat update test-project -m .. -p .
 cd ..
