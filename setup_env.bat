a34423d36759abacc0f4731c8ddde9d583b97242
David A. Velasco <dvelasco@solidgear.es>
2013-10-25 14:50:28 +0200

    Removed problematic target assignation in setup_env.bat, and improved setup of test project to avoid change of folders

--- setup_env.bat
+++ setup_env.bat
@@ -1,8 +1,6 @@
 git submodule init
 git submodule update
-call android.bat update project -p actionbarsherlock\library --target 1
-call android.bat update project -p . --target 1
+call android.bat update project -p actionbarsherlock\library
+call android.bat update project -p .
 copy /Y third_party\android-support-library\android-support-v4.jar actionbarsherlock\library\libs\android-support-v4.jar
-cd tests
-call android.bat update test-project -m .. -p .
-cd ..
+call android.bat update test-project -p tests -m ..
