ea26174440d1a1e3b16627dfd90b780c1fd87e1c
David A. Velasco <dvelasco@solidgear.es>
2013-10-28 12:49:28 +0100

    Added name to build.xml for ActionBarSherlock and fixed calls to git in Windows

--- setup_env.bat
+++ setup_env.bat
@@ -1,6 +1,6 @@
-git submodule init
-git submodule update
-call android.bat update project -p actionbarsherlock\library
+call git submodule init
+call git submodule update
+call android.bat update project -p actionbarsherlock\library -n ActionBarSherlock
 call android.bat update project -p .
 copy /Y third_party\android-support-library\android-support-v4.jar actionbarsherlock\library\libs\android-support-v4.jar
 call android.bat update test-project -p tests -m ..
--- setup_env.sh
+++ setup_env.sh
@@ -2,7 +2,7 @@
 
 git submodule init
 git submodule update
-android update project -p actionbarsherlock/library
+android update project -p actionbarsherlock/library -n ActionBarSherlock
 android update project -p .
 cp third_party/android-support-library/android-support-v4.jar actionbarsherlock/library/libs/android-support-v4.jar 
 android update test-project -p tests -m ..
