b3267c409b1e5ad5037d919f3248464534a92cea
David A. Velasco <dvelasco@solidgear.es>
2013-11-04 11:40:07 +0100

    Added workaround app project to environment setup files

--- setup_env.bat
+++ setup_env.bat
@@ -2,5 +2,6 @@ call git submodule init
 call git submodule update
 call android.bat update project -p actionbarsherlock\library -n ActionBarSherlock
 call android.bat update project -p .
+call android.bat update project -p oc_jb_workaround
 copy /Y third_party\android-support-library\android-support-v4.jar actionbarsherlock\library\libs\android-support-v4.jar
 call android.bat update test-project -p tests -m ..
--- setup_env.sh
+++ setup_env.sh
@@ -4,5 +4,6 @@ git submodule init
 git submodule update
 android update project -p actionbarsherlock/library -n ActionBarSherlock
 android update project -p .
+android update project -p oc_jb_workaround
 cp third_party/android-support-library/android-support-v4.jar actionbarsherlock/library/libs/android-support-v4.jar 
 android update test-project -p tests -m ..
