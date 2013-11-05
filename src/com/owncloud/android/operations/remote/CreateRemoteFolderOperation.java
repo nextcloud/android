67a7d8a303a70b7a25e6214eaa5cb6795cf4aff7
masensio <masensio@solidgear.es>
2013-11-05 14:52:49 +0100

    OC-1866: new class com.owncloud.android.operations.remote.CreateRemoteFolderOperation

---
+++ src/com/owncloud/android/operations/remote/CreateRemoteFolderOperation.java
@@ -0,0 +1,20 @@
+package com.owncloud.android.operations.remote;
+
+import com.owncloud.android.operations.RemoteOperation;
+import com.owncloud.android.operations.RemoteOperationResult;
+
+import eu.alefzero.webdav.WebdavClient;
+
+public class CreateRemoteFolderOperation extends RemoteOperation {
+
+    public CreateRemoteFolderOperation() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    protected RemoteOperationResult run(WebdavClient client) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
