afebd9625ea9ec2aeed352736a7237064efe04bf
masensio <masensio@solidgear.es>
2013-11-22 14:51:01 +0100

    OC-2168: Add new class REadRemoteFileOperation to library

---
+++ oc_framework/src/com/owncloud/android/oc_framework/operations/remote/ReadRemoteFileOperation.java
@@ -0,0 +1,19 @@
+package com.owncloud.android.oc_framework.operations.remote;
+
+import com.owncloud.android.oc_framework.network.webdav.WebdavClient;
+import com.owncloud.android.oc_framework.operations.RemoteOperation;
+import com.owncloud.android.oc_framework.operations.RemoteOperationResult;
+
+public class ReadRemoteFileOperation extends RemoteOperation {
+
+	public ReadRemoteFileOperation() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected RemoteOperationResult run(WebdavClient client) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
