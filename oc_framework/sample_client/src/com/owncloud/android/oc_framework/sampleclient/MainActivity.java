21f4c7e6a830f4d14b9c55ff5c7d00c67ee03528
David A. Velasco <dvelasco@solidgear.es>
2014-01-09 16:32:18 +0100

    Created empty sample project to show oc_framework functionality

---
+++ oc_framework/sample_client/src/com/owncloud/android/oc_framework/sampleclient/MainActivity.java
@@ -0,0 +1,15 @@
+package com.owncloud.android.oc_framework.sampleclient;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
