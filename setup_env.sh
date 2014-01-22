de367c009ae95b0d18624d0e2f87ff3b38db242b
David A. Velasco <dvelasco@solidgear.es>
2014-01-22 10:16:53 +0100

    Updated scrips files to setup environment

--- setup_env.sh
+++ setup_env.sh
@@ -3,7 +3,7 @@
 git submodule init
 git submodule update
 android update project -p actionbarsherlock/library -n ActionBarSherlock
-android update project -p oc_framework -n ownCloudFramework
+android update lib-project -p owncloud-android-library
 android update project -p .
 android update project -p oc_jb_workaround
 cp third_party/android-support-library/android-support-v4.jar actionbarsherlock/library/libs/android-support-v4.jar 
