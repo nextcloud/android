5d53f3c0ba97e6e30c1fa14cf7edc635fa13b433
David A. Velasco <dvelasco@solidgear.es>
2014-03-17 12:28:54 +0100

    Recursive refresh of folders in full synchronization does not retrieve information about shares

--- src/com/owncloud/android/operations/SynchronizeFolderOperation.java
+++ src/com/owncloud/android/operations/SynchronizeFolderOperation.java
@@ -192,11 +192,8 @@ public class SynchronizeFolderOperation extends RemoteOperation {
             sendLocalBroadcast(EVENT_SINGLE_FOLDER_CONTENTS_SYNCED, mLocalFolder.getRemotePath(), result);
         }
         
-        if (result.isSuccess() && mIsShareSupported) {
-            RemoteOperationResult shareResult = refreshSharesForFolder(client);
-            if (shareResult.getCode() != ResultCode.FILE_NOT_FOUND) {
-                result = shareResult;
-            } // else , keep the previous result ; being conservative for servers where Sharing API is supported, but disabled
+        if (result.isSuccess() && mIsShareSupported && !mSyncFullAccount) {
+            refreshSharesForFolder(client); // share result is ignored 
         }
         
         if (!mSyncFullAccount) {            
