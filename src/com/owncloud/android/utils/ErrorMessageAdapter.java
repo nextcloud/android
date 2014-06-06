cfe589231a558f3f7e5ce483ae46f1422182e8e1
masensio <masensio@solidgear.es>
2014-06-06 09:24:47 +0200

    Fix issue 534: missing string argument in ErrorMessageAdapter for R.string.error_upload_local_file_not_copied

--- src/com/owncloud/android/utils/ErrorMessageAdapter.java
+++ src/com/owncloud/android/utils/ErrorMessageAdapter.java
@@ -62,7 +62,8 @@ public class ErrorMessageAdapter {
                 if (result.getCode() == ResultCode.LOCAL_STORAGE_FULL
                         || result.getCode() == ResultCode.LOCAL_STORAGE_NOT_COPIED) {
                     message = String.format(res.getString(R.string.error__upload__local_file_not_copied), 
-                            ((UploadFileOperation) operation).getFileName());
+                            ((UploadFileOperation) operation).getFileName(), 
+                            res.getString(R.string.app_name));
                     
                 } else if (result.getCode() == ResultCode.QUOTA_EXCEEDED) {
                     message = res.getString(R.string.failed_upload_quota_exceeded_text);
