1dbba08c7be41a0a5fd58d3e221c4dfb4ec97499
David A. Velasco <dvelasco@solidgear.es>
2014-09-02 10:13:14 +0200

    Improved messages for operations on files not existing anymore in the server

--- src/com/owncloud/android/utils/ErrorMessageAdapter.java
+++ src/com/owncloud/android/utils/ErrorMessageAdapter.java
@@ -189,7 +189,10 @@ public class ErrorMessageAdapter {
             }
         } else if (operation instanceof MoveFileOperation) {
 
-            if (result.getCode() == ResultCode.INVALID_MOVE_INTO_DESCENDANT)  {
+            if (result.getCode() == ResultCode.FILE_NOT_FOUND) {
+                message = res.getString(R.string.move_file_not_found);
+                
+            } else if (result.getCode() == ResultCode.INVALID_MOVE_INTO_DESCENDANT)  {
                 message = res.getString(R.string.move_file_invalid_into_descendent);
 
             } else if (result.getCode() == ResultCode.INVALID_OVERWRITE) {
