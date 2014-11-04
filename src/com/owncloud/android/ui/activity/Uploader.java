2790c2379df2a4fcf01c9f3894eb90b6ab7d0415
jabarros <jabarros@solidgear.es>
2014-11-04 11:15:59 +0100

    Fixed. App crashes when accesing to an account where you have not accessed after uploading a file in another

--- src/com/owncloud/android/ui/activity/Uploader.java
+++ src/com/owncloud/android/ui/activity/Uploader.java
@@ -476,11 +476,9 @@ public class Uploader extends SherlockListActivity implements OnItemClickListene
                 mParents.add(dir);
         }
         //Make sure that path still exists, if it doesn't pop the stack and try the previous path
-        if (mParents.size() > 1) {
-            while(!mStorageManager.fileExists(generatePath(mParents))){
+            while(!mStorageManager.fileExists(generatePath(mParents)) && mParents.size() > 1){
                 mParents.pop();
             }
-        }
     }
 
     
