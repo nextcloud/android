bceecf9d176d829177ace887c8468f07dc1aeb4d
tobiasKaminsky <tobias@kaminsky.me>
2015-01-22 21:21:02 +0100

    - moved timeout to PINCODE_TIMEOUT

--- src/com/owncloud/android/authentication/PinCheck.java
+++ src/com/owncloud/android/authentication/PinCheck.java
@@ -10,14 +10,15 @@ public class PinCheck extends Activity {
 
     private static Long timestamp = 0l;
     private static Long lastStart = 0l;
+    private static int PINCODE_TIMEOUT = 10000;
     
     public static void setUnlockTimestamp() {
         timestamp = System.currentTimeMillis();
     }
 
     public static boolean checkIfPinEntry(){
-        if ((System.currentTimeMillis() - timestamp) > 10000 &&
-            (System.currentTimeMillis() - lastStart) > 10000){
+        if ((System.currentTimeMillis() - timestamp) > PINCODE_TIMEOUT &&
+            (System.currentTimeMillis() - lastStart) > PINCODE_TIMEOUT){
             SharedPreferences appPrefs = PreferenceManager.getDefaultSharedPreferences(MainApp.getAppContext());
             if (appPrefs.getBoolean("set_pincode", false)) {
                 lastStart = System.currentTimeMillis();
