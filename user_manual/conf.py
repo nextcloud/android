931bc1e62cb4629e56621daa5715a841a4e53c79
Daniel Molkentin <daniel@molkentin.de>
2015-01-27 23:29:49 +0100

    manual: omit ocdoc/* during doc generation

--- user_manual/conf.py
+++ user_manual/conf.py
@@ -64,7 +64,7 @@ release = '@VERSION@'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build','scripts/*']
+exclude_patterns = ['_build','scripts/*', 'ocdoc/*']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
