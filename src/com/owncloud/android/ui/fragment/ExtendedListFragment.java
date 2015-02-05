f728c502c55c85f950d142a99fac2fce49c4a9a3
David A. Velasco <dvelasco@solidgear.es>
2015-02-05 16:34:15 +0100

    Fixed some log levels

--- src/com/owncloud/android/ui/fragment/ExtendedListFragment.java
+++ src/com/owncloud/android/ui/fragment/ExtendedListFragment.java
@@ -103,7 +103,7 @@ implements OnItemClickListener, OnEnforceableRefreshListener {
     
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Log_OC.e(TAG, "onCreateView");
+        Log_OC.d(TAG, "onCreateView");
 
         View v = inflater.inflate(R.layout.list_fragment, null);
         
@@ -154,7 +154,7 @@ implements OnItemClickListener, OnEnforceableRefreshListener {
     @Override
     public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
-        Log_OC.e(TAG, "onSaveInstanceState()");
+        Log_OC.d(TAG, "onSaveInstanceState()");
         savedInstanceState.putInt(KEY_SAVED_LIST_POSITION, getReferencePosition());
         savedInstanceState.putIntegerArrayList(KEY_INDEXES, mIndexes);
         savedInstanceState.putIntegerArrayList(KEY_FIRST_POSITIONS, mFirstPositions);
