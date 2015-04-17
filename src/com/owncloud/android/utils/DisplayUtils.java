eac77d8749e037c0e75a3c9f507409c7441f3cf5
masensio <masensio@solidgear.es>
2015-04-17 14:10:41 +0200

    Show an error message for invalid server url

--- src/com/owncloud/android/utils/DisplayUtils.java
+++ src/com/owncloud/android/utils/DisplayUtils.java
@@ -266,14 +266,14 @@ public class DisplayUtils {
             } else if (url.indexOf("@") != -1) {
                 hostStart = url.indexOf("@") + "@".length();
             }
-            
+
             int hostEnd = url.substring(hostStart).indexOf("/");
             // Handle URL which doesn't have a path (path is implicitly '/')
             hostEnd = (hostEnd == -1 ? url.length() : hostStart + hostEnd);
-            
+
             String host = url.substring(hostStart, hostEnd);
             host = (toASCII ? IDN.toASCII(host) : IDN.toUnicode(host));
-            
+
             return url.substring(0, hostStart) + host + url.substring(hostEnd);
         } else {
             return url;
