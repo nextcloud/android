65ff5a374aef368792a0238d59a598d0cb45437a
masensio <masensio@solidgear.es>
2015-06-04 18:41:07 +0200

    Fix, title is not correct when menu is shown and rotate

--- src/com/owncloud/android/ui/activity/FileActivity.java
+++ src/com/owncloud/android/ui/activity/FileActivity.java
@@ -373,7 +373,7 @@ public class FileActivity extends ActionBarActivity
             /** Called when a drawer has settled in a completely closed state. */
             public void onDrawerClosed(View view) {
                 super.onDrawerClosed(view);
-                updateActionBarTitleAndHomeButton(null);
+                updateActionBarTitleAndHomeButton(mFile);
                 invalidateOptionsMenu();
             }
 
@@ -406,9 +406,16 @@ public class FileActivity extends ActionBarActivity
         /// choose the appropiate title
         if (chosenFile == null) {
             // mFile determines the title
-            inRoot = (mFile == null || mFile.getParentId() == 0);
+            OCFile currentDir = null;
+            if (mFile != null && mFile.isFolder()) {
+                currentDir = mFile;
+            } else {
+                currentDir = getStorageManager().getFileById(mFile.getParentId());
+            }
+            inRoot = (mFile == null || currentDir.getParentId() == 0);
             if (!inRoot) {
-                title = mFile.getFileName();
+                    title = mFile.getFileName();
+
             }
 
         } else {
