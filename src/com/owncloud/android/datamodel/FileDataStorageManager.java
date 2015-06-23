a54f9fdcae7c2bcbd9fda423dd0f5d3a662a1ce6
LukeOwncloud <owncloud@ohrt.org>
2015-06-23 13:49:58 +0200

    return false if remove failed
    if OCFile is null --> cannot delete ==> return false.

--- src/com/owncloud/android/datamodel/FileDataStorageManager.java
+++ src/com/owncloud/android/datamodel/FileDataStorageManager.java
@@ -443,7 +443,7 @@ public class FileDataStorageManager {
 
 
     public boolean removeFile(OCFile file, boolean removeDBData, boolean removeLocalCopy) {
-        boolean success = true;
+        boolean success = false;
         if (file != null) {
             if (file.isFolder()) {
                 success = removeFolder(file, removeDBData, removeLocalCopy);
@@ -491,7 +491,7 @@ public class FileDataStorageManager {
 
 
     public boolean removeFolder(OCFile folder, boolean removeDBData, boolean removeLocalContent) {
-        boolean success = true;
+        boolean success = false;
         if (folder != null && folder.isFolder()) {
             if (removeDBData && folder.getFileId() != -1) {
                 success = removeFolderInDb(folder);
