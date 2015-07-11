29156ff73b1fb336a47929fa0c9f8c7ddea451dd
tobiasKaminsky <tobias@kaminsky.me>
2015-07-11 09:31:35 +0200

    add checker pattern for FileList

--- src/com/owncloud/android/ui/adapter/FileListListAdapter.java
+++ src/com/owncloud/android/ui/adapter/FileListListAdapter.java
@@ -30,6 +30,7 @@ import android.accounts.Account;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.preference.PreferenceManager;
 import android.text.format.DateUtils;
@@ -321,6 +322,13 @@ public class FileListListAdapter extends BaseAdapter implements ListAdapter {
                             task.execute(file);
                         }
                     }
+
+                    if (file.getMimetype().equalsIgnoreCase("image/png")){
+                        Drawable backrepeat = mContext.getResources().
+                                              getDrawable(R.drawable.backrepeat);
+                        fileIcon.setBackground(backrepeat);
+                    }
+
                 } else {
                     fileIcon.setImageResource(DisplayUtils.getFileTypeIconId(file.getMimetype(),
                             file.getFileName()));
