b5c1cf9fe91f0ccfbbb6d627124c352b016d8bcd
masensio <masensio@solidgear.es>
2015-08-10 14:58:56 +0200

    Media player stops when its current file is deleted

--- src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.java
+++ src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.java
@@ -38,6 +38,9 @@ import android.content.SharedPreferences;
 import android.content.SyncRequest;
 import android.content.pm.PackageManager;
 import android.content.res.Resources.NotFoundException;
+import android.database.Cursor;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
@@ -167,6 +170,8 @@ public class FileDisplayActivity extends HookActivity
     private OCFile mWaitingToSend;
 
     private Collection<MenuItem> mDrawerMenuItemstoShowHideList;
+    private MediaServiceBinder mMediaServiceBinder =  null;
+    private MediaServiceConnection mMediaServiceConnection = null;
 
     private MediaServiceBinder mMediaServiceBinder =  null;
     private MediaServiceConnection mMediaServiceConnection = null;
@@ -1637,6 +1642,14 @@ public class FileDisplayActivity extends HookActivity
         }
     };
 
+
+
+
+    @Override
+    public void onSavedCertificate() {
+        startSyncFolderOperation(getCurrentDir(), false);
+    }
+
     /**
      * Updates the view associated to the activity after the finish of some operation over files
      * in the current account.
@@ -1727,7 +1740,10 @@ public class FileDisplayActivity extends HookActivity
 
     public void setMediaServiceConnection() {
         mMediaServiceConnection = newMediaConnection();// mediaServiceConnection;
-        bindService(new Intent(this, MediaService.class), mMediaServiceConnection, Context.BIND_AUTO_CREATE);
+        if (mMediaServiceConnection != null) {
+            bindService(new Intent(this, MediaService.class), mMediaServiceConnection,
+                    Context.BIND_AUTO_CREATE);
+        }
     }
 
     private void tryStopPlaying(OCFile file) {
