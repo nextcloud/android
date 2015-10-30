b5ffd5cd7cdd1b08cd11478ff0affb763af1fb4d
tobiasKaminsky <tobias@kaminsky.me>
2015-10-30 12:52:12 +0100

    fixed #1230

--- src/com/owncloud/android/ui/fragment/OCFileListFragment.java
+++ src/com/owncloud/android/ui/fragment/OCFileListFragment.java
@@ -492,10 +492,11 @@ public class OCFileListFragment extends ExtendedListFragment {
                     } else {
                         mContainerActivity.getFileOperationsHelper().openFile(file);
                     }
-                    
+                } else {
+                    // automatic download, preview on finish
+                    ((FileDisplayActivity) mContainerActivity).startDownloadForPreview(file);
                 }
             }
-
         } else {
             Log_OC.d(TAG, "Null object in ListAdapter!!");
         }
