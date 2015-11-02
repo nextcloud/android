42986aadd4c3c895bd6b0c9886424851ae3dc17a
tobiasKaminsky <tobias@kaminsky.me>
2015-11-02 09:32:20 +0100

    "set picture as" finished

--- src/com/owncloud/android/ui/helpers/FileOperationsHelper.java
+++ src/com/owncloud/android/ui/helpers/FileOperationsHelper.java
@@ -44,7 +44,6 @@ import com.owncloud.android.lib.common.utils.Log_OC;
 import com.owncloud.android.lib.resources.shares.OCShare;
 import com.owncloud.android.lib.resources.shares.ShareType;
 import com.owncloud.android.lib.resources.status.OwnCloudVersion;
-import com.owncloud.android.providers.FileContentProvider;
 import com.owncloud.android.services.OperationsService;
 import com.owncloud.android.services.observer.FileObserverService;
 import com.owncloud.android.ui.activity.FileActivity;
