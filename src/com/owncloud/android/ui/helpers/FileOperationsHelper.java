d12d3d76a10c70b3faa8a2d60f8920bba8bfff41
tobiasKaminsky <tobias@kaminsky.me>
2015-11-19 20:30:02 +0100

    extract string ressource

--- src/com/owncloud/android/ui/helpers/FileOperationsHelper.java
+++ src/com/owncloud/android/ui/helpers/FileOperationsHelper.java
@@ -528,7 +528,8 @@ public class FileOperationsHelper {
                 Intent intent = new Intent(Intent.ACTION_ATTACH_DATA);
                 intent.setDataAndType(sendUri, file.getMimetype());
                 intent.putExtra("mimeType", file.getMimetype());
-                mFileActivity.startActivityForResult(Intent.createChooser(intent, "Set As"), 200);
+                mFileActivity.startActivityForResult(Intent.createChooser(intent,
+                                                    mFileActivity.getString(R.string.set_as)), 200);
             } else {
 //                 TODO re-enable after resized images is available
 //                Uri sendUri = Uri.parse("content://" + DiskLruImageCacheFileProvider.AUTHORITY + file.getRemotePath());
