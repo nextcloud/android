cbbc14cde4d8c65892ebe589ef88fadf56b112b2
刘胜坤 <allan@dingyantec.com>
2015-12-14 19:59:27 +0800

    Add select all files when upload files to remote server.

--- src/com/owncloud/android/ui/fragment/LocalFileListFragment.java
+++ src/com/owncloud/android/ui/fragment/LocalFileListFragment.java
@@ -98,6 +98,18 @@ public class LocalFileListFragment extends ExtendedListFragment {
         
         Log_OC.i(TAG, "onActivityCreated() stop");
     }
+
+    public void CheckAllFile(boolean value)
+    {
+        ListView lst = (ListView)getListView();
+        int pos;
+        for (pos = 0; pos < lst.getCount(); pos++) {
+            File file = (File) mAdapter.getItem(pos);
+            if (file.isFile()) {
+                lst.setItemChecked(pos, value);
+            }
+        }
+    }
     
     /**
      * Checks the file clicked over. Browses inside if it is a directory.
