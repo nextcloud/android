00fcca4d5bf5a81ad5fc95f8e71242eaddace895
tobiasKaminsky <tobias@kaminsky.me>
2015-12-22 20:22:41 +0100

    corrected indention

--- src/com/owncloud/android/ui/activity/FileActivity.java
+++ src/com/owncloud/android/ui/activity/FileActivity.java
@@ -399,8 +399,8 @@ public class FileActivity extends AppCompatActivity
         mDrawerItems.add(new NavigationDrawerItem(mDrawerTitles[2], mDrawerContentDescriptions[2],
                 R.drawable.ic_action_settings));
         // Logs
-            mDrawerItems.add(new NavigationDrawerItem(mDrawerTitles[3],
-                    mDrawerContentDescriptions[3],R.drawable.ic_log));
+        mDrawerItems.add(new NavigationDrawerItem(mDrawerTitles[3], mDrawerContentDescriptions[3],
+                R.drawable.ic_log));
 
         // setting the nav drawer list adapter
         mNavigationDrawerAdapter = new NavigationDrawerListAdapter(getApplicationContext(), this,
