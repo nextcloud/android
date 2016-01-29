d6653ef68465d6ac822149c1983888e519aa5b9b
masensio <masensio@solidgear.es>
2016-01-29 10:57:58 +0100

    Add android:manageSpaceActivity to AndroidManifest.xml. Create empty activity ManageSpaceActivity.java

---
+++ src/com/owncloud/android/ui/activity/ManageSpaceActivity.java
@@ -0,0 +1,17 @@
+package com.owncloud.android.ui.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.owncloud.android.R;
+
+public class ManageSpaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_space);
+    }
+
+
+}
