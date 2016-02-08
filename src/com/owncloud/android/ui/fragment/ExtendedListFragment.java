8d400a6577d15a1bf2a4bc165564a91f53623829
tobiasKaminsky <tobias@kaminsky.me>
2016-02-08 08:58:59 +0100

    save gridColumns on start/stop

--- src/com/owncloud/android/ui/fragment/ExtendedListFragment.java
+++ src/com/owncloud/android/ui/fragment/ExtendedListFragment.java
@@ -21,7 +21,9 @@
 package com.owncloud.android.ui.fragment;
 
 import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
