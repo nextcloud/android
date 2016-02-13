c82511ce1996f827ab72853f2859183a405517bf
Bartosz Przybylski <bartoszp@wolfram.com>
2016-02-13 20:37:22 +0100

    tabs to spaces

--- src/com/owncloud/android/MainApp.java
+++ src/com/owncloud/android/MainApp.java
@@ -125,6 +125,7 @@ public class MainApp extends Application {
             @Override
             public void onActivityStopped(Activity activity) {
                 Log_OC.d(activity.getClass().getSimpleName(), "onStop() ending" );
+                WhatsNewActivity.runIfNeeded(activity);
                 PassCodeManager.getPassCodeManager().onActivityStopped(activity);
             }
 
