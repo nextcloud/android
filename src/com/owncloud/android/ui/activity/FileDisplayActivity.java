a0596e0ca193e74f712fea88b57e16fd7611d549
Andy Scherzinger <info@andy-scherzinger.de>
2016-04-05 10:57:30 +0200

    merged avatar branch + circular avatar in drawer header

--- src/com/owncloud/android/ui/activity/FileDisplayActivity.java
+++ src/com/owncloud/android/ui/activity/FileDisplayActivity.java
@@ -302,7 +302,7 @@ public class FileDisplayActivity extends HookActivity
             setFile(file);
 
             if (mAccountWasSet) {
-                setUsernameInDrawer(getAccount().name);
+                setAccountInDrawer(getAccount());
             }
 
             if (!stateWasRecovered) {
