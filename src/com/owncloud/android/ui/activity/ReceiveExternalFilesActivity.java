7ccfa6a5d732797e3861af6fadcc783dd61d8258
Andy Scherzinger <info@andy-scherzinger.de>
2016-04-14 13:45:04 +0200

    removed now unused code

--- src/com/owncloud/android/ui/activity/ReceiveExternalFilesActivity.java
+++ src/com/owncloud/android/ui/activity/ReceiveExternalFilesActivity.java
@@ -457,9 +457,6 @@ public class ReceiveExternalFilesActivity extends FileActivity
     }
 
     private Vector<OCFile> sortFileList(Vector<OCFile> files) {
-        SharedPreferences sharedPreferences = android.preference.PreferenceManager
-                .getDefaultSharedPreferences(this);
-
         // Read sorting order, default to sort by name ascending
         FileStorageUtils.mSortOrder = PreferenceManager.getSortOrder(this);
         FileStorageUtils.mSortAscending = PreferenceManager.getSortAscending(this);
