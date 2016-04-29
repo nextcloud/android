6bda29bc7f1f1bbd126afc0f814bc495016f5cbd
Andy Scherzinger <info@andy-scherzinger.de>
2016-04-29 14:52:01 +0200

    shorten setup to git submodule init/update

--- setup_env.bat
+++ setup_env.bat
@@ -1,25 +1,13 @@
 @echo off
 
-:: Use argument to decide which build system should be used
-if "%1" == "gradle" goto initForGradle
-goto invalidInput
-
 :initForGradle
-echo "Executing Gradle setup..."
+echo "Creating development environment setup..."
 goto initDefault
 
 :initDefault
 call git submodule init
 call git submodule update
-call android.bat update lib-project -p owncloud-android-library
-call android.bat update project -p .
-call android.bat update project -p oc_jb_workaround
-call android.bat update test-project -p tests -m ..
 goto complete
-
-:invalidInput
-echo "Input argument invalid."
-echo "Usage: %0 [gradle]."goto exit
 
 :complete
 echo "...setup complete."
--- setup_env.sh
+++ setup_env.sh
@@ -1,30 +1,12 @@
 #!/bin/bash -e
 
-
 function initDefault {
     git submodule init
     git submodule update
-    android update lib-project -p owncloud-android-library
-    android update project -p .
-    android update project -p oc_jb_workaround
-    android update test-project -p tests -m ..
 }
 
-#No args
-if [ $# -lt 1 ]; then
-        echo "No args found"
-        echo "Usage : $0 [gradle]"        exit
-fi
-
-#checking args
-case "$1" in
-
-    "gradle")  echo  "Creating gradle environment"
-        initDefault
-        ;;
-
-    *)  echo "Argument not recognized"
-        echo "Usage : $0 [gradle]"       ;;
-esac
+echo  "Creating development environment setup..."
+initDefault
+echo "...setup complete."
 
 exit
