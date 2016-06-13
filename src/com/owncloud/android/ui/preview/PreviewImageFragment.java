d06c4ef8d9358da9b4dc05d55953af6de53da1b1
tobiasKaminsky <tobias@kaminsky.me>
2016-06-13 21:16:27 +0200

    changes due to CR

--- src/com/owncloud/android/ui/preview/PreviewImageFragment.java
+++ src/com/owncloud/android/ui/preview/PreviewImageFragment.java
@@ -499,13 +499,13 @@ public class PreviewImageFragment extends FileFragment {
                 Log_OC.d(TAG, "Showing image with resolution " + bitmap.getWidth() + "x" +
                         bitmap.getHeight());
 
-                if (result.ocFile.getMimetype().equalsIgnoreCase("image/png")){
+                if (result.ocFile.getMimetype().equalsIgnoreCase("image/png")) {
                     Drawable backrepeat = getResources().getDrawable(R.drawable.backrepeat);
                     imageView.setBackground(backrepeat);
                 }
 
-                if (result.ocFile.getMimetype().equalsIgnoreCase("image/gif")){
-                    imageView.setGifImage(result.ocFile);
+                if (result.ocFile.getMimetype().equalsIgnoreCase("image/gif")) {
+                    imageView.setGIFImage(result.ocFile);
                 } else {
                     imageView.setImageBitmap(bitmap);
                 }
