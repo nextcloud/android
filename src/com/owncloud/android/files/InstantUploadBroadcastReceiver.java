54d1c42ea7c6cd3fcf0ed8a0e6f3181cd25887ea
Andy Scherzinger <info@andy-scherzinger.de>
2016-07-09 20:48:17 +0200

    fix AccountUtils calls

--- src/com/owncloud/android/files/InstantUploadBroadcastReceiver.java
+++ src/com/owncloud/android/files/InstantUploadBroadcastReceiver.java
@@ -92,7 +92,7 @@ public class InstantUploadBroadcastReceiver extends BroadcastReceiver {
             return;
         }
 
-        Account account = FileStorageUtils.getInstantUploadAccount(context);
+        Account account = AccountUtils.getInstantUploadAccount(context);
         if (account == null) {
             Log_OC.w(TAG, "No account found for instant upload, aborting");
             return;
@@ -172,7 +172,7 @@ public class InstantUploadBroadcastReceiver extends BroadcastReceiver {
             return;
         }
 
-        Account account = FileStorageUtils.getInstantVideoUploadAccount(context);
+        Account account = AccountUtils.getInstantVideoUploadAccount(context);
         if (account == null) {
             Log_OC.w(TAG, "No account found for instant upload, aborting");
             return;
