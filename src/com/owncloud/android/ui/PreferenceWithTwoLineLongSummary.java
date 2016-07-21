62f80f834d559bcf13789c525850d7c54b4fbfa6
Andy Scherzinger <info@andy-scherzinger.de>
2016-07-21 16:54:05 +0200

    fixed copyright

--- src/com/owncloud/android/ui/PreferenceWithTwoLineLongSummary.java
+++ src/com/owncloud/android/ui/PreferenceWithTwoLineLongSummary.java
@@ -1,10 +1,10 @@
 /**
- *   ownCloud Android client application
+ *   Nextcloud Android client application
  *
- *   Copyright (C) 2014 ownCloud Inc.
+ *   Copyright (C) 2016 Nextcloud
  *
  *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License version 2,
+ *   it under the terms of the GNU General Public License version 3,
  *   as published by the Free Software Foundation.
  *
  *   This program is distributed in the hope that it will be useful,
@@ -14,7 +14,6 @@
  *
  *   You should have received a copy of the GNU General Public License
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
  */
 
 package com.owncloud.android.ui;
