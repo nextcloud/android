2d05f45a6d78894f555be3779f4795de2d0136e8
Andy Scherzinger <info@andy-scherzinger.de>
2016-08-02 17:53:27 +0200

    minor formattings

--- src/com/owncloud/android/ui/fragment/ShareFileFragment.java
+++ src/com/owncloud/android/ui/fragment/ShareFileFragment.java
@@ -489,19 +489,16 @@ public class ShareFileFragment extends Fragment
      * @param shareView Root view in the fragment.
      */
     private void initHideFileListingListener(View shareView) {
-        mOnHideFileListingPermissionInteractionListener =
-                new OnHideFileListingPermissionInteractionListener();
+        mOnHideFileListingPermissionInteractionListener = new OnHideFileListingPermissionInteractionListener();
 
         ((SwitchCompat) shareView.findViewById(R.id.shareViaLinkHideListPermissionSwitch)).
                 setOnCheckedChangeListener(mOnHideFileListingPermissionInteractionListener);
-
     }
 
     /**
      * Listener for user actions that start any update on the edit permissions for the public link.
      */
-    private class OnEditPermissionInteractionListener
-            implements CompoundButton.OnCheckedChangeListener {
+    private class OnEditPermissionInteractionListener implements CompoundButton.OnCheckedChangeListener {
 
         /**
          * Called by R.id.shareViaLinkEditPermissionSwitch to set or clear the edit permission.
@@ -535,8 +532,7 @@ public class ShareFileFragment extends Fragment
     /**
      * Listener for user actions that start any update on the hide file listing permissions for the public link.
      */
-    private class OnHideFileListingPermissionInteractionListener
-            implements CompoundButton.OnCheckedChangeListener {
+    private class OnHideFileListingPermissionInteractionListener implements CompoundButton.OnCheckedChangeListener {
 
         /**
          * Called by R.id.shareViaLinkHideListPermissionSwitch to set or clear the edit permission.
@@ -599,7 +595,6 @@ public class ShareFileFragment extends Fragment
         mListener = null;
     }
 
-
     /**
      * Get known server capabilities from DB
      * <p/>
@@ -612,7 +607,6 @@ public class ShareFileFragment extends Fragment
                     getCapability(mAccount.name);
         }
     }
-
 
     /**
      * Get users and groups from the DB to fill in the "share with" list.
