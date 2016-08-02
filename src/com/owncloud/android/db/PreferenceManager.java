1ce514972be077caade1a9dffd5c2b18364e9252
tobiasKaminsky <tobias@kaminsky.me>
2016-08-02 21:36:11 +0200

    "move upload" is first default when on first startup

--- src/com/owncloud/android/db/PreferenceManager.java
+++ src/com/owncloud/android/db/PreferenceManager.java
@@ -137,7 +137,7 @@ public abstract class PreferenceManager {
      */
     public static int getUploaderBehaviour(Context context) {
         return getDefaultSharedPreferences(context)
-                .getInt(AUTO_PREF__UPLOADER_BEHAVIOR, 0);
+                .getInt(AUTO_PREF__UPLOADER_BEHAVIOR, 1);
     }
 
     /**
