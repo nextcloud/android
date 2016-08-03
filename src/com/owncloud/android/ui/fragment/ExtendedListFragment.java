a8c3311e69b3950db8d1c3220b3bc4a19f487c3b
Andy Scherzinger <info@andy-scherzinger.de>
2016-08-03 19:44:40 +0200

    CR

--- src/com/owncloud/android/ui/fragment/ExtendedListFragment.java
+++ src/com/owncloud/android/ui/fragment/ExtendedListFragment.java
@@ -20,10 +20,7 @@
 
 package com.owncloud.android.ui.fragment;
 
-import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
