c206c944bcb05c941e605c07f365d67fd71a8f25
tobiasKaminsky <tobias@kaminsky.me>
2016-09-04 10:52:32 +0200

    links are now extracted to setup.xml

--- src/com/owncloud/android/ui/activity/ParticipateActivity.java
+++ src/com/owncloud/android/ui/activity/ParticipateActivity.java
@@ -1,16 +1,12 @@
 package com.owncloud.android.ui.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
-import android.text.util.Linkify;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.owncloud.android.R;
-import com.owncloud.android.datamodel.UploadsStorageManager;
-import com.owncloud.android.ui.fragment.UploadListFragment;
 
 /**
  * Created by tobi on 03.09.16.
@@ -34,21 +30,33 @@ public class ParticipateActivity extends FileActivity {
         TextView betaView = (TextView) findViewById(R.id.participate_betaView);
         if (betaView != null) {
             betaView.setMovementMethod(LinkMovementMethod.getInstance());
+            betaView.setText(Html.fromHtml(getString(R.string.participate_beta,
+                                           getString(R.string.fdroid_beta_link),
+                                           getString(R.string.beta_apk_link))));
         }
 
         TextView rcView = (TextView) findViewById(R.id.participate_rcView);
         if (rcView != null) {
             rcView.setMovementMethod(LinkMovementMethod.getInstance());
+            rcView.setText(Html.fromHtml(getString(R.string.participate_release_candidate,
+                                         getString(R.string.play_store_register_beta),
+                                         getString(R.string.fdroid_link))));
         }
 
         TextView participateView = (TextView) findViewById(R.id.participate_participateView);
         if (participateView != null) {
             participateView.setMovementMethod(LinkMovementMethod.getInstance());
+            participateView.setText(Html.fromHtml(getString(R.string.participate_participate,
+                                                  getString(R.string.irc_weblink),
+                                                  getString(R.string.help_link))));
         }
 
         TextView contributeView = (TextView) findViewById(R.id.participate_contributeView);
         if (contributeView != null) {
             contributeView.setMovementMethod(LinkMovementMethod.getInstance());
+            contributeView.setText(Html.fromHtml(getString(R.string.participate_contribute,
+                                                 getString(R.string.contributing_link),
+                                                 getString(R.string.irc_weblink))));
         }
     }
 
