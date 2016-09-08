5977fed28dc36d6e7658a87f68a2759c8d98a4da
Andy Scherzinger <info@andy-scherzinger.de>
2016-09-08 19:34:39 +0200

    MagicUrls for Login + Unit tests for the parser

---
+++ src/com/owncloud/android/authentication/LoginUrlInfo.java
@@ -0,0 +1,10 @@
+package com.owncloud.android.authentication;
+
+/**
+ * Data object holding the login url fields.
+ */
+public class LoginUrlInfo {
+    String serverAddress;
+    String username;
+    String password;
+}
