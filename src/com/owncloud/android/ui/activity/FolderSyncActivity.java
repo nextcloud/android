20dbdb872fe79d4f235f9a3a2a8f9dd65b047558
Andy Scherzinger <info@andy-scherzinger.de>
2016-09-21 15:43:41 +0200

    initial add for recycler view implementation with headers (WIP)

--- src/com/owncloud/android/ui/activity/FolderSyncActivity.java
+++ src/com/owncloud/android/ui/activity/FolderSyncActivity.java
@@ -1,17 +1,45 @@
+/**
+ *   Nextcloud Android client application
+ *
+ *   @author Andy Scherzinger
+ *   Copyright (C) 2016 Andy Scherzinger
+ *   Copyright (C) 2016 Nextcloud
+ *
+ *   This program is free software; you can redistribute it and/or
+ *   modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ *   License as published by the Free Software Foundation; either
+ *   version 3 of the License, or any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public
+ *   License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.owncloud.android.ui.activity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.owncloud.android.MainApp;
 import com.owncloud.android.R;
+import com.owncloud.android.ui.adapter.FolderSyncAdapter;
 
 /**
  * Activity displaying all auto-synced folders and/or instant upload media folders.
  */
 public class FolderSyncActivity extends DrawerActivity {
     private static final String TAG = FolderSyncActivity.class.getSimpleName();
+    private RecyclerView mRecyclerView;
+    private FolderSyncAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +59,24 @@ public class FolderSyncActivity extends DrawerActivity {
 
     private void setupContent() {
         // TODO setup/initialize UI
+        mRecyclerView = (RecyclerView) findViewById(android.R.id.list);
+
+        final int gridWidth = 4;
+        mAdapter = new FolderSyncAdapter(this, gridWidth, new FolderSyncAdapter.ClickListener() {
+            @Override
+            public void onClick(View view, int section, int relative, int absolute) {
+                selectItem(FolderSyncActivity.this);
+            }
+        }, mRecyclerView);
+
+        final GridLayoutManager lm = new GridLayoutManager(this, gridWidth);
+        mAdapter.setLayoutManager(lm);
+        mRecyclerView.setLayoutManager(lm);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+    public static void selectItem(final Activity context) {
+        return;
     }
 
     @Override
