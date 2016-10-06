89e87c386511ca74f5bb5b6f7f49dbf7cbfffe32
AndyScherzinger <info@andy-scherzinger.de>
2016-10-06 21:52:10 +0200

    missed to push ViewType enum

---
+++ src/com/owncloud/android/ui/adapter/ViewType.java
@@ -0,0 +1,10 @@
+package com.owncloud.android.ui.adapter;
+
+/**
+ * Enumeration of available/supported view types for file list/grid items
+ */
+public enum ViewType {
+    LIST_ITEM,
+    GRID_IMAGE,
+    GRID_ITEM
+}
