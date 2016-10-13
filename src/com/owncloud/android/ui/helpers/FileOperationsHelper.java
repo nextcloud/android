bb823fa4b175ab750d8322711d3f7995fcc4a32e
Iskra Delta <iskradelta@no-reply.github.com>
2016-10-13 10:57:15 +0000

    FindBugs appeased

--- src/com/owncloud/android/ui/helpers/FileOperationsHelper.java
+++ src/com/owncloud/android/ui/helpers/FileOperationsHelper.java
@@ -52,8 +52,10 @@ import com.owncloud.android.ui.dialog.ShareLinkToDialog;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.List;
 import java.util.regex.Matcher;
