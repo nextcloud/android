6c12644cf70b5ffc796a2e3a7d1980adbfcf2254
Iskra Delta <iskradelta@no-reply.github.com>
2016-10-13 10:57:15 +0000

    FindBugs appeased

--- src/com/owncloud/android/ui/activity/LogHistoryActivity.java
+++ src/com/owncloud/android/ui/activity/LogHistoryActivity.java
@@ -42,6 +42,7 @@ import com.owncloud.android.utils.FileStorageUtils;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.ref.WeakReference;
