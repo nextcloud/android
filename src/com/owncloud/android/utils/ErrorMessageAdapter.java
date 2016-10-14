89137a081971fba95c3e3f40791e3dfcd1ad05b9
Iskra Delta <iskradelta@no-reply.github.com>
2016-10-14 10:09:13 +0000

    PMD appeased
    Some warnings have been supressed.

--- src/com/owncloud/android/utils/ErrorMessageAdapter.java
+++ src/com/owncloud/android/utils/ErrorMessageAdapter.java
@@ -311,5 +311,6 @@ public class ErrorMessageAdapter {
         return code == ResultCode.WRONG_CONNECTION ||
                 code == ResultCode.TIMEOUT ||
                 code == ResultCode.HOST_NOT_AVAILABLE;
+        }
     }
 }
