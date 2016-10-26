1341e3145481046e084102fed923ae276854c483
AndyScherzinger <info@andy-scherzinger.de>
2016-10-26 21:41:10 +0200

    header

--- src/com/owncloud/android/ui/activity/Preferences.java
+++ src/com/owncloud/android/ui/activity/Preferences.java
@@ -5,6 +5,7 @@
  *   @author David A. Velasco
  *   Copyright (C) 2011  Bartek Przybylski
  *   Copyright (C) 2016 ownCloud Inc.
+ *   Copyright (C) 2016 Nextcloud
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License version 2,
