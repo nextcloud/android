1320fc7961a98dad71e0cc916918e83f5764c412
AndyScherzinger <info@andy-scherzinger.de>
2016-11-02 00:30:50 +0100

    Fixes after rebase

--- src/com/owncloud/android/datamodel/ThumbnailsCacheManager.java
+++ src/com/owncloud/android/datamodel/ThumbnailsCacheManager.java
@@ -416,7 +416,7 @@ public class ThumbnailsCacheManager {
                         mImageKey = (String) params[1];
                     }
 
-                    if (BitmapUtils.isImage(mFile)) {
+                    if (MimeTypeUtil.isImage(mFile)) {
                         thumbnail = doFileInBackground(mFile);
                     }
                 }
@@ -448,10 +448,10 @@ public class ThumbnailsCacheManager {
                         if (mFile.isDirectory()) {
                             imageView.setImageResource(R.drawable.ic_menu_archive);
                         } else {
-                            if (BitmapUtils.isVideo(mFile)) {
+                            if (MimeTypeUtil.isVideo(mFile)) {
                                 imageView.setImageBitmap(ThumbnailsCacheManager.mDefaultVideo);
                             } else {
-                                imageView.setImageResource(MimetypeIconUtil.getFileTypeIconId(null, mFile.getName()));
+                                imageView.setImageResource(MimeTypeUtil.getFileTypeIconId(null, mFile.getName()));
                             }
                         }
                     }
