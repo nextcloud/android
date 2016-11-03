c9036c32700c1d5f6c0b77b3deab452d87a86da4
AndyScherzinger <info@andy-scherzinger.de>
2016-11-03 15:48:37 +0100

    firther coverage issue not merge from PMD branch

--- src/com/owncloud/android/utils/ExceptionHandler.java
+++ src/com/owncloud/android/utils/ExceptionHandler.java
@@ -1,12 +1,12 @@
 package com.owncloud.android.utils;
 
-import android.content.Context;
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 
-import com.owncloud.android.MainApp;
 import com.owncloud.android.ui.activity.ErrorReportActivity;
 
 import java.io.PrintWriter;
@@ -14,51 +14,47 @@ import java.io.StringWriter;
 
 // from https://stackoverflow.com/questions/23486627/catching-error-and-user-information#answer-23486834
 public class ExceptionHandler implements Thread.UncaughtExceptionHandler {
+    private final Activity mContext;
 
-    Context context;
-    private final String LINE_SEPARATOR = "\n";
+    private static final String TAG = com.owncloud.android.ui.errorhandling.ExceptionHandler.class.getSimpleName();
 
-    public ExceptionHandler() {
-        // TODO Auto-generated constructor stub
-        context = MainApp.getAppContext();
+    public ExceptionHandler(Activity context) {
+        mContext = context;
     }
 
     @Override
-    public void uncaughtException(Thread arg0, Throwable arg1) {
-        // TODO Auto-generated method stub
-
+    public void uncaughtException(Thread arg0, Throwable exception) {
+        Log.e(TAG, "ExceptionHandler caught UncaughtException", exception);
         StringWriter stackTrace = new StringWriter();
-        arg1.printStackTrace(new PrintWriter(stackTrace));
-        final StringBuilder errorReport = new StringBuilder();
-        errorReport.append("************ CAUSE OF ERROR ************\n\n");
-        errorReport.append(stackTrace.toString());
-
-        errorReport.append("\n************ DEVICE INFORMATION ***********\n");
-        errorReport.append("Brand: ");
-        errorReport.append(Build.BRAND);
-        errorReport.append(LINE_SEPARATOR);
-        errorReport.append("Device: ");
-        errorReport.append(Build.DEVICE);
-        errorReport.append(LINE_SEPARATOR);
-        errorReport.append("Model: ");
-        errorReport.append(Build.MODEL);
-        errorReport.append(LINE_SEPARATOR);
-        errorReport.append("Id: ");
-        errorReport.append(Build.ID);
-        errorReport.append(LINE_SEPARATOR);
-        errorReport.append("Product: ");
-        errorReport.append(Build.PRODUCT);
-        errorReport.append(LINE_SEPARATOR);
-        errorReport.append("\n************ FIRMWARE ************\n");
-        errorReport.append("SDK: ");
-        errorReport.append(Build.VERSION.SDK);
-        errorReport.append(LINE_SEPARATOR);
-        errorReport.append("Release: ");
-        errorReport.append(Build.VERSION.RELEASE);
-        errorReport.append(LINE_SEPARATOR);
-        errorReport.append("Incremental: ");
-        errorReport.append(Build.VERSION.INCREMENTAL);
-        errorReport.append(LINE_SEPARATOR);
+        exception.printStackTrace(new PrintWriter(stackTrace));
+        final StringBuilder errorReport = new StringBuilder(192);
+        final String LINE_SEPARATOR = "\n";
+        errorReport.append("************ CAUSE OF ERROR ************\n\n")
+                .append(stackTrace.toString())
+                .append("\n************ DEVICE INFORMATION ***********\nBrand: ")
+                .append(Build.BRAND)
+                .append(LINE_SEPARATOR)
+                .append("Device: ")
+                .append(Build.DEVICE)
+                .append(LINE_SEPARATOR)
+                .append("Model: ")
+                .append(Build.MODEL)
+                .append(LINE_SEPARATOR)
+                .append("Id: ")
+                .append(Build.ID)
+                .append(LINE_SEPARATOR)
+                .append("Product: ")
+                .append(Build.PRODUCT)
+                .append(LINE_SEPARATOR)
+                .append("\n************ FIRMWARE ************\nSDK: ")
+                .append(Build.VERSION.SDK_INT)
+                .append(LINE_SEPARATOR)
+                .append("Release: ")
+                .append(Build.VERSION.RELEASE)
+                .append(LINE_SEPARATOR)
+                .append("Incremental: ")
+                .append(Build.VERSION.INCREMENTAL)
+                .append(LINE_SEPARATOR);
 
 
         //after this you can do whatever you want , like i start an activity and show error log there
@@ -66,7 +62,6 @@ public class ExceptionHandler implements Thread.UncaughtExceptionHandler {
         if (isUIThread()) {
             invokeLogActivity(errorReport);
         } else {  //handle non UI thread throw uncaught exception
-
             new Handler(Looper.getMainLooper()).post(new Runnable() {
                 @Override
                 public void run() {
@@ -83,20 +78,18 @@ public class ExceptionHandler implements Thread.UncaughtExceptionHandler {
 //        sendIntent.setType("text/plain");
 //        context.startActivity(sendIntent);
 
-        Intent sendIntent = new Intent(context, ErrorReportActivity.class);
+        Intent sendIntent = new Intent(mContext, ErrorReportActivity.class);
         sendIntent.putExtra(Intent.EXTRA_TEXT, errorReport.toString());
         sendIntent.setAction(Intent.ACTION_SEND);
         sendIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         sendIntent.setType("text/plain");
-        context.startActivity(sendIntent);
+        mContext.startActivity(sendIntent);
 
-
+        android.os.Process.killProcess(android.os.Process.myPid());
         System.exit(1);
-//        android.os.Process.killProcess(android.os.Process.myPid());
-
     }
 
-    private boolean isUIThread(){
+    private boolean isUIThread() {
         return Looper.getMainLooper().getThread() == Thread.currentThread();
     }
 }
