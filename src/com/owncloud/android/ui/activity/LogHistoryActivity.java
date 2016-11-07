c5901e7a702bb39645423aa75f0028936b295a1e
AndyScherzinger <info@andy-scherzinger.de>
2016-11-07 22:33:37 +0100

    fix codacy findings

--- src/com/owncloud/android/ui/activity/LogHistoryActivity.java
+++ src/com/owncloud/android/ui/activity/LogHistoryActivity.java
@@ -42,7 +42,6 @@ import com.owncloud.android.utils.FileStorageUtils;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.ref.WeakReference;
--- src/com/owncloud/android/ui/helpers/FileOperationsHelper.java
+++ src/com/owncloud/android/ui/helpers/FileOperationsHelper.java
@@ -52,10 +52,8 @@ import com.owncloud.android.ui.dialog.ShareLinkToDialog;
 
 import java.io.BufferedReader;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
 import java.util.Collection;
 import java.util.List;
 import java.util.regex.Matcher;
@@ -496,8 +494,7 @@ public class FileOperationsHelper {
 
     public void sendDownloadedFile(OCFile file) {
         if (file != null) {
-            String storagePath = file.getStoragePath();
-            Intent sendIntent = new Intent(android.content.Intent.ACTION_SEND);
+            Intent sendIntent = new Intent(Intent.ACTION_SEND);
             // set MimeType
             sendIntent.setType(file.getMimetype());
             sendIntent.putExtra(
