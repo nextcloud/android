e9224a6ebbe23ea573db1943333d3a50461b15c4
AndyScherzinger <info@andy-scherzinger.de>
2016-11-22 23:06:15 +0100

    fix codacy warning

--- src/main/java/com/owncloud/android/ui/helpers/FileOperationsHelper.java
+++ src/main/java/com/owncloud/android/ui/helpers/FileOperationsHelper.java
@@ -478,8 +478,7 @@ public class FileOperationsHelper {
         if (hideFileListing) {
             updateShareIntent.putExtra(OperationsService.EXTRA_SHARE_PERMISSIONS, OCShare.CREATE_PERMISSION_FLAG);
         } else {
-            OwnCloudVersion serverVersion =
-                    com.owncloud.android.authentication.AccountUtils.getServerVersion(mFileActivity.getAccount());
+            OwnCloudVersion serverVersion = AccountUtils.getServerVersion(mFileActivity.getAccount());
 
             if (serverVersion != null && serverVersion.isNotReshareableFederatedSupported()) {
                 updateShareIntent.
