3ff3dcc62df52f389f3aa340237b7fda40376ecc
davigonz <dgonzalez@solidgear.es>
2016-12-28 10:27:05 +0100

    Fix bug maintenance mode in share view

--- src/main/java/com/owncloud/android/providers/UsersAndGroupsSearchProvider.java
+++ src/main/java/com/owncloud/android/providers/UsersAndGroupsSearchProvider.java
@@ -266,15 +266,18 @@ public class UsersAndGroupsSearchProvider extends ContentProvider {
                 // the thread may die before, an exception will occur, and the message will be left on the screen
                 // until the app dies
 
-                Toast.makeText(
-                        getContext().getApplicationContext(),
-                        ErrorMessageAdapter.getErrorCauseMessage(
-                                result,
-                                null,
-                                getContext().getResources()
-                        ),
-                        Toast.LENGTH_SHORT
-                ).show();
+                // Edited: this toast message has no sense. If operation is being passed as null to getErrorCauseMessage(),
+                // the returned message is always null and therefore an empty toast is shown. Pending to review and change/delete
+
+//                Toast.makeText(
+//                        getContext().getApplicationContext(),
+//                        ErrorMessageAdapter.getErrorCauseMessage(
+//                                result,
+//                                null,
+//                                getContext().getResources()
+//                        ),
+//                        Toast.LENGTH_SHORT
+//                ).show();
             }
         });
     }
