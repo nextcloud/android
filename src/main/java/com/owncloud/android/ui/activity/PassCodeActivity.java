5b44f16015ce23c7a975ffdd09133b69f898426b
masaaki matsuo <masaaki@m20.jp>
2017-01-07 01:40:47 +0900

    Fix passcodelock layout file

--- src/main/java/com/owncloud/android/ui/activity/PassCodeActivity.java
+++ src/main/java/com/owncloud/android/ui/activity/PassCodeActivity.java
@@ -157,7 +157,6 @@ public class PassCodeActivity extends AppCompatActivity {
                 // will receive and confirm pass code value
                 mPassCodeHdr.setText(R.string.pass_code_configure_your_pass_code);
                 mPassCodeHdrExplanation.setVisibility(View.VISIBLE);
-                setCancelButtonEnabled(true);
 
                 View view = findViewById(android.R.id.content);
                 Snackbar
@@ -178,6 +177,7 @@ public class PassCodeActivity extends AppCompatActivity {
                 //the app was in the passcodeconfirmation
                 requestPassCodeConfirmation();
             }
+            setCancelButtonEnabled(true);
 
         } else if (ACTION_CHECK_WITH_RESULT.equals(getIntent().getAction())) {
             /// pass code preference has just been disabled in Preferences;
@@ -308,7 +308,6 @@ public class PassCodeActivity extends AppCompatActivity {
             });
         } else {
             cancel.setVisibility(View.GONE);
-            cancel.setVisibility(View.INVISIBLE);
             cancel.setOnClickListener(null);
         }
     }
