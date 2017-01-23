356f0cb15f63429b894cfdba45bb772d55a3eac4
Mario Danic <mario@lovelyhq.com>
2017-01-23 10:39:00 +0100

    Combine ifs

--- src/main/java/com/owncloud/android/ui/preview/PreviewTextFragment.java
+++ src/main/java/com/owncloud/android/ui/preview/PreviewTextFragment.java
@@ -245,10 +245,8 @@ public class PreviewTextFragment extends FileFragment implements SearchView.OnQu
             }, delay);
         }
 
-        if (delay == 0) {
-            if (mSearchView != null) {
-                mSearchView.clearFocus();
-            }
+        if (delay == 0 && mSearchView != null) {
+            mSearchView.clearFocus();
         }
     }
 
