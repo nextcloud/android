526f5b396ef671b493205521db96962ce463f206
AndyScherzinger <info@andy-scherzinger.de>
2017-01-23 14:22:53 +0100

    minor tweaking and fixes for ordering

--- src/com/owncloud/android/ui/activity/FileDisplayActivity.java
+++ src/com/owncloud/android/ui/activity/FileDisplayActivity.java
@@ -1072,7 +1072,7 @@ public class FileDisplayActivity extends HookActivity
                 sortByName(true);
                 break;
             case SortingOrderDialogFragment.BY_NAME_DESC:
-                sortByDate(false);
+                sortByName(false);
                 break;
             case SortingOrderDialogFragment.BY_MODIFICATION_DATE_ASC:
                 sortByDate(true);
