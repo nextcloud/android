02e1711c0410266a05bd0f155a066bda52b3186e
Mario Danic <mario@lovelyhq.com>
2017-01-25 18:14:12 +0100

    First bits of user info code

--- src/com/owncloud/android/ui/activity/ManageAccountsActivity.java
+++ src/com/owncloud/android/ui/activity/ManageAccountsActivity.java
@@ -58,6 +58,8 @@ import org.parceler.Parcels;
 import java.util.ArrayList;
 import java.util.Set;
 
+import butterknife.OnItemClick;
+
 /**
  * An Activity that allows the user to manage accounts.
  */
@@ -114,15 +116,8 @@ public class ManageAccountsActivity extends FileActivity
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Account account = mAccountListAdapter.getItem(position).getAccount();
-                intent.putExtra(KEY_ACCOUNT, Parcels.wrap(account));
-                try {
-                    OwnCloudAccount oca = new OwnCloudAccount(account, MainApp.getAppContext());
-                    intent.putExtra(KEY_DISPLAY_NAME, oca.getDisplayName());
-                } catch (com.owncloud.android.lib.common.accounts.AccountUtils.AccountNotFoundException e) {
-                    Log_OC.d(TAG, "Failed to find NC account");
-                }
-
-                startActivityForResult(intent, KEY_USER_INFO_REQUEST_CODE);
+                intent.putExtra("NEXTCLOUD_ACCOUNT", Parcels.wrap(account));
+                startActivity(intent);
             }
         });
 
