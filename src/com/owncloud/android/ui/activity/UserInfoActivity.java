83f4285cfd409096ca778ff473b3ded7aeb62d2e
Mario Danic <mario@lovelyhq.com>
2017-01-26 18:53:25 +0100

    Fix an issue with new support lib

--- src/com/owncloud/android/ui/activity/UserInfoActivity.java
+++ src/com/owncloud/android/ui/activity/UserInfoActivity.java
@@ -30,7 +30,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
