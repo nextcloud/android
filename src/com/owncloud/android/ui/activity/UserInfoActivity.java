89928158e50999560786331ab9ea4f56bfc42936
Mario Danic <mario@lovelyhq.com>
2017-01-26 12:15:59 +0100

    Implement user information magic

--- src/com/owncloud/android/ui/activity/UserInfoActivity.java
+++ src/com/owncloud/android/ui/activity/UserInfoActivity.java
@@ -30,6 +30,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
