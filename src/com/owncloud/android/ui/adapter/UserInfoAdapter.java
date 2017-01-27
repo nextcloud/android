d768f175747a7aaccfd3d58f80d42efb7b9a5530
Mario Danic <mario@lovelyhq.com>
2017-01-27 10:54:11 +0100

    Fix codacy issues

--- src/com/owncloud/android/ui/adapter/UserInfoAdapter.java
+++ src/com/owncloud/android/ui/adapter/UserInfoAdapter.java
@@ -4,17 +4,17 @@
  * @author Mario Danic
  * Copyright (C) 2017 Mario Danic
  * Copyright (C) 2017 Nextcloud GmbH.
- *
+ * <p>
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * at your option) any later version.
- *
+ * <p>
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- *
+ * <p>
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -100,9 +100,9 @@ public class UserInfoAdapter extends RecyclerView.Adapter<UserInfoAdapter.ViewHo
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.attribute_headline_tv)
-        TextView attributeHeadlineTextView;
+        public TextView attributeHeadlineTextView;
         @BindView(R.id.attribute_value_tv)
-        TextView attributeValueTextView;
+        public TextView attributeValueTextView;
 
         public ViewHolder(View itemView) {
             super(itemView);
@@ -136,11 +136,9 @@ public class UserInfoAdapter extends RecyclerView.Adapter<UserInfoAdapter.ViewHo
                     userInfo.getWebpage()));
         }
 
-        if (!TextUtils.isEmpty(userInfo.getTwitter())) {
-            if (context != null) {
-                stringPairs.add(new StringPair(context.getResources().getString(R.string.user_info_twitter),
-                        userInfo.getTwitter()));
-            }
+        if (!TextUtils.isEmpty(userInfo.getTwitter()) && context != null) {
+            stringPairs.add(new StringPair(context.getResources().getString(R.string.user_info_twitter),
+                    userInfo.getTwitter()));
         }
     }
 
