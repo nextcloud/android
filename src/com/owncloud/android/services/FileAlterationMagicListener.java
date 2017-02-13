1b1706e62cbd0911e8f73258a7191eb1af83ce11
Mario Danic <mario@lovelyhq.com>
2017-02-13 09:01:13 +0100

    Simplify new auto upload code

--- src/com/owncloud/android/services/FileAlterationMagicListener.java
+++ src/com/owncloud/android/services/FileAlterationMagicListener.java
@@ -83,43 +83,45 @@ public class FileAlterationMagicListener implements FileAlterationListener {
 
     @Override
     public void onFileCreate(final File file) {
-        final Runnable runnable = new Runnable() {
-            @Override
-            public void run() {
-                PersistableBundle bundle = new PersistableBundle();
-                // TODO extract
-                bundle.putString(SyncedFolderJobService.LOCAL_PATH, file.getAbsolutePath());
-                bundle.putString(SyncedFolderJobService.REMOTE_PATH, FileStorageUtils.getInstantUploadFilePath(
-                        syncedFolder.getRemotePath(), file.getName(),
-                        new Date().getTime(),
-                        syncedFolder.getSubfolderByDate()));
-                bundle.putString(SyncedFolderJobService.ACCOUNT, syncedFolder.getAccount());
-                bundle.putInt(SyncedFolderJobService.UPLOAD_BEHAVIOUR, syncedFolder.getUploadAction());
+        if (file != null) {
+            final Runnable runnable = new Runnable() {
+                @Override
+                public void run() {
+                    PersistableBundle bundle = new PersistableBundle();
+                    // TODO extract
+                    bundle.putString(SyncedFolderJobService.LOCAL_PATH, file.getAbsolutePath());
+                    bundle.putString(SyncedFolderJobService.REMOTE_PATH, FileStorageUtils.getInstantUploadFilePath(
+                            syncedFolder.getRemotePath(), file.getName(),
+                            new Date().getTime(),
+                            syncedFolder.getSubfolderByDate()));
+                    bundle.putString(SyncedFolderJobService.ACCOUNT, syncedFolder.getAccount());
+                    bundle.putInt(SyncedFolderJobService.UPLOAD_BEHAVIOUR, syncedFolder.getUploadAction());
 
-                JobScheduler js = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
+                    JobScheduler js = (JobScheduler) context.getSystemService(Context.JOB_SCHEDULER_SERVICE);
 
-                Long date = new Date().getTime();
-                JobInfo job = new JobInfo.Builder(
-                        date.intValue(),
-                        new ComponentName(context, SyncedFolderJobService.class))
-                        .setRequiresCharging(syncedFolder.getChargingOnly())
-                        .setMinimumLatency(10000)
-                        .setRequiredNetworkType(syncedFolder.getWifiOnly() ? JobInfo.NETWORK_TYPE_UNMETERED : JobInfo.NETWORK_TYPE_ANY)
-                        .setExtras(bundle)
-                        .setPersisted(true)
-                        .build();
+                    Long date = new Date().getTime();
+                    JobInfo job = new JobInfo.Builder(
+                            date.intValue(),
+                            new ComponentName(context, SyncedFolderJobService.class))
+                            .setRequiresCharging(syncedFolder.getChargingOnly())
+                            .setMinimumLatency(10000)
+                            .setRequiredNetworkType(syncedFolder.getWifiOnly() ? JobInfo.NETWORK_TYPE_UNMETERED : JobInfo.NETWORK_TYPE_ANY)
+                            .setExtras(bundle)
+                            .setPersisted(true)
+                            .build();
 
-                Integer result = js.schedule(job);
-                if (result <= 0) {
-                    Log_OC.d(TAG, "Job failed to start: " + result);
+                    Integer result = js.schedule(job);
+                    if (result <= 0) {
+                        Log_OC.d(TAG, "Job failed to start: " + result);
+                    }
+
+                    fileRunnable.remove(file.getAbsolutePath());
                 }
+            };
 
-                fileRunnable.remove(file.getAbsolutePath());
-            }
-        };
-
-        fileRunnable.put(file.getAbsolutePath(), runnable);
-        handler.postDelayed(runnable, 1500);
+            fileRunnable.put(file.getAbsolutePath(), runnable);
+            handler.postDelayed(runnable, 1500);
+        }
 
     }
 
