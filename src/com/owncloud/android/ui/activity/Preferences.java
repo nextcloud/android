6a1dc2fbf4bf566975bcd0d827fc63e1c7e770ae
Mario Danic <mario@lovelyhq.com>
2017-02-15 11:24:29 +0100

    Enable auto upload on Android 6+ only :-/

--- src/com/owncloud/android/ui/activity/Preferences.java
+++ src/com/owncloud/android/ui/activity/Preferences.java
@@ -368,8 +368,8 @@ public class Preferences extends PreferenceActivity
 
         mPrefInstantUploadCategory = (PreferenceCategory) findPreference("instant_uploading_category");
 
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-            // Instant upload via preferences on pre pre Lollipop
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+            // Instant upload via preferences on pre pre M
             mPrefInstantUploadPath = findPreference("instant_upload_path");
             if (mPrefInstantUploadPath != null) {
 
