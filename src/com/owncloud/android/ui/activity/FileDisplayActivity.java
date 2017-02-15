d6f3afbdb30eddc596876657f2343642b358a112
Mario Danic <mario@lovelyhq.com>
2017-02-15 07:57:11 +0100

    Make fab appear nicer

--- src/com/owncloud/android/ui/activity/FileDisplayActivity.java
+++ src/com/owncloud/android/ui/activity/FileDisplayActivity.java
@@ -389,13 +389,13 @@ public class FileDisplayActivity extends HookActivity
     }
 
     private void createMinFragments() {
-        /*OCFileListFragment listOfFiles = new OCFileListFragment();
+        OCFileListFragment listOfFiles = new OCFileListFragment();
         Bundle args = new Bundle();
         args.putBoolean(OCFileListFragment.ARG_ALLOW_CONTEXTUAL_ACTIONS, true);
         listOfFiles.setArguments(args);
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.add(R.id.left_fragment_container, listOfFiles, TAG_LIST_OF_FILES);
-        transaction.commit();*/
+        transaction.commit();
     }
 
     private void initFragmentsWithFile() {
