f0c78d585eb9cb454de8f741e25b92dcc0cb7337
Mario Danic <mario@lovelyhq.com>
2017-03-28 12:29:59 +0200

    Basics of push

--- src/main/java/com/owncloud/android/ui/activity/FileActivity.java
+++ src/main/java/com/owncloud/android/ui/activity/FileActivity.java
@@ -40,6 +40,7 @@ import com.owncloud.android.MainApp;
 import com.owncloud.android.R;
 import com.owncloud.android.authentication.AccountUtils;
 import com.owncloud.android.authentication.AuthenticatorActivity;
+import com.owncloud.android.authentication.ModifiedAuthenticatorActivity;
 import com.owncloud.android.datamodel.OCFile;
 import com.owncloud.android.ui.helpers.FileOperationsHelper;
 import com.owncloud.android.files.services.FileDownloader;
@@ -396,7 +397,13 @@ public abstract class FileActivity extends DrawerActivity
             }
 
             /// step 2 - request credentials to user
-            Intent updateAccountCredentials = new Intent(this, AuthenticatorActivity.class);
+            Intent updateAccountCredentials;
+            if (!getResources().getBoolean(R.bool.push_enabled) &&
+                    !getResources().getBoolean(R.bool.analytics_enabled)) {
+                updateAccountCredentials = new Intent(this, AuthenticatorActivity.class);
+            } else {
+                updateAccountCredentials = new Intent(this, ModifiedAuthenticatorActivity.class);
+            }
             updateAccountCredentials.putExtra(AuthenticatorActivity.EXTRA_ACCOUNT, account);
             updateAccountCredentials.putExtra(
                     AuthenticatorActivity.EXTRA_ACTION,
