52d034980111af9fade87b8d68a2da8fc1c17ab3
alejandro <aleister09@gmail.com>
2017-03-28 15:21:50 -0600

    UI Implementation displaying activities

--- src/main/java/com/owncloud/android/ui/activity/ActivityListActivity.java
+++ src/main/java/com/owncloud/android/ui/activity/ActivityListActivity.java
@@ -20,11 +20,28 @@
 
 package com.owncloud.android.ui.activity;
 
+import android.accounts.Account;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.owncloud.android.R;
+import com.owncloud.android.authentication.AccountUtils;
+import com.owncloud.android.lib.common.operations.RemoteOperation;
+import com.owncloud.android.lib.common.operations.RemoteOperationResult;
 import com.owncloud.android.lib.common.utils.Log_OC;
+import com.owncloud.android.lib.resources.activities.GetRemoteActivitiesOperation;
+import com.owncloud.android.lib.resources.activities.models.Activity;
+import com.owncloud.android.ui.adapter.ActivityListAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
 
 /**
  * Activity displaying all server side stored activity items.
@@ -33,13 +50,20 @@ public class ActivityListActivity extends FileActivity {
 
     private static final String TAG = ActivityListActivity.class.getSimpleName();
 
+
+    @BindView(android.R.id.list)
+    public RecyclerView recyclerView;
+
+    private ActivityListAdapter adapter;
+    private Unbinder unbinder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log_OC.v(TAG, "onCreate() start");
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activitiy_list_layout);
-
+        unbinder = ButterKnife.bind(this);
         // setup toolbar
         setupToolbar();
 
@@ -50,11 +74,67 @@ public class ActivityListActivity extends FileActivity {
         setupContent();
     }
 
+    public void onDestroy() {
+        super.onDestroy();
+        unbinder.unbind();
+    }
+
     /**
      * sets up the UI elements and loads all activity items.
      */
     private void setupContent() {
         // TODO add all (recycler) view relevant code + data loading + adapter etc.
+
+        adapter = new ActivityListAdapter(this);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        fetchAndSetData();
+    }
+
+    private void fetchAndSetData() {
+        Thread t = new Thread(new Runnable() {
+            public void run() {
+                Account account = AccountUtils.getCurrentOwnCloudAccount(ActivityListActivity.this);
+                RemoteOperation getRemoteNotificationOperation = new GetRemoteActivitiesOperation();
+                Log_OC.d(TAG, "BEFORE getRemoteActivitiesOperation.execute");
+                final RemoteOperationResult result =
+                        getRemoteNotificationOperation.execute(account, ActivityListActivity.this);
+
+                if (result.isSuccess() && result.getData() != null) {
+                    final ArrayList<Object> activities = result.getData();
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if (activities.size() > 0) {
+                                populateList(activities);
+                                //emptyContentContainer.setVisibility(View.GONE);
+                                recyclerView.setVisibility(View.VISIBLE);
+                            } else {
+                                //setEmptyContent(noResultsHeadline, noResultsMessage);
+                            }
+                        }
+                    });
+                } else {
+                    Log_OC.d(TAG, result.getLogMessage());
+                    // show error
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            //setEmptyContent(noResultsHeadline, result.getLogMessage());
+                        }
+                    });
+                }
+            }
+        });
+
+        t.start();
+    }
+
+    private void populateList(List<Object> notifications) {
+
+        adapter.setActivityItems(notifications);
     }
 
     @Override
