22badd92ffb144ae5bab5642f6a90772d8c1e907
alejandro <aleister09@gmail.com>
2017-03-29 12:04:35 -0600

    Support for SVG icons with Glide and Android SVG

---
+++ src/main/java/com/owncloud/android/utils/SvgDecoder.java
@@ -0,0 +1,31 @@
+package com.owncloud.android.utils;
+
+
+
+import com.bumptech.glide.load.ResourceDecoder;
+import com.bumptech.glide.load.engine.Resource;
+import com.bumptech.glide.load.resource.SimpleResource;
+import com.caverock.androidsvg.SVG;
+import com.caverock.androidsvg.SVGParseException;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+/**
+ * Decodes an SVG internal representation from an {@link InputStream}.
+ */
+public class SvgDecoder implements ResourceDecoder<InputStream, SVG> {
+    public Resource<SVG> decode(InputStream source, int width, int height) throws IOException {
+        try {
+            SVG svg = SVG.getFromInputStream(source);
+            return new SimpleResource<SVG>(svg);
+        } catch (SVGParseException ex) {
+            throw new IOException("Cannot load SVG from stream", ex);
+        }
+    }
+
+    @Override
+    public String getId() {
+        return "SvgDecoder.com.owncloud.android";
+    }
+}
