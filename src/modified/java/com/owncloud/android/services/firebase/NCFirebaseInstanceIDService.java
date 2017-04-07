047bd3cc8ef05ac3ed05a68ea77ca969d25bd8aa
Mario Danic <mario@lovelyhq.com>
2017-04-07 14:20:34 +0200

    Work a bit more on notifications

--- src/modified/java/com/owncloud/android/services/firebase/NCFirebaseInstanceIDService.java
+++ src/modified/java/com/owncloud/android/services/firebase/NCFirebaseInstanceIDService.java
@@ -19,28 +19,89 @@
  */
 package com.owncloud.android.services.firebase;
 
-import android.util.Log;
+import android.accounts.Account;
+import android.accounts.AuthenticatorException;
+import android.accounts.OperationCanceledException;
+import android.content.Context;
+import android.text.TextUtils;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.owncloud.android.MainApp;
+import com.owncloud.android.R;
+import com.owncloud.android.authentication.AccountUtils;
+import com.owncloud.android.lib.common.OwnCloudAccount;
+import com.owncloud.android.lib.common.OwnCloudClient;
+import com.owncloud.android.lib.common.OwnCloudClientManagerFactory;
+import com.owncloud.android.lib.common.operations.RemoteOperation;
+import com.owncloud.android.lib.common.operations.RemoteOperationResult;
+import com.owncloud.android.lib.common.utils.Log_OC;
+import com.owncloud.android.lib.resources.notifications.RegisterAccountDeviceForNotificationsOperation;
+import com.owncloud.android.lib.resources.notifications.RegisterAccountDeviceForProxyOperation;
+import com.owncloud.android.lib.resources.notifications.models.PushResponse;
+import com.owncloud.android.utils.PushUtils;
+
+import java.io.IOException;
+import java.security.PublicKey;
 
 public class NCFirebaseInstanceIDService extends FirebaseInstanceIdService {
     private static final String TAG = "NCFirebaseInstanceID";
 
     @Override
     public void onTokenRefresh() {
+        //You can implement this method to store the token on your server
+        if (!TextUtils.isEmpty(getResources().getString(R.string.push_server_url))) {
+            PushUtils.generateRsa2048KeyPair();
+        }
 
-        //Getting registration token
-        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-
-        //Displaying token on logcat
-        Log.d(TAG, "Refreshed token: " + refreshedToken);
-
+        sendRegistrationToServer(FirebaseInstanceId.getInstance().getToken());
     }
 
     private void sendRegistrationToServer(String token) {
-        //You can implement this method to store the token on your server
-        //Not required for current project
+        String pushTokenHash = PushUtils.generateSHA512Hash(token);
+        PublicKey devicePublicKey = (PublicKey) PushUtils.readKeyFromFile(true);
+        if (devicePublicKey != null) {
+            String publicKey = devicePublicKey.toString();
+
+            Context context = MainApp.getAppContext();
+            for (Account account : AccountUtils.getAccounts(context)) {
+                try {
+                    OwnCloudAccount ocAccount = new OwnCloudAccount(account, context);
+                    OwnCloudClient mClient = OwnCloudClientManagerFactory.getDefaultSingleton().
+                            getClientFor(ocAccount, context);
+
+                    RemoteOperation registerAccountDeviceForNotificationsOperation =
+                            new RegisterAccountDeviceForNotificationsOperation(pushTokenHash,
+                        publicKey, context.getResources().getString(R.string.push_server_url));
+
+                    RemoteOperationResult remoteOperationResult = registerAccountDeviceForNotificationsOperation.
+                            execute(mClient);
+
+                    if (remoteOperationResult.isSuccess()) {
+                        PushResponse pushResponse = remoteOperationResult.getPushResponseData();
+
+                        RemoteOperation registerAccountDeviceForProxyOperation = new
+                                RegisterAccountDeviceForProxyOperation(
+                                context.getResources().getString(R.string.push_server_url),
+                                token, pushResponse.getDeviceIdentifier(), pushResponse.getSignature(),
+                                pushResponse.getPublicKey());
+
+                        remoteOperationResult = registerAccountDeviceForProxyOperation.execute(mClient);
+                    }
+                } catch (com.owncloud.android.lib.common.accounts.AccountUtils.AccountNotFoundException e) {
+                    Log_OC.d(TAG, "Failed to find an account");
+                } catch (AuthenticatorException e) {
+                    Log_OC.d(TAG, "Failed via AuthenticatorException");
+                } catch (IOException e) {
+                    Log_OC.d(TAG, "Failed via IOException");
+                } catch (OperationCanceledException e) {
+                    Log_OC.d(TAG, "Failed via OperationCanceledException");
+                }
+
+            }
+
+        }
+
     }
 
 }
