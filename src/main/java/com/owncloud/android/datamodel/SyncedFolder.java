fd8e317bd5b2b236e47edf2c2cdc90fa551eb20b
Mario Danic <mario@lovelyhq.com>
2017-05-05 00:05:12 +0200

    Add migration procedures
    Signed-off-by: Mario Danic <mario@lovelyhq.com>

--- src/main/java/com/owncloud/android/datamodel/SyncedFolder.java
+++ src/main/java/com/owncloud/android/datamodel/SyncedFolder.java
@@ -26,7 +26,7 @@ import java.io.Serializable;
 /**
  * Synced folder entity containing all information per synced folder.
  */
-public class SyncedFolder implements Serializable {
+public class SyncedFolder implements Serializable, Cloneable {
     public static final long UNPERSISTED_ID = Long.MIN_VALUE;
     private static final long serialVersionUID = -793476118299906429L;
     private long id = UNPERSISTED_ID;
@@ -93,6 +93,15 @@ public class SyncedFolder implements Serializable {
         this.enabled = enabled;
         this.type = type;
     }
+
+    public Object clone() {
+        try {
+            return super.clone();
+        } catch( CloneNotSupportedException e) {
+            return null;
+        }
+    }
+
 
     public long getId() {
         return id;
