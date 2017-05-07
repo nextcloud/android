c5fafb9b08ef08089456f3e10b13f3ddd40c5258
tobiasKaminsky <tobias@kaminsky.me>
2017-05-07 12:44:23 +0200

    enable lint

--- scripts/lint/lint-up.rb
+++ scripts/lint/lint-up.rb
@@ -47,11 +47,10 @@ end
 
 # run Lint
 puts "running Lint..."
-# system './gradlew clean lint'
+system './gradlew clean lint'
 
 # confirm that Lint ran w/out error
-# result = $?.to_i
-result = 0
+result = $?.to_i
 if result != 0
     puts "FAIL: failed to run ./gradlew clean lint"
     exit 1
