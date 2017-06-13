f230439c85c82f23e469b4174e24a64f78261bd4
AndyScherzinger <info@andy-scherzinger.de>
2017-06-13 09:37:22 +0200

    calculateLuminance function

--- src/main/java/com/owncloud/android/utils/DisplayUtils.java
+++ src/main/java/com/owncloud/android/utils/DisplayUtils.java
@@ -731,6 +731,10 @@ public class DisplayUtils {
         return ColorUtils.HSLToColor(hsl);
     }
 
+    public static double calculateLuminance(@ColorInt int color) {
+        return (0.299 * Color.red(color) + 0.587 * Color.green(color) + 0.114 * Color.blue(color))/255;
+    }
+
     public static int primaryDarkColor() {
         OCCapability capability = getCapability();
 
