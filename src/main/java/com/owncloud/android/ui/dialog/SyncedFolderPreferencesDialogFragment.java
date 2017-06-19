035e7a1a29c2252c19fc4b79d688a2e43b1ef79f
tobiasKaminsky <tobias@kaminsky.me>
2017-06-19 14:40:45 +0200

    minor fix

--- src/main/java/com/owncloud/android/ui/dialog/SyncedFolderPreferencesDialogFragment.java
+++ src/main/java/com/owncloud/android/ui/dialog/SyncedFolderPreferencesDialogFragment.java
@@ -1,20 +1,20 @@
-/**
+/*
  * Nextcloud Android client application
  *
  * @author Andy Scherzinger
  * Copyright (C) 2016 Andy Scherzinger
  * Copyright (C) 2016 Nextcloud
- * <p>
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or any later version.
- * <p>
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
- * <p>
+ *
  * You should have received a copy of the GNU Affero General Public
  * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -92,16 +92,6 @@ public class SyncedFolderPreferencesDialogFragment extends DialogFragment {
             throw new IllegalArgumentException("The host activity must implement "
                     + OnSyncedFolderPreferenceListener.class.getCanonicalName());
         }
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        int accentColor = DisplayUtils.primaryAccentColor();
-
-//        getDialog().getButton(DialogInterface.BUTTON_POSITIVE).setTextColor(accentColor);
-//        dialog.getButton(DialogInterface.BUTTON_NEGATIVE).setTextColor(accentColor);
     }
 
     @Override
