ea24b1a75c4f7f8f7132cf78c3ffd3bdcf8c78fe
tobiaskaminsky <tobias@kaminsky.me>
2017-06-28 14:57:57 +0200

    adapt import for codacy

--- src/main/java/com/owncloud/android/ui/preview/PreviewImageFragment.java
+++ src/main/java/com/owncloud/android/ui/preview/PreviewImageFragment.java
@@ -1,18 +1,18 @@
-/**
+/*
  * ownCloud Android client application
  *
  * @author David A. Velasco
  * Copyright (C) 2015 ownCloud Inc.
- * <p>
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
  * as published by the Free Software Foundation.
- * <p>
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * <p>
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -443,8 +443,7 @@ public class PreviewImageFragment extends FileFragment {
 
         @Override
         protected LoadImage doInBackground(OCFile... params) {
-            Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_BACKGROUND +
-                    android.os.Process.THREAD_PRIORITY_MORE_FAVORABLE);
+            Process.setThreadPriority(Process.THREAD_PRIORITY_BACKGROUND + Process.THREAD_PRIORITY_MORE_FAVORABLE);
 
             Bitmap bitmapResult = null;
             Drawable drawableResult = null;
