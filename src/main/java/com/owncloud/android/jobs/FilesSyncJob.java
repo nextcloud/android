195389df35abf55b809ac168a176b3ba21c80594
Mario Danic <mario@lovelyhq.com>
2017-07-09 21:38:17 +0200

    Try to fix 4G
    Signed-off-by: Mario Danic <mario@lovelyhq.com>

--- src/main/java/com/owncloud/android/jobs/FilesSyncJob.java
+++ src/main/java/com/owncloud/android/jobs/FilesSyncJob.java
@@ -124,7 +124,7 @@ public class FilesSyncJob extends Job {
                                     .setExecutionWindow(10_000L, 10_000L)
                                     .setRequiresCharging(needsCharging)
                                     .setRequiredNetworkType(syncedFolder.getWifiOnly() ? JobRequest.NetworkType.UNMETERED :
-                                            JobRequest.NetworkType.CONNECTED)
+                                            JobRequest.NetworkType.ANY)
                                     .setExtras(bundle)
                                     .setRequirementsEnforced(true)
                                     .setUpdateCurrent(false)
