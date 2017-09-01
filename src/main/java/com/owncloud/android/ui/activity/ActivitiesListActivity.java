fb7080f7ede8bda40fd694b436c4de4df7c9231e
AndyScherzinger <info@andy-scherzinger.de>
2017-09-01 15:51:15 +0200

    apply show/hide logic from notifications to activities also

--- src/main/java/com/owncloud/android/ui/activity/ActivitiesListActivity.java
+++ src/main/java/com/owncloud/android/ui/activity/ActivitiesListActivity.java
@@ -222,12 +222,14 @@ public class ActivitiesListActivity extends FileActivity implements ActivityList
                         runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
+                                populateList(activities, ownCloudClient);
                                 if (activities.size() > 0) {
-                                    populateList(activities, ownCloudClient);
                                     swipeEmptyListRefreshLayout.setVisibility(View.GONE);
                                     swipeListRefreshLayout.setVisibility(View.VISIBLE);
                                 } else {
                                     setEmptyContent(noResultsHeadline, noResultsMessage);
+                                    swipeListRefreshLayout.setVisibility(View.GONE);
+                                    swipeEmptyListRefreshLayout.setVisibility(View.VISIBLE);
                                 }
                             }
                         });
