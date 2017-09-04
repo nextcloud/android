c7e4bb0b01201544ceba3f440d363c2611439035
Marcus Hoffmann <m.hoffmann@cartelsol.com>
2017-09-04 02:01:00 +0200

    remove all prebuilt jars
    This gets rid of all prebuilt jars in the repo.
    
    * libs/disklrucache-2.0.2.jar doesn't seem to be used anymore, app builds
      fine when removing it.
    * libs/touch-image-view.jar is indeed required but is not anywhere available
      via gradle. So instead we include the source of this long unmaintained
      library directly.

---
+++ src/main/java/com/ortiz/touch/ExtendedViewPager.java
@@ -0,0 +1,34 @@
+package com.ortiz.touch;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+public class ExtendedViewPager extends ViewPager {
+
+	public ExtendedViewPager(Context context) {
+	    super(context);
+	}
+	
+	public ExtendedViewPager(Context context, AttributeSet attrs) {
+	    super(context, attrs);
+	}
+	
+	@Override
+	protected boolean canScroll(View v, boolean checkV, int dx, int x, int y) {
+	    if (v instanceof TouchImageView) {
+	    	//
+	    	// canScrollHorizontally is not supported for Api < 14. To get around this issue,
+	    	// ViewPager is extended and canScrollHorizontallyFroyo, a wrapper around
+	    	// canScrollHorizontally supporting Api >= 8, is called.
+	    	//
+	        return ((TouchImageView) v).canScrollHorizontallyFroyo(-dx);
+	        
+	    } else {
+	        return super.canScroll(v, checkV, dx, x, y);
+	    }
+	}
+
+}
