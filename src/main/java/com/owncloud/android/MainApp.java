592ad3f89fe8f118830c68fe6c72057c615d3200
Mario Danic <mario@lovelyhq.com>
2017-10-10 18:34:11 +0200

    Fix log
    Signed-off-by: Mario Danic <mario@lovelyhq.com>

--- src/main/java/com/owncloud/android/MainApp.java
+++ src/main/java/com/owncloud/android/MainApp.java
@@ -134,7 +134,7 @@ public class MainApp extends MultiDexApplication {
                 Method m = StrictMode.class.getMethod("disableDeathOnFileUriExposure");
                 m.invoke(null);
             } catch(Exception e){
-                e.printStackTrace();
+                Log_OC.d("Debug", "Failed to disable uri exposure");
             }
         }
 
