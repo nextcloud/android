4d49735c7bdd5487a45bc211fafd6d53fc96d983
AndyScherzinger <info@andy-scherzinger.de>
2017-10-10 23:48:27 +0200

    fix formatting

--- src/main/java/com/owncloud/android/MainApp.java
+++ src/main/java/com/owncloud/android/MainApp.java
@@ -129,11 +129,11 @@ public class MainApp extends MultiDexApplication {
             Log_OC.d("Debug", "start logging");
         }
 
-        if (Build.VERSION.SDK_INT >= 24){
+        if (Build.VERSION.SDK_INT >= 24) {
             try {
                 Method m = StrictMode.class.getMethod("disableDeathOnFileUriExposure");
                 m.invoke(null);
-            } catch(Exception e){
+            } catch (Exception e) {
                 Log_OC.d("Debug", "Failed to disable uri exposure");
             }
         }
