30a5428a990b03113ffbf85f2b1aa0d58b1a3696
tobiaskaminsky <tobias@kaminsky.me>
2017-11-06 15:04:34 +0100

    check if master branch, then always update master.html

--- scripts/lint/getBranchName.sh
+++ scripts/lint/getBranchName.sh
@@ -2,4 +2,8 @@
 
 # $1: username, $2: password/token, $3: pull request number
 
-curl 2>/dev/null -u $1:$2 https://api.github.com/repos/nextcloud/android/pulls/$3 | grep \"ref\": | grep -v master | cut -d"\"" -f4
+if [ -z $3 ] ; then
+    echo "master";
+else
+    curl 2>/dev/null -u $1:$2 https://api.github.com/repos/nextcloud/android/pulls/$3 | grep \"ref\": | grep -v master | cut -d"\"" -f4
+fi
