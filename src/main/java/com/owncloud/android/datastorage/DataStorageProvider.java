c86e62f13b25495bf98d46d02c72adbdae5ab7a6
eho <edvard.holst@gmail.com>
2017-12-05 23:44:21 +0100

    Simplified external storage state verification.
    Removed the redundant check to see if external storage is at least readable. We need it to be writable.

--- src/main/java/com/owncloud/android/datastorage/DataStorageProvider.java
+++ src/main/java/com/owncloud/android/datastorage/DataStorageProvider.java
@@ -67,7 +67,7 @@ public class DataStorageProvider {
                 MainApp.getAppContext().getFilesDir().getAbsolutePath()));
 
         // Add external storage directory if available.
-        if (isExternalStorageReadable() && isExternalStorageWritable()) {
+        if (isExternalStorageWritable()) {
             mCachedStoragePoints.add(new StoragePoint(MainApp.getAppContext().getExternalFilesDir(null).getAbsolutePath(),
                     MainApp.getAppContext().getExternalFilesDir(null).getAbsolutePath()));
         }
@@ -96,20 +96,7 @@ public class DataStorageProvider {
     /* Checks if external storage is available for read and write */
     public boolean isExternalStorageWritable() {
         String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            return true;
-        }
-        return false;
-    }
-
-    /* Checks if external storage is available to at least read */
-    public boolean isExternalStorageReadable() {
-        String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state) ||
-                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
-            return true;
-        }
-        return false;
+        return Environment.MEDIA_MOUNTED.equals(state);
     }
 
 }
