27954c42874dd114447e435eea55298a6de3f9ff
Mario Danic <mario@lovelyhq.com>
2017-12-11 01:31:08 +0100

    Fix for the fix xD
    Signed-off-by: Mario Danic <mario@lovelyhq.com>

--- src/main/java/com/owncloud/android/datamodel/FilesystemDataProvider.java
+++ src/main/java/com/owncloud/android/datamodel/FilesystemDataProvider.java
@@ -139,7 +139,7 @@ public class FilesystemDataProvider {
 
             if (data.getModifiedAt() != modifiedAt) {
                 long newCrc32 = getFileChecksum(localPath);
-                if (data.getCrc32() == null || (newCrc32 != -1 && data.getCrc32().equals(Long.toString(newCrc32)))) {
+                if (data.getCrc32() == null || (newCrc32 != -1 && !data.getCrc32().equals(Long.toString(newCrc32)))) {
                     cv.put(ProviderMeta.ProviderTableMeta.FILESYSTEM_CRC32, Long.toString(newCrc32));
                     cv.put(ProviderMeta.ProviderTableMeta.FILESYSTEM_FILE_SENT_FOR_UPLOAD, 0);
                 }
