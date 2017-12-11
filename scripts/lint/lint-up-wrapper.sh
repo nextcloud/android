8332a75406b22527e0790acd4d00b6ec3c267b49
Tobias Kaminsky <tobiasKaminsky@users.noreply.github.com>
2017-12-11 14:59:17 +0100

    Update lint-up-wrapper.sh
    lint is currently not updating master.html, so adding this to debug.

--- scripts/lint/lint-up-wrapper.sh
+++ scripts/lint/lint-up-wrapper.sh
@@ -15,6 +15,8 @@ returnValue=$?
 # 1: count was increased
 # 2: count stayed the same
 
+echo "Branch: $3"
+
 if [ $3 = "master" -a $returnValue -eq 0 ]; then
     echo "New master at: https://nextcloud.kaminsky.me/index.php/s/tXwtChzyqMj6I8v"
     curl -u $4:$5 -X PUT https://nextcloud.kaminsky.me/remote.php/webdav/droneLogs/master.html --upload-file build/reports/lint/lint.html
