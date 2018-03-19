762a9ffe4884dd7c837c47f832169193369ee475
Mario Danic <mario@lovelyhq.com>
2018-03-19 18:20:57 +0100

    Use remote id
    Signed-off-by: Mario Danic <mario@lovelyhq.com>

--- src/main/java/com/owncloud/android/datamodel/FileDataStorageManager.java
+++ src/main/java/com/owncloud/android/datamodel/FileDataStorageManager.java
@@ -120,8 +120,27 @@ public class FileDataStorageManager {
     }
 
 
-    public @Nullable
-    OCFile getFileById(long id) {
+    /**
+     * This will return a OCFile by its given FileId here refered as the remoteId.
+     * Its the fileId ownCloud Core uses to identify a file even if its name has changed.
+     *
+     *
+     * @param remoteID
+     * @return
+     */
+    public OCFile getFileByRemoteId(String remoteID) {
+        Cursor c = getFileCursorForValue(ProviderTableMeta.FILE_REMOTE_ID, remoteID);
+        OCFile file = null;
+        if(c != null) {
+            if(c.moveToFirst()) {
+                file = createFileInstance(c);
+            }
+            c.close();
+        }
+        return file;
+    }
+
+    public OCFile getFileById(long id) {
         Cursor c = getFileCursorForValue(ProviderTableMeta._ID, String.valueOf(id));
         OCFile file = null;
         if (c.moveToFirst()) {
