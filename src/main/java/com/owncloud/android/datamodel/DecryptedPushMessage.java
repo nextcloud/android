2eabb05a2571968cf3c99b46da5fd52d56eb3628
Mario Danic <mario@lovelyhq.com>
2018-03-22 07:47:10 +0100

    Fix year
    Signed-off-by: Mario Danic <mario@lovelyhq.com>

--- src/main/java/com/owncloud/android/datamodel/DecryptedPushMessage.java
+++ src/main/java/com/owncloud/android/datamodel/DecryptedPushMessage.java
@@ -2,7 +2,7 @@
  * Nextcloud application
  *
  * @author Mario Danic
- * Copyright (C) 2017-2017 Mario Danic <mario@lovelyhq.com>
+ * Copyright (C) 2017-2018 Mario Danic <mario@lovelyhq.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
