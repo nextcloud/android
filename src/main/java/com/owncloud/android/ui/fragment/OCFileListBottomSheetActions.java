a791f90ae498572599b3ff9900f56b0abbc97c7a
tobiasKaminsky <tobias@kaminsky.me>
2018-04-19 12:13:27 +0200

    changes due to CR
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- src/main/java/com/owncloud/android/ui/fragment/OCFileListBottomSheetActions.java
+++ src/main/java/com/owncloud/android/ui/fragment/OCFileListBottomSheetActions.java
@@ -5,17 +5,17 @@
  * Copyright (C) 2018 Andy Scherzinger
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
- * at your option) any later version.
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package com.owncloud.android.ui.fragment;
@@ -26,7 +26,7 @@ package com.owncloud.android.ui.fragment;
  */
 public interface OCFileListBottomSheetActions {
     /**
-     * creates a folde rwithin the actual folder.
+     * creates a folder within the actual folder.
      */
     void createFolder();
 
