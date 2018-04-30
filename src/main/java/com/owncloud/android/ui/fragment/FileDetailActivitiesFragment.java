6013956150bdad5db58c9b471655a03ae2a2a073
AndyScherzinger <info@andy-scherzinger.de>
2018-04-30 19:24:48 +0200

    organize imports

--- src/main/java/com/owncloud/android/ui/fragment/FileDetailActivitiesFragment.java
+++ src/main/java/com/owncloud/android/ui/fragment/FileDetailActivitiesFragment.java
@@ -27,7 +27,6 @@ import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
@@ -60,7 +59,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import butterknife.BindAnim;
 import butterknife.BindString;
 import butterknife.BindView;
 import butterknife.ButterKnife;
