1667b3fc36c01c8aa5763beebb4e1543dc3237c3
AndyScherzinger <info@andy-scherzinger.de>
2018-05-13 22:48:28 +0200

    organize imports

--- src/main/java/com/owncloud/android/ui/fragment/FileDetailSharingFragment.java
+++ src/main/java/com/owncloud/android/ui/fragment/FileDetailSharingFragment.java
@@ -31,7 +31,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
