a733f515edda20631610d0cfc20cf6ebfbefc796
AndyScherzinger <info@andy-scherzinger.de>
2018-05-14 19:20:54 +0200

    ownCloud->Nextcloud + new line

--- src/androidTest/java/com/owncloud/android/uiautomator/InitialTest.java
+++ src/androidTest/java/com/owncloud/android/uiautomator/InitialTest.java
@@ -1,17 +1,17 @@
-/**
- * ownCloud Android client application
- * <p/>
+/*
+ * Nextcloud Android client application
+ *
  * Copyright (C) 2015 ownCloud Inc.
- * <p/>
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
  * as published by the Free Software Foundation.
- * <p/>
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * <p/>
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -63,7 +63,7 @@ public class InitialTest {
     }
 
     /**
-     * Start owncloud app
+     * Start Nextcloud app
      */
 //    @Test
 //    public void startAppFromHomeScreen() {
