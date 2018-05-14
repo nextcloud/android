00badca7b3b4248d125e91407a341f5ce9e93689
ardevd <edvard.holst@gmail.com>
2018-05-14 15:58:20 +0200

    ui:activities Fixed license header formatting.

--- src/main/java/com/owncloud/android/ui/activities/ActivitiesPresenter.java
+++ src/main/java/com/owncloud/android/ui/activities/ActivitiesPresenter.java
@@ -1,18 +1,18 @@
 /**
  * Nextcloud Android client application
- * <p>
+ *
  * Copyright (C) 2018 Edvard Holst
- * <p>
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or any later version.
- * <p>
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
- * <p>
+ *
  * You should have received a copy of the GNU Affero General Public
  * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
