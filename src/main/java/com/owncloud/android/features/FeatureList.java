e2cffb5b14292bcc9df0ae14b084ba49cc922881
tobiasKaminsky <tobias@kaminsky.me>
2018-05-18 09:27:43 +0200

    adjust what's new to 3.2.0
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- src/main/java/com/owncloud/android/features/FeatureList.java
+++ src/main/java/com/owncloud/android/features/FeatureList.java
@@ -41,7 +41,7 @@ public class FeatureList {
 
     private static final int VERSION_1_0_0 = 10000099;
     private static final int VERSION_3_0_0 = 30000099;
-    private static final int VERSION_3_1_0 = 30010000;
+    private static final int VERSION_3_2_0 = 30020000;
     private static final int BETA_VERSION_0 = 0;
 
     static public ArrayList<FeatureItem> get(boolean isMultiAccount) {
@@ -70,10 +70,10 @@ public class FeatureList {
                 R.string.whats_new_ipv6_content, VERSION_3_0_0,
                 BETA_VERSION_0, SHOW_ON_UPGRADE, false, false));
 
-        // 3.1.0
+        // 3.2.0
         featuresList.add(new FeatureItem(R.drawable.whats_new_device_credentials,
                 R.string.whats_new_device_credentials_title, R.string.whats_new_device_credentials_content,
-                VERSION_3_1_0, BETA_VERSION_0, SHOW_ON_UPGRADE, false, false));
+                VERSION_3_2_0, BETA_VERSION_0, SHOW_ON_UPGRADE, false, false));
 
         return featuresList;
     }
