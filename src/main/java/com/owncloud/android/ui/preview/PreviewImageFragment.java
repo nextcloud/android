80c923666715110d0b8e61c8a5666515bf47515c
eho <edvard.holst@gmail.com>
2018-05-22 11:03:39 +0200

    ui:preview Removed unused import.

--- src/main/java/com/owncloud/android/ui/preview/PreviewImageFragment.java
+++ src/main/java/com/owncloud/android/ui/preview/PreviewImageFragment.java
@@ -44,7 +44,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
