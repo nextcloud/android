4bce5462f2004d26c0cd2ce0aa63752dc950c3f0
tobiasKaminsky <tobias@kaminsky.me>
2018-05-28 14:28:00 +0200

    tint zip icon same as folder icon - see #2631
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- src/main/java/com/owncloud/android/utils/MimeTypeUtil.java
+++ src/main/java/com/owncloud/android/utils/MimeTypeUtil.java
@@ -98,7 +98,7 @@ public class MimeTypeUtil {
         Drawable icon = MainApp.getAppContext().getResources().getDrawable(iconId);
 
         if(R.drawable.file_zip == iconId) {
-            ThemeUtils.tintDrawable(icon, ThemeUtils.primaryColor(account));
+            ThemeUtils.tintDrawable(icon, ThemeUtils.elementColor(account));
         }
 
         return icon;
