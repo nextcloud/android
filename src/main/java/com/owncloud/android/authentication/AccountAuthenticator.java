a4c4091c23a3cd08c7584ab385b3825d1e134a45
AndyScherzinger <info@andy-scherzinger.de>
2018-06-11 17:28:01 +0200

    Fixes after rebase

--- src/main/java/com/owncloud/android/authentication/AccountAuthenticator.java
+++ src/main/java/com/owncloud/android/authentication/AccountAuthenticator.java
@@ -176,7 +176,7 @@ public class AccountAuthenticator extends AbstractAccountAuthenticator {
             }
 
             result.putString(AccountManager.KEY_ACCOUNT_NAME,  account.name);
-            result.putString(AccountManager.KEY_ACCOUNT_TYPE, MainApp.getAccountType());
+            result.putString(AccountManager.KEY_ACCOUNT_TYPE, MainApp.getAccountType(mContext));
             result.putString(AccountManager.KEY_AUTHTOKEN,     NEXTCLOUD_SSO);
             result.putString("username", userId);
 
