51ea1c13f10025257a273513c23b9a69b0645068
AndyScherzinger <info@andy-scherzinger.de>
2018-06-20 11:46:09 +0200

    raise icon size

--- src/main/java/com/owncloud/android/utils/ClipboardUtil.java
+++ src/main/java/com/owncloud/android/utils/ClipboardUtil.java
@@ -1,3 +1,23 @@
+/*
+ * Nextcloud Android client application
+ *
+ * @author Andy Scherzinger
+ * Copyright (C) 2018 Andy Scherzinger
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.owncloud.android.utils;
 
 import android.app.Activity;
@@ -9,6 +29,9 @@ import android.widget.Toast;
 import com.owncloud.android.R;
 import com.owncloud.android.lib.common.utils.Log_OC;
 
+/**
+ * Helper implementation to copy a string into the system clipboard.
+ */
 public class ClipboardUtil {
     private static final String TAG = ClipboardUtil.class.getName();
 
