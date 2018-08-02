956b37593530affa005d16f12789f9e8c63a47ac
AndyScherzinger <info@andy-scherzinger.de>
2018-08-02 10:13:06 +0200

    fix codacy issue

--- src/main/java/com/owncloud/android/utils/PermissionUtil.java
+++ src/main/java/com/owncloud/android/utils/PermissionUtil.java
@@ -77,16 +77,15 @@ public final class PermissionUtil {
 
     /**
      * request the ignore battery optimizations.
-     *
      */
     @RequiresApi(api = Build.VERSION_CODES.M)
     public static void requestIgnoreBatteryOptimizationsPermission(Context context) {
         Intent intent = new Intent();
         String packageName = context.getPackageName();
         PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
-        if (pm.isIgnoringBatteryOptimizations(packageName))
+        if (pm != null && pm.isIgnoringBatteryOptimizations(packageName)) {
             intent.setAction(Settings.ACTION_IGNORE_BATTERY_OPTIMIZATION_SETTINGS);
-        else {
+        } else {
             intent.setAction(Settings.ACTION_REQUEST_IGNORE_BATTERY_OPTIMIZATIONS);
             intent.setData(Uri.parse("package:" + packageName));
         }
