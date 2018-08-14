0b52aefeb6ea1f3eb631fdad48db532abc256402
tobiasKaminsky <tobias@kaminsky.me>
2018-08-14 12:28:06 +0200

    Change to customeViewPager to catch error
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

---
+++ src/main/java/com/owncloud/android/ui/components/CustomViewPager.java
@@ -0,0 +1,40 @@
+package com.owncloud.android.ui.components;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+import com.owncloud.android.lib.common.utils.Log_OC;
+
+public class CustomViewPager extends ViewPager {
+    public CustomViewPager(@NonNull Context context) {
+        super(context);
+    }
+
+    public CustomViewPager(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        try {
+            return super.onTouchEvent(ev);
+        } catch (IllegalArgumentException ex) {
+            Log_OC.e("CustomViewPager", "crash", ex);
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        try {
+            return super.onInterceptTouchEvent(ev);
+        } catch (IllegalArgumentException ex) {
+            Log_OC.e("CustomViewPager", "crash", ex);
+        }
+        return false;
+    }
+}
