767ca18e5e77cb9edbf39b9d18a5d9d17cba859e
David Luhmer <david-dev@live.de>
2018-08-24 17:45:31 +0200

    add missing license headers

--- src/main/java/com/owncloud/android/services/AccountManagerService.java
+++ src/main/java/com/owncloud/android/services/AccountManagerService.java
@@ -6,6 +6,25 @@ import android.os.IBinder;
 
 import com.nextcloud.android.sso.InputStreamBinder;
 
+/**
+ *  Nextcloud SingleSignOn
+ *
+ *  @author David Luhmer
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 public class AccountManagerService extends Service {
 
     private InputStreamBinder mBinder;
