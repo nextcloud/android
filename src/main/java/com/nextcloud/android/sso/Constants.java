fa8bd4a76830829ade67c39290ce0d2ebd9adceb
David Luhmer <david-dev@live.de>
2018-08-27 14:15:33 +0200

    add more exceptions / improve error handling

--- src/main/java/com/nextcloud/android/sso/Constants.java
+++ src/main/java/com/nextcloud/android/sso/Constants.java
@@ -8,7 +8,9 @@ public class Constants {
     public final static String SSO_SERVER_URL = "server_url";
 
     // Custom Exceptions
-    public static final String EXCEPTION_INVALID_TOKEN = "CE_1";
-    public static final String EXCEPTION_ACCOUNT_NOT_FOUND = "CE_2";
-    public static final String EXCEPTION_UNSUPPORTED_METHOD = "CE_3";
+    static final String EXCEPTION_INVALID_TOKEN = "CE_1";
+    static final String EXCEPTION_ACCOUNT_NOT_FOUND = "CE_2";
+    static final String EXCEPTION_UNSUPPORTED_METHOD = "CE_3";
+    static final String EXCEPTION_INVALID_REQUEST_URL = "CE_4";
+    static final String EXCEPTION_HTTP_REQUEST_FAILED = "CE_5";
 }
