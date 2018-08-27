0376571ba2fe65d197bf30ebb907212dcba88490
David Luhmer <david-dev@live.de>
2018-08-27 11:12:58 +0200

    Use constants for username/token/url

---
+++ src/main/java/com/nextcloud/android/sso/Constants.java
@@ -0,0 +1,10 @@
+package com.nextcloud.android.sso;
+
+public class Constants {
+
+    // Authenticator related constants
+    public final static String SSO_USERNAME = "username";
+    public final static String SSO_TOKEN = "token";
+    public final static String SSO_SERVER_URL = "server_url";
+
+}
