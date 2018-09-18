54aff1befc44558324aaa83041d93ed044d9ae19
AndyScherzinger <info@andy-scherzinger.de>
2018-09-18 14:48:50 +0200

    codacy: avoid unused imports

--- src/main/java/com/owncloud/android/ui/activity/UserInfoActivity.java
+++ src/main/java/com/owncloud/android/ui/activity/UserInfoActivity.java
@@ -42,7 +42,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
 import android.support.v4.graphics.drawable.DrawableCompat;
-import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
 import android.view.LayoutInflater;
