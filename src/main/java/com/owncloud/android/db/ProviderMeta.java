cad6cd6a19c5e5c15259b35d14ad3b7592e2a08d
tobiasKaminsky <tobias@kaminsky.me>
2018-09-26 07:59:15 +0200

    show unread comments
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- src/main/java/com/owncloud/android/db/ProviderMeta.java
+++ src/main/java/com/owncloud/android/db/ProviderMeta.java
@@ -32,7 +32,7 @@ import com.owncloud.android.MainApp;
 public class ProviderMeta {
 
     public static final String DB_NAME = "filelist";
-    public static final int DB_VERSION = 39;
+    public static final int DB_VERSION = 40;
 
     private ProviderMeta() {
     }
@@ -104,14 +104,15 @@ public class ProviderMeta {
         public static final String FILE_IS_ENCRYPTED = "is_encrypted";
         public static final String FILE_MOUNT_TYPE = "mount_type";
         public static final String FILE_HAS_PREVIEW = "has_preview";
+        public static final String FILE_UNREAD_COMMENTS_COUNT = "unread_comments_count";
 
-        public static final String [] FILE_ALL_COLUMNS = {_ID, FILE_PARENT, FILE_NAME
-               , FILE_CREATION, FILE_MODIFIED,
-                FILE_MODIFIED_AT_LAST_SYNC_FOR_DATA, FILE_CONTENT_LENGTH, FILE_CONTENT_TYPE, FILE_STORAGE_PATH,
-                FILE_PATH, FILE_ACCOUNT_OWNER, FILE_LAST_SYNC_DATE, FILE_LAST_SYNC_DATE_FOR_DATA, FILE_KEEP_IN_SYNC,
-                FILE_ETAG, FILE_SHARED_VIA_LINK, FILE_SHARED_WITH_SHAREE, FILE_PUBLIC_LINK, FILE_PERMISSIONS,
+        public static final String[] FILE_ALL_COLUMNS = {
+            _ID, FILE_PARENT, FILE_NAME, FILE_CREATION, FILE_MODIFIED,
+            FILE_MODIFIED_AT_LAST_SYNC_FOR_DATA, FILE_CONTENT_LENGTH, FILE_CONTENT_TYPE, FILE_STORAGE_PATH,
+            FILE_PATH, FILE_ACCOUNT_OWNER, FILE_LAST_SYNC_DATE, FILE_LAST_SYNC_DATE_FOR_DATA, FILE_KEEP_IN_SYNC,
+            FILE_ETAG, FILE_SHARED_VIA_LINK, FILE_SHARED_WITH_SHAREE, FILE_PUBLIC_LINK, FILE_PERMISSIONS,
             FILE_REMOTE_ID, FILE_UPDATE_THUMBNAIL, FILE_IS_DOWNLOADING, FILE_ETAG_IN_CONFLICT, FILE_FAVORITE,
-            FILE_HAS_PREVIEW};
+            FILE_IS_ENCRYPTED, FILE_MOUNT_TYPE, FILE_HAS_PREVIEW, FILE_UNREAD_COMMENTS_COUNT};
 
         public static final String FILE_DEFAULT_SORT_ORDER = FILE_NAME + " collate nocase asc";
 
