d9323d79512cdcb58e7e10584c8db9277c188716
tobiasKaminsky <tobias@kaminsky.me>
2018-11-07 15:36:55 +0100

    add locale
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- src/main/java/com/owncloud/android/ui/adapter/SyncedFolderAdapter.java
+++ src/main/java/com/owncloud/android/ui/adapter/SyncedFolderAdapter.java
@@ -22,6 +22,7 @@
 package com.owncloud.android.ui.adapter;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -43,6 +44,7 @@ import com.owncloud.android.utils.ThemeUtils;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -198,7 +200,8 @@ public class SyncedFolderAdapter extends SectionedRecyclerViewAdapter<SyncedFold
             holder.itemView.setTag(relativePosition % mGridWidth);
 
             if (mSyncFolderItems.get(section).getNumberOfFiles() > mGridTotal && relativePosition >= mGridTotal - 1) {
-                holder.counterValue.setText(Long.toString(mSyncFolderItems.get(section).getNumberOfFiles() - mGridTotal));
+                holder.counterValue.setText(String.format(Locale.US, "%d",
+                    mSyncFolderItems.get(section).getNumberOfFiles() - mGridTotal);)
                 holder.counterBar.setVisibility(View.VISIBLE);
                 holder.thumbnailDarkener.setVisibility(View.VISIBLE);
             } else {
@@ -208,11 +211,12 @@ public class SyncedFolderAdapter extends SectionedRecyclerViewAdapter<SyncedFold
         }
     }
 
+    @NonNull
     @Override
-    public MainViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public MainViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext()).inflate(
-                viewType == VIEW_TYPE_HEADER ?
-                        R.layout.synced_folders_item_header : R.layout.grid_sync_item, parent, false);
+            viewType == VIEW_TYPE_HEADER ?
+                R.layout.synced_folders_item_header : R.layout.grid_sync_item, parent, false);
         return new MainViewHolder(v);
     }
 
