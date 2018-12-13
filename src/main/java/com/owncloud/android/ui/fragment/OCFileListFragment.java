f0125e432f9d1d563675c65fcc398fd4ac04a022
tobiasKaminsky <tobias@kaminsky.me>
2018-12-13 11:01:27 +0100

    Capability was read too early
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
+++ src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
@@ -389,9 +389,9 @@ public class OCFileListFragment extends ExtendedListFragment implements
      * register listener on FAB.
      */
     private void registerFabListener() {
-        OCCapability capability = ((FileActivity) getActivity()).getCapabilities();
+        FileActivity activity = ((FileActivity) getActivity());
         getFabMain().setOnClickListener(v -> {
-            new OCFileListBottomSheetDialog(getContext(), capability, this).show();
+            new OCFileListBottomSheetDialog(activity, this).show();
         });
     }
 
