8800fd03f23dbf59f437a3d24e4f604c2dbcea10
tobiasKaminsky <tobias@kaminsky.me>
2019-02-06 12:39:11 +0100

    fix tests
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- src/test/java/com/owncloud/android/authentication/AuthenticatorDataUrlTest.java
+++ src/test/java/com/owncloud/android/authentication/AuthenticatorDataUrlTest.java
@@ -1,4 +1,4 @@
-/**
+/*
  *   Nextcloud Android client application
  *
  *   @author Andy Scherzinger
@@ -157,17 +157,24 @@ public class AuthenticatorDataUrlTest {
         }
     }
 
+    @Test(expected = IllegalArgumentException.class)
+    public void noDataUrlElements() {
+        AuthenticatorActivity.parseLoginDataUrl(schemeUrl, "");
+    }
+
     @Test
-    public void tooLittleDataUrlElements() {
-        for (String urlStart : urlStarts) {
-            try {
-                System.out.println(urlStart);
-                AuthenticatorActivity.parseLoginDataUrl(urlStart, urlStart);
-                Assert.fail("Illegal Argument Exception expected!");
-            } catch (IllegalArgumentException e) {
-                // all well
-                System.out.println("Expected Illegal Argument Exception caught.");
-            }
-        }
+    public void onlyOneDataUrlElements() {
+        LoginUrlInfo loginUrlInfo = AuthenticatorActivity.parseLoginDataUrl(schemeUrl, schemeUrl + userUrlPart);
+
+        Assert.assertEquals(userValue, loginUrlInfo.username);
+    }
+
+    @Test
+    public void onlyTwoDataUrlElements() {
+        LoginUrlInfo loginUrlInfo = AuthenticatorActivity.parseLoginDataUrl(schemeUrl, schemeUrl + userUrlPart +
+            plus + addressUrlPart);
+
+        Assert.assertEquals(userValue, loginUrlInfo.username);
+        Assert.assertEquals(addressValue, loginUrlInfo.serverAddress);
     }
 }
