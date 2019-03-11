1a7d6012f9fa8120acb98028e70b8f79f04eb73d
Chris Narkiewicz <hello@ezaquarii.com>
2019-03-11 22:48:57 +0000

    Move lock prefs to AppPreferences
    Signed-off-by: Chris Narkiewicz <hello@ezaquarii.com>

--- src/main/java/com/owncloud/android/authentication/PassCodeManager.java
+++ src/main/java/com/owncloud/android/authentication/PassCodeManager.java
@@ -27,6 +27,7 @@ import android.os.PowerManager;
 import android.view.Window;
 import android.view.WindowManager;
 
+import com.nextcloud.client.preferences.AppPreferences;
 import com.nextcloud.client.preferences.PreferenceManager;
 import com.owncloud.android.MainApp;
 import com.owncloud.android.ui.activity.PassCodeActivity;
@@ -50,26 +51,24 @@ public final class PassCodeManager {
         // other activities may be exempted, if needed
     }
 
+    /**
+     *  Keeping a "low" positive value is the easiest way to prevent
+     *  the pass code being requested on screen rotations.
+     */
     private static final int PASS_CODE_TIMEOUT = 5000;
-        // keeping a "low" positive value is the easiest way to prevent the pass code is requested on rotations
 
-    private static PassCodeManager passCodeManagerInstance;
-
+    private AppPreferences preferences;
     private int visibleActivitiesCounter;
 
-    public static PassCodeManager getPassCodeManager() {
-        if (passCodeManagerInstance == null) {
-            passCodeManagerInstance = new PassCodeManager();
-        }
-        return passCodeManagerInstance;
-    }
 
-    private PassCodeManager() {}
+    public PassCodeManager(AppPreferences preferences) {
+        this.preferences = preferences;
+    }
 
     public void onActivityCreated(Activity activity) {
         Window window = activity.getWindow();
         if (window != null) {
-            if (passCodeIsEnabled() || deviceCredentialsAreEnabled(activity)) {
+            if (isPassCodeEnabled() || deviceCredentialsAreEnabled(activity)) {
                 window.addFlags(WindowManager.LayoutParams.FLAG_SECURE);
             } else {
                 window.clearFlags(WindowManager.LayoutParams.FLAG_SECURE);
@@ -103,7 +102,7 @@ public final class PassCodeManager {
         }
         setUnlockTimestamp(activity);
         PowerManager powerMgr = (PowerManager) activity.getSystemService(Context.POWER_SERVICE);
-        if ((passCodeIsEnabled() || deviceCredentialsAreEnabled(activity)) && powerMgr != null
+        if ((isPassCodeEnabled() || deviceCredentialsAreEnabled(activity)) && powerMgr != null
                 && !powerMgr.isScreenOn()) {
             activity.moveTaskToBack(true);
         }
@@ -115,11 +114,11 @@ public final class PassCodeManager {
 
     private boolean passCodeShouldBeRequested(Long timestamp) {
         return (System.currentTimeMillis() - timestamp) > PASS_CODE_TIMEOUT &&
-            visibleActivitiesCounter <= 0 && passCodeIsEnabled();
+            visibleActivitiesCounter <= 0 && isPassCodeEnabled();
     }
 
-    private boolean passCodeIsEnabled() {
-        return SettingsActivity.LOCK_PASSCODE.equals(PreferenceManager.getLockPreference(MainApp.getAppContext()));
+    private boolean isPassCodeEnabled() {
+        return SettingsActivity.LOCK_PASSCODE.equals(preferences.getLockPreference());
     }
 
     private boolean deviceCredentialsShouldBeRequested(Long timestamp, Activity activity) {
@@ -128,9 +127,9 @@ public final class PassCodeManager {
     }
 
     private boolean deviceCredentialsAreEnabled(Activity activity) {
-        return SettingsActivity.LOCK_DEVICE_CREDENTIALS.equals(PreferenceManager.getLockPreference(MainApp.getAppContext()))
+        return SettingsActivity.LOCK_DEVICE_CREDENTIALS.equals(preferences.getLockPreference())
                 || Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
-                        (PreferenceManager.isUseFingerprint(MainApp.getAppContext())
+                        (preferences.isFingerprintUnlockEnabled()
                                 && DeviceCredentialUtils.areCredentialsAvailable(activity));
     }
 }
