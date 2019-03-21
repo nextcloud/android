2443626502447a4992e4556f700cc98068d07694
Andy Scherzinger <info@andy-scherzinger.de>
2019-03-21 10:01:40 +0100

    Fixes #3776 - allowing state loss when closing dialog while stopping activity
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- src/main/java/com/owncloud/android/ui/activity/UploadFilesActivity.java
+++ src/main/java/com/owncloud/android/ui/activity/UploadFilesActivity.java
@@ -707,7 +707,7 @@ public class UploadFilesActivity extends FileActivity implements
     @Override
     protected void onStop() {
         if (dialog != null) {
-            dialog.dismiss();
+            dialog.dismissAllowingStateLoss();
         }
 
         super.onStop();
