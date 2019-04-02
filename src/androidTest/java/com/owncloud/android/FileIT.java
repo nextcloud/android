cb72ddb804cb4c35cea8abd9bea5fd5096a1a7d2
tobiasKaminsky <tobias@kaminsky.me>
2019-04-02 16:17:54 +0100

    verbose server output
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- src/androidTest/java/com/owncloud/android/FileIT.java
+++ src/androidTest/java/com/owncloud/android/FileIT.java
@@ -1,12 +1,17 @@
 package com.owncloud.android;
 
+import com.owncloud.android.lib.common.network.WebdavUtils;
 import com.owncloud.android.lib.common.operations.RemoteOperationResult;
 import com.owncloud.android.operations.CreateFolderOperation;
 import com.owncloud.android.operations.RemoveFileOperation;
 import com.owncloud.android.operations.common.SyncOperation;
 
+import org.apache.jackrabbit.webdav.client.methods.DeleteMethod;
+import org.apache.jackrabbit.webdav.client.methods.MkColMethod;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.IOException;
 
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -39,8 +44,40 @@ public class FileIT extends AbstractIT {
     }
 
     @Test
+    public void testMkCol() throws IOException, InterruptedException {
+        int READ_TIMEOUT = 30000;
+        int CONNECTION_TIMEOUT = 5000;
+
+        MkColMethod mkCol1 = new MkColMethod(client.getWebdavUri() + WebdavUtils.encodePath("/mkcol"));
+        client.setUseNextcloudUserAgent(true);
+        client.executeMethod(mkCol1, READ_TIMEOUT, CONNECTION_TIMEOUT);
+        mkCol1.releaseConnection();
+
+        for (int i = 0; i < 100; i++) {
+            long start = System.currentTimeMillis();
+            System.out.println("mkcol start: " + i);
+            MkColMethod mkCol = new MkColMethod(client.getWebdavUri() + WebdavUtils.encodePath("/mkcol/" + i));
+            client.setUseNextcloudUserAgent(true);
+            client.executeMethod(mkCol, READ_TIMEOUT, CONNECTION_TIMEOUT);
+
+            mkCol.releaseConnection();
+
+            if (!mkCol.succeeded()) {
+                System.out.println(mkCol.getResponseBodyAsString());
+            }
+
+            long end = System.currentTimeMillis();
+            System.out.println("mkcol end: " + i + " time: " + (end - start) / 1000l);
+        }
+
+        DeleteMethod delete = new DeleteMethod(client.getWebdavUri() + WebdavUtils.encodePath("/mkcol"));
+        client.executeMethod(delete, READ_TIMEOUT, CONNECTION_TIMEOUT);
+        delete.releaseConnection();
+    }
+
+    @Test
     public void testCreateNonExistingSubFolder() {
-        String path = "/testFolder/1/2/3/4/5/";
+        String path = "/testFolder/1/2/3/4/5/6/7/8/9/10/";
         // folder does not exist yet
         assertNull(getStorageManager().getFileByPath(path));
 
