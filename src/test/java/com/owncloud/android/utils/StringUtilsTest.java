7d97b267afd2433d698e950a5ff57682182feea2
ardevd <edvard.holst@gmail.com>
2019-04-05 10:24:32 +0200

    updated assertion failure text. return empty string if arguments are null

--- src/test/java/com/owncloud/android/utils/StringUtilsTest.java
+++ src/test/java/com/owncloud/android/utils/StringUtilsTest.java
@@ -12,16 +12,17 @@ public class StringUtilsTest {
         String text = "this is a simple test";
         String searchText = "simple";
         int dummyColorInt = 44221;
-        String expectedReturn = String.format("this is a <font color='%d'><b>%s</b></font> test", dummyColorInt, searchText);
+        String expectedReturn = String.format("this is a <font color='%d'><b>%s</b></font> test",
+                                              dummyColorInt, searchText);
 
-        assertEquals("correctly parsed text returned",
+        assertEquals("returned parsed text value was incorrect",
                      expectedReturn, StringUtils.searchAndColor(text, searchText, dummyColorInt));
     }
 
     @Test
     public void searchAndColor_assertTextReturnedIfSearchTextIsEmpty() {
         String helloWorld = "hello world";
-        assertEquals("text returned when searchText was empty",
+        assertEquals("text returned when searchText was not empty",
                      helloWorld, StringUtils.searchAndColor(helloWorld, "", 0));
     }
 }
