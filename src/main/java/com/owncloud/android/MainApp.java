78dc180afa51f901215a163a01c992fcba300eea
tobiasKaminsky <tobias@kaminsky.me>
2019-04-16 16:46:45 +0200

    enhance pass protection system
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- src/main/java/com/owncloud/android/MainApp.java
+++ src/main/java/com/owncloud/android/MainApp.java
@@ -242,18 +242,17 @@ public class MainApp extends MultiDexApplication implements
             public void onActivityCreated(Activity activity, Bundle savedInstanceState) {
                 Log_OC.d(activity.getClass().getSimpleName(), "onCreate(Bundle) starting");
                 WhatsNewActivity.runIfNeeded(activity, preferences);
-                passCodeManager.onActivityCreated(activity);
             }
 
             @Override
             public void onActivityStarted(Activity activity) {
                 Log_OC.d(activity.getClass().getSimpleName(), "onStart() starting");
-                passCodeManager.onActivityStarted(activity);
             }
 
             @Override
             public void onActivityResumed(Activity activity) {
                 Log_OC.d(activity.getClass().getSimpleName(), "onResume() starting");
+                passCodeManager.onActivityStarted(activity);
             }
 
             @Override
