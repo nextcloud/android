a52887c697a0e83ba4450337829a05ed83512b13
Aaron Mandel <aaronjmandel@gmail.com>
2019-07-09 17:07:57 -0700

    Getting ready for renaming the package

--- src/main/java/com/nextcloud/client/onboarding/FirstRunActivity.java
+++ src/main/java/com/nextcloud/client/onboarding/FirstRunActivity.java
@@ -94,21 +94,21 @@ public class FirstRunActivity extends BaseActivity implements ViewPager.OnPageCh
             }
         });
 
-        Button providerButton = findViewById(R.id.signup);
-        providerButton.setBackgroundColor(getResources().getColor(R.color.primary_dark));
-        providerButton.setTextColor(getResources().getColor(R.color.login_text_color));
-        providerButton.setVisibility(isProviderOrOwnInstallationVisible ? View.VISIBLE : View.GONE);
-        providerButton.setOnClickListener(v -> {
-            Intent authenticatorActivityIntent = new Intent(this, AuthenticatorActivity.class);
-            authenticatorActivityIntent.putExtra(AuthenticatorActivity.EXTRA_USE_PROVIDER_AS_WEBLOGIN, true);
-
-            if (getIntent().getBooleanExtra(EXTRA_ALLOW_CLOSE, false)) {
-                startActivityForResult(authenticatorActivityIntent, FIRST_RUN_RESULT_CODE);
-            } else {
-                authenticatorActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(authenticatorActivityIntent);
-            }
-        });
+//        Button providerButton = findViewById(R.id.signup);
+//        providerButton.setBackgroundColor(getResources().getColor(R.color.primary_dark));
+//        providerButton.setTextColor(getResources().getColor(R.color.login_text_color));
+//        providerButton.setVisibility(isProviderOrOwnInstallationVisible ? View.VISIBLE : View.GONE);
+//        providerButton.setOnClickListener(v -> {
+//            Intent authenticatorActivityIntent = new Intent(this, AuthenticatorActivity.class);
+//            authenticatorActivityIntent.putExtra(AuthenticatorActivity.EXTRA_USE_PROVIDER_AS_WEBLOGIN, true);
+//
+//            if (getIntent().getBooleanExtra(EXTRA_ALLOW_CLOSE, false)) {
+//                startActivityForResult(authenticatorActivityIntent, FIRST_RUN_RESULT_CODE);
+//            } else {
+//                authenticatorActivityIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+//                startActivity(authenticatorActivityIntent);
+//            }
+//        });
 
         TextView hostOwnServerTextView = findViewById(R.id.host_own_server);
         hostOwnServerTextView.setTextColor(getResources().getColor(R.color.login_text_color));
