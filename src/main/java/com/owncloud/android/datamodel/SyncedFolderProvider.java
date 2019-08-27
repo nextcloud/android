37c6600d2e425bc5d7438f42ef0271864b9b483d
tobiasKaminsky <tobias@kaminsky.me>
2019-08-27 17:22:14 +0200

    Upload files into subfolder
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- src/main/java/com/owncloud/android/datamodel/SyncedFolderProvider.java
+++ src/main/java/com/owncloud/android/datamodel/SyncedFolderProvider.java
@@ -185,14 +185,13 @@ public class SyncedFolderProvider extends Observable {
     }
 
     public SyncedFolder findByLocalPathAndAccount(String localPath, Account account) {
-
         SyncedFolder result = null;
         Cursor cursor = mContentResolver.query(
             ProviderMeta.ProviderTableMeta.CONTENT_URI_SYNCED_FOLDERS,
             null,
-            ProviderMeta.ProviderTableMeta.SYNCED_FOLDER_LOCAL_PATH + "=? AND " +
+            ProviderMeta.ProviderTableMeta.SYNCED_FOLDER_LOCAL_PATH + "LIKE ? AND " +
                 ProviderMeta.ProviderTableMeta.SYNCED_FOLDER_ACCOUNT + " =? ",
-            new String[]{localPath, account.name},
+            new String[]{localPath + "%", account.name},
             null
         );
 
