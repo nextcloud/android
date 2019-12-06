8b2793798e2d8891f4e9a42e1a842c25459b9716
Siddharth Srinivasan <13200018+siddharths2710@users.noreply.github.com>
2019-12-06 21:41:09 +0530

    Renamed refresh methods and updated ETag parameter accordingly
    Signed-off-by: Siddharth Srinivasan <13200018+siddharths2710@users.noreply.github.com>
    
    Renamed refresh methods and updated ETag parameter accordingly with sign-off
    
    Signed-off-by: dassio <dassio@icloud.com>

--- src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.java
+++ src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.java
@@ -321,7 +321,7 @@ public class FileDisplayActivity extends FileActivity
             }
         } else {
             createMinFragments(savedInstanceState);
-            refreshList(true);
+            syncAndUpdateFolder(true);
         }
 
         setIndeterminate(mSyncInProgress);
@@ -394,7 +394,7 @@ public class FileDisplayActivity extends FileActivity
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     // permission was granted
                     EventBus.getDefault().post(new TokenPushEvent());
-                    refreshList(true);
+                    syncAndUpdateFolder(true);
                     // toggle on is save since this is the only scenario this code gets accessed
                 } else {
                     // permission denied --> do nothing
@@ -689,7 +689,7 @@ public class FileDisplayActivity extends FileActivity
         updateActionBarTitleAndHomeButton(null);
     }
 
-    public void refreshListOfFilesFragment(boolean fromSearch) {
+    public void updateListOfFilesFragment(boolean fromSearch) {
         OCFileListFragment fileListFragment = getListOfFilesFragment();
         if (fileListFragment != null) {
             fileListFragment.listDirectory(MainApp.isOnlyOnDevice(), fromSearch);
@@ -1203,7 +1203,7 @@ public class FileDisplayActivity extends FileActivity
     protected void onResume() {
         Log_OC.v(TAG, "onResume() start");
         super.onResume();
-        refreshList(true);
+        syncAndUpdateFolder(false);
 
         OCFile startFile = null;
         if (getIntent() != null && getIntent().getParcelableExtra(EXTRA_FILE) != null) {
@@ -1216,7 +1216,7 @@ public class FileDisplayActivity extends FileActivity
             searchView.setQuery(searchQuery, false);
         } else if (getListOfFilesFragment() != null && !getListOfFilesFragment().isSearchFragment()
                 && startFile == null) {
-            refreshListOfFilesFragment(false);
+            updateListOfFilesFragment(false);
         } else {
             getListOfFilesFragment().listDirectory(startFile, false, false);
             updateActionBarTitleAndHomeButton(startFile);
@@ -1464,7 +1464,7 @@ public class FileDisplayActivity extends FileActivity
                     String linkedToRemotePath =
                             intent.getStringExtra(FileUploader.EXTRA_LINKED_TO_PATH);
                     if (linkedToRemotePath == null || isAscendant(linkedToRemotePath)) {
-                        refreshListOfFilesFragment(false);
+                        updateListOfFilesFragment(false);
                     }
                 }
 
@@ -1548,7 +1548,7 @@ public class FileDisplayActivity extends FileActivity
                 if (sameAccount && isDescendant) {
                     String linkedToRemotePath = intent.getStringExtra(FileDownloader.EXTRA_LINKED_TO_PATH);
                     if (linkedToRemotePath == null || isAscendant(linkedToRemotePath)) {
-                        refreshListOfFilesFragment(false);
+                        updateListOfFilesFragment(false);
                     }
                     refreshSecondFragment(
                             intent.getAction(),
@@ -1819,7 +1819,7 @@ public class FileDisplayActivity extends FileActivity
             }
             OCFile parentFile = getStorageManager().getFileById(removedFile.getParentId());
             if (parentFile != null && parentFile.equals(getCurrentDir())) {
-                refreshListOfFilesFragment(false);
+                updateListOfFilesFragment(false);
             }
             supportInvalidateOptionsMenu();
         } else {
@@ -1875,8 +1875,7 @@ public class FileDisplayActivity extends FileActivity
     private void onMoveFileOperationFinish(MoveFileOperation operation,
                                            RemoteOperationResult result) {
         if (result.isSuccess()) {
-            refreshListOfFilesFragment(false);
-            refreshList(true);
+            syncAndUpdateFolder(true);
         } else {
             try {
                 DisplayUtils.showSnackMessage(
@@ -1914,7 +1913,7 @@ public class FileDisplayActivity extends FileActivity
                 fileDetailFragment.getFileDetailSharingFragment().refreshPublicShareFromDB();
                 fileDetailFragment.getFileDetailSharingFragment().onUpdateShareInformation(result, getFile());
             }
-            refreshListOfFilesFragment(false);
+            updateListOfFilesFragment(false);
         } else {
             // Detect Failure (403) --> maybe needs password
             String password = operation.getPassword();
@@ -1967,7 +1966,7 @@ public class FileDisplayActivity extends FileActivity
 
         if (result.isSuccess()) {
             updateFileFromDB();
-            refreshListOfFilesFragment(false);
+            updateListOfFilesFragment(false);
         } else if (fileDetailFragment != null && fileDetailFragment.getView() != null) {
             String errorResponse;
 
@@ -2017,7 +2016,7 @@ public class FileDisplayActivity extends FileActivity
      */
     private void onCopyFileOperationFinish(CopyFileOperation operation, RemoteOperationResult result) {
         if (result.isSuccess()) {
-            refreshListOfFilesFragment(false);
+            updateListOfFilesFragment(false);
         } else {
             try {
                 DisplayUtils.showSnackMessage(
@@ -2071,7 +2070,7 @@ public class FileDisplayActivity extends FileActivity
             }
 
             if (getStorageManager().getFileById(renamedFile.getParentId()).equals(getCurrentDir())) {
-                refreshListOfFilesFragment(false);
+                updateListOfFilesFragment(false);
             }
 
         } else {
@@ -2131,7 +2130,7 @@ public class FileDisplayActivity extends FileActivity
      */
     @Override
     public void onTransferStateChanged(OCFile file, boolean downloading, boolean uploading) {
-        refreshListOfFilesFragment(false);
+        updateListOfFilesFragment(false);
         FileFragment details = getSecondFragment();
         if (details instanceof FileDetailFragment &&
                 file.equals(details.getFile())) {
@@ -2442,15 +2441,15 @@ public class FileDisplayActivity extends FileActivity
 
     @Override
     public void onRefresh(boolean ignoreETag) {
-        refreshList(ignoreETag);
+        syncAndUpdateFolder(ignoreETag);
     }
 
     @Override
     public void onRefresh() {
-        refreshList(true);
+        syncAndUpdateFolder(true);
     }
 
-    private void refreshList(boolean ignoreETag) {
+    private void syncAndUpdateFolder(boolean ignoreETag) {
         OCFileListFragment listOfFiles = getListOfFilesFragment();
         if (listOfFiles != null && !listOfFiles.isSearchFragment()) {
             OCFile folder = listOfFiles.getCurrentFile();
