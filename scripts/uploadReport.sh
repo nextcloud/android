585cbbd8b7a0391733ed037c7f954355988bc4b5
tobiasKaminsky <tobias@kaminsky.me>
2019-12-09 11:22:05 +0100

    wip
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- scripts/uploadReport.sh
+++ scripts/uploadReport.sh
@@ -4,15 +4,17 @@
 #2: LOG_PASSWORD
 #3: DRONE_BUILD_NUMBER
 #4: TYPE (IT or Unit)
-#5: DRONE_PULL_REQUEST
-#6: GIT_USERNAME
-#7: GIT_TOKEN
+#5: androidVersion
+#6: DRONE_PULL_REQUEST
+#7: GIT_USERNAME
+#8: GIT_TOKEN
 
 URL=https://nextcloud.kaminsky.me/remote.php/webdav/android-integrationTests
 ID=$3
 USER=$1
 PASS=$2
 TYPE=$4
+ANDROID_VERSION=$5
 
 set -e
 
@@ -25,8 +27,9 @@ fi
 find . -type d -exec curl -u $USER:$PASS -X MKCOL $URL/$ID/$(echo {} | sed s#\./##) \;
 find . -type f -exec curl -u $USER:$PASS -X PUT $URL/$ID/$(echo {} | sed s#\./##) --upload-file {} \;
 
-echo "Uploaded failing tests to https://www.kaminsky.me/nc-dev/android-integrationTests/$ID"
+echo "Uploaded failing tests to https://www.kaminsky.me/nc-dev/android-integrationTests/$ID-$ANDROID_VERSION"
 
-curl -u $6:$7 -X POST https://api.github.com/repos/nextcloud/android/issues/$5/comments -d "{ \"body\" : \"$TYPE test failed: https://www.kaminsky.me/nc-dev/android-integrationTests/$ID \" }"
+curl -u $7:$8 -X POST https://api.github.com/repos/nextcloud/android/issues/$6/comments -d "{ \"body\" : \"$TYPE test
+ failed: https://www.kaminsky.me/nc-dev/android-integrationTests/$ID \" }"
 
 exit 1
