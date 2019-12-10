7554cefb3e7b4fbfa375a786687a23a7cb1f2901
tobiasKaminsky <tobias@kaminsky.me>
2019-12-10 15:26:31 +0100

    test ui diff
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- scripts/uploadReport.sh
+++ scripts/uploadReport.sh
@@ -13,20 +13,23 @@ ID=$3
 USER=$1
 PASS=$2
 TYPE=$4
+FOLDER=$ID-$TYPE
 
 set -e
 
 if [ $TYPE = "IT" ]; then
     cd build/reports/androidTests/connected/flavors/GPLAY
-else
+elif [ $TYPE = "Unit" ]; then
     cd build/reports/tests/testGplayDebugUnitTest
+else
+    cd build/reports/short/verification/
 fi
 
-find . -type d -exec curl -u $USER:$PASS -X MKCOL $URL/$ID/$(echo {} | sed s#\./##) \;
-find . -type f -exec curl -u $USER:$PASS -X PUT $URL/$ID/$(echo {} | sed s#\./##) --upload-file {} \;
+find . -type d -exec curl -u $USER:$PASS -X MKCOL $URL/$FOLDER/$(echo {} | sed s#\./##) \;
+find . -type f -exec curl -u $USER:$PASS -X PUT $URL/$FOLDER/$(echo {} | sed s#\./##) --upload-file {} \;
 
-echo "Uploaded failing tests to https://www.kaminsky.me/nc-dev/android-integrationTests/$ID"
+echo "Uploaded failing tests to https://www.kaminsky.me/nc-dev/android-integrationTests/$FOLDER"
 
-curl -u $6:$7 -X POST https://api.github.com/repos/nextcloud/android/issues/$5/comments -d "{ \"body\" : \"$TYPE test failed: https://www.kaminsky.me/nc-dev/android-integrationTests/$ID \" }"
+curl -u $6:$7 -X POST https://api.github.com/repos/nextcloud/android/issues/$5/comments -d "{ \"body\" : \"$TYPE test failed: https://www.kaminsky.me/nc-dev/android-integrationTests/$FOLDER \" }"
 
 exit 1
---
+++ src/androidTest/java/com/nextcloud/client/ScreenshotTestRunner.java
@@ -0,0 +1,44 @@
+/*
+ *
+ * Nextcloud Android client application
+ *
+ * @author Tobias Kaminsky
+ * Copyright (C) 2019 Tobias Kaminsky
+ * Copyright (C) 2019 Nextcloud GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.nextcloud.client;
+
+import android.os.Bundle;
+
+import com.facebook.testing.screenshot.ScreenshotRunner;
+
+import androidx.test.runner.AndroidJUnitRunner;
+
+public class ScreenshotTestRunner extends AndroidJUnitRunner {
+
+    @Override
+    public void onCreate(Bundle args) {
+        super.onCreate(args);
+        ScreenshotRunner.onCreate(this, args);
+    }
+
+    @Override
+    public void finish(int resultCode, Bundle results) {
+        ScreenshotRunner.onDestroy();
+        super.finish(resultCode, results);
+    }
+}
