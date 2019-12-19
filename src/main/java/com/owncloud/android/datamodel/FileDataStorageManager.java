48d0f3e1b576b078a4226132b39b582a923279b7
tobiasKaminsky <tobias@kaminsky.me>
2019-12-19 09:10:23 +0100

    fix during CI
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>
    Signed-off-by: dassio <dassio@icloud.com>

--- src/main/java/com/owncloud/android/datamodel/FileDataStorageManager.java
+++ src/main/java/com/owncloud/android/datamodel/FileDataStorageManager.java
@@ -39,7 +39,6 @@ import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
 import com.owncloud.android.MainApp;
 import com.owncloud.android.db.ProviderMeta.ProviderTableMeta;
-import com.owncloud.android.lib.common.DirectEditing;
 import com.owncloud.android.lib.common.network.WebdavEntry;
 import com.owncloud.android.lib.common.operations.RemoteOperationResult;
 import com.owncloud.android.lib.common.utils.Log_OC;
