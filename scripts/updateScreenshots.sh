b0f31ad2c6e28f8d3c91587a9d1bf6d6d2b6b919
tobiasKaminsky <tobias@kaminsky.me>
2020-01-10 12:24:04 +0100

    Basic documentation of how to test add checklist as PR template
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- scripts/updateScreenshots.sh
+++ scripts/updateScreenshots.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+if ( [[ $(grep NC_TEST_SERVER_BASEURL ~/.gradle/gradle.properties   | grep -v "#" -c) -gt 0 ]] ); then
+    echo "This will not use server in docker. Please comment in .gradle/gradle.properties. Aborting!"
+    exit 1
+fi
+
 ## emulator
 if ( [[ $(emulator -list-avds | grep uiComparison -c) -eq 0 ]] ); then
+    avdmanager delete avd -n uiComparison
     (sleep 5; echo "no") | avdmanager create avd -n uiComparison -c 100M -k "system-images;android-27;google_apis;x86" --abi "google_apis/x86"
 fi
 
@@ -9,7 +15,7 @@ emulator -avd uiComparison -no-snapshot -gpu swiftshader_indirect -no-window -no
 PID=$(echo $!)
 
 ## server
-docker run --name=uiComparison nextcloudci/server:server-3 1>/dev/null &
+docker run --name=uiComparison nextcloudci/server 1>/dev/null &
 sleep 5
 IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' uiComparison)
 
@@ -23,7 +29,22 @@ cp gradle.properties gradle.properties_
 sed -i s"/server/$IP/" gradle.properties
 scripts/wait_for_emulator.sh
 scripts/wait_for_server.sh ${IP}
+
+## update all screenshots
 ./gradlew executeScreenshotTests -Precord
+
+## update screenshots in a class
+#./gradlew executeScreenshotTests \
+#-Precord \
+#-Pandroid.testInstrumentationRunnerArguments.class=\
+#com.owncloud.android.ui.dialog.SyncFileNotEnoughSpaceDialogFragmentTest
+
+## update single screenshot within a class
+#./gradlew executeScreenshotTests \
+#-Precord \
+#-Pandroid.testInstrumentationRunnerArguments.class=\
+#com.owncloud.android.ui.dialog.SyncFileNotEnoughSpaceDialogFragmentTest#showNotEnoughSpaceDialogForFile
+
 mv gradle.properties_ gradle.properties
 
 # tidy up
