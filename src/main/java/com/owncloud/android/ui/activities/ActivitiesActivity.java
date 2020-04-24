31df74ed17dd5ad3242217b76801ff036c0f2342
Kilian Périsset <kilian.perisset@infomaniak.com>
2020-04-24 17:16:28 +0200

    Replaced some icons by Material ones to (removed useless icons)
    Signed-off-by: Kilian Périsset <kilian.perisset@infomaniak.com>

--- src/main/java/com/owncloud/android/ui/activities/ActivitiesActivity.java
+++ src/main/java/com/owncloud/android/ui/activities/ActivitiesActivity.java
@@ -166,7 +166,7 @@ public class ActivitiesActivity extends FileActivity implements ActivityListInte
      * sets up the UI elements and loads all activity items.
      */
     private void setupContent() {
-        emptyContentIcon.setImageResource(R.drawable.ic_activity_light_grey);
+        emptyContentIcon.setImageResource(R.drawable.ic_activity);
         emptyContentProgressBar.getIndeterminateDrawable().setColorFilter(ThemeUtils.primaryAccentColor(this),
                                                                           PorterDuff.Mode.SRC_IN);
 
