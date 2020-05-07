8779f633abd852a9bef9be622d095fe16914a365
Abdourahamane BOINAIDI <abdourahamane.boinaidi@infomaniak.com>
2020-05-07 15:39:40 +0200

    add thumbnail corners on grid and list mode fix to 8dp
    Signed-off-by: Abdourahamane BOINAIDI <abdourahamane.boinaidi@infomaniak.com>

--- src/main/java/com/owncloud/android/datamodel/ThumbnailsCacheManager.java
+++ src/main/java/com/owncloud/android/datamodel/ThumbnailsCacheManager.java
@@ -427,22 +427,11 @@ public final class ThumbnailsCacheManager {
         private String mImageKey;
         private FileDataStorageManager mStorageManager;
         private GetMethod getMethod;
-        private boolean roundedCorners = false;
         private Listener mListener;
 
         public ThumbnailGenerationTask(ImageView imageView, FileDataStorageManager storageManager, Account account)
                 throws IllegalArgumentException {
             this(imageView, storageManager, account, null);
-        }
-
-        public ThumbnailGenerationTask(ImageView imageView,
-                                       FileDataStorageManager storageManager,
-                                       Account account,
-                                       List<ThumbnailGenerationTask> asyncTasks,
-                                       boolean roundedCorners)
-            throws IllegalArgumentException {
-            this(imageView, storageManager, account, asyncTasks);
-            this.roundedCorners = roundedCorners;
         }
 
         public ThumbnailGenerationTask(ImageView imageView, FileDataStorageManager storageManager,
@@ -543,11 +532,7 @@ public final class ThumbnailsCacheManager {
                         tagId = String.valueOf(((TrashbinFile) mFile).getRemoteId());
                     }
                     if (String.valueOf(imageView.getTag()).equals(tagId)) {
-                        if (roundedCorners) {
-                            BitmapUtils.setRoundedBitmap(bitmap, imageView);
-                        } else {
-                            imageView.setImageBitmap(bitmap);
-                        }
+                        BitmapUtils.setRoundedBitmap(bitmap, imageView);
                     }
                 }
             }
