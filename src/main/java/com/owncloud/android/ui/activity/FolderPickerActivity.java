67c7ef8a938a9b7892d2d2b9fcf77f80c56ea73c
Kilian Périsset <kilian.perisset@infomaniak.com>
2020-05-13 16:46:31 +0200

    Cleaned up the old toolbar progressbar dependent code
    Signed-off-by: Kilian Périsset <kilian.perisset@infomaniak.com>

--- src/main/java/com/owncloud/android/ui/activity/FolderPickerActivity.java
+++ src/main/java/com/owncloud/android/ui/activity/FolderPickerActivity.java
@@ -154,7 +154,6 @@ public class FolderPickerActivity extends FileActivity implements FileFragment.C
             ThemeUtils.setColoredTitle(getSupportActionBar(), caption, this);
         }
 
-        showProgressBar(mSyncInProgress);
         // always AFTER setContentView(...) ; to work around bug in its implementation
 
         // sets message for empty list of folders
@@ -265,7 +264,7 @@ public class FolderPickerActivity extends FileActivity implements FileFragment.C
                                                                             getApplicationContext());
 
         refreshFolderOperation.execute(getAccount(), this, null, null);
-        showProgressBar(true);
+        getListOfFilesFragment().setLoading(true);
         setBackgroundText();
     }
 
@@ -273,6 +272,8 @@ public class FolderPickerActivity extends FileActivity implements FileFragment.C
     protected void onResume() {
         super.onResume();
         Log_OC.e(TAG, "onResume() start");
+        getListOfFilesFragment().setLoading(mSyncInProgress);
+
 
         // refresh list of files
         refreshListOfFilesFragment(false);
@@ -544,7 +545,7 @@ public class FolderPickerActivity extends FileActivity implements FileFragment.C
                     DataHolderUtil.getInstance().delete(intent.getStringExtra(FileSyncAdapter.EXTRA_RESULT));
                     Log_OC.d(TAG, "Setting progress visibility to " + mSyncInProgress);
 
-                    showProgressBar(mSyncInProgress);
+                    getListOfFilesFragment().setLoading(mSyncInProgress);
 
                     setBackgroundText();
                 }
