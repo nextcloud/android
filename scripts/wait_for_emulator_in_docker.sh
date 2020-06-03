a60c94c63054121e8a1424d5304cf43c07d2ce75
tobiasKaminsky <tobias@kaminsky.me>
2020-06-03 14:44:05 +0200

    wip
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- scripts/wait_for_emulator_in_docker.sh
+++ scripts/wait_for_emulator_in_docker.sh
@@ -5,8 +5,12 @@ cd ci/android/
 for i in {1..10}; do
     docker-compose exec -T android adb devices
     [[ $? -eq 0 ]] && break
-    sleep 20
+
+    docker-compose logs
+
     [[ $i -eq 10 ]] && exit 1 # failed to wait for emulator
+
+    sleep 30
 done
 
 cd ../../scripts/
