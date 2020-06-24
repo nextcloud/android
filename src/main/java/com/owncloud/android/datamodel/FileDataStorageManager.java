4aa6440ec31ff65b3967f3e39c1fe4afff6aaf80
tobiasKaminsky <tobias@kaminsky.me>
2020-06-24 09:32:33 +0200

    another try to get rid of vanishing ringtones, but keep storage location
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- src/main/java/com/owncloud/android/datamodel/FileDataStorageManager.java
+++ src/main/java/com/owncloud/android/datamodel/FileDataStorageManager.java
@@ -30,6 +30,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.OperationApplicationException;
 import android.database.Cursor;
+import android.media.MediaScannerConnection;
 import android.net.Uri;
 import android.os.Build;
 import android.os.RemoteException;
@@ -1739,20 +1740,12 @@ public class FileDataStorageManager {
             ContentValues values = new ContentValues();
             ContentResolver contentResolver = MainApp.getAppContext().getContentResolver();
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-                if (file != null) {
-                    values.put(MediaStore.Images.Media.MIME_TYPE, file.getMimeType());
-                    values.put(MediaStore.Images.Media.TITLE, file.getFileName());
-                    values.put(MediaStore.Images.Media.DISPLAY_NAME, file.getFileName());
-                }
-                values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000);
-                values.put(MediaStore.Images.Media.RELATIVE_PATH, path);
-                values.put(MediaStore.Images.Media.IS_PENDING, 0);
-                try {
-                    contentResolver.insert(MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL_PRIMARY),
-                                           values);
-                } catch (IllegalArgumentException e) {
-                    Log_OC.e("MediaScanner", "Adding image to media scanner failed: " + e);
-                }
+                // Tell the media scanner about the new file so that it is
+                // immediately available to the user.
+                MediaScannerConnection.scanFile(MainApp.getAppContext(),
+                                                new String[]{file.getStoragePath()},
+                                                null,
+                                                null);
             } else {
                 Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
                 intent.setData(Uri.fromFile(new File(path)));
