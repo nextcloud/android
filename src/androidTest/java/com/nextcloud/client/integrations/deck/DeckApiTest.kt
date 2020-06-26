c2c038d5775b598094babcf8dc97c92c32ff160f
Chris Narkiewicz <hello@ezaquarii.com>
2020-06-26 23:38:41 +0100

    Provide DeckApi via dependency injection framework
    - renamed integration packate to integrations (plural)
    - created DI module for integrations
    - added provider for DeckApi
    - inject DeckApi to background job factory
    
    Signed-off-by: Chris Narkiewicz <hello@ezaquarii.com>

--- src/androidTest/java/com/nextcloud/client/integrations/deck/DeckApiTest.kt
+++ src/androidTest/java/com/nextcloud/client/integrations/deck/DeckApiTest.kt
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-package com.nextcloud.client.integration.deck
+package com.nextcloud.client.integrations.deck
 
 import android.content.Context
 import android.content.Intent
