d8d24908da010b581da3077fb3256c50e448fc56
tobiasKaminsky <tobias@kaminsky.me>
2020-07-08 16:03:12 +0200

    direct login works again
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- src/main/java/com/owncloud/android/authentication/AuthenticatorActivity.java
+++ src/main/java/com/owncloud/android/authentication/AuthenticatorActivity.java
@@ -1397,6 +1397,7 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity
                         webViewUser = loginUrlInfo.username;
                         webViewPassword = loginUrlInfo.password;
                         doOnResumeAndBound();
+                        checkOcServer();
                     } catch (Exception e) {
                         mServerStatusIcon = R.drawable.ic_alert;
                         mServerStatusText = getString(R.string.qr_could_not_be_read);
