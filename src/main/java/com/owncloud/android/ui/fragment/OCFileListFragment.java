f53fb6d78c6e7283b5977a209010de77042c0f7d
tobiasKaminsky <tobias@kaminsky.me>
2020-07-09 07:43:20 +0200

    fix wrong show of sort/list group and FAB in details or text preview
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
+++ src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
@@ -28,7 +28,6 @@ import android.accounts.Account;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Build;
@@ -125,7 +124,6 @@ import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.ActionBar;
 import androidx.coordinatorlayout.widget.CoordinatorLayout;
-import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.FragmentActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -996,6 +994,7 @@ public class OCFileListFragment extends ExtendedListFragment implements
                     } else if (file.isDown() && MimeTypeUtil.isVCard(file)) {
                         ((FileDisplayActivity) mContainerActivity).startContactListFragment(file);
                     } else if (PreviewTextFileFragment.canBePreviewed(file)) {
+                        setFabVisible(false);
                         ((FileDisplayActivity) mContainerActivity).startTextPreview(file, false);
                     } else if (file.isDown()) {
                         if (PreviewMediaFragment.canBePreviewed(file)) {
@@ -1114,6 +1113,8 @@ public class OCFileListFragment extends ExtendedListFragment implements
                         mActiveActionMode.finish();
                     }
                     mContainerActivity.showDetails(singleFile);
+                    setFabVisible(false);
+                    mContainerActivity.showSortListGroup(false);
                     return true;
                 }
                 case R.id.action_set_as_wallpaper: {
@@ -1752,7 +1753,7 @@ public class OCFileListFragment extends ExtendedListFragment implements
      *
      * @param visible Desired visibility for the FAB.
      */
-    void setFabVisible(final boolean visible) {
+    public void setFabVisible(final boolean visible) {
         if (mFabMain == null) {
             // is not available in FolderPickerActivity
             return;
