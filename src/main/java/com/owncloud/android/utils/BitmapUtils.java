540285560acaa41c75c60f134c90e273f6c5c9ae
tobiasKaminsky <tobias@kaminsky.me>
2020-10-29 08:24:56 +0100

    use primarey
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- src/main/java/com/owncloud/android/utils/BitmapUtils.java
+++ src/main/java/com/owncloud/android/utils/BitmapUtils.java
@@ -210,7 +210,7 @@ public final class BitmapUtils {
             try {
                 hash = md5(hash);
             } catch (NoSuchAlgorithmException e) {
-                int color = getResources().getColor(R.color.primary_dark);
+                int color = getResources().getColor(R.color.primary);
                 return new Color(android.graphics.Color.red(color),
                                  android.graphics.Color.green(color),
                                  android.graphics.Color.blue(color));
