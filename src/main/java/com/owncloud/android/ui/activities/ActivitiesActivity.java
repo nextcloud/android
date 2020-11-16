f7a4b5a9d66a0be6287e17ee978f184c2ddabea6
will <545512533@qq.com>
2020-11-16 16:51:44 +0800

    1、隐藏返回按键图标

--- src/main/java/com/owncloud/android/ui/activities/ActivitiesActivity.java
+++ src/main/java/com/owncloud/android/ui/activities/ActivitiesActivity.java
@@ -123,7 +123,7 @@ public class ActivitiesActivity extends DrawerActivity implements ActivityListIn
         // setup drawer
         setupDrawer(R.id.nav_activity);
         updateActionBarTitleAndHomeButtonByString(getString(R.string.drawer_item_activities));
-//        setDrawerIndicatorEnabled(false);
+        setDrawerIndicatorEnabled(false);
         swipeListRefreshLayout.setOnRefreshListener(() -> {
             // We set lastGiven variable to undefined here since when manually refreshing
             // activities data we want to clear the list and reset the pagination.
