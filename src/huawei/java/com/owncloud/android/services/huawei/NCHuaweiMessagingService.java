400284e35411ae76d76570cf1a88e3ae62ff1985
Han Tek Foo 00564074 <han.tek.foo@huawei.com>
2021-06-25 17:05:40 +0200

    created huawei flavor directory

---
+++ src/huawei/java/com/owncloud/android/services/huawei/NCHuaweiMessagingService.java
@@ -0,0 +1,36 @@
+/**
+ * Nextcloud Android client application
+ *
+ * @author Mario Danic
+ * Copyright (C) 2017 Mario Danic
+ * Copyright (C) 2020 Chris Narkiewicz <hello@ezaquarii.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.owncloud.android.services.huawei;
+
+import android.util.Log;
+import android.widget.Toast;
+
+import com.huawei.hms.push.HmsMessageService;
+
+public class NCHuaweiMessagingService extends HmsMessageService  {
+    private static final String TAG = "PushDemoLog";
+    @Override
+    public void onNewToken(String token) {
+        super.onNewToken(token);
+        Log.i(TAG, "receive token:" + token);
+        Toast.makeText(this, "receive token:" + token, Toast.LENGTH_LONG).show();
+    }
+}
