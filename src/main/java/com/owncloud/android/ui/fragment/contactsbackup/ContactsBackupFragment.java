04fd45d2d29a2d2f62bb431c5304dfb8e4c85980
Andy Scherzinger <info@andy-scherzinger.de>
2021-06-28 17:12:38 +0200

    remove unused import
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- src/main/java/com/owncloud/android/ui/fragment/contactsbackup/ContactsBackupFragment.java
+++ src/main/java/com/owncloud/android/ui/fragment/contactsbackup/ContactsBackupFragment.java
@@ -28,7 +28,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.storage.StorageManager;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
