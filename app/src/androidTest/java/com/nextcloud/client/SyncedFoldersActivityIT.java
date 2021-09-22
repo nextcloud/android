8ecd3733f3683fa656c90ce8f00817e0d2aa57a5
A117870935 <surinder.kumar@t-systems.com>
2021-09-22 11:46:30 +0530

    Cherry pick commit id - 3545a6c from branch feature/NMCLOUD-629. changed auto upload InstantUpload folder to Camera-Media
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- app/src/androidTest/java/com/nextcloud/client/SyncedFoldersActivityIT.java
+++ app/src/androidTest/java/com/nextcloud/client/SyncedFoldersActivityIT.java
@@ -60,7 +60,7 @@ public class SyncedFoldersActivityIT extends AbstractIT {
     public void testSyncedFolderDialog() {
         SyncedFolderDisplayItem item = new SyncedFolderDisplayItem(1,
                                                                    "/sdcard/DCIM/",
-                                                                   "/InstantUpload/",
+                                                                   "/Camera-Media/",
                                                                    true,
                                                                    false,
                                                                    false,
