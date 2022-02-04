d5d27289f5ef9c45bfde5f999bd444e10ff3d875
Álvaro Brey Vilas <alvaro.brey@nextcloud.com>
2022-02-04 10:58:05 +0100

    Fix screenshots
    Signed-off-by: Álvaro Brey Vilas <alvaro.brey@nextcloud.com>

--- scripts/uploadScreenshotSummary.sh
+++ scripts/uploadScreenshotSummary.sh
@@ -11,7 +11,7 @@ PASS=$2
 date=$(date +%F)
 echo "Uploaded screenshot summary to $PUBLIC_URL/$date/summary.html"
 
-cd build/screenshotSummary
+cd app/build/screenshotSummary
 
 find . -type d -exec curl > /dev/null 2>&1 -u $USER:$PASS -X MKCOL $DAV_URL/$date/$(echo {} | sed s#\./##) \;
 find . -type f -exec curl > /dev/null 2>&1 -u $USER:$PASS -X PUT $DAV_URL/$date/$(echo {} | sed s#\./##) --upload-file {} \;
