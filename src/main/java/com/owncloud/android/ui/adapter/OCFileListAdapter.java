1c9a3e020836f414af2f51360d978d8bd9ea755d
tobiasKaminsky <tobias@kaminsky.me>
2022-02-07 14:45:23 +0100

    For display with >= 600dp use higher file icon resolution use this resolution also default thumbnails
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- src/main/java/com/owncloud/android/ui/adapter/OCFileListAdapter.java
+++ src/main/java/com/owncloud/android/ui/adapter/OCFileListAdapter.java
@@ -594,7 +594,8 @@ public class OCFileListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
                                                                            R.drawable.file_image,
                                                                            null);
                                 }
-                                thumbnail = BitmapUtils.drawableToBitmap(drawable);
+                                int px = ThumbnailsCacheManager.getThumbnailDimension();
+                                thumbnail = BitmapUtils.drawableToBitmap(drawable, px, px);
                             }
                             final ThumbnailsCacheManager.AsyncThumbnailDrawable asyncDrawable =
                                 new ThumbnailsCacheManager.AsyncThumbnailDrawable(context.getResources(),
