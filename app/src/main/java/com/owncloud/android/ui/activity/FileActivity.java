e5708f8f1bf35bbea2201135852766798d123dec
Álvaro Brey Vilas <alvaro.brey@nextcloud.com>
2022-03-31 15:04:19 +0200

    Better approach for avoiding state loss in FileDetailFragment
    Do not overwrite file when updating share information, instead of holding the file in FDA
    
    Signed-off-by: Álvaro Brey Vilas <alvaro.brey@nextcloud.com>

--- app/src/main/java/com/owncloud/android/ui/activity/FileActivity.java
+++ app/src/main/java/com/owncloud/android/ui/activity/FileActivity.java
@@ -743,7 +743,7 @@ public abstract class FileActivity extends DrawerActivity
 
         if (result.isSuccess()) {
             if (sharingFragment != null) {
-                sharingFragment.onUpdateShareInformation(result, getFile());
+                sharingFragment.onUpdateShareInformation(result);
             }
         } else {
             DisplayUtils.showSnackMessage(this, R.string.note_could_not_sent);
@@ -757,7 +757,7 @@ public abstract class FileActivity extends DrawerActivity
         if (result.isSuccess()) {
             updateFileFromDB();
             if (sharingFragment != null) {
-                sharingFragment.onUpdateShareInformation(result, getFile());
+                sharingFragment.onUpdateShareInformation(result);
             }
         } else if (sharingFragment != null && sharingFragment.getView() != null) {
             if (TextUtils.isEmpty(result.getMessage())) {
