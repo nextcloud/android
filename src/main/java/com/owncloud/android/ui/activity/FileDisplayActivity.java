be698f44a5cc62435879099c25b1c1ab7662066e
Kumar <Saurav-Kumar@t-systems.com>
2022-04-11 14:56:24 +0530

    merge with PR https://github.com/nextcloud/android/pull/10070

--- src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.java
+++ src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.java
@@ -559,6 +559,7 @@ public class FileDisplayActivity extends FileActivity
                 Log_OC.d(this, "Switch to oc file fragment");
 
                 setLeftFragment(new OCFileListFragment());
+                browseToRoot();
             }
     }
 
@@ -1618,6 +1619,7 @@ public class FileDisplayActivity extends FileActivity
     public void browseToRoot() {
         OCFileListFragment listOfFiles = getListOfFilesFragment();
         if (listOfFiles != null) {  // should never be null, indeed
+            MainApp.showOnlyFilesOnDevice(false);
             OCFile root = getStorageManager().getFileByPath(OCFile.ROOT_PATH);
             listOfFiles.listDirectory(root, MainApp.isOnlyOnDevice(), false);
             setFile(listOfFiles.getCurrentFile());
