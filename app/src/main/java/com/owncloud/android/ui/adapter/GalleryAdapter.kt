474341be78efc267504a4a8f9032351708bab094
Kumar <Surinder.Kumar@t-systems.com>
2022-04-27 21:02:33 +0530

    Copyrights added.

--- app/src/main/java/com/owncloud/android/ui/adapter/GalleryAdapter.kt
+++ app/src/main/java/com/owncloud/android/ui/adapter/GalleryAdapter.kt
@@ -3,8 +3,10 @@
  * Nextcloud Android client application
  *
  * @author Tobias Kaminsky
+ * @author TSI-mc
  * Copyright (C) 2022 Tobias Kaminsky
  * Copyright (C) 2022 Nextcloud GmbH
+ * Copyright (C) 2022 TSI-mc
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
