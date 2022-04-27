484a6b8f69c33b387abba6185d5bb76136b308d1
Kumar <Surinder.Kumar@t-systems.com>
2022-04-27 21:02:33 +0530

    Copyrights added.

--- app/src/main/java/com/owncloud/android/ui/activity/FolderPickerActivity.java
+++ app/src/main/java/com/owncloud/android/ui/activity/FolderPickerActivity.java
@@ -1,7 +1,9 @@
 /*
  *   ownCloud Android client application
  *
+ *   @author TSI-mc
  *   Copyright (C) 2016 ownCloud Inc.
+ *   Copyright (C) 2022 TSI-mc
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License version 2,
