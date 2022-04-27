42217c395f611c62b1e686bb739260d450cac329
Kumar <Surinder.Kumar@t-systems.com>
2022-04-27 21:02:33 +0530

    Copyrights added.

--- app/src/main/java/com/nextcloud/client/preferences/AppPreferencesImpl.java
+++ app/src/main/java/com/nextcloud/client/preferences/AppPreferencesImpl.java
@@ -3,8 +3,10 @@
  *
  * @author David A. Velasco
  * @author Chris Narkiewicz Chris Narkiewicz
+ * @author TSI-mc
  * Copyright (C) 2016 ownCloud Inc.
  * Copyright (C) 2019 Chris Narkiewicz <hello@ezaquarii.com>
+ * Copyright (C) 2022 TSI-mc
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
