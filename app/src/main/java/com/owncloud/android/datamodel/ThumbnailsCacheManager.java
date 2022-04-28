obel1x/nextcloud-android

Parallel thumbnail fetching
Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

app/src/main/java/com/owncloud/android/datamodel/ThumbnailsCacheManager.java
@@ -462,19 +462,27 @@ public final class ThumbnailsCacheManager {
             mAsyncTasks = asyncTasks;
         }
 
-        public ThumbnailGenerationTask(ImageView imageView, FileDataStorageManager storageManager,
-                                       User user, List<ThumbnailGenerationTask> asyncTasks,
-                                       boolean gridViewEnabled)
+        public ThumbnailGenerationTask(ImageView imageView,
+                                       FileDataStorageManager storageManager,
+                                       User user,
+                                       List<ThumbnailGenerationTask> asyncTasks,
+                                       boolean gridViewEnabled,
+                                       String remoteId)
             throws IllegalArgumentException {
             this(imageView, storageManager, user, asyncTasks);
             this.gridViewEnabled = gridViewEnabled;
+            mImageKey = remoteId;
         }
 
         public GetMethod getGetMethod() {
             return getMethod;
         }
 
-        public ThumbnailGenerationTask(FileDataStorageManager storageManager, User user){
+        public String getImageKey() {
+            return mImageKey;
+        }
+
+        public ThumbnailGenerationTask(FileDataStorageManager storageManager, User user) {
             if (storageManager == null) {
                 throw new IllegalArgumentException("storageManager must not be NULL");
             }

nextcloud/android

app/src/main/java/com/owncloud/android/datamodel/ThumbnailsCacheManager.java
@@ -462,19 +462,27 @@ public final class ThumbnailsCacheManager {
             mAsyncTasks = asyncTasks;
         }
 
-        public ThumbnailGenerationTask(ImageView imageView, FileDataStorageManager storageManager,
-                                       User user, List<ThumbnailGenerationTask> asyncTasks,
-                                       boolean gridViewEnabled)
+        public ThumbnailGenerationTask(ImageView imageView,
+                                       FileDataStorageManager storageManager,
+                                       User user,
+                                       List<ThumbnailGenerationTask> asyncTasks,
+                                       boolean gridViewEnabled,
+                                       String imageKey)
             throws IllegalArgumentException {
             this(imageView, storageManager, user, asyncTasks);
             this.gridViewEnabled = gridViewEnabled;
+            mImageKey = imageKey;
         }
 
         public GetMethod getGetMethod() {
             return getMethod;
         }
 
-        public ThumbnailGenerationTask(FileDataStorageManager storageManager, User user){
+        public String getImageKey() {
+            return mImageKey;
+        }
+
+        public ThumbnailGenerationTask(FileDataStorageManager storageManager, User user) {
             if (storageManager == null) {
                 throw new IllegalArgumentException("storageManager must not be NULL");
             }