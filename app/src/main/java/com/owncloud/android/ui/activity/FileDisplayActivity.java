6704bb04278f0bbc12cc30d62f7e3692841209e1
tobiasKaminsky <tobias@kaminsky.me>
2022-05-12 09:23:10 +0200

    Fix Notification -> on device navigation path
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- app/src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.java
+++ app/src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.java
@@ -1498,7 +1498,6 @@ public class FileDisplayActivity extends FileActivity
     public void browseToRoot() {
         OCFileListFragment listOfFiles = getListOfFilesFragment();
         if (listOfFiles != null) {  // should never be null, indeed
-            MainApp.showOnlyFilesOnDevice(false);
             OCFile root = getStorageManager().getFileByPath(OCFile.ROOT_PATH);
             listOfFiles.listDirectory(root, MainApp.isOnlyOnDevice(), false);
             setFile(listOfFiles.getCurrentFile());
