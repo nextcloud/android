0748ba19afc85bd630de287328ea2ff4557f187c
tobiasKaminsky <tobias@kaminsky.me>
2022-05-16 14:05:00 +0200

    Harden thumbnail generation
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- app/src/main/java/com/owncloud/android/ui/fragment/GalleryFragment.java
+++ app/src/main/java/com/owncloud/android/ui/fragment/GalleryFragment.java
@@ -182,7 +182,6 @@ public class GalleryFragment extends OCFileListFragment {
 
     public void searchCompleted(boolean emptySearch, long lastTimeStamp) {
         photoSearchQueryRunning = false;
-        mAdapter.notifyDataSetChanged();
 
         if (mAdapter.isEmpty()) {
             setEmptyListMessage(SearchType.GALLERY_SEARCH);
