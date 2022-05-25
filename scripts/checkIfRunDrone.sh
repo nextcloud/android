3ce4589278eafed614123a6bd378dfb9776caad5
Álvaro Brey <alvaro.brey@nextcloud.com>
2022-05-25 09:47:41 +0200

    Fix checkIfRunDrone.sh for new getBranchBase arguments
    Signed-off-by: Álvaro Brey <alvaro.brey@nextcloud.com>

--- scripts/checkIfRunDrone.sh
+++ scripts/checkIfRunDrone.sh
@@ -1,12 +1,16 @@
 #!/bin/sh -e
 
+GIT_USERNAME=$1
+GIT_TOKEN=$2
+PR_NUMBER=$3
+
 if [ -z $3 ] ; then
     echo "Merge commit to master -> continue with CI"
     exit 0
 fi
 
-export BRANCH=$(scripts/analysis/getBranchBase.sh $1 $2 $3 | sed s'/"//'g)
-if [ $(git diff --name-only origin/$BRANCH | grep -cE "^app/src|screenshots|build.gradle|.drone.yml") -eq 0 ] ; then
+export BRANCH=$(scripts/analysis/getBranchBase.sh "${GIT_TOKEN}" "${PR_NUMBER}" | sed 's/"//g')
+if [ "$(git diff --name-only "origin/$BRANCH" | grep -cE "^app/src|screenshots|build.gradle|.drone.yml")" -eq 0 ] ; then
     echo "No source files changed"
     exit 1
 else
