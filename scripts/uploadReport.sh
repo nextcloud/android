4ae8ace68c9bc751949229cbff15e75e22538fc7
Álvaro Brey <alvaro.brey@nextcloud.com>
2022-05-30 12:08:22 +0200

    uploadReport.sh: use deleteOldComments.sh instead of duplicating code
    Signed-off-by: Álvaro Brey <alvaro.brey@nextcloud.com>

--- scripts/uploadReport.sh
+++ scripts/uploadReport.sh
@@ -1,20 +1,7 @@
 #!/usr/bin/env bash
 
-deleteOldComments() {
-    # delete all old comments, matching this type
-    echo "Deleting old comments for $BRANCH_TYPE"
-    oldComments=$(curl_gh -X GET "https://api.github.com/repos/nextcloud/android/issues/$PR/comments" | jq --arg TYPE "$BRANCH_TYPE" '.[] | (.id |tostring) + "|" + (.user.login | test("nextcloud-android-bot|github-actions") | tostring) + "|" + (.body | test([$TYPE]) | tostring)'| grep "true|true" | tr -d "\"" | cut -f1 -d"|")
-    count=$(echo $oldComments | grep true | wc -l)
-    echo "Found $count old comments"
-
-    echo "$oldComments" | while read -r comment ; do
-        echo "Deleting comment: $comment"
-        curl_gh > /dev/null 2>&1 -X DELETE "https://api.github.com/repos/nextcloud/android/issues/comments/$comment"
-    done
-}
-
 upload() {
-    deleteOldComments
+    scripts/deleteOldComments.sh "$BRANCH" "$TYPE" "$PR"
 
     cd $1
 
