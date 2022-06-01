e5ebb56728696b53ef973e6553e2ccf60b9bd4be
Álvaro Brey <alvaro.brey@nextcloud.com>
2022-06-01 10:36:36 +0200

    Reverti emoji-google to 0.9.0 to restore jacoco with gradle 7.2.1
    This reverts commit 08307992924fb8538523b8b33fd4df043db8e341.
    This reverts commit 8f46bd6275ddd119d66ca546eb8000d8de13719e.
    This reverts commit 1c0a189511d8646d4d5f894e29fa7e3bf91e4949.
    This reverts commit b2630f532a52b65f890bb5bb62d246454ffd7fce.
    
    Signed-off-by: Álvaro Brey <alvaro.brey@nextcloud.com>

--- app/src/main/java/com/nextcloud/ui/SetStatusDialogFragment.kt
+++ app/src/main/java/com/nextcloud/ui/SetStatusDialogFragment.kt
@@ -59,8 +59,6 @@ import com.owncloud.android.utils.theme.ThemeTextInputUtils
 import com.vanniktech.emoji.EmojiManager
 import com.vanniktech.emoji.EmojiPopup
 import com.vanniktech.emoji.google.GoogleEmojiProvider
-import com.vanniktech.emoji.installDisableKeyboardInput
-import com.vanniktech.emoji.installForceSingleEmoji
 import java.util.Calendar
 import java.util.Locale
 import javax.inject.Inject
@@ -171,18 +169,18 @@ class SetStatusDialogFragment :
         binding.setStatus.setOnClickListener { setStatusMessage() }
         binding.emoji.setOnClickListener { popup.show() }
 
-        popup = EmojiPopup(
-            view, binding.emoji,
-            onEmojiClickListener = {
+        popup = EmojiPopup.Builder
+            .fromRootView(view)
+            .setOnEmojiClickListener { _, _ ->
                 popup.dismiss()
                 binding.emoji.clearFocus()
                 val imm: InputMethodManager = context?.getSystemService(Context.INPUT_METHOD_SERVICE) as
                     InputMethodManager
                 imm.hideSoftInputFromWindow(binding.emoji.windowToken, 0)
             }
-        )
-        binding.emoji.installDisableKeyboardInput(popup)
-        binding.emoji.installForceSingleEmoji()
+            .build(binding.emoji)
+        binding.emoji.disableKeyboardInput(popup)
+        binding.emoji.forceSingleEmoji()
 
         val adapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_spinner_item)
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
