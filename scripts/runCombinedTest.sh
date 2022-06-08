22deea9e4f16896477b4b45d4977c16f5272ae3c
Álvaro Brey <alvaro.brey@nextcloud.com>
2022-06-08 15:40:37 +0200

    Upload logcat on failing master-IT builds
    Signed-off-by: Álvaro Brey <alvaro.brey@nextcloud.com>

--- scripts/runCombinedTest.sh
+++ scripts/runCombinedTest.sh
@@ -5,6 +5,17 @@ LOG_USERNAME=$2
 LOG_PASSWORD=$3
 DRONE_BUILD_NUMBER=$4
 
+function upload_logcat() {
+    set -x
+    log_filename="${DRONE_PULL_REQUEST}_logcat.txt.gz"
+    log_file="app/build/${log_filename}"
+    upload_path="https://nextcloud.kaminsky.me/remote.php/webdav/android-logcat/$log_filename"
+    adb logcat -d | gzip > "$log_file"
+    curl -u "${LOG_USERNAME}:${LOG_PASSWORD}" -X PUT "$upload_path" --upload-file "$log_file"
+    echo >&2 "Uploaded logcat to https://kaminsky.me/nc-dev/android-logcat/$log_filename"
+    set +x
+}
+
 scripts/deleteOldComments.sh "master" "IT" "$DRONE_PULL_REQUEST"
 
 ./gradlew assembleGplayDebugAndroidTest
@@ -13,10 +24,12 @@ scripts/wait_for_emulator.sh
 
 ./gradlew installGplayDebugAndroidTest
 scripts/wait_for_server.sh "server"
+adb logcat -c
 ./gradlew createGplayDebugCoverageReport -Pcoverage -Pandroid.testInstrumentationRunnerArguments.notAnnotation=com.owncloud.android.utils.ScreenshotTest
 stat=$?
 
 if [ ! $stat -eq 0 ]; then
+    upload_logcat
     bash scripts/uploadReport.sh "$LOG_USERNAME" "$LOG_PASSWORD" "$DRONE_BUILD_NUMBER" "master" "IT" "$DRONE_PULL_REQUEST"
 fi
 
