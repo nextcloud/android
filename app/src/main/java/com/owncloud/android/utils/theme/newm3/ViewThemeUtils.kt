e0d2977c6fea431084061d380162d781be28c559
Álvaro Brey <alvaro.brey@nextcloud.com>
2022-09-06 18:14:36 +0200

    Initial setup for material 3 utils from library
    Signed-off-by: Álvaro Brey <alvaro.brey@nextcloud.com>

---
+++ app/src/main/java/com/owncloud/android/utils/theme/newm3/ViewThemeUtils.kt
@@ -0,0 +1,43 @@
+/*
+ * Nextcloud Talk application
+ *
+ * @author Álvaro Brey
+ * Copyright (C) 2022 Álvaro Brey
+ * Copyright (C) 2022 Nextcloud GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package com.owncloud.android.utils.theme.newm3
+
+import com.nextcloud.android.common.ui.theme.MaterialSchemes
+import com.nextcloud.android.common.ui.theme.ViewThemeUtilsBase
+import com.nextcloud.android.common.ui.theme.utils.AndroidViewThemeUtils
+import com.nextcloud.android.common.ui.theme.utils.AndroidXViewThemeUtils
+import com.nextcloud.android.common.ui.theme.utils.DialogViewThemeUtils
+import com.nextcloud.android.common.ui.theme.utils.MaterialViewThemeUtils
+import javax.inject.Inject
+
+@Suppress("TooManyFunctions")
+class ViewThemeUtils @Inject constructor(
+    schemes: MaterialSchemes,
+    @JvmField
+    val platform: AndroidViewThemeUtils,
+    @JvmField
+    val material: MaterialViewThemeUtils,
+    @JvmField
+    val androidx: AndroidXViewThemeUtils,
+    @JvmField
+    val dialog: DialogViewThemeUtils
+) : ViewThemeUtilsBase(schemes)
