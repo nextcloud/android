e969024a11cae86830e03d8abd997b876ae45d8b
Álvaro Brey <alvaro.brey@nextcloud.com>
2022-09-12 17:03:12 +0200

    Replace ThemeTextUtils with new FilesSpecificViewThemeUtils class using material3 schemes
    Signed-off-by: Álvaro Brey <alvaro.brey@nextcloud.com>

---
+++ app/src/main/java/com/owncloud/android/utils/theme/newm3/FilesSpecificViewThemeUtils.kt
@@ -0,0 +1,47 @@
+/*
+ * Nextcloud Android client application
+ *
+ *  @author Álvaro Brey
+ *  Copyright (C) 2022 Álvaro Brey
+ *  Copyright (C) 2022 Nextcloud GmbH
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package com.owncloud.android.utils.theme.newm3
+
+import android.preference.PreferenceCategory
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import com.nextcloud.android.common.ui.theme.MaterialSchemes
+import com.nextcloud.android.common.ui.theme.ViewThemeUtilsBase
+import javax.inject.Inject
+
+class FilesSpecificViewThemeUtils @Inject constructor(schemes: MaterialSchemes) : ViewThemeUtilsBase(schemes) {
+    // not ported to common lib because PreferenceCategory is deprecated
+    fun themePreferenceCategory(category: PreferenceCategory) {
+        withScheme(category.context) {
+            val text: Spannable = SpannableString(category.title)
+            text.setSpan(
+                ForegroundColorSpan(it.primary),
+                0,
+                text.length,
+                Spannable.SPAN_INCLUSIVE_INCLUSIVE
+            )
+            category.title = text
+        }
+    }
+}
