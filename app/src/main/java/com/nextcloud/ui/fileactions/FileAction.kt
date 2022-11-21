e6c014d8328f652b659717d60c2c89f3c2fc7666
Felix Nüsse <felix.nuesse@t-online.de>
2022-11-21 13:39:08 +0100

    Update app/src/main/java/com/nextcloud/ui/fileactions/FileAction.kt
    Co-authored-by: Álvaro Brey <alvaro.brey@nextcloud.com>
    Signed-off-by: Felix Nüsse <felix.nuesse@t-online.de>

--- app/src/main/java/com/nextcloud/ui/fileactions/FileAction.kt
+++ app/src/main/java/com/nextcloud/ui/fileactions/FileAction.kt
@@ -66,6 +66,8 @@ enum class FileAction(@IdRes val id: Int, @StringRes val title: Int, @DrawableRe
     // locks
     UNLOCK_FILE(R.id.action_unlock_file, R.string.unlock_file, R.drawable.ic_lock_open_white),
     LOCK_FILE(R.id.action_lock_file, R.string.lock_file, R.drawable.ic_lock),
+
+    // Shortcuts
     PIN_TO_HOMESCREEN(R.id.action_pin_to_homescreen, R.string.pin_home, R.drawable.add_to_home_screen);
 
     companion object {
