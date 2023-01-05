e1513246b7aab4573d12b47e3fe11437464d870c
tobiasKaminsky <tobias@kaminsky.me>
2023-01-05 09:03:13 +0100

    add tests Rename .java to .kt only show secure filedrop / sharing on NC26+ Refactor EncryptionUtils to avoid having test arguments on public method
    Added secure filedrop
    
    Signed-off-by: Álvaro Brey <alvaro.brey@nextcloud.com>
    Signed-off-by: Tobias Kaminsky <tobias@nextcloud.com>

--- app/src/main/java/com/owncloud/android/operations/RemoveRemoteEncryptedFileOperation.java
+++ app/src/main/java/com/owncloud/android/operations/RemoveRemoteEncryptedFileOperation.java
@@ -47,6 +47,7 @@ import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
 import java.security.spec.InvalidKeySpecException;
 
 import javax.crypto.BadPaddingException;
