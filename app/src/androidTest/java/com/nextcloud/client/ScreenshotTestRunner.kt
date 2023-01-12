07d2a0c91bf219ac12e546321b776759e789754f
Álvaro Brey <alvaro.brey@nextcloud.com>
2023-01-12 17:11:45 +0100

    Move test runner back to com.nextcloud.client package
    For some reason, Dexopener uses the package of the runner to determine which packages to open.
    Which means with the new location, mockk can't work on android < P
    
    Ref: https://github.com/tmurakami/dexopener/issues/24
    
    Signed-off-by: Álvaro Brey <alvaro.brey@nextcloud.com>

--- app/src/androidTest/java/com/nextcloud/client/ScreenshotTestRunner.kt
+++ app/src/androidTest/java/com/nextcloud/client/ScreenshotTestRunner.kt
@@ -1,27 +1,27 @@
 /*
- *
  * Nextcloud Android client application
  *
- * @author Tobias Kaminsky
- * @author Álvaro Brey
- * Copyright (C) 2019 Tobias Kaminsky
- * Copyright (C) 2023 Álvaro Brey
- * Copyright (C) 2023 Nextcloud GmbH
+ *  @author Tobias Kaminsky
+ *  @author Álvaro Brey
+ *  Copyright (C) 2019 Tobias Kaminsky
+ *  Copyright (C) 2023 Álvaro Brey
+ *  Copyright (C) 2023 Nextcloud GmbH
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
-package com.nextcloud.test
+package com.nextcloud.client
 
 import android.app.Application
 import android.app.Instrumentation
@@ -29,6 +29,7 @@ import android.content.Context
 import android.os.Build
 import com.github.tmurakami.dexopener.DexOpener
 import com.karumi.shot.ShotTestRunner
+import com.nextcloud.test.TestMainApp
 
 class ScreenshotTestRunner : ShotTestRunner() {
     @Throws(ClassNotFoundException::class, IllegalAccessException::class, InstantiationException::class)
