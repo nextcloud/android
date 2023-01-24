29bcfd07fd422201225b37a973b17712b0818846
Álvaro Brey <alvaro.brey@nextcloud.com>
2023-01-24 11:21:53 +0100

    NextcloudDatabase: 67 to 68 is a manual migration
    Signed-off-by: Álvaro Brey <alvaro.brey@nextcloud.com>

--- app/src/main/java/com/nextcloud/client/database/NextcloudDatabase.kt
+++ app/src/main/java/com/nextcloud/client/database/NextcloudDatabase.kt
@@ -60,8 +60,7 @@ import com.owncloud.android.db.ProviderMeta
     version = ProviderMeta.DB_VERSION,
     autoMigrations = [
         AutoMigration(from = 65, to = 66),
-        AutoMigration(from = 66, to = 67),
-        AutoMigration(from = 67, to = 68)
+        AutoMigration(from = 66, to = 67)
     ],
     exportSchema = true
 )
