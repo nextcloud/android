2e6fca0c04d2fe1b651cbdca58e9be6ab217576a
DESKTOP-EMSE65H\Archontis <arxontisk02@gmail.com>
2023-02-25 01:39:52 +0200

    Refactor onCreate: extract initTaskRetainerFragment method
    This commit extracts the logic to initialize a fragment without UI on a seperate method called `initTaskRetainerFragment`.

--- app/src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.java
+++ app/src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.java
@@ -255,17 +255,7 @@ public class FileDisplayActivity extends FileActivity
         /// USER INTERFACE
         initLayout();
         initUI();
-
-
-        // Init Fragment without UI to retain AsyncTask across configuration changes
-        FragmentManager fm = getSupportFragmentManager();
-        TaskRetainerFragment taskRetainerFragment =
-            (TaskRetainerFragment) fm.findFragmentByTag(TaskRetainerFragment.FTAG_TASK_RETAINER_FRAGMENT);
-        if (taskRetainerFragment == null) {
-            taskRetainerFragment = new TaskRetainerFragment();
-            fm.beginTransaction()
-                .add(taskRetainerFragment, TaskRetainerFragment.FTAG_TASK_RETAINER_FRAGMENT).commit();
-        }   // else, Fragment already created and retained across configuration change
+        initTaskRetainerFragment();
 
         if (Intent.ACTION_VIEW.equals(getIntent().getAction())) {
             handleOpenFileViaIntent(getIntent());
@@ -303,6 +293,18 @@ public class FileDisplayActivity extends FileActivity
         mMenuButton.setOnClickListener(v -> openDrawer());
         mSwitchAccountButton.setOnClickListener(v -> showManageAccountsDialog());
         fastScrollUtils.fixAppBarForFastScroll(binding.appbar.appbar, binding.rootLayout);
+    }
+
+    private void initTaskRetainerFragment() {
+        // Init Fragment without UI to retain AsyncTask across configuration changes
+        FragmentManager fm = getSupportFragmentManager();
+        TaskRetainerFragment taskRetainerFragment =
+            (TaskRetainerFragment) fm.findFragmentByTag(TaskRetainerFragment.FTAG_TASK_RETAINER_FRAGMENT);
+        if (taskRetainerFragment == null) {
+            taskRetainerFragment = new TaskRetainerFragment();
+            fm.beginTransaction()
+                .add(taskRetainerFragment, TaskRetainerFragment.FTAG_TASK_RETAINER_FRAGMENT).commit();
+        }   // else, Fragment already created and retained across configuration change
     }
 
     private void checkStoragePath() {
