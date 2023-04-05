ccf0ee6d0e6ccfb437d8afb89d443fb8b003726f
A117870935 <surinder.kumar@t-systems.com>
2023-04-06 02:15:14 +0530

    Test cases added for launcher.
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

---
+++ app/src/androidTest/java/com/nmc/android/ui/LauncherActivityIT.kt
@@ -0,0 +1,28 @@
+package com.nmc.android.ui
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.owncloud.android.AbstractIT
+import com.owncloud.android.R
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class LauncherActivityIT : AbstractIT() {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(LauncherActivity::class.java)
+
+    @Test
+    fun verifyUIElements() {
+        waitForIdleSync()
+        onView(withId(R.id.ivSplash)).check(matches(isCompletelyDisplayed()))
+        onView(withId(R.id.tvSplash)).check(matches(isCompletelyDisplayed()))
+    }
+}
