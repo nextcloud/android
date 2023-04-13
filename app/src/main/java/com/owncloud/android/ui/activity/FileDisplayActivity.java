b1a1cf3c36ffab6718fc45e40a0668ba6d6cf8a4
A117870935 <surinder.kumar@t-systems.com>
2023-04-13 19:28:16 +0530

    Copyright updated.

--- app/src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.java
+++ app/src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.java
@@ -11,7 +11,7 @@
  * Copyright (C) 2016 ownCloud Inc.
  * Copyright (C) 2018 Andy Scherzinger
  * Copyright (C) 2019 Chris Narkiewicz <hello@ezaquarii.com>
- * Copyright (C) 2022 TSI-mc
+ * Copyright (C) 2023 TSI-mc
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2,
