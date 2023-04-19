905222c930831171068d0b1291115eb8b539db56
A200073727 <akshay.chaurasia@t-systems.com>
2023-04-19 15:35:40 +0530

    After  screen refresh search is no longer collapsing.

--- app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
+++ app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
@@ -1340,17 +1340,6 @@ public class OCFileListFragment extends ExtendedListFragment implements
                     }
                 }
 
-                if (searchView != null && !searchView.isIconified() && !fromSearch) {
-                    searchView.post(() -> {
-                        searchView.setQuery("", false);
-                        Activity activity;
-                        if ((activity = getActivity()) != null && activity instanceof FileDisplayActivity) {
-                            FileDisplayActivity fileDisplayActivity = (FileDisplayActivity) activity;
-                            fileDisplayActivity.hideSearchView(fileDisplayActivity.getCurrentDir());
-                        }
-                    });
-                }
-
                 mAdapter.swapDirectory(
                     accountManager.getUser(),
                     directory,
