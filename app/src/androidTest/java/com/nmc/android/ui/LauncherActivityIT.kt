aa199088dfeafdb70f70f198dd4705bf053044ec
A117870935 <surinder.kumar@t-systems.com>
2023-04-19 19:14:28 +0530

    Splash text updated, logo removed and test cases updated.
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- app/src/androidTest/java/com/nmc/android/ui/LauncherActivityIT.kt
+++ app/src/androidTest/java/com/nmc/android/ui/LauncherActivityIT.kt
@@ -4,6 +4,7 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isCompletelyDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.owncloud.android.AbstractIT
@@ -23,6 +24,10 @@ class LauncherActivityIT : AbstractIT() {
     fun verifyUIElements() {
         waitForIdleSync()
         onView(withId(R.id.ivSplash)).check(matches(isCompletelyDisplayed()))
-        onView(withId(R.id.tvSplash)).check(matches(isCompletelyDisplayed()))
+        onView(withId(R.id.splashScreenBold)).check(matches(isCompletelyDisplayed()))
+        onView(withId(R.id.splashScreenNormal)).check(matches(isCompletelyDisplayed()))
+
+        onView(withId(R.id.splashScreenBold)).check(matches(withText("Magenta")))
+        onView(withId(R.id.splashScreenNormal)).check(matches(withText("CLOUD")))
     }
 }
