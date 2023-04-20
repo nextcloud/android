f9761628fefa0afec9efb48f619683d5db76620a
A117870935 <surinder.kumar@t-systems.com>
2023-04-20 20:17:07 +0530

    Copyright added.
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- app/src/main/java/com/owncloud/android/ui/fragment/contactsbackup/BackupFragment.java
+++ app/src/main/java/com/owncloud/android/ui/fragment/contactsbackup/BackupFragment.java
@@ -2,8 +2,10 @@
  * Nextcloud Android client application
  *
  * @author Mario Danic
+ * @author TSI-mc
  * Copyright (C) 2017 Mario Danic
  * Copyright (C) 2017 Nextcloud GmbH.
+ * Copyright (C) 2023 TSI-mc
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
