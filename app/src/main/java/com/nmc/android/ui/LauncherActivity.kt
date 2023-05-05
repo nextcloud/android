25b61a38aca8265e0efea2e39448615dee088de9
Andy Scherzinger <info@andy-scherzinger.de>
2023-05-05 12:05:04 +0200

    enable "logo-only" splashscreen
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- app/src/main/java/com/nmc/android/ui/LauncherActivity.kt
+++ app/src/main/java/com/nmc/android/ui/LauncherActivity.kt
@@ -36,43 +36,37 @@ import javax.inject.Inject
 
 class LauncherActivity : BaseActivity() {
 
-    companion object {
-        const val SPLASH_DURATION = 1500L
-    }
-
-    private lateinit var binding : ActivitySplashBinding
+    private lateinit var binding: ActivitySplashBinding
 
     @Inject
     lateinit var appPreferences: AppPreferences
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        //Mandatory to call this before super method to show system launch screen for api level 31+
+        // Mandatory to call this before super method to show system launch screen for api level 31+
         installSplashScreen()
 
-        //flag to avoid redirection to AuthenticatorActivity
-        //as we need this activity to be shown
-        //Note: Should be kept before super() method
-        enableAccountHandling = false
         super.onCreate(savedInstanceState)
 
         binding = ActivitySplashBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
-        updateBoldTitleVisibility()
+        updateTitleVisibility()
         scheduleSplashScreen()
     }
 
-    private fun updateBoldTitleVisibility() {
-        //For NC this will be empty so to handle that case we have added this check
+    private fun updateTitleVisibility() {
         if (TextUtils.isEmpty(resources.getString(R.string.splashScreenBold))) {
             binding.splashScreenBold.visibility = View.GONE
+        }
+        if (TextUtils.isEmpty(resources.getString(R.string.splashScreenNormal))) {
+            binding.splashScreenNormal.visibility = View.GONE
         }
     }
 
     private fun scheduleSplashScreen() {
         Handler().postDelayed(
             {
-                //if user is null then go to authenticator activity
+                // if user is null then go to authenticator activity
                 if (!user.isPresent) {
                     startActivity(Intent(this, AuthenticatorActivity::class.java))
                 } else {
@@ -82,5 +76,9 @@ class LauncherActivity : BaseActivity() {
             },
             SPLASH_DURATION
         )
+    }
+
+    companion object {
+        const val SPLASH_DURATION = 1500L
     }
 }
