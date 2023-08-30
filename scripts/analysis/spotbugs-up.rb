5e62f4b4d835b181fa2f01280b7420193db86ef2
tobiasKaminsky <tobias@kaminsky.me>
2023-08-30 10:11:50 +0200

    Verbose to see why spotbugs does not run on GARM
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- scripts/analysis/spotbugs-up.rb
+++ scripts/analysis/spotbugs-up.rb
@@ -15,7 +15,7 @@ require 'open3'
 
 # run Spotbugs
 puts "running Spotbugs..."
-system './gradlew spotbugsGplayDebug 1>/dev/null 2>&1'
+system './gradlew spotbugsGplayDebug'
 
 # find number of warnings
 current_warning_count = `./scripts/analysis/spotbugsSummary.py --total`.to_i
