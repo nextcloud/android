c84a1da6faba5512ac4bc6c4b60ad4af346032a0
parneet-guraya <gurayaparneet@gmail.com>
2023-09-03 04:01:46 +0530

    fix warning
    Signed-off-by: parneet-guraya <gurayaparneet@gmail.com>

--- app/src/main/java/com/owncloud/android/ui/preview/PreviewMediaFragment.java
+++ app/src/main/java/com/owncloud/android/ui/preview/PreviewMediaFragment.java
@@ -374,9 +374,7 @@ public class PreviewMediaFragment extends FileFragment implements
                                 playVideo();
                             });
                         } catch (ClientFactory.CreationException e) {
-                            handler.post(() -> {
-                                Log_OC.e(TAG, "error setting up ExoPlayer", e);
-                            });
+                            handler.post(() -> Log_OC.e(TAG, "error setting up ExoPlayer", e));
                         }
                     });
                 }
