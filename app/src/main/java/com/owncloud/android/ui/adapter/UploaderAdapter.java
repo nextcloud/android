aee3ffd7da201491ad165e52aa65bd847a0abe5c
Alper Ozturk <alperozturk@lions-macbook.local>
2023-10-03 14:37:26 +0200

    Change drawable to layer drawable
    Signed-off-by: Alper Ozturk <alperozturk@lions-macbook.local>
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/ui/adapter/UploaderAdapter.java
+++ app/src/main/java/com/owncloud/android/ui/adapter/UploaderAdapter.java
@@ -23,6 +23,7 @@ package com.owncloud.android.ui.adapter;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -105,7 +106,7 @@ public class UploaderAdapter extends SimpleAdapter {
 
         if (file.isFolder()) {
             boolean isAutoUploadFolder = SyncedFolderProvider.isAutoUploadFolder(syncedFolderProvider, file, user);
-            final Drawable icon = file.getFileLayerDrawable(isAutoUploadFolder, mContext, viewThemeUtils);
+            final LayerDrawable icon = file.getFileLayerDrawable(isAutoUploadFolder, mContext, viewThemeUtils);
             fileIcon.setImageDrawable(icon);
         } else {
             // get Thumbnail if file is image
