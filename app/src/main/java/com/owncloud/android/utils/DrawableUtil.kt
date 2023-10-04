be543f66d4bc916be498f966845a00c600154c21
Alper Ozturk <alperozturk@lions-macbook.local>
2023-10-04 15:41:40 +0200

    Change top margin to 3 due to ss tests
    Signed-off-by: Alper Ozturk <alperozturk@lions-macbook.local>
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/utils/DrawableUtil.kt
+++ app/src/main/java/com/owncloud/android/utils/DrawableUtil.kt
@@ -6,7 +6,7 @@ import android.view.Gravity
 
 class DrawableUtil {
 
-    fun addDrawableAsOverlay(backgroundDrawable: Drawable, overlayDrawable: Drawable, topMargin: Int = 4): LayerDrawable {
+    fun addDrawableAsOverlay(backgroundDrawable: Drawable, overlayDrawable: Drawable, topMargin: Int = 3): LayerDrawable {
         val layerDrawable = LayerDrawable(arrayOf(backgroundDrawable, overlayDrawable))
         layerDrawable.setLayerGravity(1, Gravity.CENTER)
         layerDrawable.setLayerInsetTop(1, topMargin)
