88de0a2bd26f85a766de7a36aa8fe8a380f07a00
Alper Ozturk <alperozturk@lions-macbook.local>
2023-10-04 15:33:20 +0200

    Change top margin to 4
    Signed-off-by: Alper Ozturk <alperozturk@lions-macbook.local>
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/utils/DrawableUtil.kt
+++ app/src/main/java/com/owncloud/android/utils/DrawableUtil.kt
@@ -6,7 +6,7 @@ import android.view.Gravity
 
 class DrawableUtil {
 
-    fun addDrawableAsOverlay(backgroundDrawable: Drawable, overlayDrawable: Drawable, topMargin: Int = 6): LayerDrawable {
+    fun addDrawableAsOverlay(backgroundDrawable: Drawable, overlayDrawable: Drawable, topMargin: Int = 4): LayerDrawable {
         val layerDrawable = LayerDrawable(arrayOf(backgroundDrawable, overlayDrawable))
         layerDrawable.setLayerGravity(1, Gravity.CENTER)
         layerDrawable.setLayerInsetTop(1, topMargin)
