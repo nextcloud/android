6ae12b0e49f14a2d283b69ce76c18581560dc648
Alper Ozturk <alperozturk@lions-macbook.local>
2023-10-05 09:53:18 +0200

    Add extensions package under utils
    Signed-off-by: Alper Ozturk <alperozturk@lions-macbook.local>
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/utils/extensions/DisplayMetricsExtensions.kt
+++ app/src/main/java/com/nextcloud/utils/extensions/DisplayMetricsExtensions.kt
@@ -1,2 +1,7 @@
 package com.nextcloud.utils.extensions
 
+import android.util.DisplayMetrics
+
+fun DisplayMetrics.isLowDensityScreen(): Boolean {
+    return this.density < 2
+}
--- app/src/main/java/com/nextcloud/utils/extensions/LayerDrawableExtensions.kt
+++ app/src/main/java/com/nextcloud/utils/extensions/LayerDrawableExtensions.kt
@@ -1,2 +1,10 @@
 package com.nextcloud.utils.extensions
 
+import android.graphics.drawable.LayerDrawable
+
+fun LayerDrawable.setLayerSizeWithInsetTop(index: Int, size: Int, margin: Int) {
+    this.apply {
+        setLayerSize(index, size, size)
+        setLayerInsetTop(index, margin)
+    }
+}
