a94940a0715f7649a37a2c573a6bfec217f4d4c6
alperozturk <alper_ozturk@proton.me>
2023-11-02 14:12:05 +0100

    Convert AuthenticatorUrlUtils to kt
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/authentication/AuthenticatorUrlUtils.kt
+++ app/src/main/java/com/owncloud/android/authentication/AuthenticatorUrlUtils.kt
@@ -18,94 +18,60 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.owncloud.android.authentication
 
-package com.owncloud.android.authentication;
-
-import android.text.TextUtils;
-
-import java.net.URI;
-import java.util.Locale;
+import java.net.URI
 
 /**
  * Helper class for authenticator-URL related logic.
  */
-public final class AuthenticatorUrlUtils {
-    public static final String WEBDAV_PATH_4_0_AND_LATER = "/remote.php/webdav";
+object AuthenticatorUrlUtils {
 
-    private static final String HTTPS_PROTOCOL = "https://";
-    private static final String HTTP_PROTOCOL = "http://";
+    const val WEBDAV_PATH_4_0_AND_LATER = "/remote.php/webdav"
 
-    private AuthenticatorUrlUtils() {
-    }
-
-    public static String normalizeUrlSuffix(String url) {
-        String normalizedUrl = url;
+    fun normalizeUrlSuffix(url: String): String {
+        var normalizedUrl = url
         if (normalizedUrl.endsWith("/")) {
-            normalizedUrl = normalizedUrl.substring(0, normalizedUrl.length() - 1);
+            normalizedUrl = normalizedUrl.substring(0, normalizedUrl.length - 1)
         }
-        return trimUrlWebdav(normalizedUrl);
+        return trimUrlWebdav(normalizedUrl)
     }
 
-    public static String normalizeUrl(String url, boolean sslWhenUnprefixed) {
-        String normalizedUrl = url;
-
-        if (!TextUtils.isEmpty(normalizedUrl)) {
-            normalizedUrl = normalizedUrl.trim();
-
-            if (!normalizedUrl.toLowerCase(Locale.ROOT).startsWith(HTTP_PROTOCOL) &&
-                !normalizedUrl.toLowerCase(Locale.ROOT).startsWith(HTTPS_PROTOCOL)) {
-                if (sslWhenUnprefixed) {
-                    normalizedUrl = HTTPS_PROTOCOL + normalizedUrl;
-                } else {
-                    normalizedUrl = HTTP_PROTOCOL + normalizedUrl;
-                }
-            }
-
-            normalizedUrl = normalizeUrlSuffix(normalizedUrl);
-        }
-        return normalizedUrl != null ? normalizedUrl : "";
-    }
-
-    public static String trimWebdavSuffix(String url) {
-        String trimmedUrl = url;
+    fun trimWebdavSuffix(url: String): String {
+        var trimmedUrl = url
         while (trimmedUrl.endsWith("/")) {
-            trimmedUrl = trimmedUrl.substring(0, url.length() - 1);
+            trimmedUrl = trimmedUrl.substring(0, url.length - 1)
         }
-
-        int pos = trimmedUrl.lastIndexOf(WEBDAV_PATH_4_0_AND_LATER);
+        val pos = trimmedUrl.lastIndexOf(WEBDAV_PATH_4_0_AND_LATER)
         if (pos >= 0) {
-            trimmedUrl = trimmedUrl.substring(0, pos);
-
+            trimmedUrl = trimmedUrl.substring(0, pos)
         }
-        return trimmedUrl;
+        return trimmedUrl
     }
 
-    private static String trimUrlWebdav(String url) {
-        if (url.toLowerCase(Locale.ROOT).endsWith(WEBDAV_PATH_4_0_AND_LATER)) {
-            return url.substring(0, url.length() - WEBDAV_PATH_4_0_AND_LATER.length());
-        }
-
-        return url;
+    private fun trimUrlWebdav(url: String): String {
+        return if (url.lowercase().endsWith(WEBDAV_PATH_4_0_AND_LATER)) {
+            url.substring(0, url.length - WEBDAV_PATH_4_0_AND_LATER.length)
+        } else url
     }
 
-    public static String stripIndexPhpOrAppsFiles(String url) {
-        String strippedUrl = url;
+    fun stripIndexPhpOrAppsFiles(url: String): String {
+        var strippedUrl = url
         if (strippedUrl.endsWith("/index.php")) {
-            strippedUrl = strippedUrl.substring(0, strippedUrl.lastIndexOf("/index.php"));
+            strippedUrl = strippedUrl.substring(0, strippedUrl.lastIndexOf("/index.php"))
         } else if (strippedUrl.contains("/index.php/apps/")) {
-            strippedUrl = strippedUrl.substring(0, strippedUrl.lastIndexOf("/index.php/apps/"));
+            strippedUrl = strippedUrl.substring(0, strippedUrl.lastIndexOf("/index.php/apps/"))
         }
-
-        return strippedUrl;
+        return strippedUrl
     }
 
-    public static String normalizeScheme(String url) {
-        if (url.matches("[a-zA-Z][a-zA-Z0-9+.-]+://.+")) {
-            URI uri = URI.create(url);
-            String lcScheme = uri.getScheme().toLowerCase(Locale.ROOT);
-            return String.format("%s:%s", lcScheme, uri.getRawSchemeSpecificPart());
+    fun normalizeScheme(url: String): String {
+        return if (url.matches("[a-zA-Z][a-zA-Z0-9+.-]+://.+".toRegex())) {
+            val uri = URI.create(url)
+            val lcScheme = uri.scheme.lowercase()
+            String.format("%s:%s", lcScheme, uri.rawSchemeSpecificPart)
         } else {
-            return url;
+            url
         }
     }
 }
