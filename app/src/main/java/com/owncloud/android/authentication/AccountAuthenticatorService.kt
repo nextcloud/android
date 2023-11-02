36416d972f282b2d62cfb9b29547e4469552998e
alperozturk <alper_ozturk@proton.me>
2023-11-02 12:19:41 +0100

    Convert AccountAuthenticatorService to kt
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/authentication/AccountAuthenticatorService.kt
+++ app/src/main/java/com/owncloud/android/authentication/AccountAuthenticatorService.kt
@@ -1,42 +1,37 @@
 /**
- *   ownCloud Android client application
+ * ownCloud Android client application
  *
- *   Copyright (C) 2011  Bartek Przybylski
- *   Copyright (C) 2015 ownCloud Inc.
+ * Copyright (C) 2011  Bartek Przybylski
+ * Copyright (C) 2015 ownCloud Inc.
  *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License version 2,
- *   as published by the Free Software Foundation.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2,
+ * as published by the Free Software Foundation.
  *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http:></http:>//www.gnu.org/licenses/>.
  *
  */
+package com.owncloud.android.authentication
 
-package com.owncloud.android.authentication;
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
 
-import android.app.Service;
-import android.content.Intent;
-import android.os.IBinder;
+class AccountAuthenticatorService : Service() {
+    private var mAuthenticator: AccountAuthenticator? = null
 
-public class AccountAuthenticatorService extends Service {
-
-    private AccountAuthenticator mAuthenticator;
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
-        mAuthenticator = new AccountAuthenticator(this);
+    override fun onCreate() {
+        super.onCreate()
+        mAuthenticator = AccountAuthenticator(this)
     }
 
-    @Override
-    public IBinder onBind(Intent intent) {
-        return mAuthenticator.getIBinder();
+    override fun onBind(intent: Intent): IBinder? {
+        return mAuthenticator?.iBinder
     }
-
 }
