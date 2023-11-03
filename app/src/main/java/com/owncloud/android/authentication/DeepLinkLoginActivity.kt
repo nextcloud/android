3a6374755fccfb4b11945e3a89c26cc389c3b08c
alperozturk <alper_ozturk@proton.me>
2023-11-03 15:13:54 +0100

    Convert to kt
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/authentication/DeepLinkLoginActivity.kt
+++ app/src/main/java/com/owncloud/android/authentication/DeepLinkLoginActivity.kt
@@ -1,39 +1,36 @@
-package com.owncloud.android.authentication;
+package com.owncloud.android.authentication
 
-import android.net.Uri;
-import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
+import com.nextcloud.client.di.Injectable
+import com.owncloud.android.R
 
-import com.nextcloud.client.di.Injectable;
-import com.owncloud.android.R;
+class DeepLinkLoginActivity : AuthenticatorActivity(), Injectable {
 
-public class DeepLinkLoginActivity extends AuthenticatorActivity implements Injectable {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        if (!getResources().getBoolean(R.bool.multiaccount_support) &&
-            accountManager.getAccounts().length == 1) {
-            Toast.makeText(this, R.string.no_mutliple_accounts_allowed, Toast.LENGTH_LONG).show();
-            return;
+        if (!resources.getBoolean(R.bool.multiaccount_support) &&
+            accountManager.accounts.size == 1
+        ) {
+            Toast.makeText(this, R.string.no_mutliple_accounts_allowed, Toast.LENGTH_LONG).show()
+            return
         }
 
-        setContentView(R.layout.deep_link_login);
+        setContentView(R.layout.deep_link_login)
 
-        Uri data = getIntent().getData();
-
-        if (data != null) {
+        intent.data?.let {
             try {
-                String prefix = getString(R.string.login_data_own_scheme) + PROTOCOL_SUFFIX + "login/";
-                LoginUrlInfo loginUrlInfo = parseLoginDataUrl(prefix, data.toString());
-
-                TextView loginText = findViewById(R.id.loginInfo);
-                loginText.setText(String.format(getString(R.string.direct_login_text), loginUrlInfo.username,
-                                                loginUrlInfo.serverAddress));
-            } catch (IllegalArgumentException e) {
-                Toast.makeText(this, R.string.direct_login_failed, Toast.LENGTH_LONG).show();
+                val prefix = getString(R.string.login_data_own_scheme) + PROTOCOL_SUFFIX + "login/"
+                val loginUrlInfo = parseLoginDataUrl(prefix, it.toString())
+                val loginText = findViewById<TextView>(R.id.loginInfo)
+                loginText.text = String.format(
+                    getString(R.string.direct_login_text), loginUrlInfo.username,
+                    loginUrlInfo.serverAddress
+                )
+            } catch (e: IllegalArgumentException) {
+                Toast.makeText(this, R.string.direct_login_failed, Toast.LENGTH_LONG).show()
             }
         }
     }
