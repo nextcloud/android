456974f6971083fb2e35d658f02838ca87c352c9
alperozturk <alper_ozturk@proton.me>
2023-11-15 14:13:19 +0100

    Convert to kt
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/ui/activity/ContactsPreferenceActivity.kt
+++ app/src/main/java/com/owncloud/android/ui/activity/ContactsPreferenceActivity.kt
@@ -20,129 +20,131 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.owncloud.android.ui.activity;
+package com.owncloud.android.ui.activity
 
-import android.content.Context;
-import android.content.Intent;
-import android.os.Bundle;
-
-import com.nextcloud.client.account.User;
-import com.nextcloud.client.jobs.BackgroundJobManager;
-import com.owncloud.android.R;
-import com.owncloud.android.datamodel.OCFile;
-import com.owncloud.android.ui.fragment.FileFragment;
-import com.owncloud.android.ui.fragment.contactsbackup.BackupFragment;
-import com.owncloud.android.ui.fragment.contactsbackup.BackupListFragment;
-
-import javax.inject.Inject;
-
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import android.content.Context
+import android.content.Intent
+import android.os.Bundle
+import android.os.Parcelable
+import androidx.activity.OnBackPressedCallback
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.FragmentManager
+import com.nextcloud.client.account.User
+import com.owncloud.android.R
+import com.owncloud.android.datamodel.OCFile
+import com.owncloud.android.ui.fragment.FileFragment
+import com.owncloud.android.ui.fragment.contactsbackup.BackupFragment
+import com.owncloud.android.ui.fragment.contactsbackup.BackupFragment.Companion.create
+import com.owncloud.android.ui.fragment.contactsbackup.BackupListFragment
 
 /**
  * This activity shows all settings for contact backup/restore
  */
-public class ContactsPreferenceActivity extends FileActivity implements FileFragment.ContainerActivity {
-    public static final String TAG = ContactsPreferenceActivity.class.getSimpleName();
-    public static final String EXTRA_FILE = "FILE";
-    public static final String EXTRA_USER = "USER";
-    /**
-     * Warning: default for this extra is different between this activity and {@link BackupFragment}
-     */
-    public static final String EXTRA_SHOW_SIDEBAR = "SHOW_SIDEBAR";
-    public static final String PREFERENCE_CONTACTS_AUTOMATIC_BACKUP = "PREFERENCE_CONTACTS_AUTOMATIC_BACKUP";
-    public static final String PREFERENCE_CONTACTS_LAST_BACKUP = "PREFERENCE_CONTACTS_LAST_BACKUP";
-    public static final String BACKUP_TO_LIST = "BACKUP_TO_LIST";
+class ContactsPreferenceActivity : FileActivity(), FileFragment.ContainerActivity {
 
-    @Inject BackgroundJobManager backgroundJobManager;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.contacts_preference)
 
-    public static void startActivity(Context context) {
-        Intent intent = new Intent(context, ContactsPreferenceActivity.class);
-        context.startActivity(intent);
-    }
+        setupToolbar()
 
-    public static void startActivityWithContactsFile(Context context, User user, OCFile file) {
-        Intent intent = new Intent(context, ContactsPreferenceActivity.class);
-        intent.putExtra(EXTRA_FILE, file);
-        intent.putExtra(EXTRA_USER, user);
-        context.startActivity(intent);
-    }
+        // TODO needed?
+        // setupDrawer(R.id.nav_contacts);
 
-    public static void startActivityWithoutSidebar(Context context) {
-        Intent intent = new Intent(context, ContactsPreferenceActivity.class);
-        intent.putExtra(EXTRA_SHOW_SIDEBAR, false);
-        context.startActivity(intent);
-    }
+        val sidebar = showSidebar()
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.contacts_preference);
-
-        // setup toolbar
-        setupToolbar();
-
-        // setup drawer
-        //setupDrawer(R.id.nav_contacts); // TODO needed?
-
-        // show sidebar?
-        boolean showSidebar = getIntent().getBooleanExtra(EXTRA_SHOW_SIDEBAR, true);
-        if (!showSidebar) {
-            setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED);
-
-            if (getSupportActionBar() != null) {
-                getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-            }
-
-            if (mDrawerToggle != null) {
-                mDrawerToggle.setDrawerIndicatorEnabled(false);
-            }
-        }
-
-        Intent intent = getIntent();
+        val intent = intent
         if (savedInstanceState == null) {
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            if (intent == null || intent.getParcelableExtra(EXTRA_FILE) == null ||
-                intent.getParcelableExtra(EXTRA_USER) == null) {
-                BackupFragment fragment = BackupFragment.create(showSidebar);
-                transaction.add(R.id.frame_container, fragment);
+            val transaction = supportFragmentManager.beginTransaction()
+            if (intent?.getParcelableExtra<Parcelable?>(EXTRA_FILE) == null ||
+                intent.getParcelableExtra<Parcelable?>(EXTRA_USER) == null
+            ) {
+                val fragment = create(sidebar)
+                transaction.add(R.id.frame_container, fragment)
             } else {
-                OCFile file = intent.getParcelableExtra(EXTRA_FILE);
-                User user = intent.getParcelableExtra(EXTRA_USER);
-                BackupListFragment contactListFragment = BackupListFragment.newInstance(file, user);
-                transaction.add(R.id.frame_container, contactListFragment);
+                val file = intent.getParcelableExtra<OCFile>(EXTRA_FILE)
+                val user = intent.getParcelableExtra<User>(EXTRA_USER)
+                val contactListFragment = BackupListFragment.newInstance(file, user)
+                transaction.add(R.id.frame_container, contactListFragment)
             }
-            transaction.commit();
+            transaction.commit()
         }
+
+        handleOnBackPressed()
     }
 
-    @Override
-    public void showDetails(OCFile file) {
+    private fun handleOnBackPressed() {
+        onBackPressedDispatcher.addCallback(
+            this,
+            object : OnBackPressedCallback(true) {
+                override fun handleOnBackPressed() {
+                    if (supportFragmentManager.findFragmentByTag(BackupListFragment.TAG) != null) {
+                        supportFragmentManager.popBackStack(BACKUP_TO_LIST, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+                    } else {
+                        finish()
+                    }
+                }
+            }
+        )
+    }
+
+    private fun showSidebar(): Boolean {
+        val showSidebar = intent.getBooleanExtra(EXTRA_SHOW_SIDEBAR, true)
+        if (!showSidebar) {
+            setDrawerLockMode(DrawerLayout.LOCK_MODE_LOCKED_CLOSED)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
+            mDrawerToggle?.isDrawerIndicatorEnabled = false
+        }
+        return showSidebar
+    }
+
+    override fun showDetails(file: OCFile) {
         // not needed
     }
 
-    @Override
-    public void showDetails(OCFile file, int activeTab) {
+    override fun showDetails(file: OCFile, activeTab: Int) {
         // not needed
     }
 
-    @Override
-    public void onBrowsedDownTo(OCFile folder) {
+    override fun onBrowsedDownTo(folder: OCFile) {
         // not needed
     }
 
-    @Override
-    public void onTransferStateChanged(OCFile file, boolean downloading, boolean uploading) {
+    override fun onTransferStateChanged(file: OCFile, downloading: Boolean, uploading: Boolean) {
         // not needed
     }
 
-    @Override
-    public void onBackPressed() {
-        if (getSupportFragmentManager().findFragmentByTag(BackupListFragment.TAG) != null) {
-            getSupportFragmentManager().popBackStack(BACKUP_TO_LIST, FragmentManager.POP_BACK_STACK_INCLUSIVE);
-        } else {
-            finish();
+    companion object {
+        val TAG = ContactsPreferenceActivity::class.java.simpleName
+        const val EXTRA_FILE = "FILE"
+        const val EXTRA_USER = "USER"
+
+        /**
+         * Warning: default for this extra is different between this activity and [BackupFragment]
+         */
+        const val EXTRA_SHOW_SIDEBAR = "SHOW_SIDEBAR"
+        const val PREFERENCE_CONTACTS_AUTOMATIC_BACKUP = "PREFERENCE_CONTACTS_AUTOMATIC_BACKUP"
+        const val PREFERENCE_CONTACTS_LAST_BACKUP = "PREFERENCE_CONTACTS_LAST_BACKUP"
+        const val BACKUP_TO_LIST = "BACKUP_TO_LIST"
+
+        fun startActivity(context: Context) {
+            val intent = Intent(context, ContactsPreferenceActivity::class.java)
+            context.startActivity(intent)
+        }
+
+        @JvmStatic
+        fun startActivityWithContactsFile(context: Context, user: User?, file: OCFile?) {
+            val intent = Intent(context, ContactsPreferenceActivity::class.java)
+            intent.putExtra(EXTRA_FILE, file)
+            intent.putExtra(EXTRA_USER, user)
+            context.startActivity(intent)
+        }
+
+        @JvmStatic
+        fun startActivityWithoutSidebar(context: Context) {
+            val intent = Intent(context, ContactsPreferenceActivity::class.java)
+            intent.putExtra(EXTRA_SHOW_SIDEBAR, false)
+            context.startActivity(intent)
         }
     }
 }
