f4325e4f2cec1f5144cb545b4a8c6583b60fbb6d
Jonas Mayer <jonas.a.mayer@gmx.net>
2023-11-27 15:50:02 +0100

    add onTransferProgress for FilesUploadWorker
    Signed-off-by: Jonas Mayer <jonas.a.mayer@gmx.net>

--- app/src/main/java/com/nextcloud/client/jobs/FilesUploadWorker.kt
+++ app/src/main/java/com/nextcloud/client/jobs/FilesUploadWorker.kt
@@ -154,6 +154,7 @@ class FilesUploadWorker(
         }
     }
 
+
     @Suppress("TooGenericExceptionCaught")
     private fun upload(uploadFileOperation: UploadFileOperation, user: User): RemoteOperationResult<Any?> {
         lateinit var uploadResult: RemoteOperationResult<Any?>
@@ -362,8 +363,10 @@ class FilesUploadWorker(
             val text = String.format(context.getString(R.string.uploader_upload_in_progress_content), percent, fileName)
             notificationBuilder.setContentText(text)
             notificationManager.notify(FOREGROUND_SERVICE_ID, notificationBuilder.build())
+            //FilesUploadHelper.onTransferProgress(,progressRate,totalTransferredSoFar,totalToTransfer,fileAbsoluteName)
         }
         lastPercent = percent
+
     }
 
     override fun onStopped() {
