04e49388095f3f060f18107e2653fa84655c3102
alperozturk <alper_ozturk@proton.me>
2023-12-04 12:10:26 +0100

    Add fixme
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/files/services/FileUploader.java
+++ app/src/main/java/com/owncloud/android/files/services/FileUploader.java
@@ -1033,6 +1033,7 @@ public class FileUploader extends Service
         i.putExtra(FileUploader.KEY_ACCOUNT, user.toPlatformAccount());
         i.putExtra(FileUploader.KEY_RETRY_UPLOAD, upload);
 
+        // FIXME Conflict files not uploading successfully
         if (useFilesUploadWorker(context)) {
             new FilesUploadHelper().retryUpload(upload, user);
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
