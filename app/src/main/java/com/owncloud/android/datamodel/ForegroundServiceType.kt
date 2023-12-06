b2e0e76605e39c8efe52aeeefdbdabe7d094e90b
alperozturk <alper_ozturk@proton.me>
2023-12-06 09:56:07 +0100

    Simplify code complexity
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/datamodel/ForegroundServiceType.kt
+++ app/src/main/java/com/owncloud/android/datamodel/ForegroundServiceType.kt
@@ -21,6 +21,25 @@
 
 package com.owncloud.android.datamodel
 
+import android.content.pm.ServiceInfo
+import android.os.Build
+import androidx.annotation.RequiresApi
+
+/**
+ * Enum to specify the type of foreground service.
+ * Use this enum when starting a foreground service to indicate its purpose.
+ * Note: Foreground service type is not available for older Android versions.
+ * This wrapper is designed for compatibility on those versions.
+ */
 enum class ForegroundServiceType {
-    DataSync, MediaPlayback
+    DataSync, MediaPlayback;
+
+    @RequiresApi(Build.VERSION_CODES.Q)
+    fun getId(): Int {
+        return if (this == DataSync) {
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_DATA_SYNC
+        } else {
+            ServiceInfo.FOREGROUND_SERVICE_TYPE_MEDIA_PLAYBACK
+        }
+    }
 }
