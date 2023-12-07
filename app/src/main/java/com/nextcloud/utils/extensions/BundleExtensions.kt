487ab866754652de65746f3446b2cf3b168a3661
alperozturk <alper_ozturk@proton.me>
2023-12-07 10:03:21 +0100

    Test branch
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/utils/extensions/BundleExtensions.kt
+++ app/src/main/java/com/nextcloud/utils/extensions/BundleExtensions.kt
@@ -5,6 +5,7 @@
  * Copyright (C) 2023 Alper Ozturk
  * Copyright (C) 2023 Nextcloud GmbH
  *
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
