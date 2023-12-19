7588dc543069e25121f6f98a93cc5980ad6aa57b
alperozturk <alper_ozturk@proton.me>
2023-12-19 10:44:55 +0100

    Implement files download worker constructor
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/client/jobs/BackgroundJobFactory.kt
+++ app/src/main/java/com/nextcloud/client/jobs/BackgroundJobFactory.kt
@@ -266,6 +266,17 @@ class BackgroundJobFactory @Inject constructor(
         )
     }
 
+    private fun createFilesDownloadWorker(context: Context, params: WorkerParameters): FilesDownloadWorker {
+        return FilesDownloadWorker(
+            viewThemeUtils.get(),
+            accountManager,
+            uploadsStorageManager,
+            localBroadcastManager.get(),
+            context,
+            params
+        )
+    }
+
     private fun createPDFGenerateWork(context: Context, params: WorkerParameters): GeneratePdfFromImagesWork {
         return GeneratePdfFromImagesWork(
             appContext = context,
