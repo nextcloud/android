1fe72fd0a20a7855189830740f0a2b80f173c8fe
alperozturk <alper_ozturk@proton.me>
2023-12-22 11:01:41 +0100

    test commit

--- app/src/main/java/com/owncloud/android/ui/dialog/ChooseTemplateDialogFragment.kt
+++ app/src/main/java/com/owncloud/android/ui/dialog/ChooseTemplateDialogFragment.kt
@@ -323,7 +323,6 @@ class ChooseTemplateDialogFragment : DialogFragment(), View.OnClickListener, Tem
                 file = storageManager.getFileByPath(path)
                 result.resultData
             } catch (e: CreationException) {
-
                 Log_OC.e(TAG, "Error creating file from template!", e)
                 ""
             }
