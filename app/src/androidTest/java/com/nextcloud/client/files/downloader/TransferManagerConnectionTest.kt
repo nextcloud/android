fd20c1117eae4c47d538a1bec8d842a22479ea04
alperozturk <alper_ozturk@proton.me>
2023-12-27 11:13:15 +0100

    Fix missed_updates_are_delivered_on_connection test
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/androidTest/java/com/nextcloud/client/files/downloader/TransferManagerConnectionTest.kt
+++ app/src/androidTest/java/com/nextcloud/client/files/downloader/TransferManagerConnectionTest.kt
@@ -35,7 +35,6 @@ import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Test
-import java.util.concurrent.CountDownLatch
 
 class TransferManagerConnectionTest {
 
