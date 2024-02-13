7f8be9130eddbfc25631d69e49420aa9f62560ef
alperozturk <alper_ozturk@proton.me>
2024-02-13 15:29:08 +0100

    Add IntentExtensionTest
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/androidTest/java/com/nextcloud/extensions/IntentExtensionsTests.kt
+++ app/src/androidTest/java/com/nextcloud/extensions/IntentExtensionsTests.kt
@@ -1,27 +1,27 @@
 /*
  * Nextcloud Android client application
- *
+ *   
  * @author Alper Ozturk
  * Copyright (C) 2024 Alper Ozturk
  * Copyright (C) 2024 Nextcloud GmbH
- *
+ *   
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
+ *   
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU Affero General Public License for more details.
- *
+ *   
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <https://www.gnu.org/licenses/>.
  */
 
 package com.nextcloud.extensions
 
-import android.os.Bundle
+import android.content.Intent
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.nextcloud.test.model.OtherTestData
 import com.nextcloud.test.model.TestData
@@ -34,55 +34,55 @@ import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-class BundleExtensionTest {
-    
+class IntentExtensionTest {
+
     private val key = "testDataKey"
 
     @Test
-    fun test_GetSerializableArgument_WhenGivenValidBundle_ShouldReturnExpectedData() {
-        val bundle = Bundle()
+    fun test_GetSerializableArgument_WhenGivenValidIntent_ShouldReturnExpectedData() {
+        val intent = Intent()
         val testObject = TestData("Hello")
-        bundle.putSerializable(key, testObject)
-        val retrievedObject = bundle.getSerializableArgument(key, TestData::class.java)
+        intent.putExtra(key, testObject)
+        val retrievedObject = intent.getSerializableArgument(key, TestData::class.java)
         assertEquals(testObject, retrievedObject)
     }
 
     @Test
-    fun test_GetSerializableArgument_WhenGivenValidBundleAndWrongClassType_ShouldReturnNull() {
-        val bundle = Bundle()
+    fun test_GetSerializableArgument_WhenGivenValidIntentAndWrongClassType_ShouldReturnNull() {
+        val intent = Intent()
         val testObject = TestData("Hello")
-        bundle.putSerializable(key, testObject)
-        val retrievedObject = bundle.getSerializableArgument(key, Array<String>::class.java)
+        intent.putExtra(key, testObject)
+        val retrievedObject = intent.getSerializableArgument(key, Array<String>::class.java)
         assertNull(retrievedObject)
     }
 
     @Test
-    fun test_GetParcelableArgument_WhenGivenValidBundleAndWrongClassType_ShouldReturnNull() {
-        val bundle = Bundle()
+    fun test_GetParcelableArgument_WhenGivenValidIntentAndWrongClassType_ShouldReturnNull() {
+        val intent = Intent()
         val testObject = TestData("Hello")
-        bundle.putSerializable(key, testObject)
-        val retrievedObject = bundle.getParcelableArgument(key, OtherTestData::class.java)
+        intent.putExtra(key, testObject)
+        val retrievedObject = intent.getParcelableArgument(key, OtherTestData::class.java)
         assertNull(retrievedObject)
     }
 
     @Test
-    fun test_GetParcelableArgument_WhenGivenValidBundle_ShouldReturnExpectedData() {
-        val bundle = Bundle()
+    fun test_GetParcelableArgument_WhenGivenValidIntent_ShouldReturnExpectedData() {
+        val intent = Intent()
         val testObject = TestDataParcelable("Hello")
-        bundle.putParcelable(key, testObject)
-        val retrievedObject = bundle.getParcelableArgument(key, TestDataParcelable::class.java)
+        intent.putExtra(key, testObject)
+        val retrievedObject = intent.getParcelableArgument(key, TestDataParcelable::class.java)
         assertEquals(testObject, retrievedObject)
     }
 
     @Test
-    fun test_GetSerializableArgument_WhenGivenNullBundle_ShouldReturnNull() {
-        val retrievedObject = (null as Bundle?).getSerializableArgument(key, TestData::class.java)
+    fun test_GetSerializableArgument_WhenGivenNullIntent_ShouldReturnNull() {
+        val retrievedObject = (null as Intent?).getSerializableArgument(key, TestData::class.java)
         assertNull(retrievedObject)
     }
 
     @Test
-    fun test_GetParcelableArgument_WhenGivenNullBundle_ShouldReturnNull() {
-        val retrievedObject = (null as Bundle?).getParcelableArgument(key, TestDataParcelable::class.java)
+    fun test_GetParcelableArgument_WhenGivenNullIntent_ShouldReturnNull() {
+        val retrievedObject = (null as Intent?).getParcelableArgument(key, TestDataParcelable::class.java)
         assertNull(retrievedObject)
     }
 }
