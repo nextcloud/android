e2b309184abc4e8a5ce912c29ee51be33fa7678f
tobiasKaminsky <tobias@kaminsky.me>
2024-03-13 07:46:17 +0100

    fix database upgrade
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- app/src/main/java/com/nextcloud/client/database/NextcloudDatabase.kt
+++ app/src/main/java/com/nextcloud/client/database/NextcloudDatabase.kt
@@ -72,7 +72,7 @@ import com.owncloud.android.db.ProviderMeta
         AutoMigration(from = 75, to = 76),
         AutoMigration(from = 76, to = 77),
         AutoMigration(from = 77, to = 78),
-        AutoMigration(from = 78, to = 79),
+        AutoMigration(from = 78, to = 79, spec = DatabaseMigrationUtil.ResetCapabilitiesPostMigration::class),
     ],
     exportSchema = true
 )
