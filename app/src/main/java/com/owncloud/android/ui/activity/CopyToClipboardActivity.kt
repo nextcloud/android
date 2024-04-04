29c46b22e0e490b07cc0202c9aed792b1480ebaa
Andy Scherzinger <info@andy-scherzinger.de>
2024-04-04 14:36:44 +0200

    Add SPDX header
    Signed-off-by: Andy Scherzinger <info@andy-scherzinger.de>

--- app/src/main/java/com/owncloud/android/ui/activity/CopyToClipboardActivity.kt
+++ app/src/main/java/com/owncloud/android/ui/activity/CopyToClipboardActivity.kt
@@ -1,22 +1,8 @@
 /*
- *   ownCloud Android client application
+ * Nextcloud - Android Client
  *
- *   @author David A. Velasco
- *   @author Andy Scherzinger
- *   Copyright (C) 2015 ownCloud Inc.
- *   Copyright (C) 2018 Andy Scherzinger
- *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License version 2,
- *   as published by the Free Software Foundation.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * SPDX-FileCopyrightText: 2023 Alper Ozturk <alper_ozturk@proton.me>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 package com.owncloud.android.ui.activity
 
--- app/src/main/java/org/nextcloud/providers/cursors/FileCursor.java
+++ app/src/main/java/org/nextcloud/providers/cursors/FileCursor.java
@@ -1,23 +1,12 @@
 /*
- *   nextCloud Android client application
+ * Nextcloud - Android Client
  *
- *   @author Bartosz Przybylski
- *   Copyright (C) 2016  Bartosz Przybylski
- *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License version 2,
- *   as published by the Free Software Foundation.
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
+ * SPDX-FileCopyrightText: 2020 Tobias Kaminsky <tobias@kaminsky.me>
+ * SPDX-FileCopyrightText: 2019 Jens Mueller <tschenser@gmx.de>
+ * SPDX-FileCopyrightText: 2016 Iskra Delta <iskradelta@no-reply.github.com>
+ * SPDX-FileCopyrightText: 2015 Bartosz Przybylski <bart.p.pl@gmail.com>
+ * SPDX-License-Identifier: GPL-2.0-only AND AGPL-3.0-or-later
  */
-
 package org.nextcloud.providers.cursors;
 
 import android.database.MatrixCursor;
