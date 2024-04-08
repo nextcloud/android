757306d1c4355bffa2370e459627ec7711f2d9af
Jonas Mayer <jonas.a.mayer@gmx.net>
2024-04-08 18:05:59 +0200

    add new database version
    Signed-off-by: Jonas Mayer <jonas.a.mayer@gmx.net>

--- app/src/main/java/com/owncloud/android/db/ProviderMeta.java
+++ app/src/main/java/com/owncloud/android/db/ProviderMeta.java
@@ -25,7 +25,7 @@ import java.util.List;
  */
 public class ProviderMeta {
     public static final String DB_NAME = "filelist";
-    public static final int DB_VERSION = 80;
+    public static final int DB_VERSION = 81;
 
     private ProviderMeta() {
         // No instance
