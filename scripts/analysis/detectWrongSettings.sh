c4f5cf4cd08d8432d1b0d96c2caf35741e2bbf81
tobiasKaminsky <tobias@kaminsky.me>
2024-06-10 16:32:17 +0200

    update
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- scripts/analysis/detectWrongSettings.sh
+++ scripts/analysis/detectWrongSettings.sh
@@ -4,10 +4,12 @@
 # SPDX-FileCopyrightText: 2016 Tobias Kaminsky <tobias@kaminsky.me>
 # SPDX-License-Identifier: AGPL-3.0-or-later OR GPL-2.0-only
 
-snapshotCount=$(./gradlew dependencies | grep SNAPSHOT | grep -v "com.github.nextcloud:android-library" -c)
+snapshotCount=$(./gradlew dependencies | grep SNAPSHOT -c)
 betaCount=$(grep "<bool name=\"is_beta\">true</bool>" app/src/main/res/values/setup.xml -c)
+libraryHash=$(grep library build.gradle | cut -f2 -d'"' | grep "^[0-9a-zA-Z]\{40\}$" -c)
 
-if [[ $snapshotCount -eq 0 && $betaCount -eq 0 ]] ; then
+
+if [[ $snapshotCount -eq 0 && $betaCount -eq 0 && $libraryHash = 1 ]] ; then
     exit 0
 else
     exit 1
