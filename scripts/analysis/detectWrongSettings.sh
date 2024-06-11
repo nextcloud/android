cc18e2b7023627401b4fe32ce6c253a7c80e0458
tobiasKaminsky <tobias@kaminsky.me>
2024-06-11 07:38:22 +0200

    fix script
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- scripts/analysis/detectWrongSettings.sh
+++ scripts/analysis/detectWrongSettings.sh
@@ -6,7 +6,7 @@
 
 snapshotCount=$(./gradlew dependencies | grep SNAPSHOT -c)
 betaCount=$(grep "<bool name=\"is_beta\">true</bool>" app/src/main/res/values/setup.xml -c)
-libraryHash=$(grep library build.gradle | cut -f2 -d'"' | grep "^[0-9a-zA-Z]\{40\}$" -c)
+libraryHash=$(grep androidLibraryVersion build.gradle | cut -f2 -d'"' | grep "^[0-9a-zA-Z]\{40\}$" -c)
 
 
 if [[ $snapshotCount -gt 0 ]] ; then
@@ -18,7 +18,7 @@ if [[ $betaCount -gt 0 ]] ; then
     exit 1
 fi
  
-if [[ $libraryHash = 0 ]] ; then
+if [[ $libraryHash -eq 0 ]] ; then
     echo "Library hash is wrong!"
     exit 1
 fi
