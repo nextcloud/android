79a8afdb32055dedbfc1a67c669c32fedf2c1779
alperozturk <alper_ozturk@proton.me>
2024-07-08 11:10:13 +0200

    test with api level 29
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/androidTest/java/com/nextcloud/client/FileDisplayActivityScreenshotIT.kt
+++ app/src/androidTest/java/com/nextcloud/client/FileDisplayActivityScreenshotIT.kt
@@ -32,8 +32,7 @@ class FileDisplayActivityScreenshotIT : AbstractIT() {
 
     @get:Rule
     val permissionRule = GrantPermissionRule.grant(
-        Manifest.permission.WRITE_EXTERNAL_STORAGE,
-        Manifest.permission.MANAGE_EXTERNAL_STORAGE
+        Manifest.permission.WRITE_EXTERNAL_STORAGE
     )
 
     @Test
