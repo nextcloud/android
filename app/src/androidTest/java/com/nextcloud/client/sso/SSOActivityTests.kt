efc956d380176b8f295600d86996e1e9af9cd53e
alperozturk <alper_ozturk@proton.me>
2024-07-19 16:37:57 +0200

    add header
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/androidTest/java/com/nextcloud/client/sso/SSOActivityTests.kt
+++ app/src/androidTest/java/com/nextcloud/client/sso/SSOActivityTests.kt
@@ -1,3 +1,10 @@
+/*
+ * Nextcloud - Android Client
+ *
+ * SPDX-FileCopyrightText: 2024 Alper Ozturk <alper.ozturk@nextcloud.com>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 package com.nextcloud.client.sso
 
 import androidx.test.espresso.intent.rule.IntentsTestRule
