231b35477d0cf29bba521fd45c376e8e75b7e746
alperozturk <alper_ozturk@proton.me>
2024-08-12 14:16:49 +0200

    Add OfflineOperationEntity dao
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

---
+++ app/src/main/java/com/nextcloud/client/database/dao/OfflineOperationDao.kt
@@ -0,0 +1,30 @@
+/*
+ * Nextcloud - Android Client
+ *
+ * SPDX-FileCopyrightText: 2024 Alper Ozturk <alper.ozturk@nextcloud.com>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+package com.nextcloud.client.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.nextcloud.client.database.entity.OfflineOperationEntity
+
+@Dao
+interface OfflineOperationDao {
+    @Query("SELECT * FROM offline_operations")
+    fun getAll(): List<OfflineOperationEntity>
+
+    @Insert
+    fun insert(vararg entity: OfflineOperationEntity)
+
+    @Update
+    fun update(entity: OfflineOperationEntity)
+
+    @Delete
+    fun delete(entity: OfflineOperationEntity)
+}
