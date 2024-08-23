1bbbb9549396edce65f49eafa899648a970c0850
alperozturk <alper_ozturk@proton.me>
2024-08-23 09:48:44 +0200

    revert changes for sync operations
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/operations/common/SyncOperation.java
+++ app/src/main/java/com/owncloud/android/operations/common/SyncOperation.java
@@ -13,14 +13,11 @@ import android.content.Context;
 import android.os.Handler;
 
 import com.nextcloud.common.NextcloudClient;
-import com.nextcloud.utils.extensions.ContextExtensionsKt;
-import com.owncloud.android.R;
 import com.owncloud.android.datamodel.FileDataStorageManager;
 import com.owncloud.android.lib.common.OwnCloudClient;
 import com.owncloud.android.lib.common.operations.OnRemoteOperationListener;
 import com.owncloud.android.lib.common.operations.RemoteOperation;
 import com.owncloud.android.lib.common.operations.RemoteOperationResult;
-import com.owncloud.android.lib.common.utils.Log_OC;
 
 import androidx.annotation.NonNull;
 
@@ -49,11 +46,10 @@ public abstract class SyncOperation extends RemoteOperation {
      */
     public RemoteOperationResult execute(Context context) {
         if (storageManager.getUser().isAnonymous()) {
-            Log_OC.d("SyncOperation","Trying to execute a sync operation with anonymous account, operation cancelled");
-            return new RemoteOperationResult(RemoteOperationResult.ResultCode.ACCOUNT_EXCEPTION);
-        } else {
-            return super.execute(this.storageManager.getUser(), context);
+            throw new IllegalArgumentException("Trying to execute a sync operation with a " +
+                                                   "storage manager for an anonymous account");
         }
+        return super.execute(this.storageManager.getUser(), context);
     }
 
     public RemoteOperationResult execute(@NonNull NextcloudClient client) {
