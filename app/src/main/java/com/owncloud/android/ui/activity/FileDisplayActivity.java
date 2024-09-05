6076aca850ae625cc71aa1b6d9d0cbd48c560e24
alperozturk <alper_ozturk@proton.me>
2024-09-05 10:35:31 +0200

    Fix
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.java
+++ app/src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.java
@@ -570,8 +570,6 @@ public class FileDisplayActivity extends FileActivity
 
                 setLeftFragment(new GroupfolderListFragment());
                 getSupportFragmentManager().executePendingTransactions();
-            } else {
-                handleOpenFileViaIntent(intent);
             }
         }
     }
@@ -2378,10 +2376,7 @@ public class FileDisplayActivity extends FileActivity
     }
 
     private void handleOpenFileViaIntent(Intent intent) {
-        Uri deepLinkUri = getIntent().getData();
-        if (deepLinkUri == null || !DeepLinkHandler.Companion.isDeepLinkTypeIsNavigation(deepLinkUri.toString())) {
-            showLoadingDialog(getString(R.string.retrieving_file));
-        }
+        DisplayUtils.showSnackMessage(this, getString(R.string.retrieving_file));
 
         String userName = intent.getStringExtra(KEY_ACCOUNT);
         String fileId = intent.getStringExtra(KEY_FILE_ID);
@@ -2397,11 +2392,9 @@ public class FileDisplayActivity extends FileActivity
                 } else if (!TextUtils.isEmpty(filePath)) {
                     openFileByPath(optionalUser.get(), filePath);
                 } else {
-                    dismissLoadingDialog();
                     accountClicked(optionalUser.get().hashCode());
                 }
             } else {
-                dismissLoadingDialog();
                 DisplayUtils.showSnackMessage(this, getString(R.string.associated_account_not_found));
             }
         }
@@ -2410,11 +2403,10 @@ public class FileDisplayActivity extends FileActivity
     private void openDeepLink(Uri uri) {
         DeepLinkHandler linkHandler = new DeepLinkHandler(getUserAccountManager());
         DeepLinkHandler.Match match = linkHandler.parseDeepLink(uri);
+
         if (match == null) {
-            dismissLoadingDialog();
             handleDeepLink(uri);
         } else if (match.getUsers().isEmpty()) {
-            dismissLoadingDialog();
             DisplayUtils.showSnackMessage(this, getString(R.string.associated_account_not_found));
         } else if (match.getUsers().size() == SINGLE_USER_SIZE) {
             openFile(match.getUsers().get(0), match.getFileId());
