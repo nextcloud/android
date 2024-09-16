e6ed12830dfacad801ff541acfeb8f6294ed6444
alperozturk <alper_ozturk@proton.me>
2024-09-16 17:51:19 +0200

    Show new notifications only for file specific errors
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/utils/extensions/RemoteOperationResultExtensions.kt
+++ app/src/main/java/com/nextcloud/utils/extensions/RemoteOperationResultExtensions.kt
@@ -14,6 +14,7 @@ import com.owncloud.android.datamodel.FileDataStorageManager
 import com.owncloud.android.datamodel.OCFile
 import com.owncloud.android.lib.common.operations.RemoteOperation
 import com.owncloud.android.lib.common.operations.RemoteOperationResult
+import com.owncloud.android.lib.common.operations.RemoteOperationResult.ResultCode
 import com.owncloud.android.lib.resources.files.model.RemoteFile
 import com.owncloud.android.utils.ErrorMessageAdapter
 import com.owncloud.android.utils.FileStorageUtils
@@ -44,6 +45,28 @@ fun RemoteOperationResult<*>?.getConflictedRemoteIdsWithOfflineOperations(
     }
 
     return result.ifEmpty { null }
+}
+
+fun ResultCode.isFileSpecificError(): Boolean {
+    val errorCodes = listOf(
+        ResultCode.INSTANCE_NOT_CONFIGURED,
+        ResultCode.QUOTA_EXCEEDED,
+        ResultCode.LOCAL_STORAGE_FULL,
+        ResultCode.WRONG_CONNECTION,
+        ResultCode.UNAUTHORIZED,
+        ResultCode.OK_NO_SSL,
+        ResultCode.MAINTENANCE_MODE,
+        ResultCode.UNTRUSTED_DOMAIN,
+        ResultCode.ACCOUNT_NOT_THE_SAME,
+        ResultCode.ACCOUNT_EXCEPTION,
+        ResultCode.ACCOUNT_NOT_NEW,
+        ResultCode.ACCOUNT_NOT_FOUND,
+        ResultCode.ACCOUNT_USES_STANDARD_PASSWORD,
+        ResultCode.INCORRECT_ADDRESS,
+        ResultCode.BAD_OC_VERSION,
+    )
+
+    return !errorCodes.contains(this)
 }
 
 @Suppress("Deprecation")
