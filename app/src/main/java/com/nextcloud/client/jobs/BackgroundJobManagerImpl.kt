bdad02f310ec270e7bf66c2ed625447c432d66e9
alperozturk <alper_ozturk@proton.me>
2024-10-30 10:47:06 +0100

    schedule two-way-sync via isTwoWaySyncEnabled
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/client/jobs/BackgroundJobManagerImpl.kt
+++ app/src/main/java/com/nextcloud/client/jobs/BackgroundJobManagerImpl.kt
@@ -663,4 +663,8 @@ internal class BackgroundJobManagerImpl(
 
         workManager.enqueueUniquePeriodicWork(JOB_INTERNAL_TWO_WAY_SYNC, ExistingPeriodicWorkPolicy.UPDATE, request)
     }
+
+    override fun cancelInternal2WaySyncJob() {
+        workManager.cancelJob(JOB_INTERNAL_TWO_WAY_SYNC)
+    }
 }
