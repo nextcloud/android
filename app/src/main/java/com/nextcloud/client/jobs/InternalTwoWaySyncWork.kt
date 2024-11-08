c70dad926dc5cccd6ac8df6baeff55116c0a14dd
alperozturk <alper_ozturk@proton.me>
2024-11-08 15:06:02 +0100

    dont-use-file-download-worker-via-two-way-sync
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/client/jobs/InternalTwoWaySyncWork.kt
+++ app/src/main/java/com/nextcloud/client/jobs/InternalTwoWaySyncWork.kt
@@ -8,7 +8,7 @@
 package com.nextcloud.client.jobs
 
 import android.content.Context
-import androidx.work.Worker
+import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.nextcloud.client.account.UserAccountManager
 import com.nextcloud.client.device.PowerManagementService
@@ -17,8 +17,9 @@ import com.nextcloud.client.preferences.AppPreferences
 import com.owncloud.android.MainApp
 import com.owncloud.android.datamodel.FileDataStorageManager
 import com.owncloud.android.datamodel.OCFile
+import com.owncloud.android.lib.common.OwnCloudClientManagerFactory
 import com.owncloud.android.lib.common.utils.Log_OC
-import com.owncloud.android.operations.SynchronizeFolderOperation
+import com.owncloud.android.operations.DownloadFileOperation
 import com.owncloud.android.utils.FileStorageUtils
 import java.io.File
 
@@ -30,74 +31,71 @@ class InternalTwoWaySyncWork(
     private val powerManagementService: PowerManagementService,
     private val connectivityService: ConnectivityService,
     private val appPreferences: AppPreferences
-) : Worker(context, params) {
+) : CoroutineWorker(context, params) {
     private var shouldRun = true
 
-    override fun doWork(): Result {
-        Log_OC.d(TAG, "Worker started!")
+    override suspend fun doWork(): Result {
+        return try {
+            Log_OC.d(TAG, "InternalTwoWaySyncWork started!")
 
-        var result = true
-
-        @Suppress("ComplexCondition")
-        if (!appPreferences.isTwoWaySyncEnabled ||
-            powerManagementService.isPowerSavingEnabled ||
-            !connectivityService.isConnected ||
-            connectivityService.isInternetWalled ||
-            !connectivityService.connectivity.isWifi
-        ) {
-            Log_OC.d(TAG, "Not starting due to constraints!")
-            return Result.success()
-        }
-
-        val users = userAccountManager.allUsers
-
-        for (user in users) {
-            val fileDataStorageManager = FileDataStorageManager(user, context.contentResolver)
-            val folders = fileDataStorageManager.getInternalTwoWaySyncFolders(user)
-
-            for (folder in folders) {
-                if (!shouldRun) {
-                    Log_OC.d(TAG, "Worker was stopped!")
-                    return Result.failure()
-                }
-
-                checkFreeSpace(folder)?.let { checkFreeSpaceResult ->
-                    return checkFreeSpaceResult
-                }
-
-                Log_OC.d(TAG, "Folder ${folder.remotePath}: started!")
-                val operation = SynchronizeFolderOperation(context, folder.remotePath, user, fileDataStorageManager)
-                    .execute(context)
-
-                if (operation.isSuccess) {
-                    Log_OC.d(TAG, "Folder ${folder.remotePath}: finished!")
-                } else {
-                    Log_OC.d(TAG, "Folder ${folder.remotePath} failed!")
-                    result = false
-                }
-
-                folder.apply {
-                    internalFolderSyncResult = operation.code.toString()
-                    internalFolderSyncTimestamp = System.currentTimeMillis()
-                }
-
-                fileDataStorageManager.saveFile(folder)
+            @Suppress("ComplexCondition")
+            if (!appPreferences.isTwoWaySyncEnabled ||
+                powerManagementService.isPowerSavingEnabled ||
+                !connectivityService.isConnected ||
+                connectivityService.isInternetWalled ||
+                !connectivityService.connectivity.isWifi
+            ) {
+                Log_OC.d(TAG, "Not starting due to constraints!")
+                return Result.success()
             }
-        }
 
-        return if (result) {
-            Log_OC.d(TAG, "Worker finished with success!")
+            userAccountManager.allUsers.forEach { user ->
+                val fileDataStorageManager = FileDataStorageManager(user, context.contentResolver)
+                val folders = fileDataStorageManager.getInternalTwoWaySyncFolders(user)
+
+                folders.forEach { folder ->
+                    val files = fileDataStorageManager.getAllFilesRecursivelyInsideFolder(folder)
+                    var lastOperationCode = ""
+
+                    checkFreeSpace(folder)?.let { checkFreeSpaceResult ->
+                        return checkFreeSpaceResult
+                    }
+
+                    Log_OC.d(TAG, "Folder ${folder.remotePath}: started!")
+
+                    files.forEach { file ->
+                        if (!shouldRun) {
+                            Log_OC.d(TAG, "InternalTwoWaySyncWork was stopped!")
+                            return Result.failure()
+                        }
+
+                        if (!file.isDown) {
+                            val downloadFileOperation = DownloadFileOperation(user, file, context)
+                            val client = OwnCloudClientManagerFactory.getDefaultSingleton()
+                                .getClientFor(user.toOwnCloudAccount(), context)
+                            val downloadFileOperationResult = downloadFileOperation.execute(client)
+                            lastOperationCode = downloadFileOperationResult.code.toString()
+                        }
+                    }
+
+                    folder.apply {
+                        internalFolderSyncResult = lastOperationCode
+                        internalFolderSyncTimestamp = System.currentTimeMillis()
+                    }
+
+                    fileDataStorageManager.saveFile(folder)
+                }
+            }
+
+            Log_OC.d(TAG, "InternalTwoWaySyncWork finished with success!")
             Result.success()
-        } else {
-            Log_OC.d(TAG, "Worker finished with failure!")
+        } catch (t: Throwable) {
+            Log_OC.d(TAG, "InternalTwoWaySyncWork finished with failure!")
             Result.failure()
+        } finally {
+            Log_OC.d(TAG, "InternalTwoWaySyncWork cleanup")
+            shouldRun = false
         }
-    }
-
-    override fun onStopped() {
-        Log_OC.d(TAG, "OnStopped of worker called!")
-        shouldRun = false
-        super.onStopped()
     }
 
     @Suppress("TooGenericExceptionCaught")
