24d9fcca505390d7097c975806a4b17ead778ba7
alperozturk <alper_ozturk@proton.me>
2024-11-11 11:58:05 +0100

    dont use FileDownloadWorker in SynchronizeFolderOperation
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/operations/SynchronizeFolderOperation.java
+++ app/src/main/java/com/owncloud/android/operations/SynchronizeFolderOperation.java
@@ -15,7 +15,6 @@ import android.content.Intent;
 import android.text.TextUtils;
 
 import com.nextcloud.client.account.User;
-import com.nextcloud.client.jobs.download.FileDownloadHelper;
 import com.owncloud.android.datamodel.FileDataStorageManager;
 import com.owncloud.android.datamodel.OCFile;
 import com.owncloud.android.datamodel.e2e.v1.decrypted.DecryptedFolderMetadataFileV1;
@@ -40,7 +39,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Vector;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.function.Consumer;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
