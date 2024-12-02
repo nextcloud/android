ec86f706669df8f060f74cc7110cc067c6d20cf7
alperozturk <alper_ozturk@proton.me>
2024-12-02 13:37:08 +0100

    Change UI according to the latest adjustments
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

---
+++ app/src/main/java/com/nextcloud/client/assistant/task/TaskViewBottomSheetType.kt
@@ -0,0 +1,12 @@
+/*
+ * Nextcloud - Android Client
+ *
+ * SPDX-FileCopyrightText: 2024  Alper Ozturk <alper.ozturk@nextcloud.com>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+package com.nextcloud.client.assistant.task
+
+enum class TaskViewBottomSheetType {
+    Detail, MoreAction
+}
