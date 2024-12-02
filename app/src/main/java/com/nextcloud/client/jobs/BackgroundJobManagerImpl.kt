9855896c2774d1c05d1b6a03042c1f3d96dba2e1
alperozturk <alper_ozturk@proton.me>
2024-12-02 10:56:17 +0100

    add cancel logic
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/client/jobs/BackgroundJobManagerImpl.kt
+++ app/src/main/java/com/nextcloud/client/jobs/BackgroundJobManagerImpl.kt
@@ -730,6 +730,7 @@ internal class BackgroundJobManagerImpl(
             .build()
 
         val request = oneTimeRequestBuilder(SyncWorker::class, JOB_SYNC_FOLDER)
+            .addTag(JOB_SYNC_FOLDER)
             .setInputData(data)
             .build()
 
