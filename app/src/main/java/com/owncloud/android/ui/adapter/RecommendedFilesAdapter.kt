6e92a5aab79be9df8e80f59951703d90e7a83cb5
alperozturk <alper_ozturk@proton.me>
2024-12-13 15:47:46 +0100

    add RecommendedFilesAdapter
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

---
+++ app/src/main/java/com/owncloud/android/ui/adapter/RecommendedFilesAdapter.kt
@@ -0,0 +1,96 @@
+/*
+ * Nextcloud - Android Client
+ *
+ * SPDX-FileCopyrightText: 2024 Alper Ozturk <alper.ozturk@nextcloud.com>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+package com.owncloud.android.ui.adapter
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.owncloud.android.R
+import com.owncloud.android.databinding.RecommendedFilesListItemBinding
+import com.owncloud.android.utils.BitmapUtils
+import com.owncloud.android.utils.DisplayUtils
+import com.owncloud.android.utils.MimeTypeUtil
+import com.owncloud.android.utils.theme.ViewThemeUtils
+
+// TODO delete mock data
+data class Recommendation(
+    val id: Long,
+    val timestamp: Long,
+    val name: String,
+    val directory: String,
+    val extension: String,
+    val mimeType: String,
+    val hasPreview: Boolean,
+    val reason: String
+)
+
+class RecommendedFilesAdapter(
+    private val context: Context,
+    private val viewThemeUtils: ViewThemeUtils,
+    private val recommendations: List<Recommendation>
+) : RecyclerView.Adapter<RecommendedFilesAdapter.RecommendedFilesViewHolder>() {
+
+    inner class RecommendedFilesViewHolder(val binding: RecommendedFilesListItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        init {
+            binding.root.setOnClickListener {
+                val position = bindingAdapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    // TODO onclick item
+                }
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecommendedFilesViewHolder {
+        val binding = RecommendedFilesListItemBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+        return RecommendedFilesViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int = recommendations.size
+
+    override fun onBindViewHolder(holder: RecommendedFilesViewHolder, position: Int) {
+        val item = recommendations[position]
+
+        holder.binding.name.text = item.name
+        holder.binding.timestamp.text = DisplayUtils.getRelativeTimestamp(context,  item.timestamp)
+
+        val thumbnail = getThumbnail(item)
+        holder.binding.icon.setImageBitmap(thumbnail)
+    }
+
+    private fun getThumbnail(item: Recommendation): Bitmap {
+        var drawable = MimeTypeUtil.getFileTypeIcon(
+            item.mimeType,
+            item.name,
+            context,
+            viewThemeUtils
+        )
+
+        if (drawable == null) {
+            drawable = ResourcesCompat.getDrawable(
+                context.resources,
+                R.drawable.file_image,
+                null
+            )
+        }
+
+        if (drawable == null) {
+            drawable = ColorDrawable(Color.GRAY)
+        }
+
+        val width = DisplayUtils.convertPixelToDp(40, context).toInt()
+        return BitmapUtils.drawableToBitmap(drawable, width / 2, width / 2)
+    }
+}
