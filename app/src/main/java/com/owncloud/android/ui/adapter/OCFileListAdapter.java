4b1a30a1485e0a1deaa8224a85c1e2d9aabc0ed4
alperozturk <alper_ozturk@proton.me>
2024-12-18 11:21:15 +0100

    use api
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/ui/adapter/OCFileListAdapter.java
+++ app/src/main/java/com/owncloud/android/ui/adapter/OCFileListAdapter.java
@@ -781,7 +781,7 @@ public class OCFileListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
             return false;
         }
 
-        if (!recommendedFiles.isEmpty()) {
+        if (!recommendedFiles.isEmpty() && currentDirectory.isRootDirectory()) {
             return true;
         }
 
