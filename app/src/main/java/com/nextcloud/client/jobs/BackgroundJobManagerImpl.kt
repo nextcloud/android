bc18eb8eaaf2089e6c8e366aa3a24ef49b9d96ca
alperozturk <alper_ozturk@proton.me>
2024-12-19 09:53:12 +0100

    add tag
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/client/jobs/BackgroundJobManagerImpl.kt
+++ app/src/main/java/com/nextcloud/client/jobs/BackgroundJobManagerImpl.kt
@@ -30,6 +30,7 @@ import com.nextcloud.client.jobs.offlineOperations.OfflineOperationsWorker
 import com.nextcloud.client.jobs.sync.SyncWorker
 import com.nextcloud.client.jobs.upload.FileUploadWorker
 import com.nextcloud.client.preferences.AppPreferences
+import com.nextcloud.utils.extensions.StringConstants
 import com.nextcloud.utils.extensions.isWorkRunning
 import com.nextcloud.utils.extensions.isWorkScheduled
 import com.owncloud.android.datamodel.OCFile
@@ -721,6 +722,7 @@ internal class BackgroundJobManagerImpl(
     }
 
     override fun syncFolder(files: List<OCFile>, folderId: Long) {
+        val tag = getSyncFolderTag(folderId)
         val filePaths = files.map { it.decryptedRemotePath }
 
         val data = Data.Builder()
@@ -728,7 +730,7 @@ internal class BackgroundJobManagerImpl(
             .build()
 
         val request = oneTimeRequestBuilder(SyncWorker::class, JOB_SYNC_FOLDER)
-            .addTag(JOB_SYNC_FOLDER + folderId.toString())
+            .addTag(tag)
             .setInputData(data)
             .build()
 
@@ -736,6 +738,9 @@ internal class BackgroundJobManagerImpl(
     }
 
     override fun cancelSyncFolder(folderId: Long) {
-        workManager.cancelAllWorkByTag(JOB_SYNC_FOLDER + folderId.toString())
+        val tag = getSyncFolderTag(folderId)
+        workManager.cancelAllWorkByTag(tag)
     }
+
+    private fun getSyncFolderTag(folderId: Long): String = JOB_SYNC_FOLDER + StringConstants.SPACE + folderId.toString()
 }
