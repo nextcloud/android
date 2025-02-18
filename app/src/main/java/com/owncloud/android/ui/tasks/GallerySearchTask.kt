1a7e08adeec4deb53a83e79573e432a20f6db790
alperozturk <alper_ozturk@proton.me>
2025-02-18 15:51:57 +0100

    execute photoSearchTask onCreate and fix ss test
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/ui/tasks/GallerySearchTask.kt
+++ app/src/main/java/com/owncloud/android/ui/tasks/GallerySearchTask.kt
@@ -1,7 +1,7 @@
 /*
  * Nextcloud - Android Client
  *
- *SPDX-FileCopyrightText: 2025 Alper Ozturk <alper.ozturk@nextcloud.com>
+ * SPDX-FileCopyrightText: 2025 Alper Ozturk <alper.ozturk@nextcloud.com>
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 package com.owncloud.android.ui.tasks
