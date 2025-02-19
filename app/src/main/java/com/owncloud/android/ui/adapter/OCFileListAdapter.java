f243143ab81c5a8de59650391d2d99ada2491599
alperozturk <alper_ozturk@proton.me>
2025-02-19 15:44:34 +0100

    fix git conflict
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/ui/adapter/OCFileListAdapter.java
+++ app/src/main/java/com/owncloud/android/ui/adapter/OCFileListAdapter.java
@@ -14,7 +14,11 @@ import android.accounts.AuthenticatorException;
 import android.accounts.OperationCanceledException;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.BroadcastReceiver;
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.res.ColorStateList;
 import android.content.res.Resources;
 import android.graphics.Color;
@@ -35,6 +39,7 @@ import com.nextcloud.android.common.ui.theme.utils.ColorRole;
 import com.nextcloud.android.lib.resources.recommendations.Recommendation;
 import com.nextcloud.client.account.User;
 import com.nextcloud.client.database.entity.OfflineOperationEntity;
+import com.nextcloud.client.jobs.sync.SyncWorker;
 import com.nextcloud.client.jobs.upload.FileUploadHelper;
 import com.nextcloud.client.preferences.AppPreferences;
 import com.nextcloud.model.OCFileFilterType;
@@ -95,6 +100,7 @@ import java.util.stream.Collectors;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.VisibleForTesting;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
@@ -194,9 +200,31 @@ public class OCFileListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
 
         setHasStableIds(true);
 
+        registerFileDownloadCompletionReceiver();
+
         // initialise thumbnails cache on background thread
         new ThumbnailsCacheManager.InitDiskCacheTask().execute();
     }
+
+    private void registerFileDownloadCompletionReceiver() {
+        LocalBroadcastManager
+            .getInstance(activity)
+            .registerReceiver(fileDownloadCompletionReceiver, new IntentFilter(SyncWorker.FILE_DOWNLOAD_COMPLETION_BROADCAST));
+    }
+
+    private final BroadcastReceiver fileDownloadCompletionReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String path = intent.getStringExtra(SyncWorker.FILE_PATH);
+            OCFile file = mStorageManager.getFileByDecryptedRemotePath(path);
+
+            if (file == null) {
+                return;
+            }
+
+            notifyItemChanged(file);
+        }
+    };
 
     public boolean isMultiSelect() {
         return ocFileListDelegate.isMultiSelect();
