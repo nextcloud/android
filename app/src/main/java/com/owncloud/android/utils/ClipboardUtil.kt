47ea22db5c759ba83b5b51acb5fbad0e6e2935a6
tobiasKaminsky <tobias@kaminsky.me>
2025-02-24 14:12:27 +0100

    enhance clipboard
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- app/src/main/java/com/owncloud/android/utils/ClipboardUtil.kt
+++ app/src/main/java/com/owncloud/android/utils/ClipboardUtil.kt
@@ -1,6 +1,7 @@
 /*
  * Nextcloud - Android Client
  *
+ * SPDX-FileCopyrightText: 2024 Alper Ozturk <alper.ozturk@nextcloud.com>
  * SPDX-FileCopyrightText: 2018 Andy Scherzinger <info@andy-scherzinger.de>
  * SPDX-License-Identifier: AGPL-3.0-or-later OR GPL-2.0-only
  */
@@ -8,19 +9,25 @@ package com.owncloud.android.utils
 
 import android.app.Activity
 import android.content.ClipData
+import android.content.ClipDescription
 import android.content.ClipboardManager
 import android.content.Context
+import android.os.Build
+import android.os.PersistableBundle
 import android.text.TextUtils
 import android.widget.Toast
+import androidx.work.Data
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import com.nextcloud.client.jobs.clipboard.ClipboardClearWorker
 import com.nextcloud.utils.mdm.MDMConfig
 import com.owncloud.android.R
 import com.owncloud.android.lib.common.utils.Log_OC
+import java.util.concurrent.TimeUnit
 
-/**
- * Helper implementation to copy a string into the system clipboard.
- */
 object ClipboardUtil {
     private val TAG = ClipboardUtil::class.java.name
+    private const val WORKER_CLEAR_DELAY = 15L
 
     @JvmStatic
     @JvmOverloads
@@ -30,25 +37,50 @@ object ClipboardUtil {
             return
         }
 
-        if (!TextUtils.isEmpty(text)) {
-            try {
-                val clip = ClipData.newPlainText(
-                    activity.getString(
-                        R.string.clipboard_label,
-                        activity.getString(R.string.app_name)
-                    ),
-                    text
-                )
-                (activity.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager).setPrimaryClip(clip)
-                if (showToast) {
-                    Toast.makeText(activity, R.string.clipboard_text_copied, Toast.LENGTH_SHORT).show()
-                }
-            } catch (e: Exception) {
-                Toast.makeText(activity, R.string.clipboard_unexpected_error, Toast.LENGTH_SHORT).show()
-                Log_OC.e(TAG, "Exception caught while copying to clipboard", e)
-            }
-        } else {
+        if (TextUtils.isEmpty(text)) {
             Toast.makeText(activity, R.string.clipboard_no_text_to_copy, Toast.LENGTH_SHORT).show()
+            return
         }
+
+        try {
+            val clipboardLabel = activity.getString(R.string.clipboard_label, activity.getString(R.string.app_name))
+            val clip = getClipData(clipboardLabel, text)
+            val clipboardManager = (activity.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager)
+            clipboardManager.setPrimaryClip(clip)
+
+            if (showToast) {
+                Toast.makeText(activity, R.string.clipboard_text_copied, Toast.LENGTH_SHORT).show()
+            }
+
+            scheduleClipboardClearWorker(activity, text)
+        } catch (e: Exception) {
+            Toast.makeText(activity, R.string.clipboard_unexpected_error, Toast.LENGTH_SHORT).show()
+            Log_OC.e(TAG, "Exception caught while copying to clipboard", e)
+        }
+    }
+
+    private fun getClipData(clipboardLabel: String, text: String?): ClipData {
+        return ClipData.newPlainText(clipboardLabel, text).apply {
+            description.extras = PersistableBundle().apply {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    putBoolean(ClipDescription.EXTRA_IS_SENSITIVE, true)
+                } else {
+                    putBoolean("android.content.extra.IS_SENSITIVE", true)
+                }
+            }
+        }
+    }
+
+    private fun scheduleClipboardClearWorker(context: Context, text: String?) {
+        val data = Data.Builder()
+            .putString(ClipboardClearWorker.CLIPBOARD_TEXT, text)
+            .build()
+
+        val clearWorkRequest = OneTimeWorkRequestBuilder<ClipboardClearWorker>()
+            .setInitialDelay(WORKER_CLEAR_DELAY, TimeUnit.SECONDS)
+            .setInputData(data)
+            .build()
+
+        WorkManager.getInstance(context).enqueue(clearWorkRequest)
     }
 }
