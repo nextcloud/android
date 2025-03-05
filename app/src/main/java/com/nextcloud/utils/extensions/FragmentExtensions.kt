0a7399e9d17d0e4e3fcac1d24d5bd2288269a9bf
alperozturk <alper_ozturk@proton.me>
2025-03-05 13:21:10 +0100

    fix java.lang.IllegalStateException: Can not perform this action after onSaveInstanceState at com.owncloud.android.ui.fragment.OCFileListFragment.lambda$openActionsMenu$9(OCFileListFragment.java:702)
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/utils/extensions/FragmentExtensions.kt
+++ app/src/main/java/com/nextcloud/utils/extensions/FragmentExtensions.kt
@@ -8,6 +8,7 @@
 package com.nextcloud.utils.extensions
 
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Lifecycle
 
 inline fun <reified T : Any> Fragment.typedActivity(): T? {
     return if (isAdded && activity != null && activity is T) {
@@ -27,3 +28,6 @@ fun <T : Any> Fragment.getTypedActivity(type: Class<T>): T? {
         null
     }
 }
+
+fun Fragment.isDialogFragmentReady() =
+    isAdded && !isStateSaved && activity?.lifecycle?.currentState?.isAtLeast(Lifecycle.State.RESUMED) == true
