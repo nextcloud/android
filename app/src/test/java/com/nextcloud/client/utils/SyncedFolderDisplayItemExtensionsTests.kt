188858a0ef743ba255dc641c2ddc935e376b75ad
alperozturk <alper_ozturk@proton.me>
2025-03-19 15:07:43 +0100

    remove test due to file existance check
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/test/java/com/nextcloud/client/utils/SyncedFolderDisplayItemExtensionsTests.kt
+++ app/src/test/java/com/nextcloud/client/utils/SyncedFolderDisplayItemExtensionsTests.kt
@@ -8,8 +8,7 @@
 package com.nextcloud.client.utils
 
 import com.nextcloud.client.preferences.SubFolderRule
-import com.nextcloud.utils.extensions.filterEnabledOrWithoutParentInEnabledSet
-import com.nextcloud.utils.extensions.filterEnabledSubfoldersWithEnabledParent
+import com.nextcloud.utils.extensions.filterEnabledOrWithoutEnabledParent
 import com.owncloud.android.datamodel.MediaFolderType
 import com.owncloud.android.datamodel.SyncedFolder
 import com.owncloud.android.datamodel.SyncedFolderDisplayItem
@@ -23,7 +22,7 @@ class SyncedFolderDisplayItemExtensionsTests {
         val list = listOf(
             SyncedFolderDisplayItem(
                 1,
-                "/storage/emulated/0/DCIM/my_folder",
+                "/root/my_folder",
                 "/InstantUpload/",
                 true,
                 false,
@@ -43,7 +42,7 @@ class SyncedFolderDisplayItemExtensionsTests {
             ),
             SyncedFolderDisplayItem(
                 2,
-                "/storage/emulated/0/DCIM/my_folder/B",
+                "/root/my_folder/B",
                 "/InstantUpload/",
                 true,
                 false,
@@ -63,7 +62,7 @@ class SyncedFolderDisplayItemExtensionsTests {
             ),
             SyncedFolderDisplayItem(
                 3,
-                "/storage/emulated/0/DCIM/my_folder_2",
+                "/root/my_folder_2",
                 "/InstantUpload/",
                 true,
                 false,
@@ -83,85 +82,12 @@ class SyncedFolderDisplayItemExtensionsTests {
             )
         )
 
-        val filteredList = list.filterEnabledOrWithoutParentInEnabledSet()
+        val filteredList = list.filterEnabledOrWithoutEnabledParent()
 
         val firstItem = filteredList.find { it.id == 1L }
         val secondItem = filteredList.find { it.id == 3L }
 
         assert(firstItem != null)
         assert(secondItem != null)
-    }
-
-    @Suppress("MagicNumber", "LongMethod")
-    @Test
-    fun testFilterEnabledSubfoldersWithEnabledParent() {
-        val list = listOf(
-            SyncedFolderDisplayItem(
-                1,
-                "/storage/emulated/0/DCIM/my_folder",
-                "/InstantUpload/",
-                true,
-                false,
-                false,
-                true,
-                "test@https://nextcloud.localhost",
-                0,
-                0,
-                true,
-                1000,
-                "my_folder",
-                MediaFolderType.IMAGE,
-                false,
-                SubFolderRule.YEAR_MONTH,
-                false,
-                SyncedFolder.NOT_SCANNED_YET
-            ),
-            SyncedFolderDisplayItem(
-                2,
-                "/storage/emulated/0/DCIM/my_folder/B",
-                "/InstantUpload/",
-                true,
-                false,
-                false,
-                true,
-                "test@https://nextcloud.localhost",
-                0,
-                0,
-                false,
-                1000,
-                "B",
-                MediaFolderType.IMAGE,
-                false,
-                SubFolderRule.YEAR_MONTH,
-                false,
-                SyncedFolder.NOT_SCANNED_YET
-            ),
-            SyncedFolderDisplayItem(
-                3,
-                "/storage/emulated/0/DCIM/my_folder_2",
-                "/InstantUpload/",
-                true,
-                false,
-                false,
-                true,
-                "test@https://nextcloud.localhost",
-                0,
-                0,
-                true,
-                1000,
-                "my_folder_2",
-                MediaFolderType.IMAGE,
-                false,
-                SubFolderRule.YEAR_MONTH,
-                false,
-                SyncedFolder.NOT_SCANNED_YET
-            )
-        )
-
-        val filteredList = list.filterEnabledSubfoldersWithEnabledParent()
-
-        val firstItem = filteredList.find { it.localPath == "/storage/emulated/0/DCIM/my_folder/B" }
-
-        assert(firstItem != null && filteredList.size == 1)
     }
 }
