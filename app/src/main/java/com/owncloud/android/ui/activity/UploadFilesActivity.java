2fc215fef8298f6589e8e33f8edda624a8b9299b
alperozturk <alper_ozturk@proton.me>
2025-03-20 10:37:13 +0100

    reduce spotbugs
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/ui/activity/UploadFilesActivity.java
+++ app/src/main/java/com/owncloud/android/ui/activity/UploadFilesActivity.java
@@ -566,7 +566,9 @@ public class UploadFilesActivity extends DrawerActivity implements LocalFileList
 
         pushDirname(directory);
         ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
     }
 
     private void checkWritableFolder(File folder) {
