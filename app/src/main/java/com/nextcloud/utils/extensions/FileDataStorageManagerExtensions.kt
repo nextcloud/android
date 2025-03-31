30080b15ec4b187aab19fbf47557b4961ef77a2e
alperozturk <alper_ozturk@proton.me>
2025-03-31 10:54:21 +0200

    fix logic
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/utils/extensions/FileDataStorageManagerExtensions.kt
+++ app/src/main/java/com/nextcloud/utils/extensions/FileDataStorageManagerExtensions.kt
@@ -15,15 +15,13 @@ fun FileDataStorageManager.getDecryptedPath(file: OCFile): String {
     var entity = fileDao.getFileByEncryptedRemotePath(file.remotePath, user.accountName)
 
     while (entity != null) {
-        entity.pathDecrypted?.takeIf { it.isNotEmpty() }?.let { paths.add(it) }
+        entity.name?.takeIf { it.isNotEmpty() }?.let {
+            paths.add(it.removePrefix(OCFile.PATH_SEPARATOR))
+        }
         entity = entity.parent?.let { fileDao.getFileById(it) } ?: break
     }
 
     return paths
         .reversed()
-        .joinToString(OCFile.PATH_SEPARATOR) { path ->
-            path.split(OCFile.PATH_SEPARATOR)
-                .lastOrNull { it.isNotEmpty() }
-                .orEmpty()
-        }
+        .joinToString(OCFile.PATH_SEPARATOR)
 }
