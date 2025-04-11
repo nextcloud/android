7d96534548a27cee9e8ed81f8a224072afd1d4f5
alperozturk <alper_ozturk@proton.me>
2025-04-11 09:13:01 +0200

    apply changes
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

---
+++ app/src/main/java/com/nextcloud/repository/RemoteClientRepository.kt
@@ -0,0 +1,81 @@
+/*
+ * Nextcloud - Android Client
+ *
+ * SPDX-FileCopyrightText: 2025 Alper Ozturk <alper.ozturk@nextcloud.com>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+package com.nextcloud.repository
+
+import android.content.Context
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.coroutineScope
+import com.nextcloud.client.account.User
+import com.nextcloud.common.NextcloudClient
+import com.owncloud.android.lib.common.OwnCloudClient
+import com.owncloud.android.lib.common.OwnCloudClientManagerFactory
+import com.owncloud.android.lib.common.utils.Log_OC
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+@Suppress("TooGenericExceptionCaught", "DEPRECATION")
+class RemoteClientRepository(
+    private val user: User,
+    private val context: Context,
+    private val lifecycleOwner: LifecycleOwner
+) : ClientRepository {
+    private val tag = "ClientRepository"
+
+    override fun getNextcloudClient(onComplete: (NextcloudClient) -> Unit) {
+        lifecycleOwner.lifecycle.coroutineScope.launch(Dispatchers.IO) {
+            try {
+                val client = OwnCloudClientManagerFactory
+                    .getDefaultSingleton()
+                    .getNextcloudClientFor(user.toOwnCloudAccount(), context)
+                onComplete(client)
+            } catch (e: Exception) {
+                Log_OC.d(tag, "Exception caught getNextcloudClient(): $e")
+            }
+        }
+    }
+
+    override suspend fun getNextcloudClient(): NextcloudClient? {
+        return withContext(Dispatchers.IO) {
+            try {
+                OwnCloudClientManagerFactory
+                    .getDefaultSingleton()
+                    .getNextcloudClientFor(user.toOwnCloudAccount(), context)
+            } catch (e: Exception) {
+                Log_OC.d(tag, "Exception caught getNextcloudClient(): $e")
+                null
+            }
+        }
+    }
+
+    override fun getOwncloudClient(onComplete: (OwnCloudClient) -> Unit) {
+        lifecycleOwner.lifecycle.coroutineScope.launch(Dispatchers.IO) {
+            try {
+                val client = OwnCloudClientManagerFactory
+                    .getDefaultSingleton()
+                    .getClientFor(user.toOwnCloudAccount(), context)
+                onComplete(client)
+            } catch (e: Exception) {
+                Log_OC.d(tag, "Exception caught getOwncloudClient(): $e")
+            }
+        }
+    }
+
+    override suspend fun getOwncloudClient(): OwnCloudClient? {
+        return withContext(Dispatchers.IO) {
+            try {
+                OwnCloudClientManagerFactory
+                    .getDefaultSingleton()
+                    .getClientFor(user.toOwnCloudAccount(), context)
+            } catch (e: Exception) {
+                Log_OC.d(tag, "Exception caught getOwncloudClient(): $e")
+                null
+            }
+        }
+    }
+}
--- app/src/main/java/com/owncloud/android/ui/fragment/filesRepository/FilesRepository.kt
+++ app/src/main/java/com/owncloud/android/ui/fragment/filesRepository/FilesRepository.kt
@@ -7,39 +7,18 @@
 
 package com.owncloud.android.ui.fragment.filesRepository
 
-import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.lifecycleScope
-import com.nextcloud.android.lib.resources.recommendations.GetRecommendationsRemoteOperation
 import com.nextcloud.android.lib.resources.recommendations.Recommendation
-import com.nextcloud.repository.ClientRepositoryType
-import com.owncloud.android.lib.common.utils.Log_OC
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
 
-@Suppress("TooGenericExceptionCaught")
-class FilesRepository(private val clientRepository: ClientRepositoryType, private val lifecycleOwner: LifecycleOwner) :
-    FilesRepositoryType {
-    private val tag = "FilesRepository"
+interface FilesRepository {
 
-    override fun fetchRecommendedFiles(onCompleted: (ArrayList<Recommendation>) -> Unit) {
-        lifecycleOwner.lifecycleScope.launch(Dispatchers.IO) {
-            try {
-                val client = clientRepository.getNextcloudClient() ?: return@launch
-                val result = GetRecommendationsRemoteOperation().execute(client)
-                if (result.isSuccess) {
-                    val recommendations = result.getResultData().recommendations
-                    Log_OC.d(tag, "Recommended files fetched size: " + recommendations.size)
-
-                    withContext(Dispatchers.Main) {
-                        onCompleted(recommendations)
-                    }
-                } else {
-                    Log_OC.d(tag, "Recommended files cannot be fetched: " + result.code)
-                }
-            } catch (e: Exception) {
-                Log_OC.d(tag, "Exception caught while fetching recommended files: $e")
-            }
-        }
-    }
+    /**
+     * Fetches a list of recommended files from the Nextcloud server.
+     *
+     * This function runs on the IO dispatcher and retrieves recommendations
+     * using the Nextcloud client. The results are passed to the provided callback on the main thread.
+     *
+     * @param onCompleted A callback function that receives the list of recommended files.
+     *
+     */
+    fun fetchRecommendedFiles(onCompleted: (ArrayList<Recommendation>) -> Unit)
 }
