2fad04215b44617a797ca8854a458e9135d8d9e1
alperozturk <alper_ozturk@proton.me>
2025-04-11 17:09:05 +0200

    use androidx.core
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/utils/extensions/IntentExtensions.kt
+++ app/src/main/java/com/nextcloud/utils/extensions/IntentExtensions.kt
@@ -8,34 +8,16 @@
 package com.nextcloud.utils.extensions
 
 import android.content.Intent
-import android.os.Build
 import android.os.Parcelable
-import com.owncloud.android.lib.common.utils.Log_OC
+import androidx.core.content.IntentCompat
 import java.io.Serializable
-
-@Suppress("TopLevelPropertyNaming")
-private const val TAG = "IntentExtension"
 
 fun <T : Serializable?> Intent?.getSerializableArgument(key: String, type: Class<T>): T? {
     if (this == null) {
         return null
     }
 
-    return try {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            this.getSerializableExtra(key, type)
-        } else {
-            @Suppress("UNCHECKED_CAST", "DEPRECATION")
-            if (type.isInstance(this.getSerializableExtra(key))) {
-                this.getSerializableExtra(key) as T
-            } else {
-                null
-            }
-        }
-    } catch (e: ClassCastException) {
-        Log_OC.e(TAG, e.localizedMessage)
-        null
-    }
+    return IntentCompat.getSerializableExtra(this, key, type)
 }
 
 fun <T : Parcelable?> Intent?.getParcelableArgument(key: String, type: Class<T>): T? {
@@ -43,15 +25,5 @@ fun <T : Parcelable?> Intent?.getParcelableArgument(key: String, type: Class<T>)
         return null
     }
 
-    return try {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            this.getParcelableExtra(key, type)
-        } else {
-            @Suppress("DEPRECATION")
-            this.getParcelableExtra(key)
-        }
-    } catch (e: ClassCastException) {
-        Log_OC.e(TAG, e.localizedMessage)
-        null
-    }
+    return IntentCompat.getParcelableExtra(this, key, type)
 }
