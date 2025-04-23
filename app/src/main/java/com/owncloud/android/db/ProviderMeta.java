3d73c5e910ca0e20b14556fb7fb05298cefe885e
batpio <prbator@gmail.com>
2025-04-23 21:25:24 +0200

    Master merge fixes
    Signed-off-by: batpio <prbator@gmail.com>

--- app/src/main/java/com/owncloud/android/db/ProviderMeta.java
+++ app/src/main/java/com/owncloud/android/db/ProviderMeta.java
@@ -25,7 +25,7 @@ import java.util.List;
  */
 public class ProviderMeta {
     public static final String DB_NAME = "filelist";
-    public static final int DB_VERSION = 88;
+    public static final int DB_VERSION = 89;
 
     private ProviderMeta() {
         // No instance
