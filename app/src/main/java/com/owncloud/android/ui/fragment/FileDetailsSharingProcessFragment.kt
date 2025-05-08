06eee02dc0b2c1bed3f8ff8e0b5ee5e5efd63db4
alperozturk <alper_ozturk@proton.me>
2025-05-08 09:03:17 +0200

    show isSecureFileDrop text if encrypted
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/ui/fragment/FileDetailsSharingProcessFragment.kt
+++ app/src/main/java/com/owncloud/android/ui/fragment/FileDetailsSharingProcessFragment.kt
@@ -176,7 +176,7 @@ class FileDetailsSharingProcessFragment :
 
         permission = share?.permissions
             ?: capabilities.defaultPermissions
-            ?: sharePermissionManager.getMaximumPermission(isFolder())
+                ?: sharePermissionManager.getMaximumPermission(isFolder())
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
@@ -545,7 +545,7 @@ class FileDetailsSharingProcessFragment :
     private fun checkNextButtonAvailability() {
         var hasAnyPermission = false
         if (share != null) {
-            hasAnyPermission = SharingMenuHelper.getPermissionName(requireContext(), share) != null
+            hasAnyPermission = SharingMenuHelper.getPermissionName(requireContext(), share, isSecureShare) != null
         }
         toggleNextButtonAvailability(hasAnyPermission)
     }
