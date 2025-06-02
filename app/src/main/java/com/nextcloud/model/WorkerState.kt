2f20b7a1939c1a80b62fd096822bbfe33779a42b
alperozturk <alper_ozturk@proton.me>
2025-06-02 16:01:35 +0800

    simplify worker logic
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/model/WorkerState.kt
+++ app/src/main/java/com/nextcloud/model/WorkerState.kt
@@ -9,13 +9,12 @@ package com.nextcloud.model
 
 import com.nextcloud.client.account.User
 import com.owncloud.android.datamodel.OCFile
-import com.owncloud.android.db.OCUpload
 import com.owncloud.android.operations.DownloadFileOperation
 
 sealed class WorkerState {
     data class DownloadFinished(var currentFile: OCFile?) : WorkerState()
     data class DownloadStarted(var user: User?, var currentDownload: DownloadFileOperation?) : WorkerState()
     data class UploadFinished(var currentFile: OCFile?) : WorkerState()
-    data class UploadStarted(var user: User?, var uploads: List<OCUpload>) : WorkerState()
+    data class UploadStarted(var user: User?) : WorkerState()
     data object OfflineOperationsCompleted : WorkerState()
 }
