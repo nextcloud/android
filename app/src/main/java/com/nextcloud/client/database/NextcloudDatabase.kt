62d6fcc80d354b817e2fa11f4b323d80ec61a8d6
tobiasKaminsky <tobias@kaminsky.me>
2025-06-03 07:01:35 +0200

    Fail on missing migration
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- app/src/main/java/com/nextcloud/client/database/NextcloudDatabase.kt
+++ app/src/main/java/com/nextcloud/client/database/NextcloudDatabase.kt
@@ -104,7 +104,6 @@ abstract class NextcloudDatabase : RoomDatabase() {
                     .addLegacyMigrations(clock, context)
                     .addMigrations(RoomMigration())
                     .addMigrations(Migration67to68())
-                    .fallbackToDestructiveMigration()
                     .build()
             }
             return instance!!
