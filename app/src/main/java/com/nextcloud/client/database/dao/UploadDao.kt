e039ca1df5b6cba0b70f2b1ea308bb0240caf4c1
alperozturk <alper_ozturk@proton.me>
2025-06-04 12:47:51 +0800

    remove unused functions and use dao instead of content resolver
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

---
+++ app/src/main/java/com/nextcloud/client/database/dao/UploadDao.kt
@@ -0,0 +1,22 @@
+/*
+ * Nextcloud - Android Client
+ *
+ * SPDX-FileCopyrightText: 2025 Alper Ozturk <alper.ozturk@nextcloud.com>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+package com.nextcloud.client.database.dao
+
+import androidx.room.Dao
+import androidx.room.Query
+import com.owncloud.android.db.ProviderMeta.ProviderTableMeta
+
+@Dao
+interface UploadDao {
+    @Query(
+        "SELECT _id FROM " + ProviderTableMeta.UPLOADS_TABLE_NAME +
+            " WHERE " + ProviderTableMeta.UPLOADS_STATUS + " = :status AND " +
+            ProviderTableMeta.UPLOADS_ACCOUNT_NAME + " = :accountName AND _id IS NOT NULL"
+    )
+    fun getAllIds(status: Int, accountName: String): List<Int>
+}
