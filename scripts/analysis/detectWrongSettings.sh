08e85abe063585246d518c6b51cc5dd3948bd60a
alperozturk <alper_ozturk@proton.me>
2025-06-05 18:43:02 +0800

    update android libs path
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- scripts/analysis/detectWrongSettings.sh
+++ scripts/analysis/detectWrongSettings.sh
@@ -9,10 +9,10 @@ PROJECT_ROOT="$SCRIPT_DIR/../../"
 
 snapshotCount=$("$PROJECT_ROOT/gradlew" -p "$PROJECT_ROOT" dependencies | grep SNAPSHOT -c)
 betaCount=$(grep "<bool name=\"is_beta\">true</bool>" "$PROJECT_ROOT/app/src/main/res/values/setup.xml" -c)
-libraryHash=$(grep androidLibraryVersion "$PROJECT_ROOT/gradle/libs.versions.toml" | cut -d= -f2 | tr -d \"")
+libraryHash=$(grep androidLibraryVersion "$PROJECT_ROOT/gradle/libs.versions.toml" | cut -d= -f2 | tr -d \")
 
-lastHashes=$(curl "https://api.github.com/repos/nextcloud/android-library/commits?sha=$baseBranch" | jq ".[] .sha" | head -n 20)
 baseBranch="master"
+lastHashes=$(curl "https://api.github.com/repos/nextcloud/android-library/commits?sha=$baseBranch" | jq ".[] .sha" | head -n 20)
 
 if [[ $(echo "$lastHashes"  | grep -c $libraryHash) -ne 1  ]]; then
     echo "Library commit not within last 10 hashes, please rebase!"
@@ -27,6 +27,4 @@ if [[ $betaCount -gt 0 ]] ; then
     echo "Beta is set in setup.xml"
     exit 1
 fi
- 
 exit 0
-
