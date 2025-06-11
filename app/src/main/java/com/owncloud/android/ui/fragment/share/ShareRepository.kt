8445793a765bcd322c1acef4d62d849ccfe318a7
alperozturk <alper_ozturk@proton.me>
2025-06-11 13:46:17 +0200

    refreshSharesForFolder in share details
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

---
+++ app/src/main/java/com/owncloud/android/ui/fragment/share/ShareRepository.kt
@@ -0,0 +1,12 @@
+/*
+ * Nextcloud - Android Client
+ *
+ * SPDX-FileCopyrightText: 2025 Alper Ozturk <alper.ozturk@nextcloud.com>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+package com.owncloud.android.ui.fragment.share
+
+interface ShareRepository {
+    fun refreshSharesForFolder(remotePath: String)
+}
