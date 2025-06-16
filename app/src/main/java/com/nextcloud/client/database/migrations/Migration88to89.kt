bdae1345fadc2abcbdc78f1a1f7ec7dc7cc0cf80
alperozturk <alper_ozturk@proton.me>
2025-06-16 16:37:28 +0200

    addMissingNotesFolderPathToCapabilitiesEntity
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/client/database/migrations/Migration88to89.kt
+++ app/src/main/java/com/nextcloud/client/database/migrations/Migration88to89.kt
@@ -13,6 +13,11 @@ import com.owncloud.android.db.ProviderMeta.ProviderTableMeta
 
 val MIGRATION_88_89 = object : Migration(88, 89) {
     override fun migrate(database: SupportSQLiteDatabase) {
+        addUploadedColumnIfNotExists(database)
+        addMissingNotesFolderPathToCapabilitiesEntity(database)
+    }
+
+    fun addUploadedColumnIfNotExists(database: SupportSQLiteDatabase) {
         val tableName = ProviderTableMeta.FILE_TABLE_NAME
         val cursor = database.query("PRAGMA table_info($tableName)")
         var columnExists = false
@@ -34,4 +39,29 @@ val MIGRATION_88_89 = object : Migration(88, 89) {
                 .execSQL("ALTER TABLE $tableName ADD COLUMN `${ProviderTableMeta.FILE_UPLOADED}` INTEGER DEFAULT NULL")
         }
     }
+
+    fun addMissingNotesFolderPathToCapabilitiesEntity(database: SupportSQLiteDatabase) {
+        val tableName = ProviderTableMeta.CAPABILITIES_TABLE_NAME
+        val columnName = ProviderTableMeta.CAPABILITIES_NOTES_FOLDER_PATH
+
+        val cursor = database.query("PRAGMA table_info($tableName)")
+        var columnExists = false
+
+        while (cursor.moveToNext()) {
+            val nameIndex = cursor.getColumnIndex("name")
+            if (nameIndex != -1) {
+                val existingColumnName = cursor.getString(nameIndex)
+                if (existingColumnName == columnName) {
+                    columnExists = true
+                    break
+                }
+            }
+        }
+        cursor.close()
+
+        if (!columnExists) {
+            database.execSQL("ALTER TABLE $tableName ADD COLUMN `$columnName` TEXT DEFAULT NULL")
+        }
+    }
+
 }
