784c92aa755ecaf1e9dab4f9fab4d7e92e4a3d43
alperozturk <alper_ozturk@proton.me>
2025-06-16 16:42:38 +0200

    extract common logic
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/client/database/migrations/Migration88to89.kt
+++ app/src/main/java/com/nextcloud/client/database/migrations/Migration88to89.kt
@@ -9,59 +9,21 @@ package com.nextcloud.client.database.migrations
 
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
+import com.nextcloud.client.database.migrations.model.SQLiteColumnType
+import com.nextcloud.utils.extensions.addColumnIfNotExists
 import com.owncloud.android.db.ProviderMeta.ProviderTableMeta
 
 val MIGRATION_88_89 = object : Migration(88, 89) {
     override fun migrate(database: SupportSQLiteDatabase) {
-        addUploadedColumnIfNotExists(database)
-        addMissingNotesFolderPathToCapabilitiesEntity(database)
+        database.addColumnIfNotExists(
+            ProviderTableMeta.FILE_TABLE_NAME,
+            ProviderTableMeta.FILE_UPLOADED,
+            SQLiteColumnType.INTEGER_DEFAULT_NULL
+        )
+        database.addColumnIfNotExists(
+            ProviderTableMeta.CAPABILITIES_TABLE_NAME,
+            ProviderTableMeta.CAPABILITIES_NOTES_FOLDER_PATH,
+            SQLiteColumnType.TEXT_DEFAULT_NULL
+        )
     }
-
-    fun addUploadedColumnIfNotExists(database: SupportSQLiteDatabase) {
-        val tableName = ProviderTableMeta.FILE_TABLE_NAME
-        val cursor = database.query("PRAGMA table_info($tableName)")
-        var columnExists = false
-
-        while (cursor.moveToNext()) {
-            val nameIndex = cursor.getColumnIndex("name")
-            if (nameIndex != -1) {
-                val existingColumnName = cursor.getString(nameIndex)
-                if (existingColumnName == ProviderTableMeta.FILE_UPLOADED) {
-                    columnExists = true
-                    break
-                }
-            }
-        }
-        cursor.close()
-
-        if (!columnExists) {
-            database
-                .execSQL("ALTER TABLE $tableName ADD COLUMN `${ProviderTableMeta.FILE_UPLOADED}` INTEGER DEFAULT NULL")
-        }
-    }
-
-    fun addMissingNotesFolderPathToCapabilitiesEntity(database: SupportSQLiteDatabase) {
-        val tableName = ProviderTableMeta.CAPABILITIES_TABLE_NAME
-        val columnName = ProviderTableMeta.CAPABILITIES_NOTES_FOLDER_PATH
-
-        val cursor = database.query("PRAGMA table_info($tableName)")
-        var columnExists = false
-
-        while (cursor.moveToNext()) {
-            val nameIndex = cursor.getColumnIndex("name")
-            if (nameIndex != -1) {
-                val existingColumnName = cursor.getString(nameIndex)
-                if (existingColumnName == columnName) {
-                    columnExists = true
-                    break
-                }
-            }
-        }
-        cursor.close()
-
-        if (!columnExists) {
-            database.execSQL("ALTER TABLE $tableName ADD COLUMN `$columnName` TEXT DEFAULT NULL")
-        }
-    }
-
 }
