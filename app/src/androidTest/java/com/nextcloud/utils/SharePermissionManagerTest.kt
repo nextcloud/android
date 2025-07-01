e9b1609756f0efd847f35847f2efe1d56ea1497a
alperozturk <alper_ozturk@proton.me>
2025-07-01 10:00:02 +0200

    fix share creation via allow download and sync option
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/androidTest/java/com/nextcloud/utils/SharePermissionManagerTest.kt
+++ app/src/androidTest/java/com/nextcloud/utils/SharePermissionManagerTest.kt
@@ -255,7 +255,8 @@ class SharePermissionManagerTest {
             shareType = ShareType.USER
             permissions = 17
         }
-        ocShare.attributes = ocShare.toggleAllowDownloadAndSync(
+        ocShare.attributes = toggleAllowDownloadAndSync(
+            ocShare.attributes,
             isChecked = true,
             useV2DownloadAttributes = false
         )
