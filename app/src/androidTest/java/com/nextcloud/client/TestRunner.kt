ca3fbbdfa5ff929b71403003e403ed10992bd03f
alperozturk <alper_ozturk@proton.me>
2025-07-03 15:40:16 +0200

    use kotlin-all-open
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/androidTest/java/com/nextcloud/client/TestRunner.kt
+++ app/src/androidTest/java/com/nextcloud/client/TestRunner.kt
@@ -9,23 +9,15 @@
 package com.nextcloud.client
 
 import android.app.Application
-import android.app.Instrumentation
 import android.content.Context
-import android.os.Build
+import androidx.annotation.OpenForTesting
 import androidx.test.runner.AndroidJUnitRunner
-import com.github.tmurakami.dexopener.DexOpener
 import com.nextcloud.test.TestMainApp
 
+@OpenForTesting
 class TestRunner : AndroidJUnitRunner() {
     @Throws(ClassNotFoundException::class, IllegalAccessException::class, InstantiationException::class)
     override fun newApplication(cl: ClassLoader, className: String, context: Context): Application {
-        /*
-         * Initialize DexOpener only on API below 28 to enable mocking of Kotlin classes.
-         * On API 28+ the platform supports mocking natively.
-         */
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.P) {
-            DexOpener.install(this)
-        }
-        return Instrumentation.newApplication(TestMainApp::class.java, context)
+        return newApplication(TestMainApp::class.java, context)
     }
 }
