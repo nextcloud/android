9c355f235eef9d3be1a5b2e15f19df7f73fe8abd
alperozturk <alper_ozturk@proton.me>
2025-07-07 11:11:00 +0200

    fix permission handling issues
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/ui/adapter/LinkShareViewHolder.kt
+++ app/src/main/java/com/owncloud/android/ui/adapter/LinkShareViewHolder.kt
@@ -11,109 +11,163 @@
  *
  * SPDX-License-Identifier: AGPL-3.0-or-later OR GPL-2.0-only
  */
+package com.owncloud.android.ui.adapter
 
-package com.owncloud.android.ui.adapter;
+import android.content.Context
+import android.text.TextUtils
+import android.view.View
+import androidx.core.content.res.ResourcesCompat
+import androidx.recyclerview.widget.RecyclerView
+import com.nextcloud.utils.mdm.MDMConfig
+import com.owncloud.android.R
+import com.owncloud.android.databinding.FileDetailsShareLinkShareItemBinding
+import com.owncloud.android.datamodel.quickPermission.QuickPermissionType
+import com.owncloud.android.lib.resources.shares.OCShare
+import com.owncloud.android.lib.resources.shares.ShareType
+import com.owncloud.android.ui.fragment.util.SharePermissionManager.getSelectedType
+import com.owncloud.android.ui.fragment.util.SharePermissionManager.isSecureFileDrop
+import com.owncloud.android.utils.theme.ViewThemeUtils
 
-import android.content.Context;
-import android.text.TextUtils;
-import android.view.View;
+internal class LinkShareViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    private var binding: FileDetailsShareLinkShareItemBinding? = null
+    private var context: Context? = null
+    private var viewThemeUtils: ViewThemeUtils? = null
+    private var encrypted = false
 
-import com.nextcloud.android.lib.resources.files.FileDownloadLimit;
-import com.nextcloud.utils.mdm.MDMConfig;
-import com.owncloud.android.R;
-import com.owncloud.android.databinding.FileDetailsShareLinkShareItemBinding;
-import com.owncloud.android.datamodel.quickPermission.QuickPermissionType;
-import com.owncloud.android.lib.resources.shares.OCShare;
-import com.owncloud.android.lib.resources.shares.ShareType;
-import com.owncloud.android.ui.fragment.util.SharePermissionManager;
-import com.owncloud.android.utils.theme.ViewThemeUtils;
-
-import androidx.annotation.NonNull;
-import androidx.core.content.res.ResourcesCompat;
-import androidx.recyclerview.widget.RecyclerView;
-
-class LinkShareViewHolder extends RecyclerView.ViewHolder {
-    private FileDetailsShareLinkShareItemBinding binding;
-    private Context context;
-    private ViewThemeUtils viewThemeUtils;
-    private boolean encrypted;
-
-    public LinkShareViewHolder(@NonNull View itemView) {
-        super(itemView);
+    constructor(
+        binding: FileDetailsShareLinkShareItemBinding,
+        context: Context,
+        viewThemeUtils: ViewThemeUtils,
+        encrypted: Boolean
+    ) : this(binding.getRoot()) {
+        this.binding = binding
+        this.context = context
+        this.viewThemeUtils = viewThemeUtils
+        this.encrypted = encrypted
     }
 
-    public LinkShareViewHolder(FileDetailsShareLinkShareItemBinding binding,
-                               Context context,
-                               final ViewThemeUtils viewThemeUtils,
-                               boolean encrypted) {
-        this(binding.getRoot());
-        this.binding = binding;
-        this.context = context;
-        this.viewThemeUtils = viewThemeUtils;
-        this.encrypted = encrypted;
+    fun bind(publicShare: OCShare, listener: ShareeListAdapterListener, position: Int) {
+        val quickPermissionType = getSelectedType(publicShare, encrypted)
+
+        setName(binding, context, publicShare, quickPermissionType, position)
+        setSubline(binding, context, publicShare)
+        setPermissionName(binding, context, publicShare, quickPermissionType)
+        setOnClickListeners(binding, listener, publicShare)
+        configureCopyLink(binding, context, listener, publicShare)
     }
 
-    public void bind(OCShare publicShare, ShareeListAdapterListener listener, int position) {
-        if (ShareType.EMAIL == publicShare.getShareType()) {
-            final var res = context.getResources();
-            binding.name.setText(publicShare.getSharedWithDisplayName());
+    private fun setName(
+        binding: FileDetailsShareLinkShareItemBinding?,
+        context: Context?,
+        publicShare: OCShare,
+        quickPermissionType: QuickPermissionType,
+        position: Int
+    ) {
+        if (binding == null || context == null) {
+            return
+        }
 
-            final var emailDrawable = ResourcesCompat.getDrawable(res, R.drawable.ic_email, null);
-            binding.icon.setImageDrawable(emailDrawable);
-            binding.copyLink.setVisibility(View.GONE);
+        if (ShareType.EMAIL == publicShare.shareType) {
+            val res = context.resources
+            binding.name.text = publicShare.sharedWithDisplayName
+
+            val emailDrawable = ResourcesCompat.getDrawable(res, R.drawable.ic_email, null)
+            binding.icon.setImageDrawable(emailDrawable)
+            binding.copyLink.setVisibility(View.GONE)
         } else {
-            String label = publicShare.getLabel();
+            val label = publicShare.label
 
             if (!TextUtils.isEmpty(label)) {
-                binding.name.setText(context.getString(R.string.share_link_with_label, label));
-            } else if (SharePermissionManager.INSTANCE.isFileRequest(publicShare)) {
-                binding.name.setText(R.string.share_permission_file_request);
-            } else if (SharePermissionManager.INSTANCE.isSecureFileDrop(publicShare) && encrypted) {
-                binding.name.setText(R.string.share_permission_secure_file_drop);
+                binding.name.text = context.getString(R.string.share_link_with_label, label)
+            } else if (quickPermissionType != QuickPermissionType.NONE) {
+                binding.name.text = quickPermissionType.getText(context)
             } else {
-                int textRes = (position == 0) ? R.string.share_link : R.string.share_link_with_label;
-                Object arg = (position == 0) ? null : String.valueOf(position);
-                binding.name.setText((position == 0) ? context.getString(textRes)
-                                         : context.getString(textRes, arg));
+                val textRes = if (position == 0) R.string.share_link else R.string.share_link_with_label
+                val arg = if (position == 0) null else position.toString()
+                binding.name.text = if (position == 0)
+                    context.getString(textRes)
+                else
+                    context.getString(textRes, arg)
             }
-        }
-
-        viewThemeUtils.platform.colorImageViewBackgroundAndIcon(binding.icon);
-
-        FileDownloadLimit downloadLimit = publicShare.getFileDownloadLimit();
-        if (downloadLimit != null && downloadLimit.getLimit() > 0) {
-            int remaining = downloadLimit.getLimit() - downloadLimit.getCount();
-            String text = context.getResources().getQuantityString(R.plurals.share_download_limit_description, remaining, remaining);
-
-            binding.subline.setText(text);
-            binding.subline.setVisibility(View.VISIBLE);
-        } else {
-            binding.subline.setVisibility(View.GONE);
-        }
-
-        QuickPermissionType quickPermissionType = SharePermissionManager.INSTANCE.getSelectedType(publicShare, encrypted);
-        setPermissionName(publicShare, quickPermissionType.getText(context));
-
-        binding.overflowMenu.setOnClickListener(v -> listener.showSharingMenuActionSheet(publicShare));
-        if (!SharePermissionManager.INSTANCE.isSecureFileDrop(publicShare) && !encrypted) {
-            binding.shareByLinkContainer.setOnClickListener(v -> listener.showPermissionsDialog(publicShare));
-        }
-
-        if (MDMConfig.INSTANCE.clipBoardSupport(context)) {
-            binding.copyLink.setOnClickListener(v -> listener.copyLink(publicShare));
-        } else {
-            binding.copyLink.setVisibility(View.GONE);
         }
     }
 
-    private void setPermissionName(OCShare publicShare, String permissionName) {
-        if (TextUtils.isEmpty(permissionName) || (SharePermissionManager.INSTANCE.isSecureFileDrop(publicShare) && encrypted)) {
-            binding.permissionName.setVisibility(View.GONE);
-            return;
+    private fun setSubline(binding: FileDetailsShareLinkShareItemBinding?, context: Context?, publicShare: OCShare) {
+        if (binding == null || context == null) {
+            return
         }
 
-        binding.permissionName.setText(permissionName);
-        binding.permissionName.setVisibility(View.VISIBLE);
-        viewThemeUtils.androidx.colorPrimaryTextViewElement(binding.permissionName);
+        val downloadLimit = publicShare.fileDownloadLimit
+        if (downloadLimit != null && downloadLimit.limit > 0) {
+            val remaining = downloadLimit.limit - downloadLimit.count
+            val text = context.resources.getQuantityString(
+                R.plurals.share_download_limit_description,
+                remaining,
+                remaining
+            )
+
+            binding.subline.text = text
+            binding.subline.visibility = View.VISIBLE
+        } else {
+            binding.subline.visibility = View.GONE
+        }
+    }
+
+    private fun setPermissionName(
+        binding: FileDetailsShareLinkShareItemBinding?,
+        context: Context?,
+        publicShare: OCShare?,
+        quickPermissionType: QuickPermissionType
+    ) {
+        if (binding == null || context == null) {
+            return
+        }
+
+        val permissionName = quickPermissionType.getText(context)
+
+        if (TextUtils.isEmpty(permissionName) || (isSecureFileDrop(publicShare) && encrypted)) {
+            binding.permissionName.visibility = View.GONE
+            return
+        }
+
+        binding.permissionName.text = permissionName
+        binding.permissionName.visibility = View.VISIBLE
+        viewThemeUtils?.androidx?.colorPrimaryTextViewElement(binding.permissionName)
+    }
+
+    private fun setOnClickListeners(
+        binding: FileDetailsShareLinkShareItemBinding?,
+        listener: ShareeListAdapterListener,
+        publicShare: OCShare,
+    ) {
+        if (binding == null) {
+            return
+        }
+
+        viewThemeUtils?.platform?.colorImageViewBackgroundAndIcon(binding.icon)
+
+        binding.overflowMenu.setOnClickListener {
+            listener.showSharingMenuActionSheet(publicShare)
+        }
+        binding.shareByLinkContainer.setOnClickListener {
+            listener.showPermissionsDialog(publicShare)
+        }
+    }
+
+    private fun configureCopyLink(
+        binding: FileDetailsShareLinkShareItemBinding?,
+        context: Context?,
+        listener: ShareeListAdapterListener,
+        publicShare: OCShare,
+    ) {
+        if (binding == null || context == null) {
+            return
+        }
+
+        if (MDMConfig.clipBoardSupport(context)) {
+            binding.copyLink.setOnClickListener { v: View? -> listener.copyLink(publicShare) }
+        } else {
+            binding.copyLink.setVisibility(View.GONE)
+        }
     }
 }
