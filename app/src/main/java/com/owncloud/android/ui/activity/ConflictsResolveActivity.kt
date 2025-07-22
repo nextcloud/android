4c9c5102cb842f45d38c125bf6b5f8eeab7bc958
alperozturk <alper_ozturk@proton.me>
2025-07-22 12:29:21 +0200

    fix git conflict
    Signed-off-by: alperozturk <alper_ozturk@proton.me>
    
    [skip ci]

--- app/src/main/java/com/owncloud/android/ui/activity/ConflictsResolveActivity.kt
+++ app/src/main/java/com/owncloud/android/ui/activity/ConflictsResolveActivity.kt
@@ -147,7 +147,7 @@ class ConflictsResolveActivity : FileActivity(), OnConflictDecisionMadeListener 
     }
 
     private fun dismissConflictResolveNotification(file: OCFile?) {
-        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
         val tag = NotificationUtils.createUploadNotificationTag(file)
         notificationManager.cancel(tag, FileUploadWorker.NOTIFICATION_ERROR_ID)
     }
