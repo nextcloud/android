caed14ed9d292f231d50b3a3d2705f146026e35c
Alex Knop <knopalex@msu.edu>
2025-07-23 20:43:04 -0400

    timeouts
    Signed-off-by: Alex Knop <knopalex@msu.edu>

--- app/src/main/java/com/owncloud/android/operations/RefreshFolderOperation.java
+++ app/src/main/java/com/owncloud/android/operations/RefreshFolderOperation.java
@@ -373,7 +373,7 @@ public class RefreshFolderOperation extends RemoteOperation {
         Log_OC.d(TAG, "Checking changes in " + user.getAccountName() + remotePath);
 
         // remote request
-        result = new ReadFileRemoteOperation(remotePath).execute(client);
+        result = new ReadFileRemoteOperation(remotePath, new SessionTimeOut(3000,3000S)).execute(client);
 
         if (result.isSuccess()) {
             OCFile remoteFolder = FileStorageUtils.fillOCFile((RemoteFile) result.getData().get(0));
