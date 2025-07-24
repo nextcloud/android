77a367bb1e119121a975a990a257a6520e891b65
alperozturk <alper_ozturk@proton.me>
2025-07-24 14:23:54 +0200

    better debug
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/utils/OCFileUtils.kt
+++ app/src/main/java/com/nextcloud/utils/OCFileUtils.kt
@@ -10,41 +10,54 @@ import androidx.exifinterface.media.ExifInterface
 import com.owncloud.android.datamodel.OCFile
 import com.owncloud.android.lib.common.utils.Log_OC
 import com.owncloud.android.utils.BitmapUtils
-import java.io.File
 
 object OCFileUtils {
     private const val TAG = "OCFileUtils"
 
     @Suppress("ReturnCount")
     fun getImageSize(ocFile: OCFile, defaultThumbnailSize: Float): Pair<Int, Int> {
-        val storagePath = ocFile.storagePath ?: return fallbackSize(ocFile, defaultThumbnailSize)
+        try {
+            Log_OC.d(TAG, "Getting image size for: ${ocFile.fileName}")
 
-        if (!File(storagePath).exists()) {
-            Log_OC.d(TAG, "File not downloaded. Using imageDimension or default size.")
-            return fallbackSize(ocFile, defaultThumbnailSize)
-        }
+            if (!ocFile.exists()) {
+                Log_OC.d(TAG, "File not downloaded, using fallbackSize.")
+                return fallbackSize(ocFile, defaultThumbnailSize)
+            }
 
-        val exif = ExifInterface(storagePath)
-        val width = exif.getAttributeInt(ExifInterface.TAG_IMAGE_WIDTH, 0)
-        val height = exif.getAttributeInt(ExifInterface.TAG_IMAGE_LENGTH, 0)
+            val exif = ExifInterface(ocFile.storagePath)
+            val width = exif.getAttributeInt(ExifInterface.TAG_IMAGE_WIDTH, 0)
+            val height = exif.getAttributeInt(ExifInterface.TAG_IMAGE_LENGTH, 0)
 
-        return if (width > 0 && height > 0) {
-            Log_OC.d(TAG, "Exif used to determine width and height")
-            width to height
-        } else {
-            Log_OC.d(TAG, "Exif data missing. Using fallback size.")
-            fallbackSize(ocFile, defaultThumbnailSize)
+            return if (width > 0 && height > 0) {
+                Log_OC.d(TAG, "Exif used width: $width and height: $height")
+                width to height
+            } else {
+                Log_OC.d(TAG, "Exif data missing using fallbackSize.")
+                fallbackSize(ocFile, defaultThumbnailSize)
+            }
+        } finally {
+            Log_OC.d(TAG, "-----------------------------")
         }
     }
 
     private fun fallbackSize(file: OCFile, defaultSize: Float): Pair<Int, Int> {
-        val path = file.storagePath
-        if (path != null) {
-            val (w, h) = BitmapUtils.getImageResolution(path).let { it[0] to it[1] }
-            if (w > 0 && h > 0) return w to h
+        if (file.exists()) {
+            val (width, height) = BitmapUtils.getImageResolution(file.storagePath).let { it[0] to it[1] }
+            if (width > 0 && height > 0) {
+                Log_OC.d(TAG, "BitmapUtils.getImageResolution used width: $width and height: $height")
+                return width to height
+            }
         }
-        val width = (file.imageDimension?.width ?: defaultSize).toInt()
-        val height = (file.imageDimension?.height ?: defaultSize).toInt()
-        return width to height
+
+        file.imageDimension?.width?.let { width ->
+            file.imageDimension?.height?.let { height ->
+                Log_OC.d(TAG, "Image dimension used width: $width and height: $height")
+                return width.toInt() to height.toInt()
+            }
+        }
+
+        Log_OC.d(TAG, "Default size used width: $defaultSize and height: $defaultSize")
+        val defaultSize = defaultSize.toInt()
+        return defaultSize to defaultSize
     }
 }
