a1f14417db560f724372adf48945badf00cacca4
alperozturk <alper_ozturk@proton.me>
2025-07-25 15:31:00 +0200

    movetaskback in fda
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/utils/extensions/DrawerActivityExtensions.kt
+++ app/src/main/java/com/nextcloud/utils/extensions/DrawerActivityExtensions.kt
@@ -7,9 +7,11 @@
 
 package com.nextcloud.utils.extensions
 
+import android.content.Intent
 import com.owncloud.android.MainApp
 import com.owncloud.android.R
 import com.owncloud.android.ui.activity.DrawerActivity
+import com.owncloud.android.ui.activity.FileDisplayActivity
 
 /**
  * Determines the appropriate menu item ID based on the current ActionBar title.
@@ -48,5 +50,25 @@ fun DrawerActivity.getMenuItemIdFromTitle(): Int? {
                 DrawerActivity.menuItemId
             }
         }
+    }
+}
+
+fun DrawerActivity.showAllFiles() {
+    if (DrawerActivity.menuItemId == R.id.nav_all_files) {
+        moveTaskToBack(true)
+        return
+    }
+
+    DrawerActivity.menuItemId = R.id.nav_all_files
+    setNavigationViewItemChecked()
+
+    MainApp.showOnlyFilesOnDevice(false)
+    MainApp.showOnlyPersonalFiles(false)
+
+    Intent(applicationContext, FileDisplayActivity::class.java).apply {
+        addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        setAction(FileDisplayActivity.ALL_FILES)
+    }.run {
+        startActivity(this)
     }
 }
