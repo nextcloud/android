266701add339468ed0755a8e7546e7b73d514786
alperozturk <alper_ozturk@proton.me>
2025-07-29 11:07:47 +0200

    fix
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/authentication/LoginUrlInfo.kt
+++ app/src/main/java/com/owncloud/android/authentication/LoginUrlInfo.kt
@@ -1,17 +1,18 @@
 /*
  * Nextcloud - Android Client
  *
+ * SPDX-FileCopyrightText: 2025 Alper Ozturk <alper.ozturk@nextcloud.com>
  * SPDX-FileCopyrightText: 2016 Andy Scherzinger <info@andy-scherzinger.de>
  * SPDX-FileCopyrightText: 2016 Nextcloud
  * SPDX-License-Identifier: AGPL-3.0-or-later OR GPL-2.0-only
  */
-package com.owncloud.android.authentication;
+package com.owncloud.android.authentication
 
-/**
- * Data object holding the login url fields.
- */
-public class LoginUrlInfo {
-    String serverAddress;
-    String username;
-    String password;
+data class LoginUrlInfo(
+    var server: String,
+    var loginName: String,
+    var appPassword: String
+) {
+    fun isValid(status: Int): Boolean =
+        (status == 200 && server.isNotEmpty() && loginName.isNotEmpty() && appPassword.isNotEmpty())
 }
