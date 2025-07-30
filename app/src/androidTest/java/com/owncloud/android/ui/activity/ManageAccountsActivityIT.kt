b78f5ac37b807f037c65a53b57b8be505499e54a
alperozturk <alper_ozturk@proton.me>
2025-07-30 14:50:33 +0200

    ManageAccountsActivityIT
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/androidTest/java/com/owncloud/android/ui/activity/ManageAccountsActivityIT.kt
+++ app/src/androidTest/java/com/owncloud/android/ui/activity/ManageAccountsActivityIT.kt
@@ -1,66 +1,76 @@
 /*
  * Nextcloud - Android Client
  *
+ * SPDX-FileCopyrightText: 2025 Alper Ozturk <alper.ozturk@nextcloud.com>
  * SPDX-FileCopyrightText: 2020 Tobias Kaminsky <tobias@kaminsky.me>
  * SPDX-FileCopyrightText: 2020 Nextcloud GmbH
  * SPDX-License-Identifier: AGPL-3.0-or-later OR GPL-2.0-only
  */
-package com.owncloud.android.ui.activity;
+package com.owncloud.android.ui.activity
 
-import android.app.Activity;
+import androidx.annotation.UiThread
+import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isRoot
+import com.owncloud.android.AbstractIT
+import com.owncloud.android.lib.common.Quota
+import com.owncloud.android.lib.common.UserInfo
+import com.owncloud.android.utils.EspressoIdlingResource
+import com.owncloud.android.utils.ScreenshotTest
+import org.junit.Test
 
-import com.nextcloud.client.account.User;
-import com.owncloud.android.AbstractIT;
-import com.owncloud.android.lib.common.Quota;
-import com.owncloud.android.lib.common.UserInfo;
-import com.owncloud.android.utils.ScreenshotTest;
-
-import org.junit.Rule;
-import org.junit.Test;
-
-import java.util.ArrayList;
-
-import androidx.test.espresso.intent.rule.IntentsTestRule;
-
-public class ManageAccountsActivityIT extends AbstractIT {
-    @Rule
-    public IntentsTestRule<ManageAccountsActivity> activityRule = new IntentsTestRule<>(ManageAccountsActivity.class,
-                                                                                        true,
-                                                                                        false);
+class ManageAccountsActivityIT : AbstractIT() {
+    private val testClassName = "com.owncloud.android.ui.activity.ManageAccountsActivityIT"
 
     @Test
+    @UiThread
     @ScreenshotTest
-    public void open() {
-        Activity sut = activityRule.launchActivity(null);
-
-        shortSleep();
-
-        screenshot(sut);
+    fun open() {
+        launchActivity<ManageAccountsActivity>().use { scenario ->
+            scenario.onActivity { sut ->
+                onIdleSync {
+                    val screenShotName = createName(testClassName + "_" + "open", "")
+                    onView(isRoot()).check(matches(isDisplayed()))
+                    screenshotViaName(sut, screenShotName)
+                }
+            }
+        }
     }
 
     @Test
+    @UiThread
     @ScreenshotTest
-    public void userInfoDetail() {
-        ManageAccountsActivity sut = activityRule.launchActivity(null);
+    fun userInfoDetail() {
+        launchActivity<ManageAccountsActivity>().use { scenario ->
+            scenario.onActivity { sut ->
+                onIdleSync {
+                    EspressoIdlingResource.increment()
 
-        User user = sut.accountManager.getUser();
+                    val user = sut.accountManager.user
 
-        UserInfo userInfo = new UserInfo("test",
-                                         true,
-                                         "Test User",
-                                         "test@nextcloud.com",
-                                         "+49 123 456",
-                                         "Address 123, Berlin",
-                                         "https://www.nextcloud.com",
-                                         "https://twitter.com/Nextclouders",
-                                         new Quota(),
-                                         new ArrayList<>());
+                    val userInfo = UserInfo(
+                        "test",
+                        true,
+                        "Test User",
+                        "test@nextcloud.com",
+                        "+49 123 456",
+                        "Address 123, Berlin",
+                        "https://www.nextcloud.com",
+                        "https://twitter.com/Nextclouders",
+                        Quota(),
+                        ArrayList()
+                    )
+                    EspressoIdlingResource.decrement()
 
-        sut.showUser(user, userInfo);
+                    sut.showUser(user, userInfo)
 
-        shortSleep();
-        shortSleep();
-
-        screenshot(getCurrentActivity());
+                    val screenShotName = createName(testClassName + "_" + "open", "")
+                    onView(isRoot()).check(matches(isDisplayed()))
+                    screenshotViaName(getCurrentActivity(), screenShotName)
+                }
+            }
+        }
     }
 }
