d23eaa78eb8240e6601e72cc1b2d53b2e0bb0d42
alperozturk <alper_ozturk@proton.me>
2025-07-30 15:01:10 +0200

    UserInfoActivityIT
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/androidTest/java/com/owncloud/android/ui/activity/UserInfoActivityIT.kt
+++ app/src/androidTest/java/com/owncloud/android/ui/activity/UserInfoActivityIT.kt
@@ -1,50 +1,71 @@
 /*
  * Nextcloud - Android Client
  *
+ * SPDX-FileCopyrightText: 2025 Alper Ozturk <alper.ozturk@nextcloud.com>
  * SPDX-FileCopyrightText: 2020 Andy Scherzinger <info@andy-scherzinger.de>
  * SPDX-License-Identifier: AGPL-3.0-or-later OR GPL-2.0-only
  */
-package com.owncloud.android.ui.activity;
+package com.owncloud.android.ui.activity
 
-import android.content.Intent;
+import android.content.Intent
+import androidx.annotation.UiThread
+import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.isRoot
+import com.owncloud.android.AbstractIT
+import com.owncloud.android.lib.common.UserInfo
+import com.owncloud.android.utils.EspressoIdlingResource
+import com.owncloud.android.utils.ScreenshotTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
 
-import com.owncloud.android.AbstractIT;
-import com.owncloud.android.lib.common.UserInfo;
-import com.owncloud.android.utils.ScreenshotTest;
+class UserInfoActivityIT : AbstractIT() {
+    private val testClassName = "com.owncloud.android.ui.activity.UserInfoActivityIT"
 
-import org.junit.Rule;
-import org.junit.Test;
+    @Before
+    fun registerIdlingResource() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
+    }
 
-import androidx.test.espresso.intent.rule.IntentsTestRule;
-
-public class UserInfoActivityIT extends AbstractIT {
-    @Rule
-    public IntentsTestRule<UserInfoActivity> activityRule = new IntentsTestRule<>(UserInfoActivity.class,
-                                                                                  true,
-                                                                                  false);
+    @After
+    fun unregisterIdlingResource() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
+    }
 
     @Test
+    @UiThread
     @ScreenshotTest
-    public void fullUserInfoDetail() {
-        final Intent intent = new Intent(targetContext, UserInfoActivity.class);
-        intent.putExtra(UserInfoActivity.KEY_ACCOUNT, user);
-        UserInfo userInfo = new UserInfo("test",
-                                         true,
-                                         "Firstname Familyname",
-                                         "oss@rocks.com",
-                                         "+49 7613 672 255",
-                                         "Awesome Place Av.",
-                                         "https://www.nextcloud.com",
-                                         "nextclouders",
-                                         null,
-                                         null
-        );
-        intent.putExtra(UserInfoActivity.KEY_USER_DATA, userInfo);
-        UserInfoActivity sut = activityRule.launchActivity(intent);
+    fun fullUserInfoDetail() {
+        val intent = Intent(targetContext, UserInfoActivity::class.java).apply {
+            putExtra(UserInfoActivity.KEY_ACCOUNT, user)
 
-        shortSleep();
-        shortSleep();
+            val userInfo = UserInfo(
+                "test",
+                true,
+                "Firstname Familyname",
+                "oss@rocks.com",
+                "+49 7613 672 255",
+                "Awesome Place Av.",
+                "https://www.nextcloud.com",
+                "nextclouders",
+                null,
+                null
+            )
+            putExtra(UserInfoActivity.KEY_USER_DATA, userInfo)
+        }
 
-        screenshot(sut);
+        launchActivity<UserInfoActivity>(intent).use { scenario ->
+            scenario.onActivity { sut ->
+                onIdleSync {
+                    val screenShotName = createName(testClassName + "_" + "fullUserInfoDetail", "")
+                    onView(isRoot()).check(matches(isDisplayed()))
+                    screenshotViaName(sut, screenShotName)
+                }
+            }
+        }
     }
 }
