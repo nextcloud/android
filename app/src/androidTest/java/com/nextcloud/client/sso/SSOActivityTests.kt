3c307190a82218f2ad06644ce6fb7e4c937e8e81
alperozturk <alper_ozturk@proton.me>
2025-08-08 11:14:26 +0200

    revert SSOActivityTests
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/androidTest/java/com/nextcloud/client/sso/SSOActivityTests.kt
+++ app/src/androidTest/java/com/nextcloud/client/sso/SSOActivityTests.kt
@@ -4,45 +4,25 @@
  * SPDX-FileCopyrightText: 2024 Alper Ozturk <alper.ozturk@nextcloud.com>
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
+
 package com.nextcloud.client.sso
 
-import androidx.annotation.UiThread
-import androidx.test.core.app.launchActivity
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.IdlingRegistry
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.isRoot
+import androidx.test.espresso.intent.rule.IntentsTestRule
 import com.owncloud.android.AbstractIT
 import com.owncloud.android.ui.activity.SsoGrantPermissionActivity
-import com.owncloud.android.utils.EspressoIdlingResource
-import org.junit.After
-import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 
 class SSOActivityTests : AbstractIT() {
 
-    @Before
-    fun registerIdlingResource() {
-        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
-    }
-
-    @After
-    fun unregisterIdlingResource() {
-        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
-    }
+    @Suppress("DEPRECATION")
+    @get:Rule
+    var activityRule = IntentsTestRule(SsoGrantPermissionActivity::class.java, true, false)
 
     @Test
-    @UiThread
     fun testActivityTheme() {
-        launchActivity<SsoGrantPermissionActivity>().use { scenario ->
-            scenario.onActivity { sut ->
-                onIdleSync {
-                    assert(sut.binding != null)
-                    assert(sut.materialAlertDialogBuilder != null)
-                    onView(isRoot()).check(matches(isDisplayed()))
-                }
-            }
-        }
+        val sut = activityRule.launchActivity(null)
+        assert(sut.binding != null)
+        assert(sut.materialAlertDialogBuilder != null)
     }
 }
