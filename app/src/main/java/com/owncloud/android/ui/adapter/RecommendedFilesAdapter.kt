02a9906021535d98bea0bcc68a3bf7c3c472b0a1
alperozturk <alper_ozturk@proton.me>
2025-08-20 16:18:26 +0200

    add: oc file list grid item view holder
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/ui/adapter/RecommendedFilesAdapter.kt
+++ app/src/main/java/com/owncloud/android/ui/adapter/RecommendedFilesAdapter.kt
@@ -7,58 +7,55 @@
 
 package com.owncloud.android.ui.adapter
 
+import android.util.TypedValue
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.nextcloud.android.lib.resources.recommendations.Recommendation
-import com.owncloud.android.databinding.RecommendedFilesListItemBinding
-import com.owncloud.android.datamodel.FileDataStorageManager
+import com.owncloud.android.databinding.GridItemBinding
 import com.owncloud.android.datamodel.OCFile
 
 class RecommendedFilesAdapter(
+    private val fileListAdapter: OCFileListAdapter,
     private val recommendations: ArrayList<Recommendation>,
-    private val delegate: OCFileListDelegate,
-    private val onItemClickListener: OnItemClickListener,
-    private val storageManager: FileDataStorageManager
-) : RecyclerView.Adapter<RecommendedFilesAdapter.RecommendedFilesViewHolder>() {
+) : RecyclerView.Adapter<OCFileListGridItemViewHolder>() {
 
-    interface OnItemClickListener {
-        fun selectRecommendedFile(file: OCFile)
-        fun showRecommendedFileMoreActions(file: OCFile, view: View)
+    companion object {
+        private const val LAYOUT_ITEM_WIDTH = 120f
     }
 
-    inner class RecommendedFilesViewHolder(val binding: RecommendedFilesListItemBinding) :
-        RecyclerView.ViewHolder(binding.root)
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecommendedFilesViewHolder {
-        val binding = RecommendedFilesListItemBinding
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): OCFileListGridItemViewHolder {
+        val binding = GridItemBinding
             .inflate(LayoutInflater.from(parent.context), parent, false)
-        return RecommendedFilesViewHolder(binding)
+        return OCFileListGridItemViewHolder(binding).apply {
+            setLayoutItemWidth(binding.ListItemLayout)
+        }
     }
 
     override fun getItemCount(): Int = recommendations.size
 
     @Suppress("MagicNumber")
-    override fun onBindViewHolder(holder: RecommendedFilesViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: OCFileListGridItemViewHolder, position: Int) {
         val item = recommendations.elementAt(position)
-
         holder.binding.run {
-            name.text = item.name
-            reason.text = item.reason
-
             val filePath = item.directory + OCFile.PATH_SEPARATOR + item.name
-            val file = storageManager.getFileByDecryptedRemotePath(filePath) ?: return
+            fileListAdapter.bindRecommendedFileHolder(holder, filePath)
+        }
+    }
 
-            delegate.setThumbnail(thumbnail, shimmerThumbnail, file)
+    private fun setLayoutItemWidth(layout: ConstraintLayout) {
+        val layoutItemWidth = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            LAYOUT_ITEM_WIDTH,
+            layout.resources.displayMetrics
+        ).toInt()
 
-            container.setOnClickListener {
-                onItemClickListener.selectRecommendedFile(file)
+        layout.run {
+            val params = layoutParams.apply {
+                width = layoutItemWidth
             }
-
-            moreAction.setOnClickListener {
-                onItemClickListener.showRecommendedFileMoreActions(file, holder.itemView)
-            }
+            layoutParams = params
         }
     }
 }
