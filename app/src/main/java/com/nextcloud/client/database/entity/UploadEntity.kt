353f8870f94e3fa03853f2cc078cae2165a29e92
alperozturk <alper_ozturk@proton.me>
2025-08-27 10:55:36 +0200

    fix: correct DAO query to fetch uploads by IDs and account name
    Previously, the DAO was returning incorrect uploads because it did not filter by both
    IDs and account name properly. This fixes the query to ensure only uploads that
    match the provided IDs and the given account name are returned.
    
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/client/database/entity/UploadEntity.kt
+++ app/src/main/java/com/nextcloud/client/database/entity/UploadEntity.kt
@@ -10,7 +10,13 @@ package com.nextcloud.client.database.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.nextcloud.utils.autoRename.AutoRename
+import com.owncloud.android.datamodel.UploadsStorageManager
+import com.owncloud.android.db.OCUpload
 import com.owncloud.android.db.ProviderMeta.ProviderTableMeta
+import com.owncloud.android.db.UploadResult
+import com.owncloud.android.files.services.NameCollisionPolicy
+import com.owncloud.android.lib.resources.status.OCCapability
 
 @Entity(tableName = ProviderTableMeta.UPLOADS_TABLE_NAME)
 data class UploadEntity(
@@ -48,3 +54,27 @@ data class UploadEntity(
     @ColumnInfo(name = ProviderTableMeta.UPLOADS_FOLDER_UNLOCK_TOKEN)
     val folderUnlockToken: String?
 )
+
+fun UploadEntity.toOCUpload(capability: OCCapability? = null): OCUpload {
+    val localPath = localPath
+    var remotePath = remotePath
+    if (capability != null && remotePath != null) {
+        remotePath = AutoRename.rename(remotePath, capability)
+    }
+    val upload = OCUpload(localPath, remotePath, accountName)
+
+    fileSize?.let { upload.fileSize = it }
+    id?.let { upload.uploadId = it.toLong() }
+    status?.let { upload.uploadStatus = UploadsStorageManager.UploadStatus.fromValue(it) }
+    localBehaviour?.let { upload.localAction = it }
+    nameCollisionPolicy?.let { upload.nameCollisionPolicy = NameCollisionPolicy.deserialize(it) }
+    isCreateRemoteFolder?.let { upload.isCreateRemoteFolder = it == 1 }
+    uploadEndTimestamp?.let { upload.uploadEndTimestamp = it.toLong() }
+    lastResult?.let { upload.lastResult = UploadResult.fromValue(it) }
+    createdBy?.let { upload.createdBy = it }
+    isWifiOnly?.let { upload.isUseWifiOnly = it == 1 }
+    isWhileChargingOnly?.let { upload.isWhileChargingOnly = it == 1 }
+    folderUnlockToken?.let { upload.folderUnlockToken = it }
+
+    return upload
+}
