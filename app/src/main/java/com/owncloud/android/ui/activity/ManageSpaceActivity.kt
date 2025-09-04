57810705ab4f5294e3fe5fd39f7eac870c25802e
alperozturk <alper_ozturk@proton.me>
2025-09-04 17:04:17 +0200

    fix: manage space activity screen
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/ui/activity/ManageSpaceActivity.kt
+++ app/src/main/java/com/owncloud/android/ui/activity/ManageSpaceActivity.kt
@@ -1,23 +1,30 @@
 /*
  * Nextcloud - Android Client
  *
+ * SPDX-FileCopyrightText: 2025 Alper Ozturk <alper.ozturk@nextcloud.com>
  * SPDX-FileCopyrightText: 2022 Álvaro Brey <alvaro@alvarobrey.com>
  * SPDX-License-Identifier: AGPL-3.0-or-later OR GPL-2.0-only
  */
 package com.owncloud.android.ui.activity
 
-import android.os.AsyncTask
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
 import com.google.android.material.snackbar.Snackbar
+import com.nextcloud.android.common.ui.util.extensions.applyEdgeToEdgeWithSystemBarPadding
+import com.nextcloud.android.common.ui.util.extensions.initStatusBar
 import com.nextcloud.client.account.UserAccountManager
 import com.nextcloud.client.di.Injectable
 import com.nextcloud.client.preferences.AppPreferences
 import com.owncloud.android.R
 import com.owncloud.android.lib.common.utils.Log_OC
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.io.File
 import javax.inject.Inject
 import kotlin.system.exitProcess
@@ -33,43 +40,23 @@ class ManageSpaceActivity :
     lateinit var userAccountManager: UserAccountManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        initStatusBar(ContextCompat.getColor(this, R.color.appbar))
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_manage_space)
-        val actionBar = supportActionBar
-        if (actionBar != null) {
-            actionBar.setDisplayHomeAsUpEnabled(true)
-            actionBar.setTitle(R.string.manage_space_title)
-        }
+
         val descriptionTextView = findViewById<TextView>(R.id.general_description)
         descriptionTextView.text = getString(R.string.manage_space_description, getString(R.string.app_name))
+
         val clearDataButton = findViewById<Button>(R.id.clearDataButton)
         clearDataButton.setOnClickListener {
-            val clearDataTask = ClearDataAsyncTask()
-            clearDataTask.execute()
-        }
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        var retval = true
-        when (item.itemId) {
-            android.R.id.home -> finish()
-            else -> {
-                Log_OC.w(TAG, "Unknown menu item triggered")
-                retval = super.onOptionsItemSelected(item)
+            lifecycleScope.launch {
+                clearData()
             }
         }
-        return retval
     }
 
-    /**
-     * AsyncTask for Clear Data, saving the passcode
-     */
-    private inner class ClearDataAsyncTask : AsyncTask<Void, Void, Boolean>() {
-        private val preferences = this@ManageSpaceActivity.preferences
-        private val userAccountManager = this@ManageSpaceActivity.userAccountManager
-
-        @Suppress("MagicNumber")
-        override fun doInBackground(vararg params: Void): Boolean {
+    private suspend fun clearData() {
+        withContext(Dispatchers.IO) {
             val lockPref = preferences.lockPreference
             val passCodeEnable = SettingsActivity.LOCK_PASSCODE == lockPref
             var passCodeDigits = arrayOfNulls<String>(4)
@@ -92,56 +79,66 @@ class ManageSpaceActivity :
             }
             preferences.lockPreference = lockPref
             userAccountManager.removeAllAccounts()
-            return result
-        }
 
-        override fun onPostExecute(result: Boolean) {
-            super.onPostExecute(result)
-            if (!result) {
-                Snackbar.make(
-                    findViewById(android.R.id.content),
-                    R.string.manage_space_clear_data,
-                    Snackbar.LENGTH_LONG
-                ).show()
-            } else {
-                finishAndRemoveTask()
-                exitProcess(0)
-            }
-        }
-
-        fun clearApplicationData(): Boolean {
-            var clearResult = true
-            val appDir = File(cacheDir.parent!!)
-            if (appDir.exists()) {
-                val children = appDir.list()
-                if (children != null) {
-                    children.filter { it != LIB_FOLDER }.forEach { s ->
-                        val fileToDelete = File(appDir, s)
-                        clearResult = clearResult && deleteDir(fileToDelete)
-                        Log_OC.d(TAG, "Clear Application Data, File: " + fileToDelete.name + " DELETED *****")
-                    }
+            withContext(Dispatchers.Main) {
+                if (result) {
+                    finishAndRemoveTask()
+                    exitProcess(0)
                 } else {
-                    clearResult = false
+                    Snackbar.make(
+                        findViewById(android.R.id.content),
+                        R.string.manage_space_clear_data,
+                        Snackbar.LENGTH_LONG
+                    ).show()
                 }
             }
-            return clearResult
         }
+    }
 
-        @Suppress("ReturnCount")
-        fun deleteDir(dir: File?): Boolean {
-            if (dir != null && dir.isDirectory) {
-                dir.list()?.forEach { child ->
-                    val success = deleteDir(File(dir, child))
-                    if (!success) {
-                        Log_OC.w(TAG, "File NOT deleted $child")
-                        return false
-                    } else {
-                        Log_OC.d(TAG, "File deleted $child")
-                    }
-                } ?: return false
+    private fun clearApplicationData(): Boolean {
+        var clearResult = true
+        val appDir = File(cacheDir.parent!!)
+        if (appDir.exists()) {
+            val children = appDir.list()
+            if (children != null) {
+                children.filter { it != LIB_FOLDER }.forEach { s ->
+                    val fileToDelete = File(appDir, s)
+                    clearResult = clearResult && deleteDir(fileToDelete)
+                    Log_OC.d(TAG, "Clear Application Data, File: " + fileToDelete.name + " DELETED *****")
+                }
+            } else {
+                clearResult = false
             }
-            return dir?.delete() ?: false
         }
+        return clearResult
+    }
+
+    @Suppress("ReturnCount")
+    private fun deleteDir(dir: File?): Boolean {
+        if (dir != null && dir.isDirectory) {
+            dir.list()?.forEach { child ->
+                val success = deleteDir(File(dir, child))
+                if (!success) {
+                    Log_OC.w(TAG, "File NOT deleted $child")
+                    return false
+                } else {
+                    Log_OC.d(TAG, "File deleted $child")
+                }
+            } ?: return false
+        }
+        return dir?.delete() ?: false
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        var retval = true
+        when (item.itemId) {
+            android.R.id.home -> finish()
+            else -> {
+                Log_OC.w(TAG, "Unknown menu item triggered")
+                retval = super.onOptionsItemSelected(item)
+            }
+        }
+        return retval
     }
 
     companion object {
