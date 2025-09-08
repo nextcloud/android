19183f36285f600e1c13a0b04b2f6a21c534d13d
alperozturk <alper_ozturk@proton.me>
2025-09-08 16:07:48 +0200

    feat: enhance unified search no result page
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/ui/fragment/UnifiedSearchFragment.kt
+++ app/src/main/java/com/owncloud/android/ui/fragment/UnifiedSearchFragment.kt
@@ -296,7 +296,7 @@ class UnifiedSearchFragment :
                     count += it.entries.size
                 }
 
-                if (count == 0 && pair.first?.isNotEmpty() == true && context != null) {
+                if (count == 0 && pair.first?.isNotEmpty() == true && context != null && !adapter.isCurrentDirItemsEmpty()) {
                     showNoResult()
                 }
             }
