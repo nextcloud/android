728401ee6377b231bf6cdc73df187ee3eb2ef5e7
alperozturk <alper_ozturk@proton.me>
2025-09-12 15:50:48 +0200

    chore: fix kotlin spotless on remove file operation finish
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.kt
+++ app/src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.kt
@@ -2052,14 +2052,16 @@ class FileDisplayActivity :
             } else if (this.leftFragment is GalleryFragment) {
                 val galleryFragment = leftFragment as GalleryFragment
                 galleryFragment.onRefresh()
-            } else if (leftFragment is OCFileListFragment && 
-                SearchRemoteOperation.SearchType.FAVORITE_SEARCH == leftFragment.searchEvent?.searchType) {
-                val file = leftFragment.adapter.files.find { it.fileId == removedFile.fileId}
-                if (file != null) {
-                    
-                    val pos= leftFragment.adapter.getItemPosition(file)
-                    leftFragment.adapter.files.remove(file)
-                    leftFragment.adapter.notifyItemRemoved(pos)
+            } else if (leftFragment is OCFileListFragment &&
+                SearchRemoteOperation.SearchType.FAVORITE_SEARCH == leftFragment.searchEvent?.searchType
+            ) {
+                leftFragment.adapter?.run {
+                    val file = files.find { it.fileId == removedFile.fileId }
+                    if (file != null) {
+                        val pos = getItemPosition(file)
+                        files.remove(file)
+                        notifyItemRemoved(pos)
+                    }
                 }
             }
             supportInvalidateOptionsMenu()
