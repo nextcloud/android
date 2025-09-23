e28e6e3c3122252b9276780e201bbbc96bf3d60a
tobiasKaminsky <tobias@kaminsky.me>
2025-09-23 12:08:12 +0200

    Add missing import
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- app/src/gplay/java/com/owncloud/android/utils/PushUtils.java
+++ app/src/gplay/java/com/owncloud/android/utils/PushUtils.java
@@ -44,6 +44,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.KeyFactory;
