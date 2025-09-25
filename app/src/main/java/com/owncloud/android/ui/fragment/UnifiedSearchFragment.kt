7f200295b3b875b484bf73603512efc2bcc46bc0
alperozturk <alper_ozturk@proton.me>
2025-09-25 12:11:07 +0200

    chore: resolve merge conflict
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/ui/fragment/UnifiedSearchFragment.kt
+++ app/src/main/java/com/owncloud/android/ui/fragment/UnifiedSearchFragment.kt
@@ -6,7 +6,6 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later OR GPL-2.0-only
  */
 package com.owncloud.android.ui.fragment
-
 import android.Manifest
 import android.content.Context
 import android.content.Intent
@@ -27,25 +26,33 @@ import androidx.appcompat.widget.SearchView
 import androidx.core.view.updatePadding
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import com.nextcloud.client.account.CurrentAccountProvider
 import com.nextcloud.client.account.UserAccountManager
 import com.nextcloud.client.core.AsyncRunner
+import com.nextcloud.client.core.Clock
 import com.nextcloud.client.di.Injectable
 import com.nextcloud.client.di.ViewModelFactory
 import com.nextcloud.client.network.ClientFactory
+import com.nextcloud.client.preferences.AppPreferences
+import com.nextcloud.utils.extensions.getTypedActivity
+import com.nextcloud.utils.extensions.searchFilesByName
 import com.nextcloud.utils.extensions.typedActivity
 import com.owncloud.android.R
 import com.owncloud.android.databinding.ListFragmentBinding
 import com.owncloud.android.datamodel.FileDataStorageManager
 import com.owncloud.android.datamodel.OCFile
+import com.owncloud.android.datamodel.SyncedFolderProvider
 import com.owncloud.android.lib.common.SearchResultEntry
 import com.owncloud.android.lib.common.utils.Log_OC
 import com.owncloud.android.lib.resources.status.NextcloudVersion
+import com.owncloud.android.ui.activity.FileActivity
 import com.owncloud.android.ui.activity.FileDisplayActivity
 import com.owncloud.android.ui.adapter.UnifiedSearchItemViewHolder
 import com.owncloud.android.ui.adapter.UnifiedSearchListAdapter
 import com.owncloud.android.ui.fragment.util.PairMediatorLiveData
+import com.owncloud.android.ui.interfaces.UnifiedSearchCurrentDirItemAction
 import com.owncloud.android.ui.interfaces.UnifiedSearchListInterface
 import com.owncloud.android.ui.unifiedsearch.IUnifiedSearchViewModel
 import com.owncloud.android.ui.unifiedsearch.ProviderID
@@ -55,6 +62,9 @@ import com.owncloud.android.ui.unifiedsearch.filterOutHiddenFiles
 import com.owncloud.android.utils.DisplayUtils
 import com.owncloud.android.utils.PermissionUtil
 import com.owncloud.android.utils.theme.ViewThemeUtils
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 /**
@@ -67,7 +77,8 @@ class UnifiedSearchFragment :
     Injectable,
     UnifiedSearchListInterface,
     SearchView.OnQueryTextListener,
-    UnifiedSearchItemViewHolder.FilesAction {
+    UnifiedSearchItemViewHolder.FilesAction,
+    UnifiedSearchCurrentDirItemAction {
     private lateinit var adapter: UnifiedSearchListAdapter
     private var _binding: ListFragmentBinding? = null
     val binding get() = _binding!!
@@ -77,16 +88,20 @@ class UnifiedSearchFragment :
     companion object {
         private const val TAG = "UnifiedSearchFragment"
 
-        const val ARG_QUERY = "ARG_QUERY"
-        const val ARG_HIDDEN_FILES = "ARG_HIDDEN_FILES"
+        private const val ARG_QUERY = "ARG_QUERY"
+        private const val ARG_HIDDEN_FILES = "ARG_HIDDEN_FILES"
+        private const val CURRENT_DIR_PATH = "CURRENT_DIR"
 
-        fun newInstance(query: String?, listOfHiddenFiles: ArrayList<String>?): UnifiedSearchFragment {
-            val fragment = UnifiedSearchFragment()
-            val args = Bundle()
-            args.putString(ARG_QUERY, query)
-            args.putStringArrayList(ARG_HIDDEN_FILES, listOfHiddenFiles)
-            fragment.arguments = args
-            return fragment
+        fun newInstance(
+            query: String?,
+            listOfHiddenFiles: ArrayList<String>?,
+            currentDirPath: String
+        ): UnifiedSearchFragment = UnifiedSearchFragment().apply {
+            arguments = Bundle().apply {
+                putString(ARG_QUERY, query)
+                putString(CURRENT_DIR_PATH, currentDirPath)
+                putStringArrayList(ARG_HIDDEN_FILES, listOfHiddenFiles)
+            }
         }
     }
 
@@ -111,23 +126,26 @@ class UnifiedSearchFragment :
     @Inject
     lateinit var accountManager: UserAccountManager
 
+    @Inject
+    lateinit var appPreferences: AppPreferences
+
+    @Inject
+    lateinit var clock: Clock
+
     private var listOfHiddenFiles = ArrayList<String>()
     private var showMoreActions = false
+    private var currentDir: OCFile? = null
+    private var initialQuery: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         vm = ViewModelProvider(this, vmFactory)[UnifiedSearchViewModel::class.java]
-        setUpViewModel()
-
-        val query = savedInstanceState?.getString(ARG_QUERY) ?: arguments?.getString(ARG_QUERY)
+        initialQuery = savedInstanceState?.getString(ARG_QUERY) ?: arguments?.getString(ARG_QUERY)
+        val currentDirPath = savedInstanceState?.getString(CURRENT_DIR_PATH) ?: arguments?.getString(CURRENT_DIR_PATH)
+        currentDir = storageManager.getFileByDecryptedRemotePath(currentDirPath)
         listOfHiddenFiles =
             savedInstanceState?.getStringArrayList(ARG_HIDDEN_FILES) ?: arguments?.getStringArrayList(ARG_HIDDEN_FILES)
                 ?: ArrayList()
-
-        if (!query.isNullOrEmpty()) {
-            vm.setQuery(query)
-            vm.initialQuery()
-        }
     }
 
     @Suppress("DEPRECATION")
@@ -146,6 +164,15 @@ class UnifiedSearchFragment :
         setupAdapter()
         if (supportsOpeningCalendarContactsLocally()) {
             checkPermissions()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        typedActivity<FileDisplayActivity>()?.run {
+            setupToolbar()
+            setMainFabVisible(false)
+            updateActionBarTitleAndHomeButtonByString(null)
         }
     }
 
@@ -200,6 +227,7 @@ class UnifiedSearchFragment :
 
             vm.setQuery("")
             adapter.setData(emptyList())
+            adapter.setDataCurrentDirItems(listOf())
 
             showStartYourSearch()
             showKeyboard(searchView)
@@ -282,13 +310,14 @@ class UnifiedSearchFragment :
         }
     }
 
+    @Suppress("ComplexCondition")
     private fun setUpViewModel() {
-        vm.searchResults.observe(this, this::onSearchResultChanged)
-        vm.isLoading.observe(this) { loading ->
+        vm.searchResults.observe(viewLifecycleOwner, this::onSearchResultChanged)
+        vm.isLoading.observe(viewLifecycleOwner) { loading ->
             binding.swipeContainingList.isRefreshing = loading
         }
 
-        PairMediatorLiveData(vm.searchResults, vm.isLoading).observe(this) { pair ->
+        PairMediatorLiveData(vm.searchResults, vm.isLoading).observe(viewLifecycleOwner) { pair ->
             if (pair.second == false) {
                 var count = 0
 
@@ -296,22 +325,26 @@ class UnifiedSearchFragment :
                     count += it.entries.size
                 }
 
-                if (count == 0 && pair.first?.isNotEmpty() == true && context != null && !adapter.isCurrentDirItemsEmpty()) {
+                if (count == 0 &&
+                    pair.first?.isNotEmpty() == true &&
+                    context != null &&
+                    !adapter.isCurrentDirItemsEmpty()
+                ) {
                     showNoResult()
                 }
             }
         }
 
-        vm.error.observe(this) { error ->
+        vm.error.observe(viewLifecycleOwner) { error ->
             if (!error.isNullOrEmpty()) {
                 DisplayUtils.showSnackMessage(binding.root, error)
             }
         }
-        vm.browserUri.observe(this) { uri ->
+        vm.browserUri.observe(viewLifecycleOwner) { uri ->
             val browserIntent = Intent(Intent.ACTION_VIEW, uri)
             startActivity(browserIntent)
         }
-        vm.file.observe(this) {
+        vm.file.observe(viewLifecycleOwner) {
             showFile(it, showMoreActions)
         }
     }
@@ -337,30 +370,42 @@ class UnifiedSearchFragment :
         }
     }
 
-    override fun onResume() {
-        super.onResume()
-        typedActivity<FileDisplayActivity>()?.run {
-            setupToolbar()
-            setMainFabVisible(false)
-            updateActionBarTitleAndHomeButtonByString(null)
-        }
-    }
-
     private fun setupAdapter() {
+        val syncedFolderProvider = SyncedFolderProvider(requireContext().contentResolver, appPreferences, clock)
         val gridLayoutManager = GridLayoutManager(requireContext(), 1)
-        adapter = UnifiedSearchListAdapter(
-            supportsOpeningCalendarContactsLocally(),
-            storageManager,
-            this,
-            this,
-            currentAccountProvider.user,
-            requireContext(),
-            viewThemeUtils
-        )
-        adapter.shouldShowFooters(true)
-        adapter.setLayoutManager(gridLayoutManager)
-        binding.listRoot.layoutManager = gridLayoutManager
-        binding.listRoot.adapter = adapter
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            val client =
+                getTypedActivity(FileActivity::class.java)?.clientRepository?.getNextcloudClient() ?: return@launch
+
+            withContext(Dispatchers.Main) {
+                adapter = UnifiedSearchListAdapter(
+                    supportsOpeningCalendarContactsLocally(),
+                    storageManager,
+                    this@UnifiedSearchFragment,
+                    this@UnifiedSearchFragment,
+                    currentAccountProvider.user,
+                    requireContext(),
+                    viewThemeUtils,
+                    appPreferences,
+                    syncedFolderProvider,
+                    client,
+                    this@UnifiedSearchFragment
+                )
+
+                adapter.shouldShowFooters(true)
+                adapter.setLayoutManager(gridLayoutManager)
+                binding.listRoot.layoutManager = gridLayoutManager
+                binding.listRoot.adapter = adapter
+                searchInCurrentDirectory(initialQuery ?: "")
+
+                setUpViewModel()
+                if (!initialQuery.isNullOrEmpty()) {
+                    vm.setQuery(initialQuery!!)
+                    vm.initialQuery()
+                }
+            }
+        }
     }
 
     override fun onSearchResultClicked(searchResultEntry: SearchResultEntry) {
@@ -394,7 +439,15 @@ class UnifiedSearchFragment :
     override fun onQueryTextChange(newText: String?): Boolean {
         val closeButton = searchView?.findViewById<ImageView>(androidx.appcompat.R.id.search_close_btn)
         closeButton?.visibility = if (newText?.isEmpty() == true) View.INVISIBLE else View.VISIBLE
+        searchInCurrentDirectory(newText ?: "")
         return true
+    }
+
+    private fun searchInCurrentDirectory(query: String) {
+        currentDir?.run {
+            val files = storageManager.searchFilesByName(this, accountManager.user.accountName, query)
+            adapter.setDataCurrentDirItems(files)
+        }
     }
 
     override fun onDestroyView() {
@@ -405,5 +458,10 @@ class UnifiedSearchFragment :
     override fun showFilesAction(searchResultEntry: SearchResultEntry) {
         showMoreActions = true
         vm.openResult(searchResultEntry)
+    }
+
+    override fun openFile(remotePath: String, showMoreActions: Boolean) {
+        this.showMoreActions = showMoreActions
+        vm.openFile(remotePath)
     }
 }
