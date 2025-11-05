c136c2e7b0d1fb64870b57f5f52596d9958397b4
alperozturk <alper_ozturk@proton.me>
2025-11-05 11:00:58 +0100

    use NextcloudVersion
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/androidTest/java/com/nextcloud/utils/CheckWCFRestrictionsTests.kt
+++ app/src/androidTest/java/com/nextcloud/utils/CheckWCFRestrictionsTests.kt
@@ -8,6 +8,7 @@ package com.nextcloud.utils
 
 import com.nextcloud.utils.extensions.checkWCFRestrictions
 import com.owncloud.android.lib.resources.status.CapabilityBooleanType
+import com.owncloud.android.lib.resources.status.NextcloudVersion
 import com.owncloud.android.lib.resources.status.OCCapability
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
@@ -16,67 +17,47 @@ import org.junit.Test
 @Suppress("MagicNumber")
 class CheckWCFRestrictionsTests {
 
-    private val version29 = Triple(29, 0, 0)
-    private val version30 = Triple(30, 0, 0)
-    private val version31 = Triple(31, 0, 0)
-    private val version32 = Triple(32, 0, 0)
-    private val version33 = Triple(33, 0, 0)
-
     private fun createCapability(
-        version: Triple<Int, Int, Int>,
+        version: NextcloudVersion,
         isWCFEnabled: CapabilityBooleanType = CapabilityBooleanType.UNKNOWN
     ): OCCapability = OCCapability().apply {
-        this.versionMayor = version.first
-        this.versionMinor = version.second
-        this.versionMicro = version.third
+        this.versionMayor = version.majorVersionNumber
         this.isWCFEnabled = isWCFEnabled
     }
 
     @Test
     fun testReturnsFalseForVersionsOlderThan30() {
-        val capability = createCapability(version29)
+        val capability = createCapability(NextcloudVersion.nextcloud_29)
         assertFalse(capability.checkWCFRestrictions())
     }
 
     @Test
     fun testReturnsTrueForVersion30WhenWCFAlwaysEnabled() {
-        val capability = createCapability(version30)
+        val capability = createCapability(NextcloudVersion.nextcloud_30)
         assertTrue(capability.checkWCFRestrictions())
     }
 
     @Test
     fun testReturnsTrueForVersion31WhenWCFAlwaysEnabled() {
-        val capability = createCapability(version31)
+        val capability = createCapability(NextcloudVersion.nextcloud_31)
         assertTrue(capability.checkWCFRestrictions())
     }
 
     @Test
     fun testReturnsTrueForVersion32WhenWCFEnabled() {
-        val capability = createCapability(version32, CapabilityBooleanType.TRUE)
+        val capability = createCapability(NextcloudVersion.nextcloud_32, CapabilityBooleanType.TRUE)
         assertTrue(capability.checkWCFRestrictions())
     }
 
     @Test
     fun testReturnsFalseForVersion32WhenWCFDisabled() {
-        val capability = createCapability(version32, CapabilityBooleanType.FALSE)
+        val capability = createCapability(NextcloudVersion.nextcloud_32, CapabilityBooleanType.FALSE)
         assertFalse(capability.checkWCFRestrictions())
     }
 
     @Test
     fun testReturnsFalseForVersion32WhenWCFIsUnknown() {
-        val capability = createCapability(version32)
-        assertFalse(capability.checkWCFRestrictions())
-    }
-
-    @Test
-    fun testReturnsTrueForNewerVersionWhenWCFEnabled() {
-        val capability = createCapability(version33, CapabilityBooleanType.TRUE)
-        assertTrue(capability.checkWCFRestrictions())
-    }
-
-    @Test
-    fun testReturnsFalseForNewerVersionWhenWCFDisabled() {
-        val capability = createCapability(version33, CapabilityBooleanType.FALSE)
+        val capability = createCapability(NextcloudVersion.nextcloud_32)
         assertFalse(capability.checkWCFRestrictions())
     }
 }
