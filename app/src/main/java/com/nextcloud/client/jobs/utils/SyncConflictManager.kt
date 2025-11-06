c889012358c0f8e69ebcc54c506963f0ee78ae72
alperozturk <alper_ozturk@proton.me>
2025-11-06 14:46:03 +0100

    simplify SyncConflictManager
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/client/jobs/utils/SyncConflictManager.kt
+++ app/src/main/java/com/nextcloud/client/jobs/utils/SyncConflictManager.kt
@@ -20,7 +20,7 @@ import com.owncloud.android.authentication.AuthenticatorActivity
 import com.owncloud.android.lib.common.operations.RemoteOperationResult
 import com.owncloud.android.lib.common.operations.RemoteOperationResult.ResultCode
 import com.owncloud.android.operations.UploadFileOperation
-import com.owncloud.android.ui.activity.ConflictsResolveActivity.Companion.createIntent
+import com.owncloud.android.ui.activity.ConflictsResolveActivity
 import com.owncloud.android.utils.ErrorMessageAdapter
 import java.io.File
 import java.security.SecureRandom
@@ -28,92 +28,55 @@ import java.security.SecureRandom
 object SyncConflictManager {
     fun getNotification(
         context: Context,
-        notificationBuilder: NotificationCompat.Builder,
+        builder: NotificationCompat.Builder,
         operation: UploadFileOperation,
         result: RemoteOperationResult<Any?>,
         notifyOnSameFileExists: () -> Unit
     ): Notification? {
-        if (!handle(context, operation, result, notifyOnSameFileExists)) {
-            return null
-        }
+        if (!shouldShowConflictDialog(context, operation, result, notifyOnSameFileExists)) return null
 
-        val errorMessage = ErrorMessageAdapter.getErrorCauseMessage(
-            result,
-            operation,
-            context.resources
-        )
+        val textId = result.code.toFailedResultTitleId()
+        val errorMessage = ErrorMessageAdapter.getErrorCauseMessage(result, operation, context.resources)
 
-        val conflictsResolveIntent = if (result.code == ResultCode.SYNC_CONFLICT) {
-            conflictResolveActionIntents(context, operation)
-        } else {
-            null
-        }
-
-        val credentialIntent: PendingIntent? = if (result.code == ResultCode.UNAUTHORIZED) {
-            credentialIntent(context, operation)
-        } else {
-            null
-        }
-
-        val cancelUploadActionIntent = if (conflictsResolveIntent != null) {
-            cancelUploadActionIntent(context, operation)
-        } else {
-            null
-        }
-
-        val textId = getFailedResultTitleId(result.code)
-
-        return notificationBuilder.run {
+        return builder.apply {
             setTicker(context.getString(textId))
             setContentTitle(context.getString(textId))
+            setContentText(errorMessage)
             setAutoCancel(false)
             setOngoing(false)
             setProgress(0, 0, false)
             clearActions()
 
-            conflictsResolveIntent?.let {
+            result.code.takeIf { it == ResultCode.SYNC_CONFLICT }?.let {
                 addAction(
                     R.drawable.ic_cloud_upload,
                     context.getString(R.string.upload_list_resolve_conflict),
-                    it
+                    conflictResolvePendingIntent(context, operation)
                 )
-            }
-
-            cancelUploadActionIntent?.let {
                 addAction(
                     R.drawable.ic_delete,
                     context.getString(R.string.upload_list_cancel_upload),
-                    cancelUploadActionIntent
+                    cancelUploadPendingIntent(context, operation)
                 )
             }
 
-            credentialIntent?.let {
-                setContentIntent(it)
+            result.code.takeIf { it == ResultCode.UNAUTHORIZED }?.let {
+                setContentIntent(credentialPendingIntent(context, operation))
             }
-
-            setContentText(errorMessage)
         }.build()
     }
 
-    private fun getFailedResultTitleId(resultCode: ResultCode): Int {
-        val needsToUpdateCredentials = (resultCode == ResultCode.UNAUTHORIZED)
-
-        return if (needsToUpdateCredentials) {
-            R.string.uploader_upload_failed_credentials_error
-        } else if (resultCode == ResultCode.SYNC_CONFLICT) {
-            R.string.uploader_upload_failed_sync_conflict_error
-        } else {
-            R.string.uploader_upload_failed_ticker
-        }
+    private fun ResultCode.toFailedResultTitleId(): Int = when (this) {
+        ResultCode.UNAUTHORIZED -> R.string.uploader_upload_failed_credentials_error
+        ResultCode.SYNC_CONFLICT -> R.string.uploader_upload_failed_sync_conflict_error
+        else -> R.string.uploader_upload_failed_ticker
     }
 
-    private fun credentialIntent(context: Context, operation: UploadFileOperation): PendingIntent {
+    private fun credentialPendingIntent(context: Context, operation: UploadFileOperation): PendingIntent {
         val intent = Intent(context, AuthenticatorActivity::class.java).apply {
             putExtra(AuthenticatorActivity.EXTRA_ACCOUNT, operation.user.toPlatformAccount())
             putExtra(AuthenticatorActivity.EXTRA_ACTION, AuthenticatorActivity.ACTION_UPDATE_EXPIRED_TOKEN)
-            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-            addFlags(Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS)
-            addFlags(Intent.FLAG_FROM_BACKGROUND)
+            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS or Intent.FLAG_FROM_BACKGROUND)
         }
 
         return PendingIntent.getActivity(
@@ -124,31 +87,21 @@ object SyncConflictManager {
         )
     }
 
-    private fun conflictResolveActionIntents(
-        context: Context,
-        uploadFileOperation: UploadFileOperation
-    ): PendingIntent {
-        val intent = createIntent(
-            uploadFileOperation.file,
-            uploadFileOperation.user,
-            uploadFileOperation.ocUploadId,
+    private fun conflictResolvePendingIntent(context: Context, operation: UploadFileOperation): PendingIntent {
+        val intent = ConflictsResolveActivity.createIntent(
+            operation.file,
+            operation.user,
+            operation.ocUploadId,
             Intent.FLAG_ACTIVITY_CLEAR_TOP,
             context
         )
+        val flags = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S)
+            PendingIntent.FLAG_MUTABLE else PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
 
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            PendingIntent.getActivity(context, SecureRandom().nextInt(), intent, PendingIntent.FLAG_MUTABLE)
-        } else {
-            PendingIntent.getActivity(
-                context,
-                SecureRandom().nextInt(),
-                intent,
-                PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
-            )
-        }
+        return PendingIntent.getActivity(context, SecureRandom().nextInt(), intent, flags)
     }
 
-    private fun cancelUploadActionIntent(context: Context, operation: UploadFileOperation): PendingIntent {
+    private fun cancelUploadPendingIntent(context: Context, operation: UploadFileOperation): PendingIntent {
         val intent = Intent(context, FileUploadBroadcastReceiver::class.java).apply {
             putExtra(FileUploadBroadcastReceiver.UPLOAD_ID, operation.ocUploadId)
             putExtra(FileUploadBroadcastReceiver.REMOTE_PATH, operation.file.remotePath)
@@ -157,45 +110,42 @@ object SyncConflictManager {
 
         return PendingIntent.getBroadcast(
             context,
-            0,
+            operation.file.fileId.toInt(),
             intent,
             PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
         )
     }
 
-    private fun handle(
+    private fun shouldShowConflictDialog(
         context: Context,
         operation: UploadFileOperation,
         result: RemoteOperationResult<Any?>,
         notifyOnSameFileExists: () -> Unit
     ): Boolean {
-        if (result.isSuccess || result.isCancelled || operation.isMissingPermissionThrown) {
+        if (result.isSuccess ||
+            result.isCancelled ||
+            result.code == ResultCode.USER_CANCELLED ||
+            operation.isMissingPermissionThrown
+        ) {
             return false
         }
 
-        if (result.code == ResultCode.SYNC_CONFLICT &&
-            FileUploadHelper.instance().isSameFileOnRemote(
-                operation.user,
-                File(operation.storagePath),
-                operation.remotePath,
-                context
-            )
-        ) {
+        val isSameFileOnRemote = FileUploadHelper.instance().isSameFileOnRemote(
+            operation.user,
+            File(operation.storagePath),
+            operation.remotePath,
+            context
+        )
+
+        if (result.code == ResultCode.SYNC_CONFLICT && isSameFileOnRemote) {
             notifyOnSameFileExists()
             return false
         }
 
-        val notDelayed = result.code !in setOf(
-            ResultCode.DELAYED_FOR_WIFI,
-            ResultCode.DELAYED_FOR_CHARGING,
-            ResultCode.DELAYED_IN_POWER_SAVE_MODE
-        )
+        val delayedCodes =
+            setOf(ResultCode.DELAYED_FOR_WIFI, ResultCode.DELAYED_FOR_CHARGING, ResultCode.DELAYED_IN_POWER_SAVE_MODE)
+        val invalidCodes = setOf(ResultCode.LOCAL_FILE_NOT_FOUND, ResultCode.LOCK_FAILED)
 
-        val isValidFile = result.code !in setOf(
-            ResultCode.LOCAL_FILE_NOT_FOUND,
-            ResultCode.LOCK_FAILED
-        )
-
-        return !(!notDelayed || !isValidFile || result.code == ResultCode.USER_CANCELLED)
+        return result.code !in delayedCodes && result.code !in invalidCodes
     }
 }
