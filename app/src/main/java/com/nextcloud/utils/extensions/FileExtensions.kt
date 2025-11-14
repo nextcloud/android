d25c7b3f784d36f685ccb78b7b8ed6a6351dfda3
alperozturk <alper_ozturk@proton.me>
2025-11-14 12:25:23 +0100

    fix git conflict
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/utils/extensions/FileExtensions.kt
+++ app/src/main/java/com/nextcloud/utils/extensions/FileExtensions.kt
@@ -25,3 +25,25 @@ fun File?.logFileSize(tag: String) {
     val rawByte = this?.length() ?: -1
     Log_OC.d(tag, "onSaveInstanceState: $size, raw byte $rawByte")
 }
+
+/**
+ * Converts a non-null and non-empty [String] path into a [File] object, if it exists.
+ *
+ * @receiver String path to a file.
+ * @return [File] instance if the file exists, or `null` if the path is null, empty, or non-existent.
+ */
+@Suppress("ReturnCount")
+fun String.toFile(): File? {
+    if (isNullOrEmpty()) {
+        Log_OC.e(TAG, "given path is null or empty")
+        return null
+    }
+
+    val file = File(this)
+    if (!file.exists()) {
+        Log_OC.e(TAG, "File does not exist: $this")
+        return null
+    }
+
+    return file
+}
