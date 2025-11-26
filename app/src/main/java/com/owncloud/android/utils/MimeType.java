94887ee39874f60687aae19a8df803be5e06bc22
alperozturk <alper_ozturk@proton.me>
2025-11-26 08:54:00 +0100

    add more OCFileListAdapterHelperTest
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/utils/MimeType.java
+++ app/src/main/java/com/owncloud/android/utils/MimeType.java
@@ -22,6 +22,7 @@ public final class MimeType {
     public static final String TEXT_PLAIN = "text/plain";
     public static final String FILE = "application/octet-stream";
     public static final String PDF = "application/pdf";
+    public static final String MP4 = "video/mp4";
 
     private MimeType() {
         // No instance
