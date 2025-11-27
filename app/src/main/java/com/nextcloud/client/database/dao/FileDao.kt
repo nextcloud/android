5dfd9a018a0e801415c4e08f8898f44116353b91
alperozturk <alper_ozturk@proton.me>
2025-11-27 08:23:30 +0100

    speed up parseAndSaveShares
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/client/database/dao/FileDao.kt
+++ app/src/main/java/com/nextcloud/client/database/dao/FileDao.kt
@@ -127,7 +127,6 @@ interface FileDao {
             share_by_link = 1
          OR shared_via_users = 1
          OR permissions LIKE '%S%'
-         OR COALESCE(sharees, '') NOT IN ('', 'null', '[]')
       )
     ORDER BY ${ProviderTableMeta.FILE_DEFAULT_SORT_ORDER}
     """
