c387abcd4a9195d63060135cc086c713e2953f5f
alperozturk <alper_ozturk@proton.me>
2025-12-02 09:58:31 +0100

    fix git conflict
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/utils/extensions/OCFileExtensions.kt
+++ app/src/main/java/com/nextcloud/utils/extensions/OCFileExtensions.kt
@@ -37,23 +37,10 @@ fun List<OCFile>.hasSameContentAs(other: List<OCFile>): Boolean {
 
 fun List<OCFile>.filterFilenames(): List<OCFile> = distinctBy { it.fileName }
 
-fun List<OCFile>.filterTempFilter(): List<OCFile> = filterNot { it.isTempFile() }
-
 fun OCFile.isTempFile(): Boolean {
     val context = MainApp.getAppContext()
     val appTempPath = FileStorageUtils.getAppTempDirectoryPath(context)
     return storagePath?.startsWith(appTempPath) == true
-}
-
-fun List<OCFile>.filterHiddenFiles(): List<OCFile> = filterNot { it.isHidden }.distinct()
-
-fun List<OCFile>.filterByMimeType(mimeType: String): List<OCFile> =
-    filter { it.isFolder || it.mimeType.startsWith(mimeType) }
-
-fun List<OCFile>.limitToPersonalFiles(userId: String): List<OCFile> = filter { file ->
-    file.ownerId?.let { ownerId ->
-        ownerId == userId && !file.isSharedWithMe && !file.mounted()
-    } == true
 }
 
 fun OCFile.mediaSize(defaultThumbnailSize: Float): Pair<Int, Int> {
