d8aaed4366d9d04dc975004a4316fbe86ae303ab
Philipp Hasper <vcs@hasper.info>
2025-12-18 19:47:19 +0100

    test(slideshow): Testing deletion for remote files
    The remote + offline test case had to be ignored due to two reasons
    1) Broken App behavior
    - The UX is indeed broken, as from a user perspective, nothing happens
      with the file when deleting it. The offlineOperation is put on the worker
      stack, but the user doesn't see anything from it
    - Even when coming back online, it is completely unreliable when the
      deletion will be finally done. It might happen 5 or 10 minutes later
    2) Broken test mock
    - The mocked connectivityService doesn't work as expected, because the
      OfflineOperationsWorker has its own service, and thus might still
      execute the deletion, but just at an unforseable time during the test
      execution - see problem 1).
    
    Signed-off-by: Philipp Hasper <vcs@hasper.info>

--- app/src/androidTest/java/com/owncloud/android/ui/preview/PreviewImageActivityIT.kt
+++ app/src/androidTest/java/com/owncloud/android/ui/preview/PreviewImageActivityIT.kt
@@ -22,20 +22,34 @@ import androidx.test.espresso.matcher.ViewMatchers.isRoot
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import com.nextcloud.test.ConnectivityServiceOfflineMock
+import com.nextcloud.test.FileRemovedIdlingResource
 import com.nextcloud.test.LoopFailureHandler
 import com.owncloud.android.AbstractOnServerIT
 import com.owncloud.android.R
 import com.owncloud.android.datamodel.OCFile
+import com.owncloud.android.db.OCUpload
+import com.owncloud.android.files.services.NameCollisionPolicy
+import com.owncloud.android.lib.resources.files.ExistenceCheckRemoteOperation
 import org.hamcrest.Matchers.allOf
+import org.junit.After
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Ignore
 import org.junit.Test
 import java.io.File
 
 class PreviewImageActivityIT : AbstractOnServerIT() {
-    lateinit var testFiles: List<OCFile>
+    companion object {
+        private const val REMOTE_FOLDER: String = "/PreviewImageActivityIT/"
+    }
 
-    fun createMockedImageFiles(count: Int, localOnly: Boolean) {
+    var fileRemovedIdlingResource = FileRemovedIdlingResource(storageManager)
+
+    @Suppress("SameParameterValue")
+    private fun createLocalMockedImageFiles(count: Int): List<OCFile> {
         val srcPngFile = getFile("imageFile.png")
-        testFiles = (0 until count).map { i ->
+        return (0 until count).map { i ->
             val pngFile = File(srcPngFile.parent ?: ".", "image$i.png")
             srcPngFile.copyTo(pngFile, overwrite = true)
 
@@ -44,18 +58,47 @@ class PreviewImageActivityIT : AbstractOnServerIT() {
                 mimeType = "image/png"
                 modificationTimestamp = 1000000
                 permissions = "D" // OCFile.PERMISSION_CAN_DELETE_OR_LEAVE_SHARE. Required for deletion button to show
-                remoteId = if (localOnly) null else "abc-mocked-remote-id" // mocking the file to be on the server
             }.also {
                 storageManager.saveNewFile(it)
             }
         }
     }
 
-    fun veryImageThenDelete(index: Int) {
-        val currentFileName = testFiles[index].fileName
+    /**
+     * Create image files and upload them to the connected server.
+     *
+     * This function relies on the images not existing beforehand, as AbstractOnServerIT#deleteAllFilesOnServer()
+     * should clean up. If it does fail, likely because that clean up didn't work and there are leftovers from
+     * a previous run
+     * @param count Number of files to create
+     * @param folder Parent folder to which to upload. Must start and end with a slash
+     */
+    private fun createAndUploadImageFiles(count: Int, folder: String = REMOTE_FOLDER): List<OCFile> {
+        val srcPngFile = getFile("imageFile.png")
+        return (0 until count).map { i ->
+            val pngFile = File(srcPngFile.parent ?: ".", "image$i.png")
+            srcPngFile.copyTo(pngFile, overwrite = true)
+
+            val ocUpload = OCUpload(
+                pngFile.absolutePath,
+                folder + pngFile.name,
+                account.name
+            ).apply {
+                nameCollisionPolicy = NameCollisionPolicy.OVERWRITE
+            }
+            uploadOCUpload(ocUpload)
+
+            fileDataStorageManager.getFileByDecryptedRemotePath(folder + pngFile.name)!!
+        }
+    }
+
+    private fun veryImageThenDelete(testFile: OCFile) {
         Espresso.setFailureHandler(
-            LoopFailureHandler(targetContext, "Test failed with image file index $index, $currentFileName")
+            LoopFailureHandler(targetContext, "Test failed with image file ${testFile.fileName}")
         )
+
+        assertTrue(testFile.exists())
+        assertTrue(testFile.fileExists())
 
         onView(withId(R.id.image))
             .check(matches(isDisplayed()))
@@ -64,7 +107,7 @@ class PreviewImageActivityIT : AbstractOnServerIT() {
         onView(
             allOf(
                 isDescendantOfA(isAssignableFrom(ActionBarContainer::class.java)),
-                withText(currentFileName)
+                withText(testFile.fileName)
             )
         ).check(matches(isDisplayed()))
 
@@ -82,7 +125,7 @@ class PreviewImageActivityIT : AbstractOnServerIT() {
         onView(withText(R.string.common_remove)).perform(ViewActions.click())
 
         // Check confirmation dialog and then confirm the deletion by clicking the main button of the dialog
-        val expectedText = targetContext.getString(R.string.confirmation_remove_file_alert, currentFileName)
+        val expectedText = targetContext.getString(R.string.confirmation_remove_file_alert, testFile.fileName)
         onView(withId(android.R.id.message))
             .inRoot(isDialog())
             .check(matches(withText(expectedText)))
@@ -92,43 +135,80 @@ class PreviewImageActivityIT : AbstractOnServerIT() {
             .check(matches(withText(R.string.file_delete)))
             .perform(ViewActions.click())
 
+        // Register the idling resource to wait for successful deletion
+        fileRemovedIdlingResource.setFile(testFile)
+
+        // Wait for idle, then verify that the file is gone. Somehow waitForIdleSync() doesn't work and we need onIdle()
+        Espresso.onIdle()
+        assertFalse("test file still exists: ${testFile.fileName}", testFile.exists())
+
         Espresso.setFailureHandler(DefaultFailureHandler(targetContext))
     }
 
-    @Test
-    fun deleteFromSlideshow_localOnly_online() {
-        // Prepare local test data
-        val imageCount = 5
-        createMockedImageFiles(imageCount, localOnly = true)
-
-        // Launch the activity with the first image
-        val intent = PreviewImageActivity.previewFileIntent(targetContext, user, testFiles[0])
-        launchActivity<PreviewImageActivity>(intent).use {
-            onView(isRoot()).check(matches(isDisplayed()))
-
-            for (i in 0 until imageCount) {
-                veryImageThenDelete(i)
-            }
-        }
+    @Before
+    fun bringUp() {
+        IdlingRegistry.getInstance().register(fileRemovedIdlingResource)
     }
 
-    @Test
-    fun deleteFromSlideshow_localOnly_offline() {
+    @After
+    fun tearDown() {
+        IdlingRegistry.getInstance().unregister(fileRemovedIdlingResource)
+    }
+
+    private fun testDeleteFromSlideshow_impl(localOnly: Boolean, offline: Boolean) {
         // Prepare local test data
         val imageCount = 5
-        createMockedImageFiles(imageCount, localOnly = true)
+        val testFiles = if (localOnly) {
+            createLocalMockedImageFiles(
+                imageCount
+            )
+        } else {
+            createAndUploadImageFiles(imageCount)
+        }
 
         // Launch the activity with the first image
         val intent = PreviewImageActivity.previewFileIntent(targetContext, user, testFiles[0])
         launchActivity<PreviewImageActivity>(intent).use { scenario ->
-            scenario.onActivity { activity ->
-                activity.connectivityService = ConnectivityServiceOfflineMock()
+            if (offline) {
+                scenario.onActivity { activity ->
+                    activity.connectivityService = ConnectivityServiceOfflineMock()
+                }
             }
             onView(isRoot()).check(matches(isDisplayed()))
 
-            for (i in 0 until imageCount) {
-                veryImageThenDelete(i)
+            for (testFile in testFiles) {
+                veryImageThenDelete(testFile)
+                assertTrue(
+                    "Test file still exists on the server: ${testFile.remotePath}",
+                    ExistenceCheckRemoteOperation(testFile.remotePath, true).execute(client).isSuccess
+                )
             }
         }
+    }
+
+    @Test
+    fun deleteFromSlideshow_localOnly_online() {
+        testDeleteFromSlideshow_impl(localOnly = true, offline = false)
+    }
+
+    @Test
+    fun deleteFromSlideshow_localOnly_offline() {
+        testDeleteFromSlideshow_impl(localOnly = true, offline = true)
+    }
+
+    @Test
+    fun deleteFromSlideshow_remote_online() {
+        testDeleteFromSlideshow_impl(localOnly = false, offline = false)
+    }
+
+    @Test
+    @Ignore(
+        "Offline deletion is following a different UX and it is also brittle: Deletion might happen 10 minutes later"
+    )
+    fun deleteFromSlideshow_remote_offline() {
+        // Note: the offline mock doesn't actually do what it is supposed to. The OfflineOperationsWorker uses its
+        // own connectivityService, which is online, and may still execute the server deletion.
+        // You'll need to address this, should you activate that test. Otherwise it might not catch all error cases
+        testDeleteFromSlideshow_impl(localOnly = false, offline = true)
     }
 }
