d2c1af687b18bfac5f9cef28ff78e4c93f6c6a8b
alperozturk <alper_ozturk@proton.me>
2025-12-18 12:35:57 +0100

    fix: eTag comparison
    Signed-off-by: alperozturk <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/utils/extensions/StringExtensions.kt
+++ app/src/main/java/com/nextcloud/utils/extensions/StringExtensions.kt
@@ -25,23 +25,19 @@ fun String.removeFileExtension(): String {
     }
 }
 
-/**
- * Checks if two nullable strings are both valid (non-null, non-empty, non-blank) and equal.
- *
- * It returns `true` only when both strings meet all the following criteria:
- * - Neither string is null
- * - Neither string is empty ("")
- * - Neither string contains only whitespace characters (spaces, tabs, newlines, etc.)
- * - Both strings are equal ignoring case differences
- *
- * @param other The other nullable string to compare with this string
- * @return `true` if both strings are valid and equal ignoring case differences, `false` otherwise
- */
-fun String?.isNotBlankAndEquals(other: String?): Boolean = this != null &&
-    other != null &&
-    this.isNotBlank() &&
-    other.isNotBlank() &&
-    this.equals(other, ignoreCase = true)
+fun String?.eTagChanged(eTagOnServer: String?): Boolean {
+    if (eTagOnServer?.isEmpty() == true) {
+        // provided eTag is empty can't compare treat as eTag changed
+        return true
+    }
+
+    return if (this != null) {
+        !this.equals(eTagOnServer, ignoreCase = true)
+    } else {
+        // provided eTag is null can't compare treat as eTag changed
+        true
+    }
+}
 
 fun String.truncateWithEllipsis(limit: Int) = take(limit) + if (length > limit) StringConstants.THREE_DOT else ""
 
