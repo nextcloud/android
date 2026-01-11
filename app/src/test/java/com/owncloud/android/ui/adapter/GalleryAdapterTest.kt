c41049bb7991132d6373bbc475c596c449800a16
Philipp Hasper <vcs@hasper.info>
2026-01-11 20:42:48 +0100

    Test bugfix of #15918 and #16194: Gallery multiselect could crash
    The crash came from a collision of the improperly coded hash function
    GalleryRow#calculateHashCode(). Simply summing the row's file hashes can
    easily cause a collision as the same sum can also be achieved by two other
    file hashes.
    Take two rows with two files each. All having the same parentId=0.
    Row 1: 263512 and 148830
    Row 2: 279897 and 132445
    Summing the hashes given by calculateHashCode() will return in the same
    value for Row1 and Row2.
    PR #15918 fixed this by migrating away from this faulty hash function.
    
    This commit tests the bugfix first with a known collision and then some
    random fileIds for some additional explorative testing.
    
    This commit also removes the problematic calculateHashCode(),
    as it is now unused.
    
    Signed-off-by: Philipp Hasper <vcs@hasper.info>
    Signed-off-by: Raphael Vieira <raphaelecv@hotmail.com>

--- app/src/test/java/com/owncloud/android/ui/adapter/GalleryAdapterTest.kt
+++ app/src/test/java/com/owncloud/android/ui/adapter/GalleryAdapterTest.kt
@@ -8,6 +8,7 @@
 package com.owncloud.android.ui.adapter
 
 import android.content.Context
+import android.text.TextUtils
 import com.nextcloud.client.account.User
 import com.nextcloud.client.jobs.upload.FileUploadHelper
 import com.nextcloud.client.preferences.AppPreferences
@@ -18,14 +19,18 @@ import com.owncloud.android.datamodel.OCFile
 import com.owncloud.android.ui.activity.ComponentsGetter
 import com.owncloud.android.ui.interfaces.OCFileListFragmentInterface
 import com.owncloud.android.utils.theme.ViewThemeUtils
-import junit.framework.Assert.assertEquals
+import io.mockk.every
+import io.mockk.mockkStatic
 import org.junit.After
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.whenever
+import kotlin.random.Random
 
 class GalleryAdapterTest {
     @Mock
@@ -55,20 +60,25 @@ class GalleryAdapterTest {
     private lateinit var mocks: AutoCloseable
 
     @Before
-    fun setUp() {
+    fun setUpMocks() {
         mocks = MockitoAnnotations.openMocks(this)
+
+        whenever(transferServiceGetter.storageManager) doReturn storageManager
+        whenever(transferServiceGetter.fileUploaderHelper) doReturn fileUploadHelper
+
+        // Mocking TextUtils so OCFile#existsOnDevice() doesn't fail due to Android not being available
+        // This is needed so OCFile#toString() works for logging errors
+        mockkStatic(TextUtils::class)
+        every { TextUtils.isEmpty(any()) } answers { arg<String?>(0)?.isEmpty() ?: true }
     }
 
     @After
-    fun tearDown() {
+    fun tearDownMocks() {
         mocks.close()
     }
 
     @Test
     fun testItemCount() {
-        whenever(transferServiceGetter.storageManager) doReturn storageManager
-        whenever(transferServiceGetter.fileUploaderHelper) doReturn fileUploadHelper
-
         val thumbnailSize = 50
 
         val sut = GalleryAdapter(
@@ -96,5 +106,112 @@ class GalleryAdapterTest {
         sut.addFiles(list)
 
         assertEquals(4, sut.getFilesCount())
+    }
+
+    @Test
+    @Suppress("LongMethod")
+    fun testIdUniqueness() {
+        val thumbnailSize = 50
+
+        val sut = GalleryAdapter(
+            context,
+            user,
+            ocFileListFragmentInterface,
+            preferences,
+            transferServiceGetter,
+            viewThemeUtils,
+            5,
+            thumbnailSize
+        )
+        val rows = mutableListOf<GalleryRow>()
+
+        // Test a known (former) hash collision
+        val row1File1 = 263512L
+        val row1File2 = 148830L
+        val row2File1 = 279897L
+        val row2File2 = 132445L
+        rows.add(
+            GalleryRow(
+                listOf(
+                    OCFile("/$row1File1.md").apply {
+                        fileId = row1File1
+                        parentId = 0
+                    },
+                    OCFile("/$row1File2.md").apply {
+                        fileId = row1File2
+                        parentId = 0
+                    }
+                ),
+                thumbnailSize,
+                thumbnailSize
+            )
+        )
+        rows.add(
+            GalleryRow(
+                listOf(
+                    OCFile("/$row2File1.md").apply {
+                        fileId = row2File1
+                        parentId = 0
+                    },
+                    OCFile("/$row2File2.md").apply {
+                        fileId = row2File2
+                        parentId = 0
+                    }
+                ),
+                thumbnailSize,
+                thumbnailSize
+            )
+        )
+        val alreadyUsedFileIds = listOf(row1File1, row1File2, row2File1, row2File2)
+
+        // Generate some random Ids for some explorative testing
+        val randomFileIds = uniquePositiveRandomLongs(10000, 1000000)
+        for (i in 0..<randomFileIds.size step 2) {
+            val id1 = randomFileIds[i]
+            val id2 = randomFileIds[i + 1]
+            if (id1 in alreadyUsedFileIds || id2 in alreadyUsedFileIds) {
+                continue
+            }
+            rows.add(
+                GalleryRow(
+                    listOf(
+                        OCFile("/$id1.md").apply { fileId = id1 },
+                        OCFile("/$id2.md").apply { fileId = id2 }
+                    ),
+                    thumbnailSize,
+                    thumbnailSize
+                )
+            )
+        }
+
+        val list = listOf(GalleryItems(1649317247, rows))
+        sut.addFiles(list)
+
+        val itemIds = mutableMapOf<Long, GalleryRow>()
+        for (i in 0..<sut.sectionCount) {
+            for (j in 0..<list[i].rows.size) {
+                val itemId = sut.getItemId(i, j)
+
+                assertFalse(
+                    "Item IDs are not unique: $itemId, in section $i, row $j is already taken by ${itemIds[itemId]}",
+                    itemIds.contains(itemId)
+                )
+                itemIds[itemId] = list[i].rows[j]
+            }
+        }
+        assertEquals("Exactly one section required, else the next assert is imprecise", 1, sut.sectionCount)
+        assertEquals("The duplicate detection seems to be faulty", rows.size, itemIds.size)
+    }
+
+    fun uniquePositiveRandomLongs(count: Int, max: Long = Long.MAX_VALUE): List<Long> {
+        require(count >= 0) { "count must be non-negative" }
+        if (count == 0) return emptyList()
+
+        val set = HashSet<Long>(count)
+        while (set.size < count) {
+            // produce positive (> 0) values
+            set.add(Random.nextLong(1, max))
+        }
+        return set.toList()
     }
 }
