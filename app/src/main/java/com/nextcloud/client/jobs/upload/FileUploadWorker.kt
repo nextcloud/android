978998823f3e69ebae4d0706e4e925b0007fcf41
Raphael Vieira <raphaelecv@hotmail.com>
2026-01-12 00:17:26 +0000

    removed magic number
    Signed-off-by: Raphael Vieira <raphaelecv@hotmail.com>

--- app/src/main/java/com/nextcloud/client/jobs/upload/FileUploadWorker.kt
+++ app/src/main/java/com/nextcloud/client/jobs/upload/FileUploadWorker.kt
@@ -81,6 +81,12 @@ class FileUploadWorker(
         const val UPLOAD_IDS = "uploads_ids"
         const val CURRENT_BATCH_INDEX = "batch_index"
         const val TOTAL_UPLOAD_SIZE = "total_upload_size"
+
+        /**
+         * The maximum number of concurrent parallel uploads
+         */
+        const val MAX_CONCURRENT_UPLOADS = 10
+
         const val SHOW_SAME_FILE_ALREADY_EXISTS_NOTIFICATION = "show_same_file_already_exists_notification"
 
         val activeUploadFileOperations = ConcurrentHashMap<String, UploadFileOperation>()
@@ -283,7 +289,7 @@ class FileUploadWorker(
             return Result.success()
         }
 
-        val semaphore = Semaphore(10) // Limit to 10 parallel uploads
+        val semaphore = Semaphore(MAX_CONCURRENT_UPLOADS)
         val quotaExceeded = AtomicBoolean(false)
         val completedCount = AtomicInteger(0)
         val storageManager = FileDataStorageManager(user, context.contentResolver)
