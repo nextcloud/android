fd5dc516a1bd0e6f16b5cc2039772a767144b14d
alperozturk96 <alper_ozturk@proton.me>
2026-01-19 11:14:29 +0100

    add upload finish event for auto upload worker
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/client/jobs/upload/FileUploadBroadcastManager.kt
+++ app/src/main/java/com/nextcloud/client/jobs/upload/FileUploadBroadcastManager.kt
@@ -2,7 +2,7 @@
  * Nextcloud - Android Client
  *
  * SPDX-FileCopyrightText: 2026 Alper Ozturk <alper.ozturk@nextcloud.com>
- * SPDX-FileCopyrightText: 2023 Nextcloud GmbH
+ * SPDX-FileCopyrightText: 2026 Nextcloud GmbH
  * SPDX-License-Identifier: AGPL-3.0-or-later OR GPL-2.0-only
  */
 package com.nextcloud.client.jobs.upload
