d0ec004823142f88ee0671e023b92f27d07fd2c0
alperozturk96 <alper_ozturk@proton.me>
2026-01-21 09:15:07 +0100

    add license
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>
    Signed-off-by: Raphael Vieira <raphaelecv@hotmail.com>

--- app/src/main/java/com/owncloud/android/ui/ThemeableSwitchPreference.kt
+++ app/src/main/java/com/owncloud/android/ui/ThemeableSwitchPreference.kt
@@ -1,3 +1,10 @@
+/*
+ * Nextcloud - Android Client
+ *
+ * SPDX-FileCopyrightText: 2026 Alper Ozturk <alper.ozturk@nextcloud.com>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
 package com.owncloud.android.ui
 
 import android.content.Context
