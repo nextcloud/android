2d60b7168270eb4f0b21e1c031e48f73d0073b91
alperozturk96 <alper_ozturk@proton.me>
2026-01-22 10:07:33 +0100

    fix(auto-upload): simplify error notification logic
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>
    Signed-off-by: Raphael Vieira <raphaelecv@hotmail.com>

--- app/src/main/java/com/nextcloud/client/jobs/upload/FileUploadWorker.kt
+++ app/src/main/java/com/nextcloud/client/jobs/upload/FileUploadWorker.kt
@@ -377,7 +377,7 @@ class FileUploadWorker(
                     notificationManager,
                     operation,
                     result,
-                    showSameFileAlreadyExistsNotification = {
+                    onSameFileConflict = {
                         withContext(Dispatchers.Main) {
                             val showSameFileAlreadyExistsNotification =
                                 inputData.getBoolean(SHOW_SAME_FILE_ALREADY_EXISTS_NOTIFICATION, false)
