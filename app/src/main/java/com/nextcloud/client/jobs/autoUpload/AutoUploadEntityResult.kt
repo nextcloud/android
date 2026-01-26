bdd34b94709236f75bf7bd7edb13088ff23320ea
alperozturk96 <alper_ozturk@proton.me>
2026-01-26 09:29:57 +0100

    fix(auto-upload): handle uploaded files
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

---
+++ app/src/main/java/com/nextcloud/client/jobs/autoUpload/AutoUploadEntityResult.kt
@@ -0,0 +1,18 @@
+/*
+ * Nextcloud - Android Client
+ *
+ * SPDX-FileCopyrightText: 2026 Alper Ozturk <alper.ozturk@nextcloud.com>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+package com.nextcloud.client.jobs.autoUpload
+
+import com.nextcloud.client.database.entity.UploadEntity
+import com.owncloud.android.db.OCUpload
+
+sealed class AutoUploadEntityResult {
+    data object SyncConflict : AutoUploadEntityResult()
+    data object CreationError : AutoUploadEntityResult()
+    data object Uploaded : AutoUploadEntityResult()
+    data class Success(val data: Pair<UploadEntity, OCUpload>) : AutoUploadEntityResult()
+}
