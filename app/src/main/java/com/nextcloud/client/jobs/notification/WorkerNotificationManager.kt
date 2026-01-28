cdaf8df9002498dc0e02a52a2cd7762993c6cd56
alperozturk96 <alper_ozturk@proton.me>
2026-01-28 13:46:28 +0100

    fix(auto-upload): calls
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/client/jobs/notification/WorkerNotificationManager.kt
+++ app/src/main/java/com/nextcloud/client/jobs/notification/WorkerNotificationManager.kt
@@ -89,15 +89,4 @@ open class WorkerNotificationManager(
             notification,
             ForegroundServiceType.DataSync
         )
-
-    fun createNotification(title: String, iconId: Int): Notification = notificationBuilder
-        .setContentTitle(title)
-        .setSmallIcon(iconId)
-        .setOngoing(true)
-        .setSound(null)
-        .setVibrate(null)
-        .setOnlyAlertOnce(true)
-        .setPriority(NotificationCompat.PRIORITY_LOW)
-        .setSilent(true)
-        .build()
 }
