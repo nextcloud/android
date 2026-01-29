76796f2f918e6156c4e24f4b46cf295890447cce
alperozturk96 <alper_ozturk@proton.me>
2026-01-29 15:17:20 +0100

    fix(unified-search-list-adapter): async initialization
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/ui/adapter/UnifiedSearchItemViewHolder.kt
+++ app/src/main/java/com/owncloud/android/ui/adapter/UnifiedSearchItemViewHolder.kt
@@ -9,29 +9,35 @@ package com.owncloud.android.ui.adapter
 
 import android.content.Context
 import android.view.View
+import androidx.lifecycle.lifecycleScope
 import com.afollestad.sectionedrecyclerview.SectionedViewHolder
 import com.nextcloud.android.common.ui.theme.utils.ColorRole
-import com.nextcloud.common.NextcloudClient
 import com.nextcloud.model.SearchResultEntryType
 import com.nextcloud.utils.CalendarEventManager
 import com.nextcloud.utils.ContactManager
 import com.nextcloud.utils.GlideHelper
 import com.nextcloud.utils.extensions.getType
+import com.nextcloud.utils.extensions.getTypedActivity
 import com.owncloud.android.databinding.UnifiedSearchItemBinding
 import com.owncloud.android.datamodel.FileDataStorageManager
 import com.owncloud.android.lib.common.SearchResultEntry
+import com.owncloud.android.ui.activity.FileActivity
+import com.owncloud.android.ui.fragment.UnifiedSearchFragment
 import com.owncloud.android.ui.interfaces.UnifiedSearchListInterface
 import com.owncloud.android.utils.theme.ViewThemeUtils
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @Suppress("LongParameterList")
 class UnifiedSearchItemViewHolder(
+    private val fragment: UnifiedSearchFragment,
     private val supportsOpeningCalendarContactsLocally: Boolean,
     val binding: UnifiedSearchItemBinding,
     private val storageManager: FileDataStorageManager,
     private val listInterface: UnifiedSearchListInterface,
     private val filesAction: FilesAction,
     val context: Context,
-    private val nextcloudClient: NextcloudClient,
     private val viewThemeUtils: ViewThemeUtils
 ) : SectionedViewHolder(binding.root) {
 
@@ -55,14 +61,21 @@ class UnifiedSearchItemViewHolder(
         val entryType = entry.getType()
         viewThemeUtils.platform.colorImageView(binding.thumbnail, ColorRole.PRIMARY)
 
-        GlideHelper.loadIntoImageView(
-            context,
-            nextcloudClient,
-            entry.thumbnailUrl,
-            binding.thumbnail,
-            entryType.iconId(),
-            circleCrop = entry.rounded
-        )
+        fragment.lifecycleScope.launch(Dispatchers.IO) {
+            val client =
+                fragment.getTypedActivity(FileActivity::class.java)?.clientRepository?.getNextcloudClient() ?: return@launch
+
+            withContext(Dispatchers.Main) {
+                GlideHelper.loadIntoImageView(
+                    context,
+                    client,
+                    entry.thumbnailUrl,
+                    binding.thumbnail,
+                    entryType.iconId(),
+                    circleCrop = entry.rounded
+                )
+            }
+        }
 
         if (entry.isFile) {
             binding.more.visibility = View.VISIBLE
