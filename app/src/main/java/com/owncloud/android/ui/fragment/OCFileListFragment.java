94d5e7ac4b9bbd4a297b42df4f4e403fa5d0e8c4
Philipp Hasper <vcs@hasper.info>
2026-02-01 14:30:48 +0100

    feat(doc-scan): Rename file from external scanner
    Before, the filename from FairScan just was <unixTimestamp>.pdf. Now,
    we are giving it our own timestamped name, just like we do for images and
    videos captured via the camera intent.
    
    This uses the same fileDisplayNameTransformer as PR #16298
    
    Signed-off-by: Philipp Hasper <vcs@hasper.info>

--- app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
+++ app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
@@ -1,6 +1,7 @@
 /*
  * Nextcloud - Android Client
  *
+ * SPDX-FileCopyrightText: 2026 Philipp Hasper <vcs@hasper.info>
  * SPDX-FileCopyrightText: 2023 TSI-mc
  * SPDX-FileCopyrightText: 2018-2023 Tobias Kaminsky <tobias@kaminsky.me>
  * SPDX-FileCopyrightText: 2022 Álvaro Brey <alvaro@alvarobrey.com>
@@ -596,7 +597,7 @@ public class OCFileListFragment extends ExtendedListFragment implements
     public void scanDocUploadFromApp() {
         requireActivity().startActivityForResult(
             scanIntentExternalApp,
-            FileDisplayActivity.REQUEST_CODE__SELECT_CONTENT_FROM_APPS);
+            FileDisplayActivity.REQUEST_CODE__SELECT_CONTENT_FROM_APPS_AUTO_RENAME);
     }
 
     @Override
