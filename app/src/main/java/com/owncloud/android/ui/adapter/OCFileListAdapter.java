bc2977a7cf704db513287332a5e67c12e951c2d3
alperozturk96 <alper_ozturk@proton.me>
2026-02-13 13:32:24 +0100

    fix(file-listing): single file deletion
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/ui/adapter/OCFileListAdapter.java
+++ app/src/main/java/com/owncloud/android/ui/adapter/OCFileListAdapter.java
@@ -1083,6 +1083,13 @@ public class OCFileListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
         notifyDataSetChanged();
     }
 
+    public void removeFileIndicator(OCFile file) {
+        if (file != null) {
+            file.setFileIndicator(null);
+            notifyItemChanged(file);
+        }
+    }
+
     public void updateFileIndicators(Map<Long, FileIndicator> indicators) {
         if (indicators == null || indicators.isEmpty()) {
             return;
@@ -1104,5 +1111,14 @@ public class OCFileListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHol
                 }
             }
         });
+    }
+
+    private OCFile findOCFile(long id) {
+        for (OCFile file : mFiles) {
+            if (file.getFileId() == id) {
+                return file;
+            }
+        }
+        return null;
     }
 }
