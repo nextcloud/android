1f791d9e56a486c4773ac4f50a3c8fad3eb21258
alperozturk96 <alper_ozturk@proton.me>
2026-02-16 15:20:13 +0100

    fix build
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>
    
    # Conflicts:
    #	app/src/main/res/values/strings.xml

--- app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
+++ app/src/main/java/com/owncloud/android/ui/fragment/OCFileListFragment.java
@@ -1529,10 +1529,6 @@ public class OCFileListFragment extends ExtendedListFragment implements
         return mFile;
     }
 
-    public SearchType getCurrentSearchType() {
-        return currentSearchType;
-    }
-
     /**
      * Calls {@link OCFileListFragment#listDirectory(OCFile, boolean)} with a null parameter
      */
