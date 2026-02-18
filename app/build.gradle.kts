e507498fe235573b5a05ee7549f20228742f15de
alperozturk96 <alper_ozturk@proton.me>
2026-02-18 10:01:17 +0100

    chore: version bump to 33.0.0
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,10 +62,10 @@ configurations.configureEach {
 }
 
 // semantic versioning for version code
-val versionMajor = 3
-val versionMinor = 36
+val versionMajor = 33
+val versionMinor = 0
 val versionPatch = 0
-val versionBuild = 52 // 0-50=Alpha / 51-98=RC / 90-99=stable
+val versionBuild = 90 // 0-50=Alpha / 51-98=RC / 90-99=stable
 
 val ndkEnv = buildMap {
     file("${project.rootDir}/ndk.env").readLines().forEach {
