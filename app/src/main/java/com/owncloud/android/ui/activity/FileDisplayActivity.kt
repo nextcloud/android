7898793fe0c1ebfbcaaea471dea2badaef800fd0
alperozturk96 <alper_ozturk@proton.me>
2026-02-19 16:39:55 +0100

    use action bottom sheet for album item actions
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.kt
+++ app/src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.kt
@@ -643,6 +643,7 @@ class FileDisplayActivity :
                 leftFragment = GroupfolderListFragment()
                 supportFragmentManager.executePendingTransactions()
             }
+
             ALBUMS == action -> {
                 Log_OC.d(this, "Switch to list albums fragment")
                 replaceAlbumFragment()
