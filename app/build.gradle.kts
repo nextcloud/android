d8f35c142122ec11350672d3c0e01edca2becf20
tobiasKaminsky <tobias@kaminsky.me>
2026-02-24 10:46:56 +0100

    Screenshot tests were failing, JUnit downgrade needed
    Signed-off-by: tobiasKaminsky <tobias@kaminsky.me>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -349,6 +349,7 @@ dependencies {
     implementation(libs.compose.activity)
     implementation(libs.compose.ui.tooling.preview)
     implementation(libs.foundation)
+    implementation(libs.espresso.accessibility)
     debugImplementation(libs.compose.ui.tooling)
     // endregion
 
