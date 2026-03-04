cad76b6fedb96827b3408a025bb826d449edc856
alperozturk96 <alper_ozturk@proton.me>
2026-03-04 15:53:54 +0100

    fix(file-download-fragment): visibility of indicator
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/ui/preview/FileDownloadFragment.kt
+++ app/src/main/java/com/owncloud/android/ui/preview/FileDownloadFragment.kt
@@ -1,7 +1,7 @@
 /*
  * Nextcloud - Android Client
  *
- * SPDX-FileCopyrightText: 2023 Alper Ozturk <alper.ozturk@nextcloud.com>
+ * SPDX-FileCopyrightText: 2026 Alper Ozturk <alper.ozturk@nextcloud.com>
  * SPDX-FileCopyrightText: 2022 Álvaro Brey <alvaro@alvarobrey.com>
  * SPDX-FileCopyrightText: 2022 Tobias Kaminsky <tobias@kaminsky.me>
  * SPDX-FileCopyrightText: 2020 Chris Narkiewicz <hello@ezaquarii.com>
@@ -10,257 +10,170 @@
  * SPDX-FileCopyrightText: 2013-2015 David A. Velasco <dvelasco@solidgear.es>
  * SPDX-License-Identifier: GPL-2.0-only AND (AGPL-3.0-or-later OR GPL-2.0-only)
  */
-package com.owncloud.android.ui.preview;
+package com.owncloud.android.ui.preview
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.ViewGroup;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.nextcloud.client.account.User
+import com.nextcloud.client.di.Injectable
+import com.nextcloud.client.jobs.download.FileDownloadHelper.Companion.instance
+import com.nextcloud.utils.extensions.getParcelableArgument
+import com.nextcloud.utils.extensions.getTypedActivity
+import com.owncloud.android.R
+import com.owncloud.android.databinding.FileDownloadFragmentBinding
+import com.owncloud.android.datamodel.OCFile
+import com.owncloud.android.lib.common.utils.Log_OC
+import com.owncloud.android.ui.adapter.progressListener.DownloadProgressListener
+import com.owncloud.android.ui.fragment.FileFragment
+import com.owncloud.android.utils.theme.ViewThemeUtils
+import javax.inject.Inject
 
-import com.nextcloud.client.account.User;
-import com.nextcloud.client.di.Injectable;
-import com.nextcloud.client.jobs.download.FileDownloadHelper;
-import com.nextcloud.utils.extensions.BundleExtensionsKt;
-import com.nextcloud.utils.extensions.FileExtensionsKt;
-import com.owncloud.android.R;
-import com.owncloud.android.datamodel.OCFile;
-import com.owncloud.android.lib.common.utils.Log_OC;
-import com.owncloud.android.ui.adapter.progressListener.DownloadProgressListener;
-import com.owncloud.android.ui.fragment.FileFragment;
-import com.owncloud.android.utils.theme.ViewThemeUtils;
+class FileDownloadFragment :
+    FileFragment(),
+    View.OnClickListener,
+    Injectable {
+    private var user: User? = null
+    private var _binding: FileDownloadFragmentBinding? = null
+    private val binding get() = _binding!!
 
-import javax.inject.Inject;
+    @Inject
+    lateinit var viewThemeUtils: ViewThemeUtils
 
-import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
+    var progressListener: DownloadProgressListener? = null
+    private var listening = false
+    private var ignoreFirstSavedState = false
+    private var downloadError = false
 
-/**
- * This Fragment is used to monitor the progress of a file downloading.
- */
-public class FileDownloadFragment extends FileFragment implements OnClickListener, Injectable {
-
-    public static final String EXTRA_FILE = "FILE";
-    public static final String EXTRA_USER = "USER";
-    private static final String EXTRA_ERROR = "ERROR";
-
-    private static final String ARG_FILE = "FILE";
-    private static final String ARG_IGNORE_FIRST = "IGNORE_FIRST";
-    private static final String ARG_USER = "USER";
-
-    private View mView;
-    private User user;
-
-    @Inject ViewThemeUtils viewThemeUtils;
-    public DownloadProgressListener mProgressListener;
-    private boolean mListening;
-
-    private static final String TAG = FileDownloadFragment.class.getSimpleName();
-
-    private boolean mIgnoreFirstSavedState;
-    private boolean mError;
-
-
-    /**
-     * Public factory method to create a new fragment that shows the progress of a file download.
-     * <p>
-     * Android strongly recommends keep the empty constructor of fragments as the only public constructor, and
-     * use {@link #setArguments(Bundle)} to set the needed arguments.
-     * <p>
-     * This method hides to client objects the need of doing the construction in two steps.
-     * <p>
-     * When 'file' is null creates a dummy layout (useful when a file wasn't tapped before).
-     *
-     * @param file                      An {@link OCFile} to show in the fragment
-     * @param user                      Nextcloud user; needed to start downloads
-     * @param ignoreFirstSavedState     Flag to work around an unexpected behaviour
-     *                                  TODO better solution
-     */
-    public static Fragment newInstance(OCFile file, User user, boolean ignoreFirstSavedState) {
-        FileDownloadFragment frag = new FileDownloadFragment();
-        Bundle args = new Bundle();
-        args.putParcelable(ARG_FILE, file);
-        args.putParcelable(ARG_USER, user);
-        args.putBoolean(ARG_IGNORE_FIRST, ignoreFirstSavedState);
-        frag.setArguments(args);
-        return frag;
-    }
-
-
-    /**
-     * Creates an empty details fragment.
-     * <p>
-     * It's necessary to keep a public constructor without parameters; the system uses it when tries to
-     * re-instantiate a fragment automatically.
-     */
-    public FileDownloadFragment() {
-        super();
-        mProgressListener = null;
-        mListening = false;
-        mIgnoreFirstSavedState = false;
-        mError = false;
-    }
-
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        Bundle args = getArguments();
-        if (args == null) {
-            return;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            file = it.getParcelableArgument(ARG_FILE, OCFile::class.java)
+            ignoreFirstSavedState = it.getBoolean(ARG_IGNORE_FIRST)
+            user = it.getParcelableArgument(ARG_USER, User::class.java)
         }
-
-        setFile(BundleExtensionsKt.getParcelableArgument(args, ARG_FILE, OCFile.class));
-            // TODO better in super, but needs to check ALL the class extending FileFragment; not right now
-
-        mIgnoreFirstSavedState = args.getBoolean(ARG_IGNORE_FIRST);
-        user = BundleExtensionsKt.getParcelableArgument(args, ARG_USER, User.class);
     }
 
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        super.onCreateView(inflater, container, savedInstanceState);
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        super.onCreateView(inflater, container, savedInstanceState)
 
-        if (getArguments() != null) {
-            if (!mIgnoreFirstSavedState) {
-                setFile(BundleExtensionsKt.getParcelableArgument(requireArguments(), EXTRA_FILE, OCFile.class));
-                user = BundleExtensionsKt.getParcelableArgument(requireArguments(), EXTRA_USER, User.class);
-                mError = requireArguments().getBoolean(EXTRA_ERROR);
-                FileDownloadHelper.Companion.instance().downloadFile(user, getFile());
-            }
-            else {
-                mIgnoreFirstSavedState = false;
-            }
-        }
-
-        mView = inflater.inflate(R.layout.file_download_fragment, container, false);
-
-        ProgressBar progressBar = mView.findViewById(R.id.progressBar);
-        viewThemeUtils.platform.themeHorizontalProgressBar(progressBar);
-        mProgressListener = new DownloadProgressListener(progressBar);
-
-        (mView.findViewById(R.id.cancelBtn)).setOnClickListener(this);
-
-        (mView.findViewById(R.id.fileDownloadLL)).setOnClickListener(v -> {
-            if (getActivity() instanceof PreviewImageActivity previewImageActivity) {
-                previewImageActivity.toggleFullScreen();
-            }
-        });
-
-        if (mError) {
-            setButtonsForRemote();
-        }
-        else {
-            setButtonsForTransferring();
-        }
-
-        return mView;
-    }
-
-    @Override
-    public void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        FileExtensionsKt.logFileSize(getFile(), TAG);
-        outState.putParcelable(FileDownloadFragment.EXTRA_FILE, getFile());
-        outState.putParcelable(FileDownloadFragment.EXTRA_USER, user);
-        outState.putBoolean(FileDownloadFragment.EXTRA_ERROR, mError);
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        listenForTransferProgress();
-    }
-
-    @Override
-    public void onStop() {
-        leaveTransferProgress();
-        super.onStop();
-    }
-
-    @Override
-    public View getView() {
-        if (!mListening) {
-            listenForTransferProgress();
-        }
-        return super.getView() == null ? mView : super.getView();
-    }
-
-
-    @Override
-    public void onClick(View v) {
-        if (v.getId() == R.id.cancelBtn) {
-            containerActivity.getFileOperationsHelper().cancelTransference(getFile());
-            requireActivity().finish();
+        if (!ignoreFirstSavedState) {
+            downloadError = requireArguments().getBoolean(EXTRA_ERROR)
+            instance().downloadFile(user!!, file)
         } else {
-            Log_OC.e(TAG, "Incorrect view clicked!");
+            ignoreFirstSavedState = false
+        }
+
+        _binding = FileDownloadFragmentBinding.inflate(inflater, container, false)
+
+        viewThemeUtils.material.run {
+            colorProgressBar(binding.progressBar)
+            colorMaterialTextButton(binding.cancelBtn)
+            themeCardView(binding.progressCard)
+            themeCardView(binding.errorCard)
+        }
+
+        progressListener = DownloadProgressListener(binding.progressBar)
+
+        binding.cancelBtn.setOnClickListener(this)
+        binding.fileDownloadLL.setOnClickListener {
+            getTypedActivity(PreviewImageActivity::class.java)?.toggleFullScreen()
+        }
+
+        setButtonsForTransferring(isTransferring = !downloadError)
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.run {
+            putParcelable(EXTRA_FILE, file)
+            putParcelable(EXTRA_USER, user)
+            putBoolean(EXTRA_ERROR, downloadError)
         }
     }
 
-
-    /**
-     * Enables or disables buttons for a file being downloaded
-     */
-    private void setButtonsForTransferring() {
-        if (getView() == null) {
-            return;
-        }
-
-        getView().findViewById(R.id.cancelBtn).setVisibility(View.VISIBLE);
-
-        // show the progress bar for the transfer
-        getView().findViewById(R.id.progressBar).setVisibility(View.VISIBLE);
-        TextView progressText = getView().findViewById(R.id.progressText);
-        progressText.setText(R.string.downloader_download_in_progress_ticker);
-        progressText.setVisibility(View.VISIBLE);
-
-        // hides the error icon
-        getView().findViewById(R.id.errorText).setVisibility(View.GONE);
-        getView().findViewById(R.id.error_image).setVisibility(View.GONE);
+    override fun onStart() {
+        super.onStart()
+        listenForTransferProgress()
     }
 
-    /**
-     * Enables or disables buttons for a file not locally available
-     * <p>
-     * Currently, this is only used when a download was failed
-     */
-    private void setButtonsForRemote() {
-        if (getView() == null) {
-            return;
-        }
-
-        getView().findViewById(R.id.cancelBtn).setVisibility(View.GONE);
-
-        // hides the progress bar and message
-        getView().findViewById(R.id.progressBar).setVisibility(View.GONE);
-        getView().findViewById(R.id.progressText).setVisibility(View.GONE);
-
-        // shows the error icon and message
-        getView().findViewById(R.id.errorText).setVisibility(View.VISIBLE);
-        getView().findViewById(R.id.error_image).setVisibility(View.VISIBLE);
+    override fun onStop() {
+        leaveTransferProgress()
+        super.onStop()
     }
 
-
-    public void listenForTransferProgress() {
-        if (mProgressListener != null && !mListening && containerActivity.getFileDownloadProgressListener() != null) {
-            containerActivity.getFileDownloadProgressListener().addDataTransferProgressListener(mProgressListener, getFile());
-            mListening = true;
-            setButtonsForTransferring();
+    override fun onClick(v: View) {
+        if (v.id == R.id.cancelBtn) {
+            containerActivity.getFileOperationsHelper().cancelTransference(file)
+            requireActivity().finish()
+        } else {
+            Log_OC.e(TAG, "Incorrect view clicked!")
         }
     }
 
+    private fun setButtonsForTransferring(isTransferring: Boolean) {
+        val binding = _binding ?: return
 
-    public void leaveTransferProgress() {
-        if (mProgressListener != null && containerActivity.getFileDownloadProgressListener() != null) {
-            containerActivity.getFileDownloadProgressListener()
-                .removeDataTransferProgressListener(mProgressListener, getFile());
-            mListening = false;
+        val transferringVisibility = if (isTransferring) View.VISIBLE else View.GONE
+        val remoteVisibility = if (isTransferring) View.GONE else View.VISIBLE
+
+        binding.run {
+            downloadingContainer.visibility = transferringVisibility
+            errorContainer.visibility = remoteVisibility
+
+            progressText.apply {
+                setText(R.string.downloader_download_in_progress_ticker)
+                visibility = transferringVisibility
+            }
         }
     }
 
-    public void setError(boolean error) {
-        mError = error;
+    fun listenForTransferProgress() {
+        val listener = containerActivity.getFileDownloadProgressListener() ?: return
+        if (progressListener == null || listening) return
+
+        listener.addDataTransferProgressListener(progressListener, file)
+        listening = true
+        setButtonsForTransferring(isTransferring = true)
+    }
+
+    fun leaveTransferProgress() {
+        val listener = containerActivity.getFileDownloadProgressListener() ?: return
+        if (progressListener == null) return
+
+        listener.removeDataTransferProgressListener(progressListener, file)
+        listening = false
+    }
+
+    fun setError(error: Boolean) {
+        downloadError = error
+    }
+
+    companion object {
+        const val EXTRA_FILE: String = "FILE"
+        const val EXTRA_USER: String = "USER"
+        private const val EXTRA_ERROR = "ERROR"
+        private const val ARG_FILE = "FILE"
+        private const val ARG_IGNORE_FIRST = "IGNORE_FIRST"
+        private const val ARG_USER = "USER"
+        private val TAG: String = FileDownloadFragment::class.java.simpleName
+
+        fun newInstance(file: OCFile?, user: User?, ignoreFirstSavedState: Boolean): Fragment =
+            FileDownloadFragment().apply {
+                arguments = Bundle().apply {
+                    putParcelable(ARG_FILE, file)
+                    putParcelable(ARG_USER, user)
+                    putBoolean(ARG_IGNORE_FIRST, ignoreFirstSavedState)
+                }
+            }
     }
 }
