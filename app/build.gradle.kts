4b7239644a3fa114f332f7533fa83f8dd61881d3
Tobias Kaminsky <tobias@kaminsky.me>
2026-03-05 02:42:25 +0100

    daily dev 20260305

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -154,8 +154,8 @@ android {
             register("versionDev") {
                 applicationId = "com.nextcloud.android.beta"
                 dimension = "default"
-                versionCode = 20260304
-                versionName = "20260304"
+                versionCode = 20260305
+                versionName = "20260305"
             }
 
             register("qa") {
