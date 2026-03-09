7bcb0bdd57ad65552e5de6484f6bdc9b999d823f
alperozturk96 <alper_ozturk@proton.me>
2026-03-09 14:38:40 +0100

    use dialog with buttons
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/ui/composeComponents/alertDialog/TaskSelectionAlertDialog.kt
+++ app/src/main/java/com/nextcloud/ui/composeComponents/alertDialog/TaskSelectionAlertDialog.kt
@@ -6,107 +6,51 @@
  */
 package com.nextcloud.ui.composeComponents.alertDialog
 
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.AlertDialog
-import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.ExposedDropdownMenuAnchorType
-import androidx.compose.material3.ExposedDropdownMenuBox
-import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import com.owncloud.android.R
+import androidx.compose.ui.window.Dialog
 import com.owncloud.android.lib.resources.assistant.v2.model.TaskTypeData
 
 @Suppress("LongMethod")
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TaskSelectionAlertDialog(taskTypes: List<TaskTypeData>, onDismiss: () -> Unit, onConfirm: (TaskTypeData) -> Unit) {
-    var expanded by remember { mutableStateOf(false) }
-    var tempSelectedTask by remember { mutableStateOf(taskTypes.firstOrNull()) }
-
-    AlertDialog(
-        onDismissRequest = onDismiss,
-        title = {
-            Text(
-                text = stringResource(R.string.assistant_screen_select_task_type_title),
-                style = MaterialTheme.typography.titleLarge
-            )
-        },
-        text = {
-            ExposedDropdownMenuBox(
-                expanded = expanded,
-                onExpandedChange = { expanded = !expanded },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 16.dp)
+fun TaskSelectionAlertDialog(
+    taskTypes: List<TaskTypeData>,
+    onDismiss: () -> Unit,
+    onConfirm: (TaskTypeData) -> Unit
+) {
+    Dialog(onDismissRequest = onDismiss) {
+        Surface(
+            shape = MaterialTheme.shapes.extraLarge,
+            tonalElevation = 6.dp
+        ) {
+            LazyColumn(
+                modifier = Modifier.padding(vertical = 16.dp)
             ) {
-                OutlinedTextField(
-                    value = tempSelectedTask?.name ?: "",
-                    onValueChange = {},
-                    readOnly = true,
-                    label = { Text(stringResource(R.string.assistant_screen_select_task_type_label)) },
-                    trailingIcon = { ExposedDropdownMenuDefaults.TrailingIcon(expanded = expanded) },
-                    colors = ExposedDropdownMenuDefaults.outlinedTextFieldColors(),
-                    modifier = Modifier
-                        .menuAnchor(
-                            type = ExposedDropdownMenuAnchorType.PrimaryNotEditable,
-                            enabled = true
-                        )
-                        .fillMaxWidth()
-                )
-
-                ExposedDropdownMenu(
-                    expanded = expanded,
-                    onDismissRequest = { expanded = false }
-                ) {
-                    taskTypes.forEach { task ->
-                        DropdownMenuItem(
-                            text = {
-                                Text(
-                                    text = task.name,
-                                    style = MaterialTheme.typography.bodyLarge
-                                )
-                            },
-                            onClick = {
-                                tempSelectedTask = task
-                                expanded = false
-                            },
-                            contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding
+                items(taskTypes) { task ->
+                    TextButton(
+                        onClick = {
+                            onConfirm(task)
+                            onDismiss()
+                        },
+                        modifier = Modifier.padding(horizontal = 8.dp)
+                    ) {
+                        Text(
+                            text = task.name,
+                            style = MaterialTheme.typography.bodyLarge
                         )
                     }
                 }
             }
-        },
-        confirmButton = {
-            FilledTonalButton(
-                onClick = {
-                    tempSelectedTask?.let {
-                        onDismiss()
-                        onConfirm(it)
-                    }
-                },
-                enabled = tempSelectedTask != null
-            ) {
-                Text(text = stringResource(android.R.string.ok))
-            }
-        },
-        dismissButton = {
-            TextButton(onClick = onDismiss) {
-                Text(text = stringResource(android.R.string.cancel))
-            }
         }
-    )
+    }
 }
