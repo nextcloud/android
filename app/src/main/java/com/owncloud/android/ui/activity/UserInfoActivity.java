bbfa3cb7fb878379927448474b88cf78c79d2e4f
Daniele Verducci <daniele.verducci@ichibi.eu>
2026-03-28 16:02:57 +0100

    591: Updated file copyright text
    Signed-off-by: Daniele Verducci <daniele.verducci@ichibi.eu>

--- app/src/main/java/com/owncloud/android/ui/activity/UserInfoActivity.java
+++ app/src/main/java/com/owncloud/android/ui/activity/UserInfoActivity.java
@@ -8,6 +8,7 @@
  * SPDX-FileCopyrightText: 2017 Mario Danic <mario@lovelyhq.com>
  * SPDX-FileCopyrightText: 2017 Nextcloud GmbH
  * SPDX-FileCopyrightText: 2025 TSI-mc <surinder.kumar@t-systems.com>
+ * SPDX-FileCopyrightText: 2026 Daniele Verducci <daniele.verducci@ichibi.eu>
  * SPDX-License-Identifier: AGPL-3.0-or-later OR GPL-2.0-only
  */
 package com.owncloud.android.ui.activity;
