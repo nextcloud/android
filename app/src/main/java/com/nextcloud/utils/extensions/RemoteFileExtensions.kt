4f130835a875dea9078a55940a8b2d1ef09cedc6
alperozturk96 <alper_ozturk@proton.me>
2026-04-09 15:05:33 +0200

    fix(upload-list): handle conflict actions
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/utils/extensions/RemoteFileExtensions.kt
+++ app/src/main/java/com/nextcloud/utils/extensions/RemoteFileExtensions.kt
@@ -7,14 +7,16 @@
 
 package com.nextcloud.utils.extensions
 
+import com.nextcloud.utils.OCFileUtils
 import com.nextcloud.utils.TimeConstants
 import com.owncloud.android.lib.resources.files.model.RemoteFile
 import com.owncloud.android.utils.FileUtil
+import com.owncloud.android.utils.MimeTypeUtil
 
 fun RemoteFile.isSame(path: String?): Boolean {
     val localFile = path?.toFile() ?: return false
 
-    // remote file timestamp in milli not micro sec
+    // remote file timestamp in millisecond not microsecond
     val localLastModifiedTimestamp = localFile.lastModified() / TimeConstants.MILLIS_PER_SECOND
     val localCreationTimestamp = FileUtil.getCreationTimestamp(localFile)
     val localSize: Long = localFile.length()
@@ -22,5 +24,23 @@ fun RemoteFile.isSame(path: String?): Boolean {
     return size == localSize &&
         localCreationTimestamp != null &&
         localCreationTimestamp == creationTimestamp &&
-        modifiedTimestamp == localLastModifiedTimestamp * TimeConstants.MILLIS_PER_SECOND
+        modifiedTimestamp == localLastModifiedTimestamp * TimeConstants.MILLIS_PER_SECOND &&
+        this.areImageDimensionsSame(path)
+}
+
+@Suppress("ReturnCount")
+private fun RemoteFile.areImageDimensionsSame(path: String): Boolean {
+    if (!MimeTypeUtil.isImage(mimeType)) {
+        // can't compare it's not image
+        return true
+    }
+
+    val localFileImageDimension = OCFileUtils.getExifSize(path) ?: OCFileUtils.getBitmapSize(path)
+    if (localFileImageDimension == null) {
+        // can't compare local file image dimension is not determined
+        return true
+    }
+
+    return localFileImageDimension.first.toFloat() == imageDimension?.width &&
+        localFileImageDimension.second.toFloat() == imageDimension?.height
 }
