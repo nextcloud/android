e6ffe47fa372f1f459e4537005f0a716a84f7de8
alperozturk96 <alper_ozturk@proton.me>
2026-04-17 10:47:41 +0200

    wip
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>
    
    # Conflicts:
    #	gradle/libs.versions.toml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -375,6 +375,7 @@ dependencies {
     implementation(libs.compose.activity)
     implementation(libs.compose.ui.tooling.preview)
     implementation(libs.foundation)
+    implementation(libs.material3)
     debugImplementation(libs.compose.ui.tooling)
     // endregion
 
