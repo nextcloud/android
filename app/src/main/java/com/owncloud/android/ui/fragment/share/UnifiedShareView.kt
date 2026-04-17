d68bb10c8a55c6aa7eed3def60ad4b4624fbda20
alperozturk96 <alper_ozturk@proton.me>
2026-04-17 10:54:22 +0200

    wip
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/ui/fragment/share/UnifiedShareView.kt
+++ app/src/main/java/com/owncloud/android/ui/fragment/share/UnifiedShareView.kt
@@ -27,7 +27,6 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.KeyboardArrowDown
 import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.MoreVert
