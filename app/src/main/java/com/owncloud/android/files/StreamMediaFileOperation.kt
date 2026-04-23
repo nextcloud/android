96851f9c13a5926436b7fed6a6fd31cb5f4e2ed8
alperozturk96 <alper_ozturk@proton.me>
2026-04-23 15:42:03 +0200

    handle stream media operation better
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/files/StreamMediaFileOperation.kt
+++ app/src/main/java/com/owncloud/android/files/StreamMediaFileOperation.kt
@@ -1,78 +1,67 @@
 /*
  * Nextcloud - Android Client
  *
+ * SPDX-FileCopyrightText: 2026 Alper Ozturk <alper.ozturk@nextcloud.com>
  * SPDX-FileCopyrightText: 2018 Tobias Kaminsky <tobias@kaminsky.me>
  * SPDX-FileCopyrightText: 2018 Nextcloud GmbH
  * SPDX-License-Identifier: AGPL-3.0-or-later OR GPL-2.0-only
  */
-package com.owncloud.android.files;
+package com.owncloud.android.files
 
-import com.owncloud.android.lib.common.OwnCloudClient;
-import com.owncloud.android.lib.common.operations.RemoteOperation;
-import com.owncloud.android.lib.common.operations.RemoteOperationResult;
-import com.owncloud.android.lib.common.utils.Log_OC;
+import com.owncloud.android.lib.common.OwnCloudClient
+import com.owncloud.android.lib.common.operations.RemoteOperation
+import com.owncloud.android.lib.common.operations.RemoteOperationResult
+import com.owncloud.android.lib.common.utils.Log_OC
+import org.apache.commons.httpclient.HttpStatus
+import org.apache.commons.httpclient.methods.Utf8PostMethod
+import org.json.JSONObject
 
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.methods.Utf8PostMethod;
-import org.json.JSONObject;
+@Suppress("TooGenericExceptionCaught")
+class StreamMediaFileOperation(private val fileID: Long) : RemoteOperation<ArrayList<String>>() {
 
-import java.util.ArrayList;
+    @Deprecated("Deprecated in Java")
+    override fun run(client: OwnCloudClient): RemoteOperationResult<ArrayList<String>> {
+        val postMethod = Utf8PostMethod(client.baseUri.toString() + STREAM_MEDIA_URL + JSON_FORMAT)
 
-public class StreamMediaFileOperation extends RemoteOperation {
-    private static final String TAG = StreamMediaFileOperation.class.getSimpleName();
-    private static final int SYNC_READ_TIMEOUT = 40000;
-    private static final int SYNC_CONNECTION_TIMEOUT = 5000;
-    private static final String STREAM_MEDIA_URL = "/ocs/v2.php/apps/dav/api/v1/direct";
+        return try {
+            postMethod.apply {
+                setParameter("fileId", fileID.toString())
+                addRequestHeader(OCS_API_HEADER, OCS_API_HEADER_VALUE)
+                params.soTimeout = SYNC_READ_TIMEOUT
+            }
 
-    private final long fileID;
-
-    // JSON node names
-    private static final String NODE_OCS = "ocs";
-    private static final String NODE_DATA = "data";
-    private static final String NODE_URL = "url";
-    private static final String JSON_FORMAT = "?format=json";
-
-    public StreamMediaFileOperation(long fileID) {
-        this.fileID = fileID;
-    }
-
-    protected RemoteOperationResult run(OwnCloudClient client) {
-        RemoteOperationResult result;
-        Utf8PostMethod postMethod = null;
-
-        try {
-            postMethod = new Utf8PostMethod(client.getBaseUri() + STREAM_MEDIA_URL + JSON_FORMAT);
-            postMethod.setParameter("fileId", String.valueOf(fileID));
-
-            // remote request
-            postMethod.addRequestHeader(OCS_API_HEADER, OCS_API_HEADER_VALUE);
-
-            int status = client.executeMethod(postMethod, SYNC_READ_TIMEOUT, SYNC_CONNECTION_TIMEOUT);
+            val status = client.executeMethod(postMethod, SYNC_READ_TIMEOUT, SYNC_CONNECTION_TIMEOUT)
 
             if (status == HttpStatus.SC_OK) {
-                String response = postMethod.getResponseBodyAsString();
+                val response = postMethod.getResponseBodyAsString()
+                val url = JSONObject(response)
+                    .getJSONObject(NODE_OCS)
+                    .getJSONObject(NODE_DATA)
+                    .getString(NODE_URL)
 
-                // Parse the response
-                JSONObject respJSON = new JSONObject(response);
-                String url = respJSON.getJSONObject(NODE_OCS).getJSONObject(NODE_DATA).getString(NODE_URL);
-
-                result = new RemoteOperationResult(true, postMethod);
-                ArrayList<Object> urlArray = new ArrayList<>();
-                urlArray.add(url);
-                result.setData(urlArray);
+                RemoteOperationResult<ArrayList<String>>(true, postMethod).also {
+                    it.data = arrayListOf(url)
+                }
             } else {
-                result = new RemoteOperationResult(false, postMethod);
-                client.exhaustResponse(postMethod.getResponseBodyAsStream());
+                client.exhaustResponse(postMethod.getResponseBodyAsStream())
+                RemoteOperationResult(false, postMethod)
             }
-        } catch (Exception e) {
-            result = new RemoteOperationResult(e);
-            Log_OC.e(TAG, "Get stream url for file with id " + fileID + " failed: " + result.getLogMessage(),
-                    result.getException());
+        } catch (e: Exception) {
+            Log_OC.e(TAG, "Get stream url for file with id $fileID failed: ${e.message}", e)
+            RemoteOperationResult(e)
         } finally {
-            if (postMethod != null) {
-                postMethod.releaseConnection();
-            }
+            postMethod.releaseConnection()
         }
-        return result;
+    }
+
+    companion object {
+        private val TAG = StreamMediaFileOperation::class.java.simpleName
+        private const val SYNC_READ_TIMEOUT = 120_000
+        private const val SYNC_CONNECTION_TIMEOUT = 15_000
+        private const val STREAM_MEDIA_URL = "/ocs/v2.php/apps/dav/api/v1/direct"
+        private const val NODE_OCS = "ocs"
+        private const val NODE_DATA = "data"
+        private const val NODE_URL = "url"
+        private const val JSON_FORMAT = "?format=json"
     }
 }
