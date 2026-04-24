f81404b43d38d7c0209d2622a26ffa0a974342df
alperozturk96 <alper_ozturk@proton.me>
2026-04-24 15:25:02 +0200

    fix scrolling search
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/ui/fragment/GalleryFragment.kt
+++ app/src/main/java/com/owncloud/android/ui/fragment/GalleryFragment.kt
@@ -323,13 +323,13 @@ class GalleryFragment :
     }
 
     private fun loadMoreWhenEndReached(recyclerView: RecyclerView, dy: Int) {
-        if (dy > 0 && !isPhotoSearchQueryRunning) {
-            // scrolled vertical space not bigger than 0 or still searching
+        if (dy <= 0 || isPhotoSearchQueryRunning) {
+            Log_OC.d(TAG, "scrolling up or search query already active, do not search gallery")
             return
         }
 
         if (recyclerView.layoutManager !is GridLayoutManager) {
-            Log_OC.e(TAG, "can't load more layout manager is not grid")
+            Log_OC.e(TAG, "can't load more; layout manager is not LinearLayoutManager or GridLayoutManager")
             return
         }
 
