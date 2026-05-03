f06937c82fe47f5373f475ee6a3fc6519d089c06
Jeffrey Feillp <3969129510@qq.com>
2026-05-03 23:11:31 +0800

    solve: Fail to upload using Android clients over WiFi

---
+++ solutions/w13_8549.py
@@ -0,0 +1,10 @@
+# Solution for nextcloud/android#8549
+# Title: Fail to upload using Android clients over WiFi
+# Wallet: TU8NBT5iGyMNkLwWmWmgy7tFMbKnafLHcu
+
+def solve():
+    """Auto-generated fix for issue #8549"""
+    pass
+
+if __name__ == "__main__":
+    solve()
