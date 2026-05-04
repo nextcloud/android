b51c73e6f7848cb377bf349e8d49053f715e92d2
alperozturk96 <alper_ozturk@proton.me>
2026-05-04 16:13:07 +0200

    add tests
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

---
+++ app/src/androidTest/java/com/owncloud/android/operations/DeleteE2ERemoteOperationIT.kt
@@ -0,0 +1,51 @@
+/*
+ * Nextcloud - Android Client
+ *
+ * SPDX-FileCopyrightText: 2026 Alper Ozturk <alper.ozturk@nextcloud.com>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+package com.owncloud.android.operations
+
+import com.owncloud.android.AbstractOnServerIT
+import com.owncloud.android.lib.resources.e2ee.DeleteEncryptedFilesRemoteOperation
+import com.owncloud.android.lib.resources.users.DeletePrivateKeyRemoteOperation
+import com.owncloud.android.lib.resources.users.DeletePublicKeyRemoteOperation
+import com.owncloud.android.lib.resources.users.GetPrivateKeyRemoteOperation
+import com.owncloud.android.lib.resources.users.GetPublicKeyRemoteOperation
+import com.owncloud.android.lib.resources.users.StorePrivateKeyRemoteOperation
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class DeleteE2ERemoteOperationIT : AbstractOnServerIT() {
+
+    @Test
+    fun testDeleteEncryptedFiles() {
+        val sut = DeleteEncryptedFilesRemoteOperation()
+        val result = sut.execute(nextcloudClient)
+        assertTrue(result.isSuccess)
+    }
+
+    @Test
+    fun testDeletePrivateKey() {
+        StorePrivateKeyRemoteOperation("private_key").execute(nextcloudClient)
+
+        val sut = DeletePrivateKeyRemoteOperation()
+        val result = sut.execute(nextcloudClient)
+        assertTrue(result.isSuccess)
+
+        val getResult = GetPrivateKeyRemoteOperation().execute(nextcloudClient)
+        assertFalse(getResult.isSuccess)
+    }
+
+    @Test
+    fun testDeletePublicKey() {
+        val sut = DeletePublicKeyRemoteOperation()
+        val result = sut.execute(nextcloudClient)
+        assertTrue(result.isSuccess)
+
+        val getResult = GetPublicKeyRemoteOperation().execute(nextcloudClient)
+        assertFalse(getResult.isSuccess)
+    }
+}
