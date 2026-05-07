7bf5a18a0338e69047b99984484c1ace43e17cb5
alperozturk96 <alper_ozturk@proton.me>
2026-05-07 09:12:35 +0200

    wip
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -86,7 +86,12 @@ val ncTestServerBaseUrl = configProps["NC_TEST_SERVER_BASEURL"]
 android {
     // install this NDK version and Cmake to produce smaller APKs. Build will still work if not installed
     ndkVersion = "${ndkEnv["NDK_VERSION"]}"
-
+    externalNativeBuild {
+        cmake {
+            version = "${ndkEnv["CMAKE_VERSION"]}"
+            path = file("src/main/cpp/CMakeLists.txt")
+        }
+    }
     namespace = "com.owncloud.android"
     testNamespace = "${namespace}.test"
 
@@ -103,6 +108,10 @@ android {
         minSdk = 28
         targetSdk = 36
         compileSdk = 36
+
+        ndk {
+            abiFilters += listOf("arm64-v8a", "x86_64")
+        }
 
         buildConfigField("boolean", "CI", ciBuild.toString())
         buildConfigField("boolean", "RUNTIME_PERF_ANALYSIS", perfAnalysis.toString())
