d90f2d81ae5db93337e39eb3624072584d390f5d
alperozturk96 <alper_ozturk@proton.me>
2026-05-13 08:39:45 +0200

    fix(file-details): refresh from fda
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.kt
+++ app/src/main/java/com/owncloud/android/ui/activity/FileDisplayActivity.kt
@@ -850,6 +850,7 @@ class FileDisplayActivity :
                 leftFragment.listenForTransferProgress()
                 leftFragment.updateFileDetails(true, false)
             }
+
             FileDownloadEventBroadcaster.ACTION_DOWNLOAD_COMPLETED -> {
                 val waitedPreview = mWaitingToPreview?.remotePath == downloadedRemotePath
                 val previewStarted = waitedPreview && tryStartWaitingPreview(success)
@@ -872,18 +873,22 @@ class FileDisplayActivity :
                 startMediaPreview(file, 0, true, true, true, true)
                 true
             }
+
             MimeTypeUtil.isVCard(file.mimeType) -> {
                 startContactListFragment(file)
                 true
             }
+
             PreviewTextFileFragment.canBePreviewed(file) -> {
                 startTextPreview(file, true)
                 true
             }
+
             MimeTypeUtil.isPDF(file) -> {
                 startPdfPreview(file)
                 true
             }
+
             else -> {
                 fileOperationsHelper.openFile(file)
                 false
