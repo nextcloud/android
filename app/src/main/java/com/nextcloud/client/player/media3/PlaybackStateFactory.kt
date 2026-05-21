48c795227469dc1b968eae1278f614ce25830852
Alex Kucherenko <a.kucherenko@viseven.com>
2026-05-22 00:44:50 +0300

    Update license identifier

--- app/src/main/java/com/nextcloud/client/player/media3/PlaybackStateFactory.kt
+++ app/src/main/java/com/nextcloud/client/player/media3/PlaybackStateFactory.kt
@@ -2,7 +2,7 @@
  * Nextcloud - Android Client
  *
  * SPDX-FileCopyrightText: 2025 STRATO GmbH.
- * SPDX-License-Identifier: GPL-2.0
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
 package com.nextcloud.client.player.media3
