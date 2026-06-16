0fd0293eabbd7bf96a89879ea4b36fa2783b457f
alperozturk96 <alper_ozturk@proton.me>
2026-06-16 14:30:56 +0200

    fix adapter creation
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/ui/fragment/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/owncloud/android/ui/fragment/notifications/NotificationsFragment.kt
@@ -270,7 +270,6 @@ class NotificationsFragment :
 
     // region Data loading
     private fun fetchAndSetData(client: NextcloudClient) {
-        initializeAdapter()
         lifecycleScope.launch(Dispatchers.IO) {
             val result = GetNotificationsRemoteOperation().execute(client)
             withContext(Dispatchers.Main) {
