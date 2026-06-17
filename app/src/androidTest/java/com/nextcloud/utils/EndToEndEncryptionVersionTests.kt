d5961aab3736a36ecb4390c830c804a1cea9a714
alperozturk96 <alper_ozturk@proton.me>
2026-06-17 12:26:14 +0200

    wip
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/androidTest/java/com/nextcloud/utils/EndToEndEncryptionVersionTests.kt
+++ app/src/androidTest/java/com/nextcloud/utils/EndToEndEncryptionVersionTests.kt
@@ -16,48 +16,81 @@ class EndToEndEncryptionVersionTests {
 
     @Test
     fun testGetMaxCompatibleE2EEVersionWhenGivenUnknownShouldReturnUnknown() {
-        assertEquals(E2EVersion.UNKNOWN, E2EVersionHelper.getMaxCompatibleE2EEVersion(E2EVersion.UNKNOWN))
+        assertEquals(
+            E2EVersion.UNKNOWN,
+            E2EVersionHelper.getMaxCompatibleE2EEVersion(E2EVersion.UNKNOWN)
+        )
     }
 
     @Test
     fun testGetMaxCompatibleE2EEVersionWhenGivenV1_0ShouldReturnV1_0() {
-        assertEquals(E2EVersion.V1_0, E2EVersionHelper.getMaxCompatibleE2EEVersion(E2EVersion.V1_0))
+        assertEquals(
+            E2EVersion.V1_0,
+            E2EVersionHelper.getMaxCompatibleE2EEVersion(E2EVersion.V1_0)
+        )
     }
 
     @Test
     fun testGetMaxCompatibleE2EEVersionWhenGivenV1_1ShouldReturnV1_1() {
-        assertEquals(E2EVersion.V1_1, E2EVersionHelper.getMaxCompatibleE2EEVersion(E2EVersion.V1_1))
+        assertEquals(
+            E2EVersion.V1_1,
+            E2EVersionHelper.getMaxCompatibleE2EEVersion(E2EVersion.V1_1)
+        )
     }
 
     @Test
     fun testGetMaxCompatibleE2EEVersionWhenGivenV1_2ShouldReturnV1_2() {
-        assertEquals(E2EVersion.V1_2, E2EVersionHelper.getMaxCompatibleE2EEVersion(E2EVersion.V1_2))
+        assertEquals(
+            E2EVersion.V1_2,
+            E2EVersionHelper.getMaxCompatibleE2EEVersion(E2EVersion.V1_2)
+        )
     }
 
     @Test
     fun testGetMaxCompatibleE2EEVersionWhenGivenV1AboveClientMaxShouldReturnClientV1Max() {
-        assertEquals(E2EVersion.V1_2, E2EVersionHelper.getMaxCompatibleE2EEVersion(E2EVersion.V1_2))
+        assertEquals(
+            E2EVersion.V1_2,
+            E2EVersionHelper.getMaxCompatibleE2EEVersion(E2EVersion.V1_2)
+        )
     }
 
     @Test
     fun testGetMaxCompatibleE2EEVersionWhenGivenV2_0ShouldReturnV2_0() {
-        assertEquals(E2EVersion.V2_0, E2EVersionHelper.getMaxCompatibleE2EEVersion(E2EVersion.V2_0))
+        assertEquals(
+            E2EVersion.V2_0,
+            E2EVersionHelper.getMaxCompatibleE2EEVersion(E2EVersion.V2_0)
+        )
     }
 
     @Test
     fun testGetMaxCompatibleE2EEVersionWhenGivenV2_1ShouldReturnV2_1() {
-        assertEquals(E2EVersion.V2_1, E2EVersionHelper.getMaxCompatibleE2EEVersion(E2EVersion.V2_1))
+        assertEquals(
+            E2EVersion.V2_1,
+            E2EVersionHelper.getMaxCompatibleE2EEVersion(E2EVersion.V2_1)
+        )
     }
 
     @Test
     fun testGetMaxCompatibleE2EEVersionWhenGivenV1_2ShouldNotApplyV2CeilingShouldReturnV1_2() {
-        val result = E2EVersionHelper.getMaxCompatibleE2EEVersion(E2EVersion.V1_2)
-        assertEquals(E2EVersion.V1_2, result)
+        assertEquals(
+            E2EVersion.V1_2,
+            E2EVersionHelper.getMaxCompatibleE2EEVersion(E2EVersion.V1_2)
+        )
     }
 
     @Test
     fun testGetMaxCompatibleE2EEVersionWhenGivenV2_0ShouldNotApplyV1CeilingShouldReturnV2_0() {
-        val result = E2EVersionHelper.getMaxCompatibleE2EEVersion(E2EVersion.V2_0)
-        assertEquals(E2EVersion.V2_0, result)
+        assertEquals(
+            E2EVersion.V2_0,
+            E2EVersionHelper.getMaxCompatibleE2EEVersion(E2EVersion.V2_0)
+        )
+    }
+
+    @Test
+    fun testGetMaxCompatibleE2EEVersionWhenGivenV3_0ShouldReturnV2_1() {
+        assertEquals(
+            E2EVersion.V2_1,
+            E2EVersionHelper.getMaxCompatibleE2EEVersionFromString("3.0")
+        )
     }
 }
