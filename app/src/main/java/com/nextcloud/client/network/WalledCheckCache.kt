89ebe6ee5a5f7b413220a909a3acd5268163bbfb
alperozturk96 <alper_ozturk@proton.me>
2026-07-02 12:58:25 +0200

    use concurrent hash map instead synchronized block
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/client/network/WalledCheckCache.kt
+++ app/src/main/java/com/nextcloud/client/network/WalledCheckCache.kt
@@ -2,13 +2,12 @@
  * Nextcloud - Android Client
  *
  * SPDX-FileCopyrightText: 2026 Alper Ozturk <alper.ozturk@nextcloud.com>
- * SPDX-FileCopyrightText: 2022 Álvaro Brey <alvaro@alvarobrey.com>
- * SPDX-FileCopyrightText: 2022 Nextcloud GmbH
- * SPDX-License-Identifier: AGPL-3.0-or-later OR GPL-2.0-only
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 package com.nextcloud.client.network
 
 import com.nextcloud.client.core.ClockImpl
+import java.util.concurrent.ConcurrentHashMap
 import javax.inject.Inject
 import javax.inject.Singleton
 
@@ -16,33 +15,26 @@ import javax.inject.Singleton
 class WalledCheckCache @Inject constructor() {
     private val clock = ClockImpl()
 
-    private var connectivityCache = mutableMapOf<ConnectivityKey, Connectivity>()
-    private val walledStatusCache = mutableMapOf<ConnectivityKey, Pair<Long, Boolean>>()
+    private val connectivityCache = ConcurrentHashMap<ConnectivityKey, Connectivity>()
+    private val walledStatusCache = ConcurrentHashMap<ConnectivityKey, Pair<Long, Boolean>>()
 
-    @Synchronized
     fun setValue(key: ConnectivityKey, isWalled: Boolean) {
         walledStatusCache[key] = Pair(clock.currentTime, isWalled)
     }
 
-    @Synchronized
     fun clear(key: ConnectivityKey) {
         walledStatusCache.remove(key)
     }
 
-    @Synchronized
     fun getValue(key: ConnectivityKey): Boolean? {
         val entry = walledStatusCache[key] ?: return null
         val isExpired = (clock.currentTime - entry.first) >= CACHE_TIME_MS
         return if (isExpired) null else entry.second
     }
 
-    @Synchronized
     fun putConnectivityValue(key: ConnectivityKey, connectivity: Connectivity) {
         connectivityCache[key] = connectivity
     }
-
-    @Synchronized
-    fun getConnectivity(key: ConnectivityKey): Connectivity? = connectivityCache[key]
 
     companion object {
         private const val CACHE_TIME_MS = 10 * 60 * 1000
