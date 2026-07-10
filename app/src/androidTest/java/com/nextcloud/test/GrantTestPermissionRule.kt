dd8abedab5c32b898a42b1243a9b06383e78fc81
alperozturk96 <alper_ozturk@proton.me>
2026-07-10 13:20:01 +0200

    wip
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/androidTest/java/com/nextcloud/test/GrantTestPermissionRule.kt
+++ app/src/androidTest/java/com/nextcloud/test/GrantTestPermissionRule.kt
@@ -1,8 +1,8 @@
 /*
  * Nextcloud - Android Client
  *
- * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
- * SPDX-License-Identifier: AGPL-3.0-or-later OR GPL-2.0-only
+ * SPDX-FileCopyrightText: 2026 Alper Ozturk <alper.ozturk@nextcloud.com>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 package com.nextcloud.test
 
