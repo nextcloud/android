3f2269146bcf3374286c17b34d2826a5ad0df345
alperozturk96 <alper_ozturk@proton.me>
2026-07-13 13:07:41 +0200

    wip
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/androidTest/java/com/owncloud/android/utils/FileExportUtilsIT.kt
+++ app/src/androidTest/java/com/owncloud/android/utils/FileExportUtilsIT.kt
@@ -7,15 +7,32 @@
  */
 package com.owncloud.android.utils
 
+import android.os.Environment
+import android.provider.MediaStore
 import com.owncloud.android.AbstractIT
 import com.owncloud.android.datamodel.OCFile
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
+import org.junit.Before
 import org.junit.Test
 import java.io.File
 
 class FileExportUtilsIT : AbstractIT() {
+    @Before
+    fun clearExistingDownload() {
+        val fileName = "export.txt"
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+            targetContext.contentResolver.delete(
+                MediaStore.Downloads.EXTERNAL_CONTENT_URI,
+                "${MediaStore.MediaColumns.DISPLAY_NAME} = ?",
+                arrayOf(fileName)
+            )
+        } else {
+            File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), fileName).delete()
+        }
+    }
+
     @Test
     fun exportFile() {
         val file = createFile("export.txt", 10)
