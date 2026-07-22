a0f784650ddc4eab9a22ea1c572bf6188d6c5245
alperozturk96 <alper_ozturk@proton.me>
2026-07-22 11:36:26 +0200

    wip
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/utils/extensions/TemporalExtensions.kt
+++ app/src/main/java/com/nextcloud/utils/extensions/TemporalExtensions.kt
@@ -7,7 +7,12 @@
 
 package com.nextcloud.utils.extensions
 
-import java.time.*
+import java.time.Instant
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.OffsetDateTime
+import java.time.ZoneOffset
+import java.time.ZonedDateTime
 import java.time.temporal.Temporal
 
 fun Temporal?.toInstant(): Instant = when (this) {
