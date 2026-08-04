5b66821b4c00fdd540905be83bd2644535d70826
alperozturk96 <alper_ozturk@proton.me>
2026-08-04 08:51:01 +0200

    fix(upload-list-adapter): FileUriExposedException
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/ui/adapter/uploadList/helper/UploadListAdapterHelper.kt
+++ app/src/main/java/com/owncloud/android/ui/adapter/uploadList/helper/UploadListAdapterHelper.kt
@@ -10,6 +10,7 @@ package com.owncloud.android.ui.adapter.uploadList.helper
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
+import androidx.core.content.FileProvider
 import com.owncloud.android.R
 import com.owncloud.android.datamodel.OCFile
 import com.owncloud.android.db.OCUpload
@@ -19,6 +20,7 @@ import com.owncloud.android.ui.activity.FileActivity
 import com.owncloud.android.ui.activity.FileDisplayActivity
 import com.owncloud.android.ui.preview.PreviewImageFragment
 import com.owncloud.android.utils.DisplayUtils
+import com.owncloud.android.utils.MimeType
 import com.owncloud.android.utils.MimeTypeUtil
 import java.io.File
 
@@ -26,15 +28,16 @@ class UploadListAdapterHelper(private val activity: FileActivity) {
 
     companion object {
         private const val TAG = "UploadListAdapterHelper"
+        private const val ANY_MIME_TYPE = "*/*"
     }
 
     fun openConflictActivity(file: OCFile, upload: OCUpload) {
         file.setStoragePath(upload.localPath)
         val user = activity.accountManager.getUser(upload.accountName)
-        if (user.isPresent) {
+        user.ifPresent {
             val intent = ConflictsResolveActivity.createIntent(
                 file,
-                user.get(),
+                it,
                 upload.uploadId,
                 Intent.FLAG_ACTIVITY_NEW_TASK,
                 activity
@@ -44,12 +47,12 @@ class UploadListAdapterHelper(private val activity: FileActivity) {
     }
 
     fun onUploadingItemClick(file: OCUpload) {
-        val f = File(file.localPath)
-        if (!f.exists()) {
-            DisplayUtils.showSnackMessage(activity, R.string.local_file_not_found_message)
-        } else {
+        if (File(file.localPath).exists()) {
             openFileWithDefault(file.localPath)
+            return
         }
+
+        DisplayUtils.showSnackMessage(activity, R.string.local_file_not_found_message)
     }
 
     fun onUploadedItemClick(upload: OCUpload) {
@@ -77,17 +80,31 @@ class UploadListAdapterHelper(private val activity: FileActivity) {
     }
 
     fun openFileWithDefault(localPath: String) {
+        val uri = exposedUriFor(localPath)
+        if (uri == null) {
+            DisplayUtils.showSnackMessage(activity, R.string.error_retrieving_file)
+            return
+        }
+
         var mimetype = MimeTypeUtil.getBestMimeTypeByFilename(localPath)
-        if (mimetype == "application/octet-stream") mimetype = "*/*"
+        if (mimetype == MimeType.FILE) mimetype = ANY_MIME_TYPE
+
         try {
-            activity.startActivity(
-                Intent(Intent.ACTION_VIEW).apply {
-                    setDataAndType(Uri.fromFile(File(localPath)), mimetype)
-                }
-            )
+            val intent = Intent(Intent.ACTION_VIEW).apply {
+                setDataAndType(uri, mimetype)
+                addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+            }
+            activity.startActivity(intent)
         } catch (e: ActivityNotFoundException) {
             DisplayUtils.showSnackMessage(activity, R.string.file_list_no_app_for_file_type)
-            Log_OC.i(TAG, "Could not find app for sending log history: $e")
+            Log_OC.i(TAG, "Could not find app for opening the local file: $e")
         }
+    }
+
+    private fun exposedUriFor(localPath: String): Uri? = try {
+        FileProvider.getUriForFile(activity, activity.getString(R.string.file_provider_authority), File(localPath))
+    } catch (e: IllegalArgumentException) {
+        Log_OC.e(TAG, "Local file is outside the paths supported by the file provider: $e")
+        null
     }
 }
