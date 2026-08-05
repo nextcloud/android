8ed4653925d1dc21aeb95d0c421b3a3f84f7f144
alperozturk96 <alper_ozturk@proton.me>
2026-08-05 10:50:49 +0200

    cover nullable dialog as well
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/ui/dialog/extensions/DialogExtensions.kt
+++ app/src/main/java/com/owncloud/android/ui/dialog/extensions/DialogExtensions.kt
@@ -12,20 +12,21 @@ import androidx.appcompat.app.AlertDialog
 import com.google.android.material.button.MaterialButton
 import com.owncloud.android.utils.theme.ViewThemeUtils
 
-fun Dialog.themeButtons(util: ViewThemeUtils?) {
-    val dialog = (this as? AlertDialog) ?: return
+fun Dialog?.themeButtons(util: ViewThemeUtils?) {
+    val dialog = this ?: return
+    val alertDialog = (dialog as? AlertDialog) ?: return
     val util = util ?: return
-    val positiveButton = dialog.getButton(AlertDialog.BUTTON_POSITIVE) as? MaterialButton
+    val positiveButton = alertDialog.getButton(AlertDialog.BUTTON_POSITIVE) as? MaterialButton
     positiveButton?.let {
         util.material.colorMaterialButtonPrimaryTonal(it)
     }
 
-    val negativeButton = dialog.getButton(AlertDialog.BUTTON_NEGATIVE) as? MaterialButton
+    val negativeButton = alertDialog.getButton(AlertDialog.BUTTON_NEGATIVE) as? MaterialButton
     negativeButton?.let {
         util.material.colorMaterialButtonPrimaryBorderless(it)
     }
 
-    val neutralButton = dialog.getButton(AlertDialog.BUTTON_NEUTRAL) as? MaterialButton
+    val neutralButton = alertDialog.getButton(AlertDialog.BUTTON_NEUTRAL) as? MaterialButton
     neutralButton?.let {
         util.material.colorMaterialButtonPrimaryBorderless(neutralButton)
     }
