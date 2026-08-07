213b8a6c08c4ab7db6a4ff06303393ef43741c7f
alperozturk96 <alper_ozturk@proton.me>
2026-08-07 08:55:10 +0200

    simplify FileThumbnailGenerator
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/utils/thumbnail/FileThumbnailGenerator.kt
+++ app/src/main/java/com/nextcloud/utils/thumbnail/FileThumbnailGenerator.kt
@@ -8,10 +8,9 @@
 package com.nextcloud.utils.thumbnail
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.Color
-import android.graphics.Point
-import android.os.AsyncTask
-import android.view.WindowManager
+import android.graphics.drawable.Drawable
 import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.core.content.ContextCompat
@@ -19,7 +18,6 @@ import androidx.core.content.res.ResourcesCompat
 import androidx.core.graphics.drawable.toDrawable
 import com.elyeproj.loaderviewlibrary.LoaderImageView
 import com.nextcloud.client.account.UserAccountManager
-import com.nextcloud.client.di.Injectable
 import com.nextcloud.client.preferences.AppPreferences
 import com.owncloud.android.R
 import com.owncloud.android.datamodel.FileDataStorageManager
@@ -32,243 +30,185 @@ import com.owncloud.android.lib.common.utils.Log_OC
 import com.owncloud.android.lib.resources.files.model.ServerFileInterface
 import com.owncloud.android.utils.BitmapUtils
 import com.owncloud.android.utils.DisplayUtils
+import com.owncloud.android.utils.MimeType
 import com.owncloud.android.utils.MimeTypeUtil
 import com.owncloud.android.utils.theme.ViewThemeUtils
+import java.util.Collections
+import java.util.concurrent.Executors
 import javax.inject.Inject
+import javax.inject.Provider
+import javax.inject.Singleton
 import kotlin.math.roundToInt
 
+@Singleton
 class FileThumbnailGenerator @Inject constructor(
-    private val storageManager: FileDataStorageManager,
+    private val storageManager: Provider<FileDataStorageManager>,
     private val preferences: AppPreferences,
     private val viewThemeUtils: ViewThemeUtils,
     private val context: Context,
     private val accountManager: UserAccountManager
-) : Injectable {
+) {
 
     companion object {
         private const val TAG = "FileThumbnailGenerator"
+        private const val SHIMMER_DELAY_MS = 100L
+        private const val MIN_THREADS = 3
+        private const val CORES_PER_THREAD = 2
     }
 
-    private val tasks: MutableList<ThumbnailGenerationTask> = ArrayList()
+    private val executor = Executors.newFixedThreadPool(
+        maxOf(MIN_THREADS, Runtime.getRuntime().availableProcessors() / CORES_PER_THREAD)
+    )
 
-    fun execute(
-        file: OCFile,
-        thumbnailView: ImageView,
-        gridView: Boolean,
-        shimmerThumbnail: LoaderImageView?
-    ) {
-        if (file.remoteId == null || !file.isPreviewAvailable) {
-            setThumbnailFirstTimeForFile(
-                file,
-                thumbnailView,
-                gridView,
-                shimmerThumbnail,
-            )
+    private val tasks = Collections.synchronizedList(mutableListOf<ThumbnailGenerationTask>())
+
+    fun setThumbnail(file: OCFile, view: ImageView, isGrid: Boolean, shimmer: LoaderImageView?) {
+        if (file.remoteId == null) {
+            DisplayUtils.stopShimmer(shimmer, view)
+            view.setImageDrawable(file.mimeIcon())
             return
         }
 
-        setThumbnailFromCache(
-            file,
-            thumbnailView,
-            gridView,
-            shimmerThumbnail
-        )
-    }
-
-    private fun setThumbnailFirstTimeForFile(
-        file: OCFile,
-        thumbnailView: ImageView,
-        gridView: Boolean,
-        shimmerThumbnail: LoaderImageView?
-    ) {
-        if (file.remoteId != null) {
-            generateNewThumbnailIfNecessary(
-                file,
-                thumbnailView,
-                gridView,
-                shimmerThumbnail,
-            )
+        if (!file.isPreviewAvailable) {
+            generate(file, view, isGrid, shimmer)
             return
         }
 
-        DisplayUtils.stopShimmer(shimmerThumbnail, thumbnailView)
-        val icon = MimeTypeUtil.getFileTypeIcon(file.mimeType, file.fileName, context, viewThemeUtils)
-        thumbnailView.setImageDrawable(icon)
-    }
-
-    fun setThumbnailFromCache(
-        file: OCFile,
-        thumbnailView: ImageView,
-        gridView: Boolean,
-        shimmerThumbnail: LoaderImageView?
-    ) {
-        val thumbnail = file.smallThumbnail
-        if (thumbnail == null || file.isUpdateThumbnailNeeded) {
-            generateNewThumbnailIfNecessary(
-                file,
-                thumbnailView,
-                gridView,
-                shimmerThumbnail,
-            )
-            setThumbnailBackgroundForPNGFileIfNeeded(file, context, thumbnailView)
-            return
+        val cached = file.smallThumbnail
+        if (cached == null || file.isUpdateThumbnailNeeded) {
+            generate(file, view, isGrid, shimmer)
+        } else {
+            show(cached, file, view, isGrid, shimmer)
         }
 
-        DisplayUtils.stopShimmer(shimmerThumbnail, thumbnailView)
+        applyPngBackground(file, view)
+    }
+
+    fun cancelPendingTasks() {
+        synchronized(tasks) {
+            tasks.forEach { task ->
+                task.cancel(true)
+                task.getMethod?.abort()
+            }
+            tasks.clear()
+        }
+    }
+
+    private fun show(bitmap: Bitmap, file: OCFile, view: ImageView, isGrid: Boolean, shimmer: LoaderImageView?) {
+        DisplayUtils.stopShimmer(shimmer, view)
 
         if (MimeTypeUtil.isVideo(file)) {
-            val withOverlay = ThumbnailsCacheManager.addVideoOverlay(thumbnail, context)
-            thumbnailView.setImageBitmap(withOverlay)
+            view.setImageBitmap(ThumbnailsCacheManager.addVideoOverlay(bitmap, context))
         } else {
-            BitmapUtils.setRoundedBitmapAccordingToListType(gridView, thumbnail, thumbnailView)
+            BitmapUtils.setRoundedBitmapAccordingToListType(isGrid, bitmap, view)
         }
-
-        setThumbnailBackgroundForPNGFileIfNeeded(file, context, thumbnailView)
     }
 
-    private fun generateNewThumbnailIfNecessary(
-        file: OCFile,
-        thumbnailView: ImageView,
-        gridView: Boolean,
-        shimmerThumbnail: LoaderImageView?
-    ) {
-        if (!ThumbnailsCacheManager.cancelPotentialThumbnailWork(file, thumbnailView)) {
+    private fun generate(file: OCFile, view: ImageView, isGrid: Boolean, shimmer: LoaderImageView?) {
+        if (!ThumbnailsCacheManager.cancelPotentialThumbnailWork(file, view)) {
             return
         }
 
-        var thumbnail = file.smallThumbnail
-
-        if (thumbnail != null) {
-            // If thumbnail is already in cache, display it immediately
-            thumbnailView.setImageBitmap(thumbnail)
-            DisplayUtils.stopShimmer(shimmerThumbnail, thumbnailView)
+        file.smallThumbnail?.let { cached ->
+            view.setImageBitmap(cached)
+            DisplayUtils.stopShimmer(shimmer, view)
             return
         }
 
-        for (task in tasks) {
-            if (file.remoteId != null && task.imageKey != null &&
-                file.remoteId == task.imageKey
-            ) {
-                return
-            }
+        startTask(file, view, isGrid, shimmer)
+    }
+
+    @Suppress("TooGenericExceptionCaught")
+    private fun startTask(file: OCFile, view: ImageView, isGrid: Boolean, shimmer: LoaderImageView?) {
+        if (tasks.any { it.imageKey == file.remoteId }) {
+            return
         }
 
-        thumbnailView.tag = file.fileId
+        view.tag = file.fileId
 
         try {
-            val task =
-                ThumbnailGenerationTask(
-                    thumbnailView,
-                    storageManager,
-                    accountManager.user,
-                    tasks,
-                    gridView,
-                    file.remoteId
-                )
-            var drawable = MimeTypeUtil.getFileTypeIcon(
-                file.mimeType,
-                file.fileName,
-                context,
-                viewThemeUtils
-            )
-            if (drawable == null) {
-                drawable = ResourcesCompat.getDrawable(
-                    context.resources,
-                    R.drawable.file_image,
-                    null
-                )
-            }
-            if (drawable == null) {
-                drawable = Color.GRAY.toDrawable()
-            }
-
-            val px = ThumbnailsCacheManager.getThumbnailDimension()
-            thumbnail = BitmapUtils.drawableToBitmap(drawable, px, px)
-            val asyncDrawable =
-                AsyncThumbnailDrawable(
-                    context.resources,
-                    thumbnail, task
-                )
-
-            shimmerThumbnail?.postDelayed({
-                if (thumbnailView.getDrawable() == null) {
-                    if (gridView) {
-                        configShimmerGridImageSize(shimmerThumbnail, preferences.getGridColumns())
-                    }
-                    DisplayUtils.startShimmer(shimmerThumbnail, thumbnailView)
-                }
-            }, 100)
-
-            task.setListener(object : ThumbnailGenerationTask.Listener {
-                override fun onSuccess() {
-                    DisplayUtils.stopShimmer(shimmerThumbnail, thumbnailView)
-                }
-
-                override fun onError() {
-                    DisplayUtils.stopShimmer(shimmerThumbnail, thumbnailView)
-                    val icon =
-                        MimeTypeUtil.getFileTypeIcon(file.mimeType, file.fileName, context, viewThemeUtils)
-                    thumbnailView.setImageDrawable(icon)
-                    thumbnailView.invalidate()
-                    Log_OC.w(TAG, "setting thumbnail failed, using icon from mime type")
-                }
-            })
-
-            thumbnailView.setImageDrawable(asyncDrawable)
+            val task = newTask(file, view, isGrid, shimmer)
+            view.setImageDrawable(AsyncThumbnailDrawable(context.resources, file.placeholder(), task))
+            startShimmerLater(view, isGrid, shimmer)
             tasks.add(task)
-            task.executeOnExecutor(
-                AsyncTask.THREAD_POOL_EXECUTOR,
-                ThumbnailGenerationTaskObject(
-                    file,
-                    file.remoteId
-                )
-            )
-            thumbnailView.invalidate()
+            task.execute(file)
+            view.invalidate()
         } catch (e: Exception) {
-            Log_OC.d(TAG, "ThumbnailGenerationTask : " + e.message)
+            Log_OC.d(TAG, "ThumbnailGenerationTask: ${e.message}")
         }
     }
 
-    private fun configShimmerGridImageSize(thumbnailShimmer: LoaderImageView, gridColumns: Float) {
-        try {
-            val targetLayoutParams = thumbnailShimmer.layoutParams as FrameLayout.LayoutParams
+    private fun newTask(
+        file: OCFile,
+        view: ImageView,
+        isGrid: Boolean,
+        shimmer: LoaderImageView?
+    ): ThumbnailGenerationTask = ThumbnailGenerationTask(
+        view,
+        storageManager.get(),
+        accountManager.user,
+        tasks,
+        isGrid,
+        file.remoteId
+    ).apply {
+        setListener(object : ThumbnailGenerationTask.Listener {
+            override fun onSuccess() = DisplayUtils.stopShimmer(shimmer, view)
 
-            val screenSize = getScreenSize()
-            val marginLeftAndRight = targetLayoutParams.leftMargin + targetLayoutParams.rightMargin
-            val size = (screenSize.x / gridColumns - marginLeftAndRight).roundToInt()
+            override fun onError() {
+                DisplayUtils.stopShimmer(shimmer, view)
+                view.setImageDrawable(file.mimeIcon())
+                view.invalidate()
+                Log_OC.w(TAG, "setting thumbnail failed, using icon from mime type")
+            }
+        })
+    }
 
-            val params = FrameLayout.LayoutParams(size, size)
-            params.setMargins(
-                targetLayoutParams.leftMargin,
-                targetLayoutParams.topMargin,
-                targetLayoutParams.rightMargin,
-                targetLayoutParams.bottomMargin
-            )
-            thumbnailShimmer.setLayoutParams(params)
-        } catch (exception: Exception) {
-            Log_OC.e("ConfigShimmer", exception.message)
+    @Suppress("DEPRECATION")
+    private fun ThumbnailGenerationTask.execute(file: OCFile) {
+        executeOnExecutor(executor, ThumbnailGenerationTaskObject(file, file.remoteId))
+    }
+
+    private fun startShimmerLater(view: ImageView, isGrid: Boolean, shimmer: LoaderImageView?) {
+        shimmer?.postDelayed({
+            if (view.drawable != null) {
+                return@postDelayed
+            }
+
+            if (isGrid) {
+                shimmer.resizeToGridCell(preferences.gridColumns)
+            }
+
+            DisplayUtils.startShimmer(shimmer, view)
+        }, SHIMMER_DELAY_MS)
+    }
+
+    private fun LoaderImageView.resizeToGridCell(columns: Float) {
+        val current = layoutParams as? FrameLayout.LayoutParams ?: return
+        val horizontalMargin = current.leftMargin + current.rightMargin
+        val screenWidth = context.resources.displayMetrics.widthPixels
+        val size = (screenWidth / columns - horizontalMargin).roundToInt()
+
+        layoutParams = FrameLayout.LayoutParams(size, size).apply {
+            setMargins(current.leftMargin, current.topMargin, current.rightMargin, current.bottomMargin)
         }
     }
 
-    private fun getScreenSize(): Point {
-        val windowManager = context.getSystemService(Context.WINDOW_SERVICE) as WindowManager?
-        if (windowManager != null) {
-            val displaySize = Point()
-            windowManager.getDefaultDisplay().getSize(displaySize)
-            return displaySize
-        } else {
-            throw Exception("WindowManager not found")
-        }
+    private fun OCFile.mimeIcon(): Drawable? = MimeTypeUtil.getFileTypeIcon(mimeType, fileName, context, viewThemeUtils)
+
+    private fun OCFile.placeholder(): Bitmap {
+        val drawable = mimeIcon()
+            ?: ResourcesCompat.getDrawable(context.resources, R.drawable.file_image, null)
+            ?: Color.GRAY.toDrawable()
+        val size = ThumbnailsCacheManager.getThumbnailDimension()
+
+        return BitmapUtils.drawableToBitmap(drawable, size, size)
     }
 
-    private fun setThumbnailBackgroundForPNGFileIfNeeded(
-        file: ServerFileInterface,
-        context: Context,
-        thumbnailView: ImageView
-    ) {
-        if ("image/png".equals(file.mimeType, ignoreCase = true)) {
-            val color = ContextCompat.getColor(context, R.color.bg_default)
-            thumbnailView.setBackgroundColor(color)
+    private fun applyPngBackground(file: ServerFileInterface, view: ImageView) {
+        if (!MimeType.PNG.equals(file.mimeType, ignoreCase = true)) {
+            return
         }
+
+        view.setBackgroundColor(ContextCompat.getColor(context, R.color.bg_default))
     }
 }
