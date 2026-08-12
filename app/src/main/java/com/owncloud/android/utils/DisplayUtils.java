21f83498d5a9c067dacca855fa41d2484dce0f8a
alperozturk96 <alper_ozturk@proton.me>
2026-08-12 13:49:14 +0200

    remove unused orientation logic
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/utils/DisplayUtils.java
+++ app/src/main/java/com/owncloud/android/utils/DisplayUtils.java
@@ -26,7 +26,6 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.content.res.Resources;
 import android.graphics.Point;
 import android.graphics.drawable.Drawable;
@@ -651,10 +650,6 @@ public final class DisplayUtils {
 
     public static boolean isRTL() {
         return TextUtils.getLayoutDirectionFromLocale(Locale.getDefault()) == View.LAYOUT_DIRECTION_RTL;
-    }
-
-    public static boolean isOrientationLandscape() {
-        return MainApp.getAppContext().getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE;
     }
 
     static public void showServerOutdatedSnackbar(Activity activity, int length) {
