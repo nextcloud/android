4427998be79d2d1be662f8a3fc1f7a4102497198
alperozturk96 <alper_ozturk@proton.me>
2026-08-12 14:27:41 +0200

    wip
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/operations/albums/ReadAlbumItemsOperation.kt
+++ app/src/main/java/com/owncloud/android/operations/albums/ReadAlbumItemsOperation.kt
@@ -1,6 +1,7 @@
 /*
  * Nextcloud - Android Client
  *
+ * SPDX-FileCopyrightText: 2026 Alper Ozturk <alper.ozturk@nextcloud.com>
  * SPDX-FileCopyrightText: 2026 TSI-mc <surinder.kumar@t-systems.com>
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
