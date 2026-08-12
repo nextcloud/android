8e2d30121f906f01ded6c7845525b0b8fdb4ed4a
alperozturk96 <alper_ozturk@proton.me>
2026-08-12 14:27:27 +0200

    wip
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/operations/albums/ReadAlbumItemsOperation.kt
+++ app/src/main/java/com/owncloud/android/operations/albums/ReadAlbumItemsOperation.kt
@@ -8,73 +8,52 @@ package com.owncloud.android.operations.albums
 
 import com.nextcloud.common.SessionTimeOut
 import com.nextcloud.common.defaultSessionTimeOut
+import com.nextcloud.utils.extensions.toAlbumItem
 import com.owncloud.android.datamodel.FileDataStorageManager
 import com.owncloud.android.lib.common.OwnCloudClient
 import com.owncloud.android.lib.common.operations.RemoteOperation
 import com.owncloud.android.lib.common.operations.RemoteOperationResult
 import com.owncloud.android.lib.resources.albums.ReadAlbumItemsRemoteOperation
 import com.owncloud.android.lib.resources.files.model.RemoteFile
-import com.owncloud.android.utils.FileStorageUtils
 
 class ReadAlbumItemsOperation
 @JvmOverloads
 constructor(
-    private val mRemotePath: String,
+    private val remotePath: String,
     private val storageManager: FileDataStorageManager?,
     private val sessionTimeOut: SessionTimeOut = defaultSessionTimeOut
 ) : RemoteOperation<List<RemoteFile>>() {
+
     @Deprecated("Deprecated in Java")
     @Suppress("TooGenericExceptionCaught")
-    override fun run(client: OwnCloudClient): RemoteOperationResult<List<RemoteFile>> {
-        var result: RemoteOperationResult<List<RemoteFile>>? = null
-        try {
-            result = ReadAlbumItemsRemoteOperation(mRemotePath, sessionTimeOut).execute(client)
-            if (result.isSuccess) {
-                // get data from remote folder
-                val mFolderAndFiles = saveAlbumData(result.resultData, storageManager)
+    override fun run(client: OwnCloudClient): RemoteOperationResult<List<RemoteFile>> = try {
+        val result = ReadAlbumItemsRemoteOperation(remotePath, sessionTimeOut).execute(client)
 
-                // Result of the operation
-                result.apply {
-                    // Add data to the result
-                    resultData = mFolderAndFiles
-                }
-            }
-        } catch (e: Exception) {
-            result = RemoteOperationResult(e)
+        when {
+            result.isSuccess -> result.apply { resultData = cacheAlbumItems(resultData) }
+            else -> result
         }
-
-        return result
+    } catch (e: Exception) {
+        RemoteOperationResult(e)
     }
 
-    companion object {
-        private val TAG: String = ReadAlbumItemsRemoteOperation::class.java.simpleName
+    /**
+     * Thumbnails and previews resolve album items through the database, so items it does not know yet are stored.
+     * Items without a file id cannot be resolved later on and are dropped.
+     */
+    private fun cacheAlbumItems(remoteFiles: List<RemoteFile>): List<RemoteFile> {
+        val albumItems = remoteFiles.filter { it.localId > 0 }
+        val storageManager = storageManager ?: return albumItems
 
-        private fun saveAlbumData(
-            remoteFiles: List<RemoteFile>,
-            storageManager: FileDataStorageManager?
-        ): List<RemoteFile> {
-            val files = mutableListOf<RemoteFile>()
-
-            for (remoteFile in remoteFiles) {
-                // if no fileId received then skip adding
-                if (remoteFile.localId <= 0) {
-                    continue
+        albumItems
+            .filter { storageManager.getFileByLocalId(it.localId) == null }
+            .forEach { remoteFile ->
+                val albumItem = remoteFile.toAlbumItem().apply {
+                    lastSyncDateForProperties = System.currentTimeMillis()
                 }
-                // check if file already exit in db or not
-                // if not then store it in db to show thumbnail and image preview
-                var ocFile = storageManager?.getFileByLocalId(remoteFile.localId)
-                if (ocFile == null) {
-                    ocFile = FileStorageUtils.fillOCFile(remoteFile)
-                    // no remote id is received from response
-                    // so localId will be remoteId else thumb will not generated
-                    ocFile.remoteId = remoteFile.localId.toString()
-                    ocFile.lastSyncDateForProperties = System.currentTimeMillis()
-                    storageManager?.saveFile(ocFile)
-                }
-                files.add(remoteFile)
+                storageManager.saveFile(albumItem)
             }
 
-            return files
-        }
+        return albumItems
     }
 }
