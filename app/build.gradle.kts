f487dbec1f72731497d12f951d4c07e323dcc44a
Tobias Kaminsky <tobias@kaminsky.me>
2026-08-28 05:01:33 +0200

    daily dev 20260828
    Update Nextcloud Android library

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -172,8 +172,8 @@ android {
             register("versionDev") {
                 applicationId = "com.nextcloud.android.beta"
                 dimension = "default"
-                versionCode = 20260827
-                versionName = "20260827"
+                versionCode = 20260828
+                versionName = "20260828"
             }
 
             register("qa") {
