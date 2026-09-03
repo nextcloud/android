72d2968837efaab322b81055b979124683d22fca
alperozturk96 <alper_ozturk@proton.me>
2026-09-03 08:27:50 +0200

    wip
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/client/database/NextcloudDatabase.kt
+++ app/src/main/java/com/nextcloud/client/database/NextcloudDatabase.kt
@@ -102,7 +102,8 @@ import com.owncloud.android.db.ProviderMeta
         // manual migration used for 99 to 100
         AutoMigration(from = 100, to = 101, spec = DatabaseMigrationUtil.ResetCapabilitiesPostMigration::class),
         AutoMigration(from = 101, to = 102, spec = DatabaseMigrationUtil.ResetCapabilitiesPostMigration::class),
-        AutoMigration(from = 102, to = 103, spec = DatabaseMigrationUtil.ResetCapabilitiesPostMigration::class)
+        AutoMigration(from = 102, to = 103, spec = DatabaseMigrationUtil.ResetCapabilitiesPostMigration::class),
+        AutoMigration(from = 103, to = 104)
     ],
     exportSchema = true
 )
