cd4a3b0e4047189a514bb0f3cd1c310b036c5a4d
alperozturk96 <alper_ozturk@proton.me>
2026-09-03 08:40:32 +0200

    wip
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>
    
    # Conflicts:
    #	app/schemas/com.nextcloud.client.database.NextcloudDatabase/104.json

--- app/src/main/java/com/owncloud/android/db/ProviderMeta.java
+++ app/src/main/java/com/owncloud/android/db/ProviderMeta.java
@@ -299,6 +299,7 @@ public class ProviderMeta {
         public static final String CAPABILITIES_CLIENT_INTEGRATION_JSON = "client_integration_json";
         public static final String CAPABILITIES_MOD_REWRITE_WORKING = "mod_rewrite_working";
         public static final String CAPABILITIES_CHUNKED_UPLOAD_MAX_SIZE = "chunked_upload_max_size";
+        public static final String CAPABILITIES_SHARING_JSON = "sharing_json";
 
         //Columns of Uploads table
         public static final String UPLOADS_LOCAL_PATH = "local_path";
