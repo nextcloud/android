2c8e25e4c5c4875b06e2d7558c369b30dd304e2d
alperozturk96 <alper_ozturk@proton.me>
2026-09-03 09:17:05 +0200

    wip
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/utils/extensions/UserExtensions.kt
+++ app/src/main/java/com/nextcloud/utils/extensions/UserExtensions.kt
@@ -1,7 +1,7 @@
 /*
  * Nextcloud - Android Client
  *
- * SPDX-FileCopyrightText: 2026 Nextcloud GmbH and Nextcloud contributors
+ * SPDX-FileCopyrightText: 2026 Alper Ozturk <alper.ozturk@nextcloud.com>
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
