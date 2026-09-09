067d29618c41b7fd011e9f574a85aaf09d38eed2
alperozturk96 <alper_ozturk@proton.me>
2026-09-09 10:30:57 +0200

    wip
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/db/ProviderMeta.java
+++ app/src/main/java/com/owncloud/android/db/ProviderMeta.java
@@ -24,7 +24,7 @@ import java.util.List;
  */
 public class ProviderMeta {
     public static final String DB_NAME = "filelist";
-    public static final int DB_VERSION = 105;
+    public static final int DB_VERSION = 106;
 
     private ProviderMeta() {
         // No instance
