2bf00931d5a8bf49033bc66eb2cc38e1a0d59b2f
alperozturk96 <alper_ozturk@proton.me>
2026-09-09 14:56:07 +0200

    fix(e2ee): event listen
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/utils/extensions/OCFileListFragmentExtensions.kt
+++ app/src/main/java/com/nextcloud/utils/extensions/OCFileListFragmentExtensions.kt
@@ -9,11 +9,13 @@ package com.nextcloud.utils.extensions
 
 import android.os.Bundle
 import androidx.lifecycle.lifecycleScope
+import com.nextcloud.utils.e2ee.model.E2EEAction
 import com.owncloud.android.R
 import com.owncloud.android.datamodel.OCFile
 import com.owncloud.android.lib.common.utils.Log_OC
 import com.owncloud.android.ui.activity.FileActivity
 import com.owncloud.android.ui.dialog.setupEncryption.SetupEncryptionDialogFragment
+import com.owncloud.android.ui.dialog.setupEncryption.SetupEncryptionDialogFragment.Companion.ARG_ACTION
 import com.owncloud.android.ui.dialog.setupEncryption.SetupEncryptionDialogFragment.Companion.ARG_FILE_PATH
 import com.owncloud.android.ui.dialog.setupEncryption.SetupEncryptionDialogFragment.Companion.RESULT_REQUEST_KEY
 import com.owncloud.android.ui.dialog.setupEncryption.SetupEncryptionDialogFragment.Companion.SUCCESS
@@ -23,7 +25,7 @@ import kotlinx.coroutines.launch
 
 private const val TAG = "OCFileListFragmentExtensions"
 
-fun OCFileListFragment.showEncryptionDialog(remotePath: String?) {
+fun OCFileListFragment.showEncryptionDialog(remotePath: String?, action: E2EEAction) {
     if (parentFragmentManager.findFragmentByTag(SetupEncryptionDialogFragment.SETUP_ENCRYPTION_DIALOG_TAG) != null) {
         return
     }
@@ -31,8 +33,8 @@ fun OCFileListFragment.showEncryptionDialog(remotePath: String?) {
     val user = accountManager.user
     val connectivityService = typedActivity<FileActivity>()?.connectivityService
     connectivityService?.isNetworkAndServerAvailable { result ->
-        if (result == true) {
-            SetupEncryptionDialogFragment.newInstance(user, remotePath)
+        if (result) {
+            SetupEncryptionDialogFragment.newInstance(user, remotePath, action)
                 .show(parentFragmentManager, SetupEncryptionDialogFragment.SETUP_ENCRYPTION_DIALOG_TAG)
             return@isNetworkAndServerAvailable
         }
@@ -55,30 +57,37 @@ fun OCFileListFragment.listenEncryptionDialogResult() {
             return@setFragmentResultListener
         }
 
-        val fileRemotePath = bundle.getString(ARG_FILE_PATH, null)
-        if (fileRemotePath == null) {
-            Log_OC.e(TAG, "file path is null")
+        val action = bundle.getSerializableArgument(ARG_ACTION, E2EEAction::class.java)
+        if (action == null) {
+            Log_OC.e(TAG, "no pending encryption action, nothing to continue with")
             return@setFragmentResultListener
         }
 
-        val file: OCFile? = mContainerActivity.getStorageManager().getFileByDecryptedRemotePath(fileRemotePath)
-        if (file == null) {
-            Log_OC.e(TAG, "file is null, cannot toggle encryption")
-            return@setFragmentResultListener
-        }
+        when (action) {
+            E2EEAction.NEW_FOLDER -> createFolder(true)
 
-        if (file.isRootDirectory) {
-            Log_OC.d(
-                TAG,
-                "result of setup encryption triggered in root directory, this call is for " +
-                    "creating encrypted folder"
-            )
-            createFolder(true)
-            return@setFragmentResultListener
-        }
+            E2EEAction.OPEN -> folderFromResult(bundle)?.let { clickHandler.openAfterKeySetup(it) }
 
-        lifecycleScope.launch {
-            folderEncryption.toggle(file.toEncryptionEvent(true))
+            E2EEAction.ENCRYPT -> folderFromResult(bundle)?.let { file ->
+                lifecycleScope.launch {
+                    folderEncryption.toggle(file.toEncryptionEvent(true))
+                }
+            }
         }
     }
+}
+
+private fun OCFileListFragment.folderFromResult(bundle: Bundle): OCFile? {
+    val remotePath = bundle.getString(ARG_FILE_PATH, null)
+    if (remotePath == null) {
+        Log_OC.e(TAG, "file path is null")
+        return null
+    }
+
+    val file = mContainerActivity.storageManager.getFileByEncryptedRemotePath(remotePath)
+    if (file == null) {
+        Log_OC.e(TAG, "file is null, cannot continue encryption action")
+    }
+
+    return file
 }
