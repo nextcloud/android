933c55057e6e1bfe992fbafab3ca998af58fa671
alperozturk96 <alper_ozturk@proton.me>
2026-09-11 10:25:27 +0200

    test upsert 2
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/owncloud/android/MainApp.java
+++ app/src/main/java/com/owncloud/android/MainApp.java
@@ -139,7 +139,6 @@ public class MainApp extends Application implements HasAndroidInjector, NetworkC
 
     private static String storagePath;
 
-
     private static boolean mOnlyOnDevice;
     private static boolean mOnlyPersonalFiles;
 
