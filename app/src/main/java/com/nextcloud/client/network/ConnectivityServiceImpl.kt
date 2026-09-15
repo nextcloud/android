47c770f7744df2d7fdb4a05edd76f5e57a4f50f6
alperozturk96 <alper_ozturk@proton.me>
2026-09-15 11:36:37 +0200

    do not cancel previous job so that list action such as create can react
    Signed-off-by: alperozturk96 <alper_ozturk@proton.me>

--- app/src/main/java/com/nextcloud/client/network/ConnectivityServiceImpl.kt
+++ app/src/main/java/com/nextcloud/client/network/ConnectivityServiceImpl.kt
@@ -46,7 +46,6 @@ class ConnectivityServiceImpl(
 
     // region private values
     private val scope = CoroutineScope(Dispatchers.IO)
-    private var availabilityCheckJob: Job? = null
     private var notifyJob: Job? = null
     private val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
     private val listeners = mutableSetOf<NetworkChangeListener>()
@@ -82,8 +81,7 @@ class ConnectivityServiceImpl(
 
     // region overridden methods
     override fun isNetworkAndServerAvailable(onCompleted: (Boolean) -> Unit) {
-        availabilityCheckJob?.cancel()
-        availabilityCheckJob = scope.launch {
+        scope.launch {
             val available = !isInternetWalled()
             Log_OC.d(TAG, "isNetworkAndServerAvailable: $available")
             withContext(Dispatchers.Main) {
