850a6bdb6bd784e76859f72c2a805dcfdd5b2903
Raphael Vieira <raphaelecv@hotmail.com>
2026-09-26 19:36:29 +0100

    removed mistaken change
    Signed-off-by: Raphael Vieira <raphaelecv@hotmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -24,9 +24,6 @@ pluginManagement {
         mavenCentral()
     }
 }
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
-}
 
 @Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
